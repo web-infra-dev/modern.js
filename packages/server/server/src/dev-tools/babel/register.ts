2f4ebc3b915c65e9779356e71936f3729d35ed37
wangyiming <wangyiming.fe@bytedance.com>
2021-10-11 13:13:21 +0800

    fix(server): add copy condition

--- packages/server/server/src/dev-tools/babel/register.ts
+++ packages/server/server/src/dev-tools/babel/register.ts
@@ -1,6 +1,4 @@
 import path from 'path';
-import fs from 'fs';
-import JSON5 from 'json5';
 import { resolveBabelConfig } from '@modern-js/server-utils';
 import { ModernServerOptions } from '../../type';
 
@@ -10,33 +8,14 @@ export const enableRegister = (
   projectRoot: string,
   config: ModernServerOptions['config'],
 ) => {
-  const tsconfigPath = path.join(projectRoot, 'tsconfig.json');
-  const isTsProject = fs.existsSync(tsconfigPath);
+  const TS_CONFIG_FILENAME = `tsconfig.json`;
+  const tsconfigPath = path.resolve(projectRoot, TS_CONFIG_FILENAME);
 
-  let babelConfig = {};
-  if (isTsProject) {
-    const tsConfig = fs.readFileSync(tsconfigPath, 'utf-8');
-    const tsConfigJson = JSON5.parse(tsConfig);
-    const { compilerOptions } = tsConfigJson;
-
-    // Todo: use common package
-    require('tsconfig-paths').register({
-      baseUrl: compilerOptions.baseUrl || path.dirname(tsconfigPath),
-      paths: compilerOptions?.paths ? compilerOptions?.paths : {},
-    });
-
-    babelConfig = resolveBabelConfig(projectRoot, config, {
-      tsconfigPath,
-      syntax: 'es6+',
-      type: 'commonjs',
-    });
-  } else {
-    babelConfig = resolveBabelConfig(projectRoot, config, {
-      tsconfigPath: '',
-      syntax: 'es6+',
-      type: 'commonjs',
-    });
-  }
+  const babelConfig = resolveBabelConfig(projectRoot, config, {
+    tsconfigPath,
+    syntax: 'es6+',
+    type: 'commonjs',
+  });
 
   return require('@babel/register')({
     ...babelConfig,
@@ -51,7 +30,6 @@ export const enableRegister = (
         );
       },
     ],
-    // ignore: [/node_modules/],
     extensions: ['.js', '.ts'],
     babelrc: false,
     root: projectRoot,
