701be3e410869ddad9c9e7c894b034a221c6ca38
targeral <2654955040@qq.com>
2021-10-11 15:24:09 +0800

    Feat plugin tailwind (#35)
    * feat(plugin-tailwind): add tailwind plugin
    
    Co-authored-by: 高博文 <gaobowen.targeral@bytedance.com>

---
+++ packages/cli/plugin-tailwind/src/cli.ts
@@ -0,0 +1,73 @@
+import { Import, PLUGIN_SCHEMAS } from '@modern-js/utils';
+import { getTailwindConfig } from './tailwind';
+
+const core: typeof import('@modern-js/core') = Import.lazy(
+  '@modern-js/core',
+  require,
+);
+
+export default core.createPlugin(
+  () => ({
+    validateSchema() {
+      return PLUGIN_SCHEMAS['@modern-js/plugin-tailwind'];
+    },
+    config() {
+      return {
+        tools: {
+          // TODO: Add interface about postcss config
+          // TODO: In module project, also is called, but should not be called.
+          postcss: (config: Record<string, any>) => {
+            const { value: modernConfig } = core.useResolvedConfigContext();
+            const tailwindConfig = getTailwindConfig(modernConfig, {
+              pureConfig: {
+                content: [
+                  './config/html/**/*.html',
+                  './config/html/**/*.ejs',
+                  './config/html/**/*.hbs',
+                  './src/**/*.js',
+                  './src/**/*.jsx',
+                  './src/**/*.ts',
+                  './src/**/*.tsx',
+                  // about storybook
+                  './storybook/**/*',
+                  './styles/**/*.less',
+                  './styles/**/*.css',
+                  './styles/**/*.sass',
+                  './styles/**/*.scss',
+                ],
+              },
+            });
+            if (Array.isArray(config.postcssOptions.plugins)) {
+              config.postcssOptions.plugins.push(
+                require('tailwindcss')(tailwindConfig),
+              );
+            } else {
+              config.postcssOptions.plugins = [
+                require('tailwindcss')(tailwindConfig),
+              ];
+            }
+          },
+        },
+      };
+    },
+    moduleTailwindConfig() {
+      const { value: modernConfig } = core.useResolvedConfigContext();
+      const tailwindConfig = getTailwindConfig(modernConfig, {
+        pureConfig: {
+          content: [
+            './src/**/*.js',
+            './src/**/*.jsx',
+            './src/**/*.ts',
+            './src/**/*.tsx',
+            './styles/**/*.less',
+            './styles/**/*.css',
+            './styles/**/*.sass',
+            './styles/**/*.scss',
+          ],
+        },
+      });
+      return require('tailwindcss')(tailwindConfig);
+    },
+  }),
+  { name: '@modern-js/plugin-tailwind' },
+) as any;
---
+++ packages/cli/plugin-tailwind/src/tailwind.ts
@@ -0,0 +1,78 @@
+import type { NormalizedConfig } from '@modern-js/core';
+import { Import, applyOptionsChain, logger } from '@modern-js/utils';
+
+const cloneDeep: typeof import('lodash.clonedeep') = Import.lazy(
+  'lodash.clonedeep',
+  require,
+);
+const merge: typeof import('lodash.merge') = Import.lazy(
+  'lodash.merge',
+  require,
+);
+
+const checkIfExistNotAllowKeys = (
+  tailwindConfig: Record<string, any>,
+): [boolean, string] => {
+  const notAllowExistKeys = ['theme'];
+  let notAllowKey = '';
+
+  const ret = Object.keys(tailwindConfig).some(
+    key => notAllowExistKeys.includes(key) && (notAllowKey = key),
+  );
+
+  return [ret, notAllowKey];
+};
+
+const getPureDesignSystemConfig = (
+  designSystemConfig: Record<string, any> & {
+    supportSytledComponents: boolean;
+  },
+) => {
+  const pureDesignSystemConfig = cloneDeep(designSystemConfig);
+  delete pureDesignSystemConfig.supportStyledComponents;
+  return pureDesignSystemConfig;
+};
+
+const getTailwindConfig = (
+  config: NormalizedConfig,
+  option: { pureConfig?: Record<string, any> } = {},
+) => {
+  const purgeConfig = merge(
+    {
+      // TODO: how the operating environment is determined
+      enabled: process.env.NODE_ENV === 'production',
+      // TODO: Remove or not
+      layers: ['utilities'],
+      content: [],
+    },
+    option.pureConfig || {},
+  );
+  const defaultTailwindConfig = {
+    purge: purgeConfig,
+  };
+  const tailwindConfig = applyOptionsChain(
+    defaultTailwindConfig,
+    (config.tools as any).tailwind || {},
+  );
+
+  const designSystem = getPureDesignSystemConfig(
+    (config.source as any).designSystem || {},
+  );
+
+  const [exist, key] = checkIfExistNotAllowKeys(tailwindConfig);
+
+  if (exist) {
+    logger.error(
+      `should not exist '${key}' on tools.tailwind, please remove it`,
+    );
+    // eslint-disable-next-line no-process-exit
+    process.exit(0);
+  }
+
+  // Because there is no default theme configuration
+  tailwindConfig.theme = designSystem || {};
+
+  return tailwindConfig;
+};
+
+export { getTailwindConfig };
