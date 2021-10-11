a48567f199dbcbd85ded2bf4cef5bf665b93457b
孔嘉聪 <kongjiacong@bytedance.com>
2021-10-11 09:27:59 +0800

    fix(server): fix js start bug & fix image return error

--- packages/server/server/src/dev-tools/babel/register.ts
+++ packages/server/server/src/dev-tools/babel/register.ts
@@ -10,27 +10,33 @@ export const enableRegister = (
   projectRoot: string,
   config: ModernServerOptions['config'],
 ) => {
-  const tsConfigPath = path.join(projectRoot, 'tsconfig.json');
-  const isTsProject = fs.existsSync(tsConfigPath);
+  const tsconfigPath = path.join(projectRoot, 'tsconfig.json');
+  const isTsProject = fs.existsSync(tsconfigPath);
+
+  let babelConfig = {};
   if (isTsProject) {
-    const tsConfig = fs.readFileSync(tsConfigPath).toString();
+    const tsConfig = fs.readFileSync(tsconfigPath, 'utf-8');
     const tsConfigJson = JSON5.parse(tsConfig);
     const { compilerOptions } = tsConfigJson;
 
+    // Todo: use common package
     require('tsconfig-paths').register({
-      baseUrl: compilerOptions.baseUrl || path.dirname(tsConfigPath),
+      baseUrl: compilerOptions.baseUrl || path.dirname(tsconfigPath),
       paths: compilerOptions?.paths ? compilerOptions?.paths : {},
     });
+
+    babelConfig = resolveBabelConfig(projectRoot, config, {
+      tsconfigPath,
+      syntax: 'es6+',
+      type: 'commonjs',
+    });
+  } else {
+    babelConfig = resolveBabelConfig(projectRoot, config, {
+      tsconfigPath: '',
+      syntax: 'es6+',
+      type: 'commonjs',
+    });
   }
-
-  const TS_CONFIG_FILENAME = `tsconfig.json`;
-  const tsconfigPath = path.resolve(projectRoot, TS_CONFIG_FILENAME);
-
-  const babelConfig = resolveBabelConfig(projectRoot, config, {
-    tsconfigPath,
-    syntax: 'es6+',
-    type: 'commonjs',
-  });
 
   return require('@babel/register')({
     ...babelConfig,
