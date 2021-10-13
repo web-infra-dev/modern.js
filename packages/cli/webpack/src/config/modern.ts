c5d021b47273eab0f828950422f672969032ac81
await-ovo <13152410380@163.com>
2021-10-13 21:56:24 +0800

    fix(webpack): modern build & bff alias

--- packages/cli/webpack/src/config/modern.ts
+++ packages/cli/webpack/src/config/modern.ts
@@ -1,5 +1,10 @@
+import { createBabelChain } from '@modern-js/babel-chain';
 import { IAppContext, NormalizedConfig } from '@modern-js/core';
-import { removeLeadingSlash } from '@modern-js/utils';
+import {
+  applyOptionsChain,
+  isUseSSRBundle,
+  removeLeadingSlash,
+} from '@modern-js/utils';
 import { ClientWebpackConfig } from './client';
 
 class ModernWebpackConfig extends ClientWebpackConfig {
@@ -16,10 +21,50 @@ class ModernWebpackConfig extends ClientWebpackConfig {
     this.jsChunkname = this.jsChunkname.replace(/\.js$/, '-es6.js');
 
     this.jsFilename = this.jsFilename.replace(/\.js$/, '-es6.js');
+
+    this.babelChain = createBabelChain();
   }
 
-  name() {
-    this.chain.name('modern');
+  // FIXME: The BFF create-request api depends on webpack config name: client | server.
+  // name() {
+  //   this.chain.name('modern');
+  // }
+
+  loaders() {
+    const loaders = super.loaders();
+
+    const babelOptions = loaders.oneOf('js').use('babel').get('options');
+
+    loaders
+      .oneOf('js')
+      .use('babel')
+      .options({
+        ...babelOptions,
+        presets: [
+          [
+            require.resolve('@modern-js/babel-preset-app'),
+            {
+              appDirectory: this.appDirectory,
+              target: 'client',
+              useLegacyDecorators: !this.options.output?.enableLatestDecorators,
+              useBuiltIns: false,
+              useModern: true,
+              chain: this.babelChain,
+              styledCompontents: applyOptionsChain(
+                {
+                  pure: true,
+                  displayName: true,
+                  ssr: isUseSSRBundle(this.options),
+                  transpileTemplateLiterals: true,
+                },
+                (this.options.tools as any)?.styledComponents,
+              ),
+            },
+          ],
+        ],
+      });
+
+    return loaders;
   }
 }
 
