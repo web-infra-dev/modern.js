af8442649e0fe7a49ab99b692b355abf7913b304
Ma Tianqi <matianqi.tqma@bytedance.com>
2021-10-14 18:13:43 +0800

    fix(ssg): schema and internal plugin

--- packages/toolkit/utils/src/constants.ts
+++ packages/toolkit/utils/src/constants.ts
@@ -57,6 +57,7 @@ export const INTERNAL_PLUGINS: {
   '@modern-js/plugin-sass': { cli: '@modern-js/plugin-sass/cli' },
   '@modern-js/plugin-esbuild': { cli: '@modern-js/plugin-esbuild/cli' },
   '@modern-js/plugin-proxy': { cli: '@modern-js/plugin-proxy/cli' },
+  '@modern-js/plugin-ssg': { cli: '@modern-js/plugin-ssg/cli' },
   '@modern-js/plugin-bff': {
     cli: '@modern-js/plugin-bff/cli',
     server: '@modern-js/plugin-bff/server',
@@ -171,7 +172,7 @@ export const PLUGIN_SCHEMAS = {
   '@modern-js/plugin-ssg': [
     {
       target: 'output.ssg',
-      schema: { oneOf: [{ type: 'boolean' }, { instanceof: 'Function' }] },
+      schema: { oneOf: [{ type: 'string' }, { instanceof: 'Function' }] },
     },
   ],
   '@modern-js/plugin-ssr': [
