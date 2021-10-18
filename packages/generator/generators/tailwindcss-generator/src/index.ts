dd5c32708f777b57a9d8deefbe18409ee75b0438
Belinda Cao <caohuilin@bytedance.com>
2021-10-18 11:45:08 +0800

    feat: tailwindcss generator (#85)
    * feat(generator): tailwindcss generator
    
    * fix(generator): express dependence name

---
+++ packages/generator/generators/tailwindcss-generator/src/index.ts
@@ -0,0 +1,53 @@
+import path from 'path';
+import { GeneratorContext, GeneratorCore } from '@modern-js/codesmith';
+import { AppAPI } from '@modern-js/codesmith-api-app';
+import { DependenceGenerator, i18n } from '@modern-js/generator-common';
+import { getPackageVersion } from '@modern-js/generator-utils';
+
+const getGeneratorPath = (generator: string, distTag: string) => {
+  if (process.env.CODESMITH_ENV === 'development') {
+    return path.dirname(require.resolve(generator));
+  } else if (distTag) {
+    return `${generator}@${distTag}`;
+  }
+  return generator;
+};
+
+const handleTemplateFile = async (
+  context: GeneratorContext,
+  generator: GeneratorCore,
+  appApi: AppAPI,
+) => {
+  await appApi.runSubGenerator(
+    getGeneratorPath(DependenceGenerator, context.config.distTag),
+    undefined,
+    {
+      ...context.config,
+      dependencies: {
+        ...(context.config.dependencies || {}),
+        tailwindcss: await getPackageVersion('tailwindcss'),
+      },
+    },
+  );
+};
+
+export default async (context: GeneratorContext, generator: GeneratorCore) => {
+  const appApi = new AppAPI(context, generator);
+
+  const { locale } = context.config;
+  i18n.changeLanguage({ locale });
+  appApi.i18n.changeLanguage({ locale });
+
+  if (!(await appApi.checkEnvironment())) {
+    // eslint-disable-next-line no-process-exit
+    process.exit(1);
+  }
+
+  generator.logger.debug(`start run @modern-js/tailwindcss-generator`);
+  generator.logger.debug(`context=${JSON.stringify(context)}`);
+  generator.logger.debug(`context.data=${JSON.stringify(context.data)}`);
+
+  await handleTemplateFile(context, generator, appApi);
+
+  generator.logger.debug(`forge @modern-js/tailwindcss-generator succeed `);
+};
