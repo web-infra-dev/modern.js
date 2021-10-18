680e50a53a82ab062cfd0b67be21458c97a972ff
Belinda Cao <caohuilin@bytedance.com>
2021-10-18 17:04:01 +0800

    fix: add enable electron success info (#89)

--- packages/generator/generators/electron-generator/src/locale/en.ts
+++ packages/generator/generators/electron-generator/src/locale/en.ts
@@ -1,1 +1,7 @@
-export const EN_LOCALE = { success: `Enable "Electron" Success` };
+export const EN_LOCALE = {
+  success: `Enable "Electron" Success!
+You can run the following command in the directory of the project：
+{packageManager} dev:electron # Run and debug the Electron project according to the requirements of the development environment
+{packageManager} build:electron # Build the Electron project according to the requirements of the product environment
+`,
+};
--- packages/generator/generators/electron-generator/src/locale/zh.ts
+++ packages/generator/generators/electron-generator/src/locale/zh.ts
@@ -1,1 +1,6 @@
-export const ZH_LOCALE = { success: `启用 Electron 模式成功` };
+export const ZH_LOCALE = {
+  success: `启用 Electron 模式成功!
+可在项目的目录下运行以下命令：
+{packageManager} dev:electron # 按开发环境的要求，启动 Electron
+{packageManager} build:electron # 按产品环境的要求，构建 Electron 项目`,
+};
