de98d498a02de89852f1f08b4a8bb5afdb041f35
anc95 <1481988258@qq.com>
2021-10-21 11:04:06 +0800

    feat: createStore used in testing (#113)
    * feat: micro frontend debug
    
    * feat: the createStore used in testing
    
    * feat: the createStore used in testing

--- packages/cli/plugin-testing/src/runtime-testing/index.ts
+++ packages/cli/plugin-testing/src/runtime-testing/index.ts
@@ -1,3 +1,4 @@
 export { default as renderApp } from './customRender';
 export * from '@testing-library/react';
+export * from './reduck';
 // export { testBff } from '@modern-js/testing-plugin-bff';
