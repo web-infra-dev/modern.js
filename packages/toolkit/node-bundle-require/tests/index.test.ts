1420642ef4e0bc70e5c9f2f76098a686404b6a0c
Ma Tianqi <matianqi.tqma@bytedance.com>
2021-10-25 15:23:24 +0800

    fix(config): load config error

---
+++ packages/toolkit/node-bundle-require/tests/index.test.ts
@@ -0,0 +1,7 @@
+import path from 'path'
+import { bundleRequire } from '..'
+
+test('main', async () => {
+  const result = await bundleRequire(path.join(__dirname, './fixture/input.ts'))
+  expect(result.default.a.filename.endsWith('a.ts')).toEqual(true)
+})
