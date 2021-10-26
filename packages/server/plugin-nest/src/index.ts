ff6a39635b3eef592265fdd016ee6257c09f9fbb
tqma113 <matianqi.tqma@bytedance.com>
2021-10-26 16:59:55 +0800

    fix(nest): deps (#155)
    * fix(nest): deps
    
    * fix(nest): deps

--- packages/server/plugin-nest/src/index.ts
+++ packages/server/plugin-nest/src/index.ts
@@ -1,4 +1,4 @@
-import { INestApplication } from '@nestjs/common';
+import type { INestApplication } from '@nestjs/common';
 import './runtime';
 
 export type CustomFactory = (modules: any[]) => Promise<INestApplication>;
