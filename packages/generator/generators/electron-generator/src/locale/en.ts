0c54636aa6f379a677d1655931e4b1a1df4c7a03
皮锟 <pikun@bytedance.com>
2021-10-27 10:42:21 +0800

    feat: improve log of enable electron

--- packages/generator/generators/electron-generator/src/locale/en.ts
+++ packages/generator/generators/electron-generator/src/locale/en.ts
@@ -1,6 +1,7 @@
 export const EN_LOCALE = {
   success: `Enable "Electron" Success!
-You can run the following command in the directory of the project：
+You also need to do some configuration, see more: https://modernjs.dev/docs/start/electron#%E5%90%AF%E7%94%A8-electron.
+After configuration, you can run the following command in the directory of the project：
 {packageManager} dev:electron # Run and debug the Electron project according to the requirements of the development environment
 {packageManager} build:electron # Build the Electron project according to the requirements of the product environment
 `,
