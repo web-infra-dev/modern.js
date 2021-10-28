d1f2725dea5cee22e9a7fdbdf7abd2ccff3e5d7f
anc95 <1481988258@qq.com>
2021-10-28 20:13:47 +0800

    chore: remove unused var DEFAULT_CLIENT_REQUEST_CREATOR

--- packages/server/bff-utils/tests/client.test.ts
+++ packages/server/bff-utils/tests/client.test.ts
@@ -1,11 +1,7 @@
 import fs from 'fs';
 import assert from 'assert';
 import { path } from '@modern-js/utils';
-import {
-  generateClient,
-  getMethodAndStatementFromName,
-  DEFAULT_CLIENT_REQUEST_CREATOR,
-} from '@/index';
+import { generateClient, getMethodAndStatementFromName } from '@/index';
 import { checkSource } from '@/client/check-source';
 import { getRouteName } from '@/client/get-route-name';
 import { HttpMethod } from '@/constant';
