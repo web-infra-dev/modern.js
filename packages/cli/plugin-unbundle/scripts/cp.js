9178685450c755b142cf7c9f4ea64da12a486c51
Ma Tianqi <matianqi.tqma@bytedance.com>
2021-10-29 18:29:21 +0800

    fix(unbundle): cp for node 14

--- packages/cli/plugin-unbundle/scripts/cp.js
+++ packages/cli/plugin-unbundle/scripts/cp.js
@@ -1,7 +1,9 @@
 const path = require('path');
-const fs = require('fs/promises');
+const fs = require('fs');
 
 const src = path.resolve(__dirname, '../src/client');
 const dist = path.resolve(__dirname, '../dist/client');
 
-fs.cp(src, dist, { recursive: true });
+fs.cp(src, dist, { recursive: true }, () => {
+  console.info('Copy done.');
+});
