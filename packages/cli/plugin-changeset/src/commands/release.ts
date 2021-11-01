1a44036e1ccaf8773f5a1ef0659f93a48424c94e
Belinda Cao <caohuilin9588@foxmail.com>
2021-11-01 21:25:40 +0800

    feat(plugin-changeset): release command use changeset command (#250)

--- packages/cli/plugin-changeset/src/commands/release.ts
+++ packages/cli/plugin-changeset/src/commands/release.ts
@@ -1,57 +1,18 @@
-import {
-  path,
-  getPackageManager,
-  isModernjsMonorepo,
-  fs,
-} from '@modern-js/utils';
-import { tag as gitTag } from '@changesets/git';
 import { CHANGESET_PATH, execaWithStreamLog } from '../utils';
 
 interface PublishOptions {
   tag: string;
   ignoreScripts: boolean;
 }
-// eslint-disable-next-line max-statements
 export async function release(options: PublishOptions) {
-  const appDir = process.cwd();
-  const isMonorepo = isModernjsMonorepo(appDir);
-  const packageManager = getPackageManager(process.cwd());
-
   const params = ['publish'];
 
-  const { tag, ignoreScripts } = options;
+  const { tag } = options;
 
   if (tag) {
     params.push('--tag');
     params.push(tag);
   }
 
-  if (!isMonorepo || packageManager === 'yarn') {
-    await execaWithStreamLog('node', [CHANGESET_PATH, ...params]);
-    return;
-  }
-
-  params.push('-r');
-  params.push('--filter');
-  params.push('{./packages}');
-  params.push('--report-summary');
-
-  if (ignoreScripts) {
-    params.push('--ignore-scripts');
-  }
-
-  await execaWithStreamLog(packageManager, params);
-
-  const pnpmPublishSummaryFile = path.join(appDir, 'pnpm-publish-summary.json');
-  const publishInfo: {
-    publishedPackages: Array<{ name: string; version: string }>;
-  } = await fs.readJSON(pnpmPublishSummaryFile, 'utf-8');
-
-  await Promise.all(
-    (publishInfo.publishedPackages || []).map(pkg =>
-      gitTag(`${pkg.name}@${pkg.version}`, appDir),
-    ),
-  );
-
-  await fs.remove(pnpmPublishSummaryFile);
+  await execaWithStreamLog('node', [CHANGESET_PATH, ...params]);
 }
