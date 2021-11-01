a9478d13c33793fc365457ae69f2794f0076e82d
jason.xu <jason.xu@bytedance.com>
2021-11-01 20:33:07 +0800

    feat: init integration tests

---
+++ tests/integration/alias-set/src/App.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable node/no-unsupported-features/es-syntax */
+import React from 'react';
+import test from '@common/test';
+
+function App() {
+  return <div>Hello Modern.js! {test} </div>;
+}
+
+export default App;
+/* eslint-enable node/no-unsupported-features/es-syntax */
