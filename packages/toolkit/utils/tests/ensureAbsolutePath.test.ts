aa9c6ff9481e56063b848f5e1f4faf9bdde9ff10
matianqi.tqma <matianqi.tqma@bytedance.com>
2021-11-01 17:03:50 +0800

    fix(util): testcase for windows

--- packages/toolkit/utils/tests/ensureAbsolutePath.test.ts
+++ packages/toolkit/utils/tests/ensureAbsolutePath.test.ts
@@ -2,12 +2,14 @@ import { ensureAbsolutePath } from '@/ensureAbsolutePath';
 
 describe('ensure absolute file path', () => {
   test('should return filePath directly', () => {
-    expect(ensureAbsolutePath('/a/b', '/a/b/c/d/e.jsx')).toEqual(
-      '/a/b/c/d/e.jsx',
-    );
+    expect(
+      ensureAbsolutePath('/a/b', '/a/b/c/d/e.jsx').endsWith('/a/b/c/d/e.jsx'),
+    ).toBeTruthy();
   });
 
   test(`should resolve absolute path`, () => {
-    expect(ensureAbsolutePath('/a/b', 'c/d/e.jsx')).toEqual('/a/b/c/d/e.jsx');
+    expect(
+      ensureAbsolutePath('/a/b', 'c/d/e.jsx').endsWith('/a/b/c/d/e.jsx'),
+    ).toBeTruthy();
   });
 });
