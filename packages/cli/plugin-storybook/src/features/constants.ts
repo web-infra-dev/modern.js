5eaaaa65b4a3715952c74c4356648d612e08d6fd
targeral <targeral@outlook.com>
2021-11-04 15:37:57 +0800

    fix(plugin-storybook): fix stories error path

--- packages/cli/plugin-storybook/src/features/constants.ts
+++ packages/cli/plugin-storybook/src/features/constants.ts
@@ -1,7 +1,7 @@
 import { path } from '@modern-js/utils';
 
-export const STORYBOOK_USER_CONFIG_PATH = '.storybook';
-export const STORYBOOK_CONFIG_PATH = `.storybook`;
+export const STORYBOOK_USER_CONFIG_PATH = 'config/storybook';
+export const STORYBOOK_CONFIG_PATH = `config/storybook`;
 export const STORYBOOK_PORT = 9009;
 export const STORYBOOK_DIST_DIR_NAME = 'storybook-static';
 export const STORYBOOK_TEMPLATE_DIR = path.join(__dirname, '../../template');
