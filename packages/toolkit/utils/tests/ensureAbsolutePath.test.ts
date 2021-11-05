f5a419c8ab018e4da80ba850d1811d5a57b09dc0
matianqi.tqma <matianqi.tqma@bytedance.com>
2021-11-05 10:54:08 +0800

    test(util): fix test case

--- packages/toolkit/utils/tests/ensureAbsolutePath.test.ts
+++ packages/toolkit/utils/tests/ensureAbsolutePath.test.ts
@@ -1,15 +1,16 @@
+import path from 'path';
 import { ensureAbsolutePath } from '@/ensureAbsolutePath';
 
 describe('ensure absolute file path', () => {
   test('should return filePath directly', () => {
     expect(
-      ensureAbsolutePath('/a/b', '/a/b/c/d/e.jsx').endsWith('/a/b/c/d/e.jsx'),
+      path.isAbsolute(ensureAbsolutePath('/a/b', '/a/b/c/d/e.jsx')),
     ).toBeTruthy();
   });
 
   test(`should resolve absolute path`, () => {
     expect(
-      ensureAbsolutePath('/a/b', 'c/d/e.jsx').endsWith('/a/b/c/d/e.jsx'),
+      path.isAbsolute(ensureAbsolutePath('/a/b', 'c/d/e.jsx')),
     ).toBeTruthy();
   });
 });
