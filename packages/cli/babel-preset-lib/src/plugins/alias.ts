d38e432997286fe07906045c2e0b359faa06924f
Ma Tianqi <matianqi.tqma@bytedance.com>
2021-11-09 15:09:22 +0800

    fix(babel-lib): alias path error

--- packages/cli/babel-preset-lib/src/plugins/alias.ts
+++ packages/cli/babel-preset-lib/src/plugins/alias.ts
@@ -47,12 +47,12 @@ export const aliasPlugin = (alias: AliasOption): [string, PluginOptions] => {
       );
       const fileName = path.basename(result);
       // 如果是同级文件，则返回的是 ''
-      const filePath = path.normalize(
-        `${relativePath.length === 0 ? '.' : relativePath}/${fileName}`,
-      );
-      return path
-        .normalize(filePath.startsWith('.') ? filePath : `./${filePath}`)
+      const filePath = path
+        .normalize(
+          `${relativePath.length === 0 ? '.' : relativePath}/${fileName}`,
+        )
         .replace(/\\/, '/');
+      return filePath.startsWith('.') ? filePath : `./${filePath}`;
     }
     return resolvePath(sourcePath, currentFile, opts);
   };
