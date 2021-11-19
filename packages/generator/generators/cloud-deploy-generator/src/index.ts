3a95944cc8a52c66ae2951b3b28291d5919138ba
caohuilin <caohuilin@bytedance.com>
2021-11-19 17:54:44 +0800

    fix(generator): fix generator text

--- packages/generator/generators/cloud-deploy-generator/src/index.ts
+++ packages/generator/generators/cloud-deploy-generator/src/index.ts
@@ -69,7 +69,7 @@ const handleTemplateFile = async (
     );
   } else {
     generator.logger.info(
-      `未云函数部署应用, 你可能希望使用其他容器部署应用, 相关内容可以查看官网: \n- ${docLinks.docker}\n`,
+      `未选择云函数部署应用, 你可能希望使用其他容器部署应用, 相关内容可以查看官网: \n- ${docLinks.docker}\n`,
     );
   }
 
