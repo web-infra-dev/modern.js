a83a12d91232f60a0154ee04cd121bcc57ade467
kky <68810266+zllkjc@users.noreply.github.com>
2021-11-26 15:03:51 +0800

    fix: support static host dev (#332)
    * fix: support static host dev
    
    * docs: add changeset
    
    Co-authored-by: Belinda Cao <caohuilin9588@foxmail.com>

---
+++ .changeset/fifty-llamas-hammer.md
@@ -0,0 +1,5 @@
+---
+"@modern-js/plugin-static-hosting": patch
+---
+
+support static host on dev command
--- packages/cli/static-hosting/src/index.ts
+++ packages/cli/static-hosting/src/index.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import { logger, fs } from '@modern-js/utils';
+import { logger, fs, isDev } from '@modern-js/utils';
 import { createPlugin, useAppContext } from '@modern-js/core';
 import { ServerRoute } from '@modern-js/types';
 
@@ -12,39 +12,45 @@ const MAIN_ENTRY_NAME = 'main';
  * or server.baseUrl(automatically apply to basename)
  */
 
-export default createPlugin(() => ({
-  config() {
+export default createPlugin(() => {
+  if (isDev()) {
+    return {};
+  } else {
     return {
-      output: {
-        htmlPath: '.',
-        disableHtmlFolder: true,
+      config() {
+        return {
+          output: {
+            htmlPath: '.',
+            disableHtmlFolder: true,
+          },
+        };
+      },
+      // simply handle some routes and make modern servers work as much as possible
+      modifyServerRoutes({ routes }: { routes: ServerRoute[] }) {
+        const updated = routes.map(route => {
+          if (route.entryName === MAIN_ENTRY_NAME) {
+            return {
+              ...route,
+              entryPath: route.entryPath.replace('main', 'index'),
+            };
+          }
+          return route;
+        });
+
+        return { routes: updated };
+      },
+      async afterBuild() {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const { distDirectory } = useAppContext();
+        const mainHtml = path.join(distDirectory, 'main.html');
+        const indexHtml = path.join(distDirectory, 'index.html');
+
+        // set main entry to index, for access from root path
+        if (fs.existsSync(mainHtml) && !fs.existsSync(indexHtml)) {
+          logger.info('Moving main.html to index.html for static hosting');
+          fs.moveSync(mainHtml, path.join(distDirectory, 'index.html'));
+        }
       },
     };
-  },
-  // simply handle some routes and make modern servers work as much as possible
-  modifyServerRoutes({ routes }: { routes: ServerRoute[] }) {
-    const updated = routes.map(route => {
-      if (route.entryName === MAIN_ENTRY_NAME) {
-        return {
-          ...route,
-          entryPath: route.entryPath.replace('main', 'index'),
-        };
-      }
-      return route;
-    });
-
-    return { routes: updated };
-  },
-  async afterBuild() {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const { distDirectory } = useAppContext();
-    const mainHtml = path.join(distDirectory, 'main.html');
-    const indexHtml = path.join(distDirectory, 'index.html');
-
-    // set main entry to index, for access from root path
-    if (fs.existsSync(mainHtml) && !fs.existsSync(indexHtml)) {
-      logger.info('Moving main.html to index.html for static hosting');
-      fs.moveSync(mainHtml, path.join(distDirectory, 'index.html'));
-    }
-  },
-}));
+  }
+});
