f46c108bea92de93f16f64f7bc0f1869633c341d
wangyiming <wangyiming.fe@bytedance.com>
2021-12-20 15:17:50 +0800

    feat: support bff test

--- packages/review/testing-plugin-bff/src/env.ts
+++ packages/review/testing-plugin-bff/src/env.ts
@@ -1,4 +1,5 @@
 import NodeEnvironment from 'jest-environment-node';
+import { compatRequire } from '@modern-js/utils';
 import { createApp } from './app';
 import { bff_info_key } from './constant';
 
@@ -6,12 +7,16 @@ export default class extends NodeEnvironment {
   app: any;
 
   async setup() {
-    const bff_info = (global as any)[bff_info_key];
+    const bff_info = (this.global as any)[bff_info_key];
+    const plugins = bff_info.plugins.map((plugin: { pluginPath: string }) =>
+      compatRequire(plugin.pluginPath),
+    );
+
     // eslint-disable-next-line no-multi-assign
     this.global.app = this.app = await createApp(
       bff_info.appDir,
       bff_info.modernUserConfig,
-      bff_info.plugins,
+      plugins,
       bff_info.routes,
     );
   }
