702232997cee5d67733125561bcb0b1d78942098
targeral <targeral@outlook.com>
2021-12-22 17:12:01 +0800

    Feat compat jupiter (#375)
    * feat: add compate change
    
    1. fix watch functions
    2. add CORE_INIT_OPTION_FILE env vars
    
    * feat: add CORE_INIT_OPTION_FILE env
    
    * feat(plugin-storybook): add resolves modules path
    
    * chore: add changeset
    
    Co-authored-by: 高博文 <gaobowen.targeral@bytedance.com>

--- packages/cli/plugin-storybook/src/features/constants.ts
+++ packages/cli/plugin-storybook/src/features/constants.ts
@@ -5,3 +5,6 @@ export const STORYBOOK_CONFIG_PATH = `config/storybook`;
 export const STORYBOOK_PORT = 9009;
 export const STORYBOOK_DIST_DIR_NAME = 'storybook-static';
 export const STORYBOOK_TEMPLATE_DIR = path.join(__dirname, '../../template');
+export const CURRENT_PKG_PATH = path.dirname(
+  require.resolve('@modern-js/plugin-storybook/package.json'),
+);
