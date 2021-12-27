afe96b4456e2faa3286bbca48f3f5055781e48bd
ycaptain <cz.ycaptain@gmail.com>
2021-12-27 20:34:18 +0800

    fix(testing, app-tools): add schema validation and types to test plugin; fix app-tools types (#384)
    * fix(testing): lack typesVerions for re-exporting types
    
    * fix(testing): fix type lost when redeclareing modules
    
    * feat(testing): export all @modern-js/testing inner types
    
    * fix(app-tools): export types and reference it in handlebars to fix 'app-tools is not a module'
    
    * test(testing): test render component environment
    
    * fix(integration): temporarilly rename test file in plugin-testing-app to avoid test error by conf
    
    * fix(testing): remove useless README and workspace project
    
    * fix: workspace:* to workspace:^1.0.0

---
+++ .changeset/chatty-starfishes-cover.md
@@ -0,0 +1,8 @@
+---
+"@modern-js/core": minor
+"@modern-js/plugin-testing": minor
+---
+
+add modern config schema and types of testing, tools.jest.
+add typesVersions for re-exporting types of @modern-js/plugin-testing.
+fix type lost when redeclareing modules.
--- packages/cli/plugin-testing/type.d.ts
+++ packages/cli/plugin-testing/type.d.ts
@@ -2,3 +2,13 @@ declare module '@modern-js/runtime/testing' {
   export * from '@testing-library/react';
   export { renderApp, createStore } from '@modern-js/plugin-testing';
 }
+
+declare module '@modern-js/core' {
+  interface UserConfig {
+    testing?: import('@modern-js/testing').TestConfig;
+  }
+
+  interface ToolsConfig {
+    jest?: import('@modern-js/testing').TestConfig['jest'];
+  }
+}
