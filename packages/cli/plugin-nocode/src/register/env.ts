90f78383a4a18789b581d1b3e24630e8ff8aeef3
郑凡恺 <zhengfankai@bytedance.com>
2021-12-27 11:46:46 +0800

    feat: nocode 插件接入外网 butter 的注册服务，默认带上token，绕过鉴权

--- packages/cli/plugin-nocode/src/register/env.ts
+++ packages/cli/plugin-nocode/src/register/env.ts
@@ -1,20 +1,20 @@
 const isStaging = ['STG', 'STAGING'].includes(
   (process.env.DEPLOY_ENV || '').toUpperCase(),
 );
-const isLocalDebug = process.env.BUTTER_LOCAL_DEBUG === 'true';
+// const isLocalDebug = process.env.BUTTER_LOCAL_DEBUG === 'true';
 
 const PROD_SERVICE_ID = 'ttzu9f';
 const STG_SERVICE_ID = 'ttiyts';
 
 const SERVICE_ID = isStaging ? STG_SERVICE_ID : PROD_SERVICE_ID;
-// eslint-disable-next-line import/no-mutable-exports
-let BUTTER_HOST = isStaging
-  ? 'https://butter-staging.bytedance.net'
-  : 'https://butter.bytedance.net';
+// let BUTTER_HOST = isStaging
+//   ? 'https://butter-staging.bytedance.net'
+//   : 'https://butter.bytedance.net';
 
-if (isLocalDebug) {
-  BUTTER_HOST = 'http://localhost:3000';
-}
+// if (isLocalDebug) {
+//   BUTTER_HOST = 'http://localhost:3000';
+// }
+const BUTTER_HOST = `https://butter.bytedance.com`;
 
 const BUTTER_REGISTER_ENDPOINT = `${BUTTER_HOST}/open-api/v1/register`;
 const BUTTER_UNREGISTER_ENDPOINT = `${BUTTER_HOST}/open-api/v1/unregister`;
