d8d367e3f11e6792a36939c46b328ac77b27de0f
ycaptain <cz.ycaptain@gmail.com>
2021-12-27 08:27:56 +0800

    test(testing): test render component environment

--- packages/cli/plugin-testing/type.d.ts
+++ packages/cli/plugin-testing/type.d.ts
@@ -1,6 +1,6 @@
-
 declare module '@modern-js/runtime/testing' {
-  export * from '@modern-js/plugin-testing/dist/types/runtime-testing';
+  export * from '@testing-library/react';
+  export { renderApp, createStore } from '@modern-js/plugin-testing';
 }
 
 declare module '@modern-js/core' {
---
+++ tests/integration/plugin-testing-app/README.md
@@ -0,0 +1,22 @@
+# Your MWA
+
+## Get Started
+
+按开发环境的要求，运行和调试项目
+
+```
+pnpm run dev
+```
+
+继续创建更多项目要素，比如应用入口
+
+```
+pnpm run new
+```
+
+其他
+
+```
+pnpm run build        # 按产品环境的要求，构建项目
+pnpm run start        # 按产品环境的要求，运行项目
+```
