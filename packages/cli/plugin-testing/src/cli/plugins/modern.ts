cd49a2c8f2a1f40a9ad67ecb2f147c3724858a67
Ming <527990618@163.com>
2021-12-28 17:37:52 +0800

    fix: testMatch on windows (#421)
    Co-authored-by: wangyiming <wangyiming.fe@bytedance.com>

--- packages/cli/plugin-testing/src/cli/plugins/modern.ts
+++ packages/cli/plugin-testing/src/cli/plugins/modern.ts
@@ -46,7 +46,13 @@ export default (webpackConfig: any, userConfig: any, pwd: string) =>
         utils.setJestConfig({
           rootDir: pwd || process.cwd(),
           // todo: diffrent test root for diffrent solutions
-          testMatch: [`<rootDir>/(src|tests|electron)/**/*.test.[jt]s?(x)`],
+          // testMatch: [`<rootDir>/(src|tests|electron)/**/*.test.[jt]s?(x)`],
+          // testMatch bug on windows, issue: https://github.com/facebook/jest/issues/7914
+          testMatch: [
+            `<rootDir>/src/**/*.test.[jt]s?(x)`,
+            `<rootDir>/tests/**/*.test.[jt]s?(x)`,
+            `<rootDir>/electron/**/*.test.[jt]s?(x)`,
+          ],
         });
 
         return next(utils);
