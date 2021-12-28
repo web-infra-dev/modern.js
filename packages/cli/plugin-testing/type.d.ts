5d7a76641f5d510a1f0612632eae5d9bc6825f9f
wangyiming <wangyiming.fe@bytedance.com>
2021-12-28 15:22:06 +0800

    fix: testMatch on windows

--- packages/cli/plugin-testing/type.d.ts
+++ packages/cli/plugin-testing/type.d.ts
@@ -1,3 +1,4 @@
+import '@modern-js/core';
 declare module '@modern-js/runtime/testing' {
   export * from '@testing-library/react';
   export { renderApp, createStore } from '@modern-js/plugin-testing';
