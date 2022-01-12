10798e9ddf3aacc3b2396b6dd386cf408868b167
Belinda Cao <caohuilin9588@foxmail.com>
2022-01-12 18:11:42 +0800

    fix: dependence generator add sourceTypeFile config (#471)
    * fix: dependence generator add sourceTypeFile config
    
    * fix: delete no use config
    
    * fix: fix append type content

--- packages/generator/generators/bff-generator/src/index.ts
+++ packages/generator/generators/bff-generator/src/index.ts
@@ -230,7 +230,9 @@ const handleTemplateFile = async (
     const typePath = path.join(appDir, 'src', 'modern-app-env.d.ts');
     if (fs.existsSync(typePath)) {
       const npmrc = fs.readFileSync(typePath, 'utf-8');
-      fs.writeFileSync(typePath, `${npmrc}${appendTypeContent}\n`, 'utf-8');
+      if (!npmrc.includes(appendTypeContent)) {
+        fs.writeFileSync(typePath, `${npmrc}${appendTypeContent}\n`, 'utf-8');
+      }
     } else {
       fs.ensureFileSync(typePath);
       fs.writeFileSync(typePath, appendTypeContent, 'utf-8');
