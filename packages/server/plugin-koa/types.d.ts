b26a7f36e895e10464f0a8a545e0edec2c321ddb
kky <68810266+zllkjc@users.noreply.github.com>
2022-01-18 12:42:44 +0800

    fix: clone server context (#492)
    * fix: clone server context
    
    * docs: add changeset

--- packages/server/plugin-koa/types.d.ts
+++ packages/server/plugin-koa/types.d.ts
@@ -1,7 +1,15 @@
 /// <reference path="./dist/types/index.d.ts" />
-
 declare module '@modern-js/runtime/server' {
-  import { Context } from 'koa';
+
+  import { Context, Middleware } from 'koa';
+
+  type KoaOptions = {
+    addMiddleware: (...input: Middleware[]) => void;
+  };
+
+  type KoaAttacher = (options: KoaOptions) => void;
 
   export function useContext(): Context;
+
+  export function hook(attacher: KoaAttacher): KoaAttacher;
 }
