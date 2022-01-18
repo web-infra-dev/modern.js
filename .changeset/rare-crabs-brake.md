146dcd859eedcb5a87e2f4f8fbe6fcb701f38940
kky <68810266+zllkjc@users.noreply.github.com>
2022-01-18 12:42:44 +0800

    fix: clone server context (#492)
    * fix: clone server context
    
    * docs: add changeset

---
+++ .changeset/rare-crabs-brake.md
@@ -0,0 +1,13 @@
+---
+"@modern-js/runtime": patch
+"@modern-js/bff-runtime": patch
+"@modern-js/plugin-egg": patch
+"@modern-js/plugin-express": patch
+"@modern-js/plugin-koa": patch
+"@modern-js/plugin-nest": patch
+"@modern-js/plugin-server": patch
+"@modern-js/server": patch
+"@modern-js/server-utils": patch
+---
+
+modify server framework plugin hook types
--- packages/server/plugin-egg/types.d.ts
+++ packages/server/plugin-egg/types.d.ts
@@ -1,6 +1,18 @@
 /// <reference path="./dist/types/index.d.ts" />
 
-import type { Context } from './dist/types/context';
 declare module '@modern-js/runtime/server' {
+  import type { Next } from 'koa'
+  import type { Context } from 'egg'
+
   export function useContext(): Context;
+
+  type RequestHandler = () => (ctx: Context, next: Next) => Promise<void>
+
+  type EggOptions = {
+    addMiddleware: (...input: RequestHandler[]) => void;
+  };
+
+  type EggAttacher = (options: EggOptions) => void;
+
+  export function hook(attacher: EggAttacher): EggAttacher;
 }
