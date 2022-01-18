9ddcf781792acdf02549d0130dd784b3bd415aea
zhouxiao.shaw <zhouxiao.shaw@bytedance.com>
2022-01-18 14:50:20 +0800

    docs: contributing add run e2e test script

--- packages/cli/plugin-micro-frontend/type.d.ts
+++ packages/cli/plugin-micro-frontend/type.d.ts
@@ -1,4 +1,5 @@
 declare module '@modern-js/runtime' {
   export const useModuleApp: typeof import('./dist/types/runtime').useModuleApp;
   export const useModuleApps: typeof import('./dist/types/runtime').useModuleApps;
+  export const useLegacyModuleApps: typeof import('./dist/types/runtime').useLegacyModuleApps;
 }
