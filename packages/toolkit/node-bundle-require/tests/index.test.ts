794f978587106d76a2b4e8ad05129acff6485bba
Rickie Zheng <rqzheng2008@126.com>
2022-01-21 15:23:43 +0800

    fix: fix esbuild error when project's tsconfig.json sets "target:es5" (#520)

--- packages/toolkit/node-bundle-require/tests/index.test.ts
+++ packages/toolkit/node-bundle-require/tests/index.test.ts
@@ -5,7 +5,14 @@ test('require', async () => {
   const result = await bundleRequire(
     path.join(__dirname, './fixture/input.ts'),
   );
+  // when tsconfig.json sets `compilerOptions.target` to `es5`
+  // normally it will met error
+  // So we need to manually set esbuild's target to esnext to avoid that
+  // These two cases above use ES6+ ability, to test whether esbuild successfuly
+  // works on non-ES5 files
+  // reference: https://github.com/evanw/esbuild/releases/tag/v0.12.6
   expect(result.default.a.filename.endsWith('a.ts')).toEqual(true);
+  expect(result.default.a.showFileName().endsWith('a.ts')).toEqual(true);
 });
 
 describe('external regexp', () => {
