a72821df564fbab023d96c1c1eab7c902aaab1b4
await-ovo <41503212+await-ovo@users.noreply.github.com>
2022-01-28 15:44:02 +0800

    feat(plugin-analyze): add beforeGenerateRoutes hook (#473)
    chore(plugin-analyze): add test cases

---
+++ packages/cli/plugin-analyze/tests/generateCode.test.ts
@@ -0,0 +1,64 @@
+import { createImportStatements } from '../src/generateCode';
+
+describe('generate code', () => {
+  test('should create import statements sucessfully', () => {
+    const statement = createImportStatements([
+      {
+        value: 'react',
+        specifiers: [
+          {
+            local: 'React',
+          },
+          {
+            imported: 'useState',
+          },
+          {
+            imported: 'useEffect',
+          },
+        ],
+      },
+    ]);
+    expect(statement).toEqual(
+      `import React, { useState, useEffect } from 'react';\n`,
+    );
+  });
+
+  test('should merge statements', () => {
+    const statement = createImportStatements([
+      {
+        value: 'a',
+        specifiers: [
+          {
+            local: 'b',
+            imported: 'c',
+          },
+        ],
+      },
+      {
+        value: 'a',
+        specifiers: [
+          {
+            imported: 'd',
+          },
+        ],
+      },
+    ]);
+    expect(statement).toEqual(`import { c as b, d } from 'a';\n\n`);
+  });
+
+  test(`should create import statement with initilize string`, () => {
+    expect(
+      createImportStatements([
+        {
+          value: 'a',
+          specifiers: [
+            {
+              local: 'a',
+            },
+          ],
+          initialize: 'console.log(a);',
+        },
+      ]),
+    ).toEqual(`import a from 'a';\nconsole.log(a);`);
+  });
+});
