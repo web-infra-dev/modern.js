35b4580c32f62ef985dd4a8a9095b134ca6b64b4
kky <68810266+zllkjc@users.noreply.github.com>
2022-01-28 21:11:14 +0800

    fix: fix mock and add server test (#509)

--- packages/server/server/src/dev-tools/mock/index.ts
+++ packages/server/server/src/dev-tools/mock/index.ts
@@ -1,28 +1,9 @@
 import path from 'path';
 import { fs } from '@modern-js/utils';
-import { match } from 'path-to-regexp';
 import { NextFunction } from '../../type';
 import { ModernServerContext } from '../../libs/context';
 import { AGGRED_DIR } from '../../constants';
-import getMockData, { MockApi } from './getMockData';
-
-const getMatched = (context: ModernServerContext, mockApiList: MockApi[]) => {
-  const { path: targetPathname, method: targetMethod } = context;
-
-  const matched = mockApiList.find(mockApi => {
-    const { method, path: pathname } = mockApi;
-    if (method.toLowerCase() === targetMethod.toLowerCase()) {
-      return match(pathname, {
-        encode: encodeURI,
-        decode: decodeURIComponent,
-      })(targetPathname);
-    }
-
-    return false;
-  });
-
-  return matched;
-};
+import getMockData, { getMatched } from './getMockData';
 
 export const createMockHandler = ({ pwd }: { pwd: string }) => {
   const exts = ['.ts', '.js'];
