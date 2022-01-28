a2bc45f0186544563b730ed531f7bf0101932a5f
Zhou xiao <zhouxiao.shaw@bytedance.com>
2022-01-28 18:25:06 +0800

    build: optimize build commands to optimize with modification time as an indicator (#570)

--- scripts/build/bin/modern.js
+++ scripts/build/bin/modern.js
@@ -10,11 +10,35 @@ process.env.CORE_INIT_OPTION_FILE = path.resolve(
   '../src/cli_core_init.js',
 );
 
+function getLastModifiedRecursive(candidate) {
+  // 检测路径不存在，并且为 node_modules 下目录跳过
+  if (/node_modules/.test(candidate) || !fs.existsSync(candidate)) {
+    return 0;
+  }
+  const stats = fs.statSync(candidate);
+  let candidateTimes = [stats.mtimeMs, stats.ctimeMs, stats.birthtimeMs];
+  if (stats.isDirectory()) {
+    const files = fs.readdirSync(candidate);
+    candidateTimes = candidateTimes.concat(
+      files.map(f => getLastModifiedRecursive(path.join(candidate, f))),
+    );
+  }
+  return Math.max.apply(null, candidateTimes);
+}
+
 const distDir = path.join(process.cwd(), 'dist');
-if (!fs.existsSync(distDir)) {
+const pkgLastModifyTime = getLastModifiedRecursive(process.cwd());
+const distLastModifyTime = getLastModifiedRecursive(distDir);
+
+if (
+  pkgLastModifyTime > distLastModifyTime ||
+  process.env.DISABLE_IGNORE_BUILD
+) {
   require(`${kProjectRoot}/packages/cli/core/src/cli.ts`);
 } else {
   // eslint-disable-next-line no-console
-  console.log('The build directory `dist` already existed, skip this build');
-  // IGNORE 非常简单的一个缓存策略，只要文件夹存在就不会再次执行
+  console.log(
+    `The built directory The build content has not been modified and does not need to be rebuilt`,
+  );
+  // IGNORE 如果目录源码的修改时间小于等于 dist 的时间跳过重新构建节省时间
 }
