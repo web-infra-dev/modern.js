55972e6c8e34e74dd99d4ac50a9dafa658f376ec
Ming <wangyiming.fe@bytedance.com>
2022-02-10 13:05:27 +0800

    fix: create route.json failure (#599)

--- packages/solutions/app-tools/src/utils/routes.ts
+++ packages/solutions/app-tools/src/utils/routes.ts
@@ -5,7 +5,7 @@ import { fs, ROUTE_SPEC_FILE } from '@modern-js/utils';
 const generateRoutes = async (appContext: IAppContext) => {
   const { serverRoutes, distDirectory } = appContext;
   const output = JSON.stringify({ routes: serverRoutes }, null, 2);
-  await fs.writeFile(path.join(distDirectory, ROUTE_SPEC_FILE), output);
+  await fs.outputFile(path.join(distDirectory, ROUTE_SPEC_FILE), output);
 };
 
 export { generateRoutes };
