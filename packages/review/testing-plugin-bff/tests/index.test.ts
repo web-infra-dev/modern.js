698ad9e88435422a9aeba3b10df0f4b1c09b49df
Ming <wangyiming.fe@bytedance.com>
2022-02-16 19:51:27 +0800

    feat: support bff test for api service (#627)
    * feat: support bff test for api service
    
    * test: add test cases for bff test

--- packages/review/testing-plugin-bff/tests/index.test.ts
+++ packages/review/testing-plugin-bff/tests/index.test.ts
@@ -1,11 +1,41 @@
-import * as path from 'path';
-import { isBFFProject } from '../src';
-import mockAPI from '../src/mockAPI';
+import path from 'path';
+import createPlugin, { setJestConfigForBFF } from '../src';
+
+const root = path.resolve(__dirname, '../../../../');
+expect.addSnapshotSerializer({
+  test: val => typeof val === 'string' && val.includes(root),
+  print: val =>
+    typeof val === 'string'
+      ? `"${val.replace(root, '').replace(/\\/g, '/')}"`
+      : (val as string),
+});
 
 describe('testing-plugin-bff', () => {
-  test('isBFFProject', () => {
-    const appPath = path.resolve(__dirname, './fixtures/bff1');
-    expect(isBFFProject(appPath)).toBe(true);
-    expect(mockAPI).toBeInstanceOf(Function);
+  test('plugin', async () => {
+    expect(createPlugin).toBeDefined();
+    expect(createPlugin).toBeInstanceOf(Function);
+  });
+
+  test('setJestConfigForBFF', async () => {
+    const appDir = path.normalize(path.resolve(__dirname, './fixtures/bff1'));
+    const mockUtils = {
+      _jestConfig: {},
+      get jestConfig() {
+        return this._jestConfig;
+      },
+      setJestConfig(config: any) {
+        this._jestConfig = config;
+      },
+    };
+
+    await setJestConfigForBFF({
+      pwd: appDir,
+      userConfig: {},
+      plugins: [],
+      routes: [],
+      utils: mockUtils,
+    });
+
+    expect(mockUtils.jestConfig).toMatchSnapshot();
   });
 });
