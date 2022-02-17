abff162699c9315dbde4f2c34fa4c49395193d77
zllkjc <kongjiacong@bytedance.com>
2022-02-17 18:59:24 +0800

    fix: prevent ssr compiler to send socket message

--- packages/server/server/src/dev-tools/socket-server.ts
+++ packages/server/server/src/dev-tools/socket-server.ts
@@ -21,6 +21,8 @@ export default class SocketServer {
 
   private stats?: Stats;
 
+  private timer: NodeJS.Timeout | null = null;
+
   constructor(options: DevServerOptions) {
     this.options = options;
   }
@@ -50,7 +52,7 @@ export default class SocketServer {
       logger.error(err);
     });
 
-    setInterval(() => {
+    this.timer = setInterval(() => {
       this.wsServer.clients.forEach(socket => {
         const extWs = socket as ExtWebSocket;
         if (!extWs.isAlive) {
@@ -85,23 +87,23 @@ export default class SocketServer {
       });
 
       if (this.options.client.logging) {
-        this.sockWrite('logging', this.options.client.logging);
+        this.singleWrite(connection, 'logging', this.options.client.logging);
       }
 
       if (this.options.hot || this.options.hot === 'only') {
-        this.sockWrite('hot');
+        this.singleWrite(connection, 'hot');
       }
 
       if (this.options.liveReload) {
-        this.sockWrite('liveReload');
+        this.singleWrite(connection, 'liveReload');
       }
 
       if (this.options.client.progress) {
-        this.sockWrite('progress', this.options.client.progress);
+        this.singleWrite(connection, 'progress', this.options.client.progress);
       }
 
       if (this.options.client.overlay) {
-        this.sockWrite('overlay', this.options.client.overlay);
+        this.singleWrite(connection, 'overlay', this.options.client.overlay);
       }
 
       // send first stats to active client sock if stats exist
@@ -126,10 +128,23 @@ export default class SocketServer {
     });
   }
 
+  public singleWrite(
+    socket: ws,
+    type: string,
+    data?: Record<string, any> | string | boolean,
+  ) {
+    this.send(socket, JSON.stringify({ type, data }));
+  }
+
   public close() {
     this.sockets.forEach(socket => {
       socket.close();
     });
+
+    if (this.timer) {
+      clearInterval(this.timer);
+      this.timer = null;
+    }
   }
 
   // get standard stats
