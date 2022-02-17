735b2a81edfb63d391b1d2e35a3ca4394ce1edeb
kky <68810266+zllkjc@users.noreply.github.com>
2022-02-17 20:17:32 +0800

    fix: ssr by entries cause ssg not work (#640)
    * fix: ssr by entries cause ssg not work
    
    * fix: prevent ssr compiler to send socket message
    
    * docs: add changeset
    
    * chore: add unit test

--- packages/server/server/tests/dev.test.ts
+++ packages/server/server/tests/dev.test.ts
@@ -53,14 +53,31 @@ describe('test dev tools', () => {
 
     const socket = {
       state: 1,
+      readyState: 1,
+      data: '',
       close() {
         socket.state = 0;
+      },
+      send(data: string) {
+        socket.data = data;
+      },
+      on() {
         // empty
       },
     };
+
+    socketServer.onConnect(socket);
+
     socketServer.sockets = [socket];
+    socketServer.sockWrite('test');
+    expect(socket.data).toBe(JSON.stringify({ type: 'test' }));
+
+    socketServer.singleWrite(socket, 'single');
+    expect(socket.data).toBe(JSON.stringify({ type: 'single' }));
+
     socketServer.close();
     expect(socket.state).toBe(0);
+    app.close();
   });
 
   test('should dev server plugin work correctly', () => {
