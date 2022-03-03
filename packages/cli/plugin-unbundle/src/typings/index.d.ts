a082a991e3e1ff7b827a7dfbfa882d9340d9bb98
chenhuang444 <huangchen.foobar@bytedance.com>
2022-03-03 14:22:34 +0800

    feat(plugin-unbundle): add config option for ignoring package in unbundle
    also fix unbundle crash on server restart

--- packages/cli/plugin-unbundle/src/typings/index.d.ts
+++ packages/cli/plugin-unbundle/src/typings/index.d.ts
@@ -8,6 +8,19 @@ declare module '@modern-js/core' {
   interface OutputConfig {
     disableAutoImportStyle?: boolean;
   }
+
+  interface DevConfig {
+    unbundle?: {
+      /**
+       * Some package A may require another package B that is intended for Node.js
+       * use only. In such a case, if package B cannot be converted to ESM, it will
+       * cause package A to fail during unbundle development, even though package B
+       * is not really required. Package B can thus be safely ignored via this option
+       * to ensure transpilation of package A to ESM
+       */
+      ignore?: string | string[];
+    };
+  }
 }
 
 declare module 'acorn-class-fields';
