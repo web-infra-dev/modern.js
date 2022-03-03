0778b597ad4c394afbfdb8003f7a2990f7fea973
Belinda Cao <caohuilin@bytedance.com>
2022-03-03 14:07:31 +0800

    feat: package manager support npm (#665)
    * feat: package manager support npm
    
    * docs: changeset
    
    * feat: adjust module template tests directory
    
    * docs: changeset
    
    * feat: add package manager schema test
    
    * feat: upgrade @modern-js/codesmith-api-app
    
    * docs: changeset

--- packages/generator/generator-common/src/common/package-manager.ts
+++ packages/generator/generator-common/src/common/package-manager.ts
@@ -4,11 +4,13 @@ import { i18n, localeKeys } from '../locale';
 export enum PackageManager {
   Pnpm = 'pnpm',
   Yarn = 'yarn',
+  Npm = 'npm',
 }
 
 export const PackageManagerName: Record<string, () => string> = {
   [PackageManager.Pnpm]: () => 'pnpm',
   [PackageManager.Yarn]: () => 'Yarn',
+  [PackageManager.Npm]: () => 'npm',
 };
 
 export const PackageManagerSchema: Schema = {
@@ -17,8 +19,13 @@ export const PackageManagerSchema: Schema = {
   label: () => i18n.t(localeKeys.packageManager.self),
   mutualExclusion: true,
   when: (_values, extra) => !extra?.isMonorepoSubProject,
-  items: Object.values(PackageManager).map(packageManager => ({
-    key: packageManager,
-    label: PackageManagerName[packageManager],
-  })),
+  items: (_values, extra) =>
+    Object.values(PackageManager)
+      .filter(packageManager =>
+        extra?.isMonorepo ? packageManager !== PackageManager.Npm : true,
+      )
+      .map(packageManager => ({
+        key: packageManager,
+        label: PackageManagerName[packageManager],
+      })),
 };
