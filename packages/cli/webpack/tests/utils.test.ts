0ad75faa18e2eadc809c312edfad4587b7f9a342
neverland <chenjiahan.jait@bytedance.com>
2022-03-04 15:23:25 +0800

    feat: improve fork-ts-checker conditions and options (#680)
    Co-authored-by: Belinda Cao <caohuilin@bytedance.com>

--- packages/cli/webpack/tests/utils.test.ts
+++ packages/cli/webpack/tests/utils.test.ts
@@ -1,5 +1,4 @@
 import { mergeRegex } from '../src/utils/mergeRegex';
-import { generateMetaTags } from '../src/utils/generateMetaTags';
 
 describe('mergeRegex', () => {
   it('should merge regexp correctly', () => {
@@ -8,23 +7,5 @@ describe('mergeRegex', () => {
 
   it('should merge string correctly', () => {
     expect(mergeRegex('\\.js$', '\\.ts$')).toEqual(/\.js$|\.ts$/);
-  });
-});
-
-describe('generateMetaTags', () => {
-  it('should return empty string when params is empty', () => {
-    expect(generateMetaTags()).toEqual('');
-  });
-
-  it('should generate meta tag correctly', () => {
-    const options = {
-      foo: {
-        name: 'viewport',
-        content: 'width=500, initial-scale=1',
-      },
-    };
-    expect(generateMetaTags(options).trim()).toEqual(
-      '<meta name="viewport" content="width=500, initial-scale=1">',
-    );
   });
 });
