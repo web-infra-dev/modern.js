668f0c01d5caecefed01e58b9a695a95f533740d
zhouxiao.shaw <zhouxiao.shaw@bytedance.com>
2022-03-07 17:17:49 +0800

    chore(workflow): add unit test

--- packages/cli/plugin-garfish/tests/cli.test.tsx
+++ packages/cli/plugin-garfish/tests/cli.test.tsx
@@ -1,30 +1,38 @@
 import '@testing-library/jest-dom';
 import GarfishPlugin from '../src/cli';
 
-// jest.mock('@modern-js/core', () => {
-//   const originalModule = jest.requireActual('@modern-js/core');
-//   return {
-//     __esModule: true,
-//     ...originalModule,
-//     default: jest.fn(() => 'mocked baz'),
-//     useResolvedConfigContext: () => ({
-//       runtime: {
-//         masterApp: {
-//           manifest: {
-//             componentKey: 'test-dynamic-key',
-//           },
-//         },
-//       },
-//       deploy: {
-//         microFrontend: 'dynamicComponentKey',
-//       },
-//     }),
-//   };
-// });
+jest.mock('@modern-js/core', () => {
+  const originalModule = jest.requireActual('@modern-js/core');
+  return {
+    __esModule: true,
+    ...originalModule,
+    default: jest.fn(() => 'mocked baz'),
+    useResolvedConfigContext: () => ({
+      runtime: {
+        masterApp: {},
+      },
+      deploy: {
+        microFrontend: true,
+      },
+    }),
+  };
+});
 
 describe('plugin-garfish cli', () => {
-  test('cli garfish plugin', () => {
+  test('cli garfish basename', () => {
     expect(GarfishPlugin.name).toBe('@modern-js/plugin-garfish');
-    expect(GarfishPlugin.name).toBe('@modern-js/plugin-garfish');
+    const pluginLifeCycle = GarfishPlugin.initializer();
+    const basename = '/test';
+    const resolveConfig = {
+      resolved: {
+        runtime: {
+          router: {
+            historyOptions: { basename }
+          },
+          masterApp: {}
+        }
+      }
+    };
+    expect((pluginLifeCycle as any).resolvedConfig(resolveConfig).resolved.runtime.masterApp.basename).toBe(basename);
   });
 });
