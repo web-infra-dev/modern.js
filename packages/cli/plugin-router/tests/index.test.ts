9f57bc0599945e05859ecce5e6e459bd3e42f7c6
kky <68810266+zllkjc@users.noreply.github.com>
2022-03-16 14:57:38 +0800

    fix(router): Not concat server.baseUrl and router.basename (#738)
    * fix: use server.baseUrl first, not concat basename
    
    * docs: add changeset

--- packages/cli/plugin-router/tests/index.test.ts
+++ packages/cli/plugin-router/tests/index.test.ts
@@ -1,9 +1,17 @@
 import plugin, { useHistory, useParams } from '../src';
+import cliPlugin from '../src/cli';
 
 describe('plugin-router', () => {
   it('default', () => {
     expect(plugin).toBeDefined();
     expect(useHistory).toBeDefined();
     expect(useParams).toBeDefined();
+  });
+});
+
+describe('cli-router', () => {
+  test('should hooks work correctly', async () => {
+    const hooks: any = cliPlugin.initializer();
+    expect(hooks.config).toBeDefined();
   });
 });
