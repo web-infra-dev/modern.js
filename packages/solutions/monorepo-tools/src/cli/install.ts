849e85d4ed32e7403d5bd4623fda01a661630e3e
Belinda Cao <caohuilin@bytedance.com>
2022-03-26 10:13:58 +0800

    Feat publish runtime core (#805)
    * fix: build error
    
    * feat: publish runtime core

--- packages/solutions/monorepo-tools/src/cli/install.ts
+++ packages/solutions/monorepo-tools/src/cli/install.ts
@@ -1,14 +1,15 @@
+import type { PluginAPI } from '@modern-js/core';
 import { Command } from 'commander';
 import { install, IInstallCommandOption } from '../commands';
 
-export const installCli = (program: Command) => {
+export const installCli = (program: Command, api: PluginAPI) => {
   program
     .command('install [project...]')
     .usage('[options]')
     .description('install deps for some projects')
     .action(
       async (installProjectNames: string[], option: IInstallCommandOption) => {
-        await install(installProjectNames, option);
+        await install(installProjectNames, option, api);
       },
     );
 };
