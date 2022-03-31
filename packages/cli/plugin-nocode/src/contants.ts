2d745dbcd7316d7ea77199b2b56247bff0159a49
Zfkkk <kgaikj2cu@icloud.com>
2022-03-31 16:19:52 +0800

    feat: nocode plugin support style-lodaer & butter registry (#839)
    Co-authored-by: targeral <targeral@outlook.com>

--- packages/cli/plugin-nocode/src/contants.ts
+++ packages/cli/plugin-nocode/src/contants.ts
@@ -5,3 +5,6 @@ export const MODE = {
   MODEL: 'MODEL',
   BLOCK: 'BLOCK',
 };
+
+export const STARRY_BLOCK_RUNTIME = '@modern-js-block/runtime';
+export const STARRY_MODEL_RUNTIME = '@modern-js-model/runtime';
