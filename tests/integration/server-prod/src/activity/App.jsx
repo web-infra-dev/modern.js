e3c9cc5da0055a5327f407fa7b965f4511755021
neverland <chenjiahan.jait@bytedance.com>
2022-04-02 15:13:10 +0800

    chore: remove never reported eslint-disable (#855)

--- tests/integration/server-prod/src/activity/App.jsx
+++ tests/integration/server-prod/src/activity/App.jsx
@@ -1,7 +1,5 @@
-/* eslint-disable node/no-unsupported-features/es-syntax */
 import React from 'react';
 
 const App = () => <div>Activity page</div>;
 
 export default App;
-/* eslint-enable node/no-unsupported-features/es-syntax */
--- tests/integration/server-prod/src/server-prod/App.jsx
+++ tests/integration/server-prod/src/server-prod/App.jsx
@@ -1,7 +1,5 @@
-/* eslint-disable node/no-unsupported-features/es-syntax */
 import React from 'react';
 
 const App = () => <div className="text-center">Hello, Modern.js !</div>;
 
 export default App;
-/* eslint-enable node/no-unsupported-features/es-syntax */
