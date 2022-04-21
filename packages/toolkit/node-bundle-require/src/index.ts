fbcf14b5a7725ea2e591ef7f003073595f03676b
chenjiahan <chenjiahan.jait@bytedance.com>
2022-04-21 14:02:27 +0800

    fix: should remove module.children

--- packages/toolkit/node-bundle-require/src/index.ts
+++ packages/toolkit/node-bundle-require/src/index.ts
@@ -10,6 +10,18 @@ export async function bundleRequire(filepath: string, options?: Options) {
   const req = options?.require || require;
   try {
     mod = await req(configFile);
+
+    // Webpack will check require history for persistent cache.
+    // If webpack can not resolve the file, the previous cache pack will become invalid.
+    // The bundled file is temporary, so we should clear the require history to avoid breaking the webpack cache.
+    if (require.cache[configFile]) {
+      delete require.cache[configFile];
+    }
+    if (module.children) {
+      module.children = module.children.filter(
+        item => item.filename !== configFile,
+      );
+    }
   } finally {
     onExitProcess(() => {
       // Remove the configFile before exit process
