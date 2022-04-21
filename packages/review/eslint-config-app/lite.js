868b9cdbfd4bcf0c18a01db62628754565b9c1f7
xuchaobei <jason.xu@bytedance.com>
2022-04-21 17:11:44 +0800

    chore: eslint rules (#924)
    * refactor: react eslint rules
    
    * chore: add .eslintrc for each package
    
    * chore: changeset

--- packages/review/eslint-config-app/lite.js
+++ packages/review/eslint-config-app/lite.js
@@ -1,4 +1,4 @@
 module.exports = {
   // https://eslint.org/docs/user-guide/configuring#extending-configuration-files
-  extends: ['./base.js', './ts.js', './prettier'],
+  extends: ['./base.js', './react', './ts.js', './prettier'],
 };
