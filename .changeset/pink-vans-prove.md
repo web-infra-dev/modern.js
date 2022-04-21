0e0bdd1a22bd6b0e64c3d98688f7deb5aa5528fd
Belinda Cao <caohuilin@bytedance.com>
2022-04-21 14:45:49 +0800

    fix: generator template in js project (#921)
    * fix: js generator
    
    * docs: changeset
    
    * fix: sub project eslint file
    
    * fix: test

---
+++ .changeset/pink-vans-prove.md
@@ -0,0 +1,8 @@
+---
+'@modern-js/bff-generator': minor
+'@modern-js/dependence-generator': minor
+'@modern-js/module-generator': minor
+'@modern-js/server-generator': minor
+---
+
+fix: js generator problem
--- packages/generator/generators/server-generator/src/index.ts
+++ packages/generator/generators/server-generator/src/index.ts
@@ -150,7 +150,7 @@ const handleTemplateFile = async (
 
   const appendTypeContent = FrameworkAppendTypeContent[framework as Framework];
 
-  if (appendTypeContent) {
+  if (appendTypeContent && language === Language.TS) {
     const typePath = path.join(appDir, 'src', 'modern-app-env.d.ts');
     if (fs.existsSync(typePath)) {
       const npmrc = fs.readFileSync(typePath, 'utf-8');
