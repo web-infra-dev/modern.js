8e5e9308ee8006f8c5e49b879edcfc39c1105f7d
neverland <chenjiahan.jait@bytedance.com>
2022-04-21 15:08:43 +0800

    fix: failed to generate webpack cache (#923)
    * fix: failed to generate webpack cache
    
    * fix: should remove module.children
    
    * chore: update
    
    * chore: delete some code

--- packages/toolkit/node-bundle-require/src/index.ts
+++ packages/toolkit/node-bundle-require/src/index.ts
@@ -3,6 +3,15 @@ import { bundle, Options } from './bundle';
 
 export { bundle };
 
+function deleteRequireCache(path: string) {
+  if (require.cache[path]) {
+    delete require.cache[path];
+  }
+  if (module.children) {
+    module.children = module.children.filter(item => item.filename !== path);
+  }
+}
+
 export async function bundleRequire(filepath: string, options?: Options) {
   const configFile = await bundle(filepath, options);
 
@@ -10,8 +19,12 @@ export async function bundleRequire(filepath: string, options?: Options) {
   const req = options?.require || require;
   try {
     mod = await req(configFile);
+
+    // Webpack will check require history for persistent cache.
+    // If webpack can not resolve the file, the previous cache pack will become invalid.
+    // The bundled file is temporary, so we should clear the require history to avoid breaking the webpack cache.
+    deleteRequireCache(configFile);
   } finally {
-    // Remove the configFile after executed
     fs.unlinkSync(configFile);
   }
 
