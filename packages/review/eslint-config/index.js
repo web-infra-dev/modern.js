283cbcc5389924b74b3fee0948ebf6573e9f5fc8
jason.xu <jason.xu@bytedance.com>
2022-04-21 17:04:52 +0800

    fix: remove react eslint rules for eslint-config

--- packages/review/eslint-config/index.js
+++ packages/review/eslint-config/index.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: [
     '@modern-js-app/eslint-config/base',
-    '@modern-js-app/eslint-config/react',
     '@modern-js-app/eslint-config/ts',
     '@modern-js-app/eslint-config/ts.withType',
     '@modern-js-app/eslint-config/prettier',
