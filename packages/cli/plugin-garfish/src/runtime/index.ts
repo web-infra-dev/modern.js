0e10ae246ebd7a7a5a831667e367d19a44b7250b
zhouxiao.shaw <zhouxiao.shaw@bytedance.com>
2022-04-26 14:55:23 +0800

    fix(garfish-plugin): 兼容旧版本导出 garfish 实例

--- packages/cli/plugin-garfish/src/runtime/index.ts
+++ packages/cli/plugin-garfish/src/runtime/index.ts
@@ -1,4 +1,4 @@
 export { default } from './plugin';
 export { useModuleApps, useModuleApp } from './useModuleApps';
 export type { Manifest, ModuleInfo } from './useModuleApps';
-export { default as Garfish } from 'garfish';
+export { default as Garfish, default as garfish } from 'garfish';
