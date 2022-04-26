46c928e27d2ee09eccadf33a5e49cf29a0b9688e
Ming <wangyiming.fe@bytedance.com>
2022-04-26 15:32:08 +0800

    chore: add node.test.ts to test files (#945)

--- packages/server/create-request/jest.config.js
+++ packages/server/create-request/jest.config.js
@@ -4,8 +4,4 @@ const sharedConfig = require('@scripts/jest-config');
 module.exports = {
   ...sharedConfig,
   rootDir: __dirname,
-  modulePathIgnorePatterns: [
-    // TODO: 暂时无法解决（Property exprName of TSTypeQuery expected node to be of a type ["TSEntityName","TSImportType"] but instead got "MemberExpression"）问题，先绕过
-    'tests/node.test.ts',
-  ],
 };
