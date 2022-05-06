b7cb10ca7f8e38fbd489e8a85d5940700a1e7e14
kky <68810266+zllkjc@users.noreply.github.com>
2022-05-06 13:34:33 +0800

    fix: router build (#987)
    * fix: support input count params
    
    * chore: fix test case
    
    * docs: add changeset

--- packages/cli/plugin-ssr/src/prefetch.tsx
+++ packages/cli/plugin-ssr/src/prefetch.tsx
@@ -23,7 +23,7 @@ const prefetch = async (
       const data = loadersData[id];
       if (data._error) {
         ssrContext.logger.error('App Prefetch Loader', data._error);
-        ssrContext.metrics.emitCounter('app.prefetch.loader.error');
+        ssrContext.metrics.emitCounter('app.prefetch.loader.error', 1);
         delete data._error;
       }
     });
