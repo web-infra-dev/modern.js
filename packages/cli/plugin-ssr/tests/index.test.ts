f6834f242a7e16e61463c8eeedf9acc331030143
kky <68810266+zllkjc@users.noreply.github.com>
2022-05-06 11:30:42 +0800

    feat: add metrics and logger (#981)
    * feat: add metrics and logger
    
    * chore: add test
    
    * chore: modify logger output
    
    * chore: fix test
    
    * chore: add _error field in loader rtn

--- packages/cli/plugin-ssr/tests/index.test.ts
+++ packages/cli/plugin-ssr/tests/index.test.ts
@@ -1,9 +1,21 @@
 import plugin from '../src';
 import cliPlugin from '../src/cli';
+import { time } from '../src/serverRender/measure';
 
 describe('plugin-ssr', () => {
   it('default', () => {
     expect(plugin).toBeDefined();
     expect(cliPlugin).toBeDefined();
+  });
+
+  it('should time work correctly', async () => {
+    const end = time();
+    await new Promise((resolve, _reject) => {
+      setTimeout(() => {
+        resolve(null);
+      }, 1000);
+    });
+    const cost = end();
+    expect(typeof cost === 'number').toBeTruthy();
   });
 });
