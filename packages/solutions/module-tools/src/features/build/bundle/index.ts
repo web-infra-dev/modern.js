0638a9ad6341014bc5dcf6298668f1b871d1a4d1
fupeng.antik <fupeng.antik@bytedance.com>
2022-05-23 23:33:35 +0800

    feat: add buildPreset config, support bundle

---
+++ packages/solutions/module-tools/src/features/build/bundle/index.ts
@@ -0,0 +1,10 @@
+import { runSpeedy } from './runSpeedy';
+import { startRollup } from './runRollup';
+import type { TaskBuildConfig } from '../../../types';
+
+
+export const buildInBundleMode = async (config: TaskBuildConfig) => {
+  Promise.all([startRollup(config), runSpeedy(config)]);
+};
+
+
