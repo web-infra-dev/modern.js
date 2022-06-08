8e5480d484c2fa5b7cac534a13216d96539fff07
zhouxiao.shaw@bytedance.net <zhouxiao.shaw@bytedance.com>
2022-06-08 14:03:32 +0800

    fix(garfish-plugin): lose App static attribute

--- packages/cli/plugin-garfish/src/runtime/index.ts
+++ packages/cli/plugin-garfish/src/runtime/index.ts
@@ -2,3 +2,4 @@ export { default } from './plugin';
 export { useModuleApps, useModuleApp } from './useModuleApps';
 export type { Manifest, ModuleInfo } from './useModuleApps';
 export { default as Garfish, default as garfish } from 'garfish';
+export { default as hoistNonReactStatics } from 'hoist-non-react-statics';
