68a9652f7256c01a7c75fdf9609a45c8889f66d0
10Derozan <fupeng.antik@gmail.com>
2022-06-08 15:11:56 +0800

    fix: build and test error (#1171)
    * fix: require path error in tests
    
    * fix: type
    
    * chore: solve todo and useless console

--- packages/solutions/module-tools/tests/generate-dts.test.ts
+++ packages/solutions/module-tools/tests/generate-dts.test.ts
@@ -1,4 +1,4 @@
-import { getTscBinPath } from '../src/tasks/generator-dts/utils';
+import { getTscBinPath } from '../src/features/build/bundless/generator-dts/utils';
 
 describe('generate dts', () => {
   test('should throw error when tsc bin is not exist', () => {
