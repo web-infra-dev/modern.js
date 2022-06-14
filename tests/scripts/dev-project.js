0bfd5b77ea6ef02c3a7bed926e16a45a2b5378df
zhouxiao.shaw@bytedance.net <zhouxiao.shaw@bytedance.com>
2022-06-14 22:56:28 +0800

    chore: fix workflow error

--- tests/scripts/dev-project.js
+++ tests/scripts/dev-project.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env zx
 const { $ } = require('zx');
+const killPort = require('kill-port');
+const { exampleInfo } = require('../utils/testCase');
+
+const ports = Object.keys(exampleInfo).map(key => exampleInfo[key].port);
 
 async function runAllExample() {
+  await Promise.all(ports.map(port => killPort(port)));
   await $`pnpm --parallel --filter "@cypress-test/*" run dev`;
 }
 
