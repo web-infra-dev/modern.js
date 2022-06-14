3e99f8c465254f747a6a6be4a6c57fafdac7c700
zhouxiao.shaw@bytedance.net <zhouxiao.shaw@bytedance.com>
2022-06-14 21:54:32 +0800

    chore: fix workflow error

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -2,8 +2,6 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(_on, _config) {
-      // implement node event listeners here
-    },
+    supportFile: false,
   },
 });
