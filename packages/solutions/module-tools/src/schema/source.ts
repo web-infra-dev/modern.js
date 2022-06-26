0e119572df6e07807ba3a2324bbddc9186ad0164
targeral <targeral@outlook.com>
2022-06-26 15:01:22 +0800

    test: add schema unit tests

--- packages/solutions/module-tools/src/schema/source.ts
+++ packages/solutions/module-tools/src/schema/source.ts
@@ -1,16 +1,9 @@
 export const sourceSchema = [
-  // 启动区块运行时
-  {
-    target: 'source.enableBlockRuntime',
-    schema: { type: 'boolean' },
-  },
   {
     target: 'source.jsxTransformRuntime',
     schema: {
       // https://babeljs.io/docs/en/babel-preset-react#runtime
       enum: ['classic', 'automatic'],
-      // TODO: 目前default是无效的
-      default: 'automatic',
     },
   },
 ];
