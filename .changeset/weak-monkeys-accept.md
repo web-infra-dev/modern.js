8effb95b0ac209c1ca38f622232f2b23c62f66ec
neverland <chenjiahan.jait@bytedance.com>
2022-06-27 16:33:30 +0800

    perf(babel): skip babel-plugin-import if package not installed (#1275)
    * perf(babel): skip babel-plugin-import if package not installed
    
    * chore: update snapshot
    
    * chore: allow resolve paths to be array

---
+++ .changeset/weak-monkeys-accept.md
@@ -0,0 +1,8 @@
+---
+'@modern-js/babel-preset-base': patch
+'@modern-js/core': patch
+'@modern-js/server-core': patch
+'@modern-js/utils': patch
+---
+
+perf(babel): skip babel-plugin-import if package not installed
--- packages/server/core/src/loadPlugins.ts
+++ packages/server/core/src/loadPlugins.ts
@@ -1,19 +1,5 @@
-import { compatRequire } from '@modern-js/utils';
+import { compatRequire, tryResolve } from '@modern-js/utils';
 import { createPlugin, ServerPlugin } from './plugin';
-
-const tryResolve = (name: string, appDirectory: string) => {
-  let filePath = '';
-  try {
-    filePath = require.resolve(name, { paths: [appDirectory] });
-    delete require.cache[filePath];
-  } catch (err) {
-    if ((err as any).code === 'MODULE_NOT_FOUND') {
-      throw new Error(`Can not find plugin ${name}.`);
-    }
-    throw err;
-  }
-  return filePath;
-};
 
 type Plugin = string | [string, any] | ServerPlugin;
 export const loadPlugins = (plugins: Plugin[], appDirectory: string) => {
