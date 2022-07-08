8e9e5ee8f47a308ad151eb4af9077546888938b3
chenjiahan <chenjiahan.jait@bytedance.com>
2022-07-08 17:31:43 +0800

    chore: fix regexp

--- packages/cli/webpack/src/utils/constants.ts
+++ packages/cli/webpack/src/utils/constants.ts
@@ -6,7 +6,7 @@ export const GLOBAL_CSS_REGEX = /\.global\.css$/;
 
 export const JS_REGEX = /\.(js|mjs|cjs|jsx)$/;
 
-export const TS_REGEX = /\.(ts|mts|cts|tsx)?$/;
+export const TS_REGEX = /\.(ts|mts|cts|tsx)$/;
 
 export const ASSETS_REGEX =
   /\.(woff|woff2|eot|ttf|otf|ttc|gif|png|jpe?g|webp|bmp|ico|svg)$/i;
