14ff2630ca022ff1b4fc252a6a873093ee2bc1f1
targeral <targeral@outlook.com>
2022-07-14 11:54:53 +0800

    chore: remove nocode package (#1364)

--- packages/cli/plugin-nocode/src/dev/constants.ts
+++
@@ -1,6 +0,0 @@
-const DEFAULT_PORT = 8889;
-const DEFAULT_HOST = '0.0.0.0';
-const DEFAULT_TYPE = 'block';
-const indexHtml = 'html/index/index.html';
-
-export { DEFAULT_HOST, DEFAULT_PORT, DEFAULT_TYPE, indexHtml };
