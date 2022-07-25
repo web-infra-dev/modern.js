0efefb1978d7a853fe43f286f00bcf44fb6db99b
neverland <chenjiahan.jait@bytedance.com>
2022-07-25 10:27:30 +0800

    chore: remove source code of unbundle plugin (#1398)
    * chore: remove source code of unbundle plugin
    
    * chore: update credits

--- packages/toolkit/esmpack/src/constants.ts
+++
@@ -1,3 +0,0 @@
-// [@\w] - Match a word-character or @ (valid package name)
-// (?!.*(:\/\/)) - Ignore if previous match was a protocol (ex: http://)
-export const BARE_SPECIFIER_REGEX = /^[@\w](?!.*(:\/\/))/;
