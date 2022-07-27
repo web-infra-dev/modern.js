dcce116bb49aaa3d0b644291d1a59a2390cce508
chenjiahan <chenjiahan.jait@bytedance.com>
2022-07-27 15:11:39 +0800

    feat(web-builder): add mode plugin

--- packages/builder/web-builder/src/shared/index.ts
+++ packages/builder/web-builder/src/shared/index.ts
@@ -1,3 +1,9 @@
+export const isDev = (): boolean => process.env.NODE_ENV === 'development';
+
+export const isProd = (): boolean => process.env.NODE_ENV === 'production';
+
+export const isTest = () => process.env.NODE_ENV === 'test';
+
 export function pick<T, U extends keyof T>(obj: T, keys: ReadonlyArray<U>) {
   return keys.reduce((ret, key) => {
     if (obj[key] !== undefined) {
