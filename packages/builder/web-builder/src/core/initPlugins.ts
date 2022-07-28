3d8013db08a6ef168e4fa13004d72ac54715cf58
chenjiahan <chenjiahan.jait@bytedance.com>
2022-07-28 13:15:00 +0800

    feat(web-builder): add more hooks

--- packages/builder/web-builder/src/core/initPlugins.ts
+++ packages/builder/web-builder/src/core/initPlugins.ts
@@ -1,10 +1,15 @@
 import type {
   Context,
+  OnExitFn,
   PluginStore,
+  OnAfterBuildFn,
+  OnBeforeBuildFn,
   WebBuilderPluginAPI,
   ModifyWebpackChainFn,
   ModifyWebpackConfigFn,
   ModifyBuilderConfigFn,
+  OnAfterCreateCompilerFn,
+  OnBeforeCreateCompilerFn,
 } from '../types';
 import { createAsyncHook } from './createHook';
 import { createPublicContext } from './createContext';
@@ -18,9 +23,15 @@ export async function initPlugins({
 }) {
   const publicContext = createPublicContext(context);
 
+  const onExitHook = createAsyncHook<OnExitFn>();
+  const onAfterBuildHook = createAsyncHook<OnAfterBuildFn>();
+  const onBeforeBuildHook = createAsyncHook<OnBeforeBuildFn>();
   const modifyWebpackChainHook = createAsyncHook<ModifyWebpackChainFn>();
   const modifyWebpackConfigHook = createAsyncHook<ModifyWebpackConfigFn>();
   const modifyBuilderConfigHook = createAsyncHook<ModifyBuilderConfigFn>();
+  const onAfterCreateCompilerHooks = createAsyncHook<OnAfterCreateCompilerFn>();
+  const onBeforeCreateCompilerHooks =
+    createAsyncHook<OnBeforeCreateCompilerFn>();
 
   const getBuilderConfig = () => context.config;
 
@@ -28,6 +39,15 @@ export async function initPlugins({
     context: publicContext,
     getBuilderConfig,
     isPluginExists: pluginStore.isPluginExists,
+
+    // Hooks
+    onExit: onExitHook.tap,
+    onAfterBuild: onAfterBuildHook.tap,
+    onBeforeBuild: onBeforeBuildHook.tap,
+    onAfterCreateCompiler: onAfterCreateCompilerHooks.tap,
+    onBeforeCreateCompiler: onBeforeCreateCompilerHooks.tap,
+
+    // Modifiers
     modifyWebpackChain: modifyWebpackChainHook.tap,
     modifyWebpackConfig: modifyWebpackConfigHook.tap,
     modifyBuilderConfig: modifyBuilderConfigHook.tap,
