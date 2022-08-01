a4725b9d37fb2405f43647f8f47c0c5498cdfde8
chenjiahan <chenjiahan.jait@bytedance.com>
2022-08-01 20:29:22 +0800

    feat(web-builder): add cache plugin

--- packages/builder/web-builder/src/shared/utils.ts
+++ packages/builder/web-builder/src/shared/utils.ts
@@ -6,3 +6,11 @@ export function pick<T, U extends keyof T>(obj: T, keys: ReadonlyArray<U>) {
     return ret;
   }, {} as Pick<T, U>);
 }
+
+export async function isFileExists(file: string) {
+  const { promises, constants } = await import('fs');
+  return promises
+    .access(file, constants.F_OK)
+    .then(() => true)
+    .catch(() => false);
+}
