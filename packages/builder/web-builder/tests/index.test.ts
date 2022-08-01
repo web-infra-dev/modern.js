86c63b80620ce6d52389fc399bc7cb810d55e038
chenjiahan <chenjiahan.jait@bytedance.com>
2022-08-01 17:50:02 +0800

    feat(web-builder): support builder.build

--- packages/builder/web-builder/tests/index.test.ts
+++ packages/builder/web-builder/tests/index.test.ts
@@ -1,9 +1,1 @@
-import { createBuilder } from '../src';
-
-test('local test', async () => {
-  const builder = await createBuilder();
-  const compiler = await builder.createCompiler();
-
-  // eslint-disable-next-line no-console
-  console.log(compiler);
-});
+// empty
