a458f9ad0636331197fd4c3a6fb22b8430c69130
neverland <chenjiahan.jait@bytedance.com>
2022-08-04 15:24:30 +0800

    feat(web-builder): add moment plugin (#1451)

--- packages/builder/web-builder/src/types/config/performance.ts
+++ packages/builder/web-builder/src/types/config/performance.ts
@@ -2,4 +2,5 @@ export type ConsoleType = 'log' | 'info' | 'warn' | 'error' | 'table' | 'group';
 
 export interface WebBuilderPerformanceConfig {
   removeConsole?: boolean | ConsoleType[];
+  removeMomentLocale?: boolean;
 }
