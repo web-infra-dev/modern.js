9f379789ea8a564fb4a15b4bee3c3c0de62c743e
Asuka109 <chenwenjie.asuka@bytedance.com>
2022-08-08 11:48:28 +0800

    feat(web-builder): remove dist before pre-compile config validator

--- packages/builder/web-builder/scripts/compile-ajv.js
+++ packages/builder/web-builder/scripts/compile-ajv.js
@@ -1,12 +1,14 @@
 const { ConfigValidator } = require('../dist/config/validate');
 const path = require('path');
+const fs = require('fs');
 
 async function compile() {
-  const validator = await ConfigValidator.create();
   const output = path.resolve(
     __dirname,
     '../compiled/config-validator/index.js',
   );
+  fs.rmSync(output);
+  const validator = await ConfigValidator.create();
   await ConfigValidator.serialize(validator, output);
 }
 
