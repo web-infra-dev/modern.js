27df7dc175a04f7c31ae9d099ec2e1f2eb9032ce
chenjiahan <chenjiahan.jait@bytedance.com>
2022-08-09 10:42:16 +0800

    chore: add some constants

--- packages/builder/web-builder/src/shared/constants.ts
+++ packages/builder/web-builder/src/shared/constants.ts
@@ -16,6 +16,8 @@ export const DEFAULT_DATA_URL_SIZE = 10000;
 export const ROOT_DIST_DIR = 'dist';
 export const FONT_DIST_DIR = 'font';
 export const IMAGE_DIST_DIR = 'image';
+export const JS_DIST_DIR = 'js';
+export const CSS_DIST_DIR = 'css';
 
 export const FONT_EXTENSIONS = ['woff', 'woff2', 'eot', 'ttf', 'otf', 'ttc'];
 export const IMAGE_EXTENSIONS = [
