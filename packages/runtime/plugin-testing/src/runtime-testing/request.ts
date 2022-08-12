b1f7000614f80faaa121029f51db5723530d43ea
Ming <wangyiming.fe@bytedance.com>
2022-08-12 14:19:42 +0800

    fix: adjust createServer logic in bff test plugin (#1493)

--- packages/runtime/plugin-testing/src/runtime-testing/request.ts
+++ packages/runtime/plugin-testing/src/runtime-testing/request.ts
@@ -1,6 +1,7 @@
 /* eslint-disable eslint-comments/disable-enable-pair */
 import type { SuperTest, Test } from 'supertest';
 import supertest from 'supertest';
+import { getApp } from '../cli/bff/app';
 
 function request(): SuperTest<Test>;
 
@@ -11,15 +12,13 @@ function request<T extends (...argss: any[]) => any>(
 
 function request(...args: any): SuperTest<Test> | Test {
   const [fn, ...extraArgs] = args;
-
+  const app = getApp();
   if (!fn) {
-    return supertest((global as any).app);
+    return supertest(app);
   }
-
   fn.returnHttp = true;
   const res = fn(...extraArgs);
   fn.returnHttp = false;
-
   return res;
 }
 
--- tests/integration/api-service-koa/api/context/index.ts
+++ tests/integration/api-service-koa/api/context/index.ts
@@ -1,10 +1,19 @@
-import { useContext } from '@modern-js/runtime/server';
-
-export default async () => {
+export const get = async () => {
+  const { useContext } = await import('@modern-js/runtime/server');
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const ctx = useContext();
   const { res } = ctx;
   res.setHeader('x-id', '1');
   return {
     message: 'Hello Modern.js',
+  };
+};
+
+export const post = async () => {
+  const { useContext } = await import('@modern-js/runtime/server');
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const ctx = useContext();
+  return {
+    message: ctx.message,
   };
 };
