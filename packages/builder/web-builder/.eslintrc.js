e9103c92c8ad213b8a72f7e1a4c0e55a46fb2a5c
Asuka109 <chenwenjie.asuka@bytedance.com>
2022-08-12 17:41:58 +0800

    feat(web-builder): restrict imports

--- packages/builder/web-builder/.eslintrc.js
+++ packages/builder/web-builder/.eslintrc.js
@@ -1,3 +1,33 @@
+const devDependencyPaths = Object.keys(
+  require('./package.json').devDependencies,
+);
+
+const withAllowTypeImports = key => pkg => ({
+  [key]: pkg,
+  allowTypeImports: true,
+});
+
+const restrictedImportInTypes = {
+  patterns: [{ group: '**', allowTypeImports: true }],
+};
+
+const restrictedImportInSource = {
+  paths: [
+    'webpack',
+    'lodash',
+    'ts-loader',
+    'typescript',
+    ...devDependencyPaths,
+  ].map(withAllowTypeImports('name')),
+  patterns: [
+    '**/plugins/*',
+    '**/webpackPlugins/*',
+    '*-webpack-plugin',
+    ['@modern-js/utils/*', '!@modern-js/utils/chalk'],
+  ].map(withAllowTypeImports('group')),
+};
+
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ['@modern-js'],
   ignorePatterns: ['compiled/', 'vitest.config.ts'],
@@ -7,4 +37,29 @@ module.exports = {
   rules: {
     'import/order': 0,
   },
+  overrides: [
+    {
+      files: ['./src/types/**/*.ts'],
+      rules: {
+        '@typescript-eslint/no-restricted-imports': [
+          'error',
+          restrictedImportInTypes,
+        ],
+      },
+    },
+    {
+      files: ['./src/**/*.{ts,js}'],
+      excludedFiles: [
+        './src/webpackPlugins/**/*',
+        '**/*.test.*',
+        './src/types/**/*',
+      ],
+      rules: {
+        '@typescript-eslint/no-restricted-imports': [
+          'error',
+          restrictedImportInSource,
+        ],
+      },
+    },
+  ],
 };
