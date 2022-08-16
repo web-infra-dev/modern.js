a8d2779f8770d25ad7b0a7464b8315a0abf7c29f
Ming <wangyiming.fe@bytedance.com>
2022-08-16 12:10:35 +0800

    fix: add server hook and types

--- packages/server/plugin-egg/src/runtime/index.ts
+++ packages/server/plugin-egg/src/runtime/index.ts
@@ -1,5 +1,7 @@
+import type { Middleware } from 'koa';
 import { useContext, Context } from '../context';
 
+export const hook = (attacher: Middleware) => attacher;
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 declare module '@modern-js/runtime/server' {
