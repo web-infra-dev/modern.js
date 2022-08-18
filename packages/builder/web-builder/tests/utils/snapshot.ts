ee64914bf5f5074ee0f956b6525bb727311185e0
chenjiahan <chenjiahan.jait@bytedance.com>
2022-08-18 15:39:08 +0800

    chore: format all windows path

--- packages/builder/web-builder/tests/utils/snapshot.ts
+++ packages/builder/web-builder/tests/utils/snapshot.ts
@@ -4,6 +4,11 @@ export const setPathSerializer = () => {
   const formatWindowsPath = (val: string) => val.replace(/\\/g, '/');
 
   expect.addSnapshotSerializer({
+    test: val => typeof val === 'string' && val.includes('\\\\'),
+    print: val => formatWindowsPath(val as string),
+  });
+
+  expect.addSnapshotSerializer({
     test: val => typeof val === 'string' && val.includes('node_modules'),
     print: val =>
       formatWindowsPath((val as string).split('node_modules').pop()!),
