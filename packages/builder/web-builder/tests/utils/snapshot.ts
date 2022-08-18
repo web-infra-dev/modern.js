8d095686fec602bee548b63dd678205aa3ec2a17
chenjiahan <chenjiahan.jait@bytedance.com>
2022-08-18 16:06:13 +0800

    chore: snapshot quote

--- packages/builder/web-builder/tests/utils/snapshot.ts
+++ packages/builder/web-builder/tests/utils/snapshot.ts
@@ -4,17 +4,18 @@ import { expect } from 'vitest';
 export const setPathSerializer = () => {
   expect.addSnapshotSerializer({
     test: val => typeof val === 'string' && /\\/g.test(val),
-    print: val => normalizeToPosixPath(val as string),
+    print: val => `"${normalizeToPosixPath(val as string)}"`,
   });
 
   expect.addSnapshotSerializer({
     test: val => typeof val === 'string' && val.includes('node_modules'),
     print: val =>
-      normalizeToPosixPath((val as string).split('node_modules').pop()),
+      `"${normalizeToPosixPath((val as string).split('node_modules').pop())}"`,
   });
 
   expect.addSnapshotSerializer({
     test: val => typeof val === 'string' && val.includes('packages'),
-    print: val => normalizeToPosixPath((val as string).split('packages').pop()),
+    print: val =>
+      `"${normalizeToPosixPath((val as string).split('packages').pop())}"`,
   });
 };
