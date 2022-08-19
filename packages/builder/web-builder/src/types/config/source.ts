57af1f1bd03e688fcb5b76e3a5fdad139fc6001b
chenjiahan <chenjiahan.jait@bytedance.com>
2022-08-19 10:09:31 +0800

    fix: allow void

--- packages/builder/web-builder/src/types/config/source.ts
+++ packages/builder/web-builder/src/types/config/source.ts
@@ -2,7 +2,7 @@ import type { JSONValue } from '../utils';
 import type { WebpackAlias } from '../thirdParty';
 
 export interface SourceConfig {
-  alias?: WebpackAlias | ((alias: WebpackAlias) => WebpackAlias);
+  alias?: WebpackAlias | ((alias: WebpackAlias) => WebpackAlias | void);
   preEntry?: string | string[];
   globalVars?: Record<string, JSONValue>;
   resolveExtensionPrefix?: string;
