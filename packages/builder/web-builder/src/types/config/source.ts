06b2cd2b23374a3f88fc4a11216daed3f1cb757c
chenjiahan <chenjiahan.jait@bytedance.com>
2022-08-22 17:19:36 +0800

    chore(web-builder): add type utils to improve tools typing

--- packages/builder/web-builder/src/types/config/source.ts
+++ packages/builder/web-builder/src/types/config/source.ts
@@ -1,8 +1,8 @@
-import type { JSONValue } from '../utils';
+import type { ChainedConfig, JSONValue } from '../utils';
 import type { WebpackAlias } from '../thirdParty';
 
 export interface SourceConfig {
-  alias?: WebpackAlias | ((alias: WebpackAlias) => WebpackAlias | void);
+  alias?: ChainedConfig<WebpackAlias>;
   preEntry?: string | string[];
   globalVars?: Record<string, JSONValue>;
   resolveExtensionPrefix?: string;
