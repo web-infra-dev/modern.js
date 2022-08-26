1c5fc57564dddf55725af3d6e4a23e454b2d8dff
Asuka109 <chenwenjie.asuka@bytedance.com>
2022-08-26 11:52:28 +0800

    test(PluginImageCompress): add snapshot test

--- packages/builder/plugin-image-compress/tests/index.test.ts
+++ packages/builder/plugin-image-compress/tests/index.test.ts
@@ -1,34 +1,13 @@
 import { it, expect, describe } from 'vitest';
-import { normalizeFinalOptions } from '../src';
+import { createStubBuilder } from '@modern-js/webpack-builder/stub';
+import { PluginImage } from '@modern-js/webpack-builder/plugins/image';
+import { PluginImageCompress } from '../src';
 
-describe('normalizeFinalOptions', () => {
-  it('should handle on empty array', () => {
-    expect(normalizeFinalOptions([])).toMatchInlineSnapshot('[]');
-  });
-
-  it('should handle on array of codec', () => {
-    expect(normalizeFinalOptions(['avif', 'mozjpeg'])).toMatchSnapshot();
-  });
-
-  it('should handle on composed options', () => {
-    expect(
-      normalizeFinalOptions({ use: ['avif', 'mozjpeg'] }),
-    ).toMatchSnapshot();
-  });
-
-  it('should handle on empty codec options', () => {
-    expect(normalizeFinalOptions({ use: 'mozjpeg' })).toMatchSnapshot();
-  });
-
-  it('should handle on partial codec options', () => {
-    expect(
-      normalizeFinalOptions({ use: 'mozjpeg', color_space: 1 }),
-    ).toMatchSnapshot();
-  });
-
-  it('should handle on codec options with ext tester', () => {
-    expect(
-      normalizeFinalOptions({ use: 'mozjpeg', test: /\.jpeg$/ }),
-    ).toMatchSnapshot();
+describe('plugin/image-compress', () => {
+  it('should generate correct options', async () => {
+    const builder = createStubBuilder({
+      plugins: [PluginImage(), PluginImageCompress()],
+    });
+    expect(await builder.unwrapWebpackConfig()).toMatchInlineSnapshot();
   });
 });
