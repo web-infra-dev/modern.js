4fca538c6ee8a291d87746776728fa08da068bd0
caohuilin <caohuilin@bytedance.com>
2022-08-30 20:30:15 +0800

    fix: more_solution text

--- packages/generator/generators/upgrade-generator/src/locale/en.ts
+++ packages/generator/generators/upgrade-generator/src/locale/en.ts
@@ -3,7 +3,8 @@ export const EN_LOCALE = {
   modernVersion: 'Modern.js Latest Version',
   tooltip: {
     no_solution: 'You should install Modern.js solution tools first',
-    more_solution: 'The project not allow containers more solution tools',
+    more_solution:
+      'The project is not allowed to contain more than one solution tool',
   },
   success: 'Upgrade Modern.js package version success!',
 };
