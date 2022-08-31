6b6a534830f2e84139edda9564ab8503281c0e39
neverland <chenjiahan.jait@bytedance.com>
2022-08-31 23:50:43 +0800

    feat(builder): support builder.startDevServer API (#1628)
    * feat(builder): support builder.startDevServer API
    
    * chore: export getAddressUrls method
    
    * chore: fix app listen
    
    * chore: set node env
    
    * chore: remove skip

--- tests/integration/webpack-builder/scripts/dev.ts
+++ tests/integration/webpack-builder/scripts/dev.ts
@@ -1,31 +1,6 @@
-import { Server } from '@modern-js/server';
 import { createBuilder } from './shared';
 
 (async function main() {
-  process.env.NODE_ENV = 'development';
-
   const builder = await createBuilder();
-  const compiler = await builder.createCompiler();
-
-  const server = new Server({
-    pwd: process.cwd(),
-    dev: {
-      hot: true,
-      liveReload: true,
-    },
-    compiler,
-    config: {
-      source: {},
-      tools: {},
-      server: {},
-    } as any,
-  });
-
-  const app = await server.init();
-
-  app.listen(8080, async (err: Error) => {
-    if (err) {
-      throw err;
-    }
-  });
+  await builder.startDevServer();
 })();
