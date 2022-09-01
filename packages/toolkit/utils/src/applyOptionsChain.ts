cce0729542346e0fd36fbde1202f796bd8754166
Asuka109 <chenwenjie.asuka@bytedance.com>
2022-09-01 14:17:43 +0800

    style(utils): improve implementation of `applyOptionsChain`

--- packages/toolkit/utils/src/applyOptionsChain.ts
+++ packages/toolkit/utils/src/applyOptionsChain.ts
@@ -1,30 +1,17 @@
 // eslint-disable-next-line import/no-useless-path-segments
 import { isFunction, logger, isPlainObject } from './index';
 
+export type OptionsChainFunction<T, U> = (config: T, utils?: U) => T | void;
+
 export function applyOptionsChain<T, U>(
   defaults: T,
   options?:
     | T
-    | ((config: T, utils?: U) => T | void)
-    | Array<T | ((config: T, utils?: U) => T | void)>,
+    | OptionsChainFunction<T, U>
+    | Array<T | OptionsChainFunction<T, U>>,
   utils?: U,
-  mergeFn?: typeof Object.assign,
-): T;
-export function applyOptionsChain<T, U>(
-  defaults: T,
-  options:
-    | T
-    | ((config: T, utils: U) => T | void)
-    | Array<T | ((config: T, utils: U) => T | void)>,
-  utils: U,
-  mergeFn?: typeof Object.assign,
-): T;
-export function applyOptionsChain<T>(
-  defaults: T,
-  options?: unknown,
-  utils?: unknown,
   mergeFn = Object.assign,
-) {
+): T {
   if (!options) {
     return defaults;
   }
@@ -43,7 +30,7 @@ export function applyOptionsChain<T>(
     }
   } else if (Array.isArray(options)) {
     return options.reduce(
-      (memo, cur) => applyOptionsChain(memo, cur, utils, mergeFn),
+      (memo: T, cur) => applyOptionsChain<T, U>(memo, cur, utils, mergeFn),
       defaults,
     );
   } else {
