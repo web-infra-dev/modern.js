d4560678654c426672acfe1f5232b3150d71c9e7
chenjiahan <chenjiahan.jait@bytedance.com>
2022-09-02 11:35:30 +0800

    fix: type issue

--- packages/server/server/src/dev-tools/https/index.ts
+++ packages/server/server/src/dev-tools/https/index.ts
@@ -1,7 +1,13 @@
+import type Buffer from 'buffer';
 import * as devcert from 'devcert';
 import type { DevServerHttpsOptions } from '../../types';
 
-export const genHttpsOptions = async (userOptions: DevServerHttpsOptions) => {
+export const genHttpsOptions = async (
+  userOptions: DevServerHttpsOptions,
+): Promise<{
+  key?: Buffer | string;
+  cert?: Buffer | string;
+}> => {
   const httpsOptions: { key?: string; cert?: string } =
     typeof userOptions === 'boolean' ? {} : userOptions;
 
