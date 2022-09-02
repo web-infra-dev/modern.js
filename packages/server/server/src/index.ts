d28f746f617577552300dd78a7f10932187775a6
neverland <chenjiahan.jait@bytedance.com>
2022-09-02 12:17:24 +0800

    feat(server): export DevServerHttpsOptions type, improve doc (#1643)
    * feat(server): export DevServerHttpsOptions type, improve doc
    
    * fix: type issue

--- packages/server/server/src/index.ts
+++ packages/server/server/src/index.ts
@@ -1,8 +1,12 @@
 import { DevServer as Server } from './server';
-import type { DevServerOptions, ModernDevServerOptions } from './types';
+import type {
+  DevServerOptions,
+  DevServerHttpsOptions,
+  ModernDevServerOptions,
+} from './types';
 
 export { Server };
-export type { DevServerOptions, ModernDevServerOptions };
+export type { DevServerOptions, DevServerHttpsOptions, ModernDevServerOptions };
 
 export default (options: ModernDevServerOptions): Promise<Server> => {
   if (options == null) {
