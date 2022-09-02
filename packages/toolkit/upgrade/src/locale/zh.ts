9f13d8c7376653ab2358fc44736ecf7deb5cdf5b
Belinda Cao <caohuilin@bytedance.com>
2022-09-02 11:34:47 +0800

    feat: upgrade doc (#1642)

--- packages/toolkit/upgrade/src/locale/zh.ts
+++ packages/toolkit/upgrade/src/locale/zh.ts
@@ -1,8 +1,8 @@
 export const ZH_LOCALE = {
   command: {
-    describe: '升级 Modern.js 相关依赖至最新版本',
-    distTag: '生成项目时生成器使用特殊的 npm Tag',
-    registry: '在创建过程中定制 npm registry',
+    describe: '升级 Modern.js 到最新版本',
+    distTag: '升级时底层生成器使用特殊的 npm Tag',
+    registry: '定制 npm registry',
     debug: '开启 Debug 模式，打印调试日志信息',
     cwd: '项目路径',
   },
