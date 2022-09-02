1828637775a637e1de7d8d01bb6fc7e6546fea7c
neverland <chenjiahan.jait@bytedance.com>
2022-09-02 10:52:27 +0800

    feat: add source.enableAsyncEntry config (#1639)
    * feat: add source.enableAsyncEntry config
    
    * chore: add integration test case for async entry

---
+++ tests/integration/async-entry/modern.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from '@modern-js/app-tools';
+
+export default defineConfig({
+  runtime: {
+    router: true,
+    state: true,
+  },
+  source: {
+    enableAsyncEntry: true,
+  },
+});
