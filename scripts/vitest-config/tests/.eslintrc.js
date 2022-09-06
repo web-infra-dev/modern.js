7e239a126d7f2fa34f7e504a5aa58cd852673064
Asuka109 <chenwenjie.asuka@bytedance.com>
2022-09-06 11:03:11 +0800

    test(vitest-config): add path replacer (#1625)
    * test(vitest-config): add path replacer
    
    * feat(webpack-builder): escape quotation marks in snapshots
    
    * test(vitest-config): add tests for `createSnapshotSerializer`
    
    * revert(webpack-builder): remove tsconfig and eslint config
    
    * feat(vitest-config): normalize path to posix format
    
    * feat(vitest-config): replace windows root path with unix root path
    
    * feat(vitest-config): use `normalizeToPosixPath` to format path instead of regexp
    
    * Revert "feat(vitest-config): use `normalizeToPosixPath` to format path instead of regexp"
    
    This reverts commit be3321464fd539371144a24e9a81cd6b3a6cfe8c.
    
    * fix(vitest-config): failed to print path of snapshot in win32
    
    * test(webpack-builder): update snapshots
    
    * feat(vitest-config): `PathMatcher::match` can receive string or RegExp
    
    * test(webpack-builder): update snapshots
    
    Co-authored-by: neverland <chenjiahan.jait@bytedance.com>

--- scripts/vitest-config/tests/.eslintrc.js
+++ scripts/vitest-config/tests/.eslintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   extends: ['@modern-js'],
-  ignorePatterns: ['compiled/', 'vitest.config.ts'],
   parserOptions: {
     project: require.resolve('./tsconfig.json'),
   },
---
+++ scripts/vitest-config/tests/utils.test.ts
@@ -0,0 +1,88 @@
+import { describe, expect, it } from 'vitest';
+import {
+  compilePathMatcherRegExp,
+  createSnapshotSerializer,
+  joinPathParts,
+  matchUpwardPathsAsUnknown,
+  upwardPaths,
+} from '../src/utils';
+
+describe('joinPathParts', () => {
+  it('should join path parts', () => {
+    expect(joinPathParts('whatever', 0, ['a', 'b', 'c'])).toBe('/a');
+    expect(joinPathParts('whatever', 1, ['a', 'b', 'c'])).toBe('/a/b');
+    expect(joinPathParts('whatever', 2, ['a', 'b', 'c'])).toBe('/a/b/c');
+  });
+});
+
+describe('upwardPaths', () => {
+  it('should get upward paths', () => {
+    expect(upwardPaths('/a/b/c')).toEqual(['/a/b/c', '/a/b', '/a', '/']);
+  });
+});
+
+describe('compilePathMatcherSource', () => {
+  it('should compile string path matcher', () => {
+    const regExp = compilePathMatcherRegExp('/a/b/c');
+    expect(regExp.source).toBe('^\\/a\\/b\\/c(?=\\/)|^\\/a\\/b\\/c$');
+    expect(regExp.test('/a/b/c')).toBe(true);
+    expect(regExp.test('/a/b/c/')).toBe(true);
+    expect(regExp.test('/a/b/c/d')).toBe(true);
+    expect(regExp.test('/a/b/cd')).toBe(false);
+    expect(regExp.test('/a/c/c/')).toBe(false);
+  });
+});
+
+describe('matchUpwardPathsAsUnknown', () => {
+  it('should match upward paths', () => {
+    expect(matchUpwardPathsAsUnknown('/a/b/c')).toEqual([
+      { mark: 'unknown', match: '/a/b' },
+      { mark: 'unknown', match: '/a' },
+    ]);
+  });
+  it('should match upward paths with win32', () => {
+    expect(matchUpwardPathsAsUnknown('C:\\Windows\\User\\workspace')).toEqual([
+      { mark: 'unknown', match: '/c/Windows/User' },
+      { mark: 'unknown', match: '/c/Windows' },
+      { mark: 'unknown', match: '/c' },
+    ]);
+  });
+});
+
+describe('createSnapshotSerializer', () => {
+  it.each([
+    ['/a/b/c/d/e/f/g', '"<ROOT>/d/e/f/g"'],
+    ['/a/b/c/foo', '"<ROOT>/foo"'],
+    ['/a/b/foo/bar', '"<UNKNOWN>/foo/bar"'],
+    [
+      '/a/b/c/node_modules/.pnpm/babel-loader@8.2.5_2cad51bbe9c2876396f118aa6395be78/node_modules/babel-loader/lib/index.js',
+      '"<ROOT>/node_modules/<PNPM_INNER>/babel-loader/lib/index.js"',
+    ],
+  ])('should handle with posix path %s', (value, expected) => {
+    const serializer = createSnapshotSerializer({
+      replace: [
+        { match: '/a/b/c', mark: 'root' },
+        { match: '/a/b/c/d', mark: 'workspace' },
+        { match: '/a/b/c/d/e', mark: 'workspace2' },
+      ],
+    });
+    expect(serializer.test(value)).toBe(true);
+    expect(serializer.print(value)).toBe(expected);
+  });
+  it.each([
+    ['A:\\b\\c\\d\\e\\f\\g', '"<ROOT>/d/e/f/g"'],
+    ['A:\\b\\c\\foo', '"<ROOT>/foo"'],
+    ['A:\\b\\foo\\bar', '"<UNKNOWN>/foo/bar"'],
+    ['Z:\\b\\c\\foo', '"/z/b/c/foo"'],
+  ])('should handle with windows path %s', (value, expected) => {
+    const serializer = createSnapshotSerializer({
+      replace: [
+        { match: 'A:\\b\\c', mark: 'root' },
+        { match: 'A:\\b\\c\\d', mark: 'workspace' },
+        { match: 'A:\\b\\c\\d\\e', mark: 'workspace2' },
+      ],
+    });
+    expect(serializer.test(value)).toBe(true);
+    expect(serializer.print(value)).toBe(expected);
+  });
+});
