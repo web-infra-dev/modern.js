7f837f313da6ac8b2170e0fe01d3aab28bedb559
Asuka109 <chenwenjie.asuka@bytedance.com>
2022-09-08 00:20:05 +0800

    test(webpack-builder): add basic e2e case

---
+++ tests/e2e/webpack-builder/basic/build.test.ts
@@ -0,0 +1,10 @@
+import { createStubBuilder } from '@modern-js/webpack-builder/stub';
+import { expect, test } from '@playwright/test';
+
+test('basic', async ({ page }) => {
+  const builder = createStubBuilder({ webpack: true });
+  await builder.build();
+  await page.goto('http://localhost:3000');
+
+  expect(await page.evaluate('window.answer')).toBe(42);
+});
