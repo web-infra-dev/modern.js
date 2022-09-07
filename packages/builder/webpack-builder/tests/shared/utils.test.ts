e14cc820add925d0279f99d02e3217815f5153d8
chenjiahan <chenjiahan.jait@bytedance.com>
2022-09-07 13:57:55 +0800

    feat(builder): support inspectBuilderConfig method

--- packages/builder/webpack-builder/tests/shared/utils.test.ts
+++ packages/builder/webpack-builder/tests/shared/utils.test.ts
@@ -1,8 +1,11 @@
+import webpack from 'webpack';
 import { expect, describe, it } from 'vitest';
 import {
-  getPackageNameFromModulePath,
+  stringifyConfig,
   MODULE_PATH_REGEX,
+  getPackageNameFromModulePath,
 } from '../../src/shared';
+import type { BuilderConfig, WebpackConfig } from '../../src/types';
 
 describe('getPackageNameFromModulePath', () => {
   it('should parse correct path fragment in npm/yarn', async () => {
@@ -47,5 +50,29 @@ describe('getPackageNameFromModulePath', () => {
 
     modulePath = '/path/to/node_modules/.pnpm/package-name/index.js';
     expect(getPackageNameFromModulePath(modulePath)).toBe('npm.package-name');
+  });
+});
+
+describe('stringifyConfig', () => {
+  it('should stringify webpack config correctly', async () => {
+    const { DefinePlugin } = webpack;
+    const webpackConfig: WebpackConfig = {
+      mode: 'development',
+      plugins: [new DefinePlugin({ foo: 'bar' })],
+    };
+
+    expect(await stringifyConfig(webpackConfig)).toMatchSnapshot();
+  });
+
+  it('should stringify builder config correctly', async () => {
+    const builderConfig: BuilderConfig = {
+      tools: {
+        webpackChain(chain) {
+          chain.devtool('eval');
+        },
+      },
+    };
+
+    expect(await stringifyConfig(builderConfig)).toMatchSnapshot();
   });
 });
