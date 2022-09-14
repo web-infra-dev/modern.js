d5ec5cce03093794e789b376e35201dec6384bae
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2022-09-14 18:30:16 +0800

    feat: add webpack-builder-plugin-node-polyfill

---
+++ packages/builder/plugin-node-polyfill/src/index.ts
@@ -0,0 +1,42 @@
+import type { BuilderPlugin } from '@modern-js/webpack-builder';
+
+/**
+ * Usage:
+ *
+ *   const PluginNodePolyfill = await import('@modern-js/webpack-builder-plugin-node-polyfill');
+ *
+ *   pluginStore.addPlugins([ PluginNodePolyfill() ]);
+ */
+export default (): BuilderPlugin => ({
+  name: 'webpack-builder-plugin-node-polyfill',
+
+  async setup(api) {
+    const { default: webpack } = await import('webpack');
+
+    const nodeLibsBrowser = require('node-libs-browser');
+
+    api.modifyWebpackChain(async (chain, { CHAIN_ID }) => {
+      chain
+        .plugin(CHAIN_ID.PLUGIN.NODE_POLYFILL_PROVIDE)
+        .use(webpack.ProvidePlugin, [
+          {
+            Buffer: [nodeLibsBrowser.buffer, 'Buffer'],
+            process: [nodeLibsBrowser.process],
+          },
+        ]);
+
+      const fallback = Object.keys(nodeLibsBrowser).reduce<
+        Record<string, string | false>
+      >((previous, name) => {
+        if (nodeLibsBrowser[name]) {
+          previous[name] = nodeLibsBrowser[name];
+        } else {
+          previous[name] = false;
+        }
+        return previous;
+      }, {});
+
+      chain.resolve.fallback.merge(fallback);
+    });
+  },
+});
