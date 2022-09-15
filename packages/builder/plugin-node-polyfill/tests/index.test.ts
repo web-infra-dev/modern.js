f3183b2c4f4b978473d9954385de6e4028184fb5
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2022-09-15 15:29:30 +0800

    chore: named export

--- packages/builder/plugin-node-polyfill/tests/index.test.ts
+++ packages/builder/plugin-node-polyfill/tests/index.test.ts
@@ -1,6 +1,6 @@
 import { expect, describe, it } from 'vitest';
 import { createStubBuilder } from '@modern-js/webpack-builder/stub';
-import PluginNodePolyfill from '../src';
+import { PluginNodePolyfill } from '../src';
 
 describe('plugins/node-polyfill', () => {
   it('should add node-polyfill config', async () => {
