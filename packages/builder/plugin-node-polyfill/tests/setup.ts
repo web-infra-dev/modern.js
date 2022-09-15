e74648c57bd26bf0bbf0bc1329af8a1867927a25
gaoyuan <gaoyuan.1226@bytedance.com>
2022-09-15 15:53:20 +0800

    feat: add webpack-builder-plugin-node-polyfill (#1701)
    * feat: add webpack-builder-plugin-node-polyfill
    
    * fix: lock
    
    * chore: pkg standard
    
    * chore: named export

--- packages/builder/plugin-node-polyfill/tests/setup.ts
+++ packages/builder/plugin-node-polyfill/tests/setup.ts
@@ -4,9 +4,6 @@ import { createSnapshotSerializer } from '@scripts/vitest-config';
 
 expect.addSnapshotSerializer(
   createSnapshotSerializer({
-    replace: [
-      { mark: 'root', match: path.resolve(__dirname, '../../../..') },
-      { mark: 'workspace', match: path.resolve(__dirname, '..') },
-    ],
+    replace: [{ mark: 'root', match: path.resolve(__dirname, '../../../..') }],
   }),
 );
