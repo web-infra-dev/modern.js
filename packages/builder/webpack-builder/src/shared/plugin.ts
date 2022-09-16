7f2656e18f3e5c698df0a5336a53212c02fb7797
Asuka109 <chenwenjie.asuka@bytedance.com>
2022-09-16 16:02:10 +0800

    feat(webpack-builder): `loadBuiltinPlugin` can infer concreted type of return value

--- packages/builder/webpack-builder/src/shared/plugin.ts
+++ packages/builder/webpack-builder/src/shared/plugin.ts
@@ -1,5 +1,3 @@
-import { BuilderPlugin } from '../types';
-
 export const builtinPluginExports = {
   hmr: 'PluginHMR',
   svg: 'PluginSvg',
@@ -43,13 +41,57 @@ export const builtinPluginExports = {
   assetsRetry: 'PluginAssetsRetry',
 } as const;
 
-export type BuiltinPluginExport = typeof builtinPluginExports;
-export type BuiltinPluginName = keyof BuiltinPluginExport;
+export type BuiltinPluginTypes = {
+  hmr: typeof import('../plugins/hmr').PluginHMR;
+  svg: typeof import('../plugins/svg').PluginSvg;
+  pug: typeof import('../plugins/pug').PluginPug;
+  copy: typeof import('../plugins/copy').PluginCopy;
+  font: typeof import('../plugins/font').PluginFont;
+  html: typeof import('../plugins/html').PluginHtml;
+  basic: typeof import('../plugins/basic').PluginBasic;
+  cache: typeof import('../plugins/cache').PluginCache;
+  entry: typeof import('../plugins/entry').PluginEntry;
+  image: typeof import('../plugins/image').PluginImage;
+  media: typeof import('../plugins/media').PluginMedia;
+  target: typeof import('../plugins/target').PluginTarget;
+  output: typeof import('../plugins/output').PluginOutput;
+  moment: typeof import('../plugins/moment').PluginMoment;
+  define: typeof import('../plugins/define').PluginDefine;
+  devtool: typeof import('../plugins/devtool').PluginDevtool;
+  resolve: typeof import('../plugins/resolve').PluginResolve;
+  fallback: typeof import('../plugins/fallback').PluginFallback;
+  progress: typeof import('../plugins/progress').PluginProgress;
+  minimize: typeof import('../plugins/minimize').PluginMinimize;
+  manifest: typeof import('../plugins/manifest').PluginManifest;
+  fileSize: typeof import('../plugins/fileSize').PluginFileSize;
+  cleanOutput: typeof import('../plugins/cleanOutput').PluginCleanOutput;
+  moduleScopes: typeof import('../plugins/moduleScopes').PluginModuleScopes;
+  babel: typeof import('../plugins/babel').PluginBabel;
+  tsLoader: typeof import('../plugins/tsLoader').PluginTsLoader;
+  tsChecker: typeof import('../plugins/tsChecker').PluginTsChecker;
+  css: typeof import('../plugins/css').PluginCss;
+  sass: typeof import('../plugins/sass').PluginSass;
+  less: typeof import('../plugins/less').PluginLess;
+  react: typeof import('../plugins/react').PluginReact;
+  bundleAnalyzer: typeof import('../plugins/bundleAnalyzer').PluginBundleAnalyzer;
+  toml: typeof import('../plugins/toml').PluginToml;
+  yaml: typeof import('../plugins/yaml').PluginYaml;
+  splitChunks: typeof import('../plugins/splitChunks').PluginSplitChunks;
+  inspector: typeof import('../plugins/inspector').PluginInspector;
+  sri: typeof import('../plugins/sri').PluginSRI;
+  startUrl: typeof import('../plugins/startUrl').PluginStartUrl;
+  inlineChunk: typeof import('../plugins/inlineChunk').PluginInlineChunk;
+  assetsRetry: typeof import('../plugins/assetsRetry').PluginAssetsRetry;
+};
 
-export const loadBuiltinPlugin = async (plugin: BuiltinPluginName) => {
+export type BuiltinPluginName = keyof BuiltinPluginTypes;
+
+export const loadBuiltinPlugin = async <T extends BuiltinPluginName>(
+  plugin: T,
+) => {
   const exportName = builtinPluginExports[plugin];
   const module = await import(`../plugins/${plugin}`);
-  return module[exportName] as () => BuilderPlugin;
+  return module[exportName] as BuiltinPluginTypes[T];
 };
 
 export const loadBuiltinPlugins = (plugins: readonly BuiltinPluginName[]) =>
