547ad6c29d36adf907e150d13ea125e12db71d2b
Asuka109 <chenwenjie.asuka@bytedance.com>
2022-09-16 17:28:38 +0800

    refactor(webpack-builder): use `awaitablePlugins` to refactor `applyBasicPlugins`...

--- packages/builder/webpack-builder/src/shared/plugin.ts
+++ packages/builder/webpack-builder/src/shared/plugin.ts
@@ -1,179 +1,84 @@
-export const builtinPluginExports = {
-  hmr: 'PluginHMR',
-  svg: 'PluginSvg',
-  pug: 'PluginPug',
-  copy: 'PluginCopy',
-  font: 'PluginFont',
-  html: 'PluginHtml',
-  basic: 'PluginBasic',
-  cache: 'PluginCache',
-  entry: 'PluginEntry',
-  image: 'PluginImage',
-  media: 'PluginMedia',
-  target: 'PluginTarget',
-  output: 'PluginOutput',
-  moment: 'PluginMoment',
-  define: 'PluginDefine',
-  devtool: 'PluginDevtool',
-  resolve: 'PluginResolve',
-  fallback: 'PluginFallback',
-  progress: 'PluginProgress',
-  minimize: 'PluginMinimize',
-  manifest: 'PluginManifest',
-  fileSize: 'PluginFileSize',
-  cleanOutput: 'PluginCleanOutput',
-  moduleScopes: 'PluginModuleScopes',
-  babel: 'PluginBabel',
-  tsLoader: 'PluginTsLoader',
-  tsChecker: 'PluginTsChecker',
-  css: 'PluginCss',
-  sass: 'PluginSass',
-  less: 'PluginLess',
-  react: 'PluginReact',
-  bundleAnalyzer: 'PluginBundleAnalyzer',
-  toml: 'PluginToml',
-  yaml: 'PluginYaml',
-  splitChunks: 'PluginSplitChunks',
-  inspector: 'PluginInspector',
-  sri: 'PluginSRI',
-  startUrl: 'PluginStartUrl',
-  inlineChunk: 'PluginInlineChunk',
-  assetsRetry: 'PluginAssetsRetry',
-} as const;
+import _ from '@modern-js/utils/lodash';
+import { BuilderPlugin } from '../types';
 
-export type BuiltinPluginTypes = {
-  hmr: typeof import('../plugins/hmr').PluginHMR;
-  svg: typeof import('../plugins/svg').PluginSvg;
-  pug: typeof import('../plugins/pug').PluginPug;
-  copy: typeof import('../plugins/copy').PluginCopy;
-  font: typeof import('../plugins/font').PluginFont;
-  html: typeof import('../plugins/html').PluginHtml;
-  basic: typeof import('../plugins/basic').PluginBasic;
-  cache: typeof import('../plugins/cache').PluginCache;
-  entry: typeof import('../plugins/entry').PluginEntry;
-  image: typeof import('../plugins/image').PluginImage;
-  media: typeof import('../plugins/media').PluginMedia;
-  target: typeof import('../plugins/target').PluginTarget;
-  output: typeof import('../plugins/output').PluginOutput;
-  moment: typeof import('../plugins/moment').PluginMoment;
-  define: typeof import('../plugins/define').PluginDefine;
-  devtool: typeof import('../plugins/devtool').PluginDevtool;
-  resolve: typeof import('../plugins/resolve').PluginResolve;
-  fallback: typeof import('../plugins/fallback').PluginFallback;
-  progress: typeof import('../plugins/progress').PluginProgress;
-  minimize: typeof import('../plugins/minimize').PluginMinimize;
-  manifest: typeof import('../plugins/manifest').PluginManifest;
-  fileSize: typeof import('../plugins/fileSize').PluginFileSize;
-  cleanOutput: typeof import('../plugins/cleanOutput').PluginCleanOutput;
-  moduleScopes: typeof import('../plugins/moduleScopes').PluginModuleScopes;
-  babel: typeof import('../plugins/babel').PluginBabel;
-  tsLoader: typeof import('../plugins/tsLoader').PluginTsLoader;
-  tsChecker: typeof import('../plugins/tsChecker').PluginTsChecker;
-  css: typeof import('../plugins/css').PluginCss;
-  sass: typeof import('../plugins/sass').PluginSass;
-  less: typeof import('../plugins/less').PluginLess;
-  react: typeof import('../plugins/react').PluginReact;
-  bundleAnalyzer: typeof import('../plugins/bundleAnalyzer').PluginBundleAnalyzer;
-  toml: typeof import('../plugins/toml').PluginToml;
-  yaml: typeof import('../plugins/yaml').PluginYaml;
-  splitChunks: typeof import('../plugins/splitChunks').PluginSplitChunks;
-  inspector: typeof import('../plugins/inspector').PluginInspector;
-  sri: typeof import('../plugins/sri').PluginSRI;
-  startUrl: typeof import('../plugins/startUrl').PluginStartUrl;
-  inlineChunk: typeof import('../plugins/inlineChunk').PluginInlineChunk;
-  assetsRetry: typeof import('../plugins/assetsRetry').PluginAssetsRetry;
+/**
+ * Wrap plugin initializers as Awaitable.
+ * @example
+ * const loader = () => awaitablePlugins([
+ *   import('../plugins/basic').then(m => m.PluginBasic())
+ * ] as const);
+ * 
+ * const [pluginBasicInstance] = await loader();
+ * const [PluginBasic] = [...loader()];
+ */
+const awaitablePlugins = <T extends readonly PromiseLike<BuilderPlugin>[]>(
+  loaders: T,
+): T & PromiseLike<{ -readonly [P in keyof T]: Awaited<T[P]> }> => {
+  const then: PromiseLike<any>['then'] = (...args) =>
+    Promise.all(loaders).then(...args);
+  return Object.assign(_.cloneDeep(loaders), { then });
 };
 
-export type BuiltinPluginName = keyof BuiltinPluginTypes;
+export const applyMinimalPlugins = () =>
+  awaitablePlugins([
+    import('../plugins/basic').then(m => m.PluginBasic()),
+    import('../plugins/entry').then(m => m.PluginEntry()),
+    import('../plugins/cache').then(m => m.PluginCache()),
+    import('../plugins/target').then(m => m.PluginTarget()),
+    import('../plugins/output').then(m => m.PluginOutput()),
+    import('../plugins/devtool').then(m => m.PluginDevtool()),
+    import('../plugins/resolve').then(m => m.PluginResolve()),
+  ] as const);
 
-export const loadBuiltinPlugin = async <T extends BuiltinPluginName>(
-  plugin: T,
-) => {
-  const exportName = builtinPluginExports[plugin];
-  const module = await import(`../plugins/${plugin}`);
-  return module[exportName] as BuiltinPluginTypes[T];
-};
+export const applyBasicPlugins = () =>
+  awaitablePlugins([
+    ...applyMinimalPlugins(),
+    import('../plugins/copy').then(m => m.PluginCopy()),
+    import('../plugins/html').then(m => m.PluginHtml()),
+    import('../plugins/image').then(m => m.PluginImage()),
+    import('../plugins/define').then(m => m.PluginDefine()),
+    import('../plugins/tsLoader').then(m => m.PluginTsLoader()),
+    import('../plugins/babel').then(m => m.PluginBabel()),
+    import('../plugins/css').then(m => m.PluginCss()),
+    import('../plugins/sass').then(m => m.PluginSass()),
+    import('../plugins/less').then(m => m.PluginLess()),
+    import('../plugins/react').then(m => m.PluginReact()),
+  ] as const);
 
-export const loadBuiltinPlugins = (plugins: readonly BuiltinPluginName[]) =>
-  plugins.map(loadBuiltinPlugin);
-
-export const applyBuiltinPlugins = async (
-  plugins: readonly BuiltinPluginName[],
-) => {
-  const pluginInitializers = await Promise.all(loadBuiltinPlugins(plugins));
-  return pluginInitializers.map(fn => fn());
-};
-
-export const presetMinimal = [
-  'basic',
-  'entry',
-  'cache',
-  'target',
-  'output',
-  'devtool',
-  'resolve',
-] as const;
-
-export const applyMinimalPlugins = () => applyBuiltinPlugins(presetMinimal);
-
-export const presetBasic = [
-  ...presetMinimal,
-  'copy',
-  'html',
-  'image',
-  'define',
-  'tsLoader',
-  'babel',
-  'css',
-  'sass',
-  'less',
-  'react',
-] as const;
-
-export const applyBasicPlugins = () => applyBuiltinPlugins(presetBasic);
-
-export const presetDefault = [
-  ...presetMinimal,
-  // fileSize plugin will read the previous dist files.
-  // So we should register fileSize plugin before cleanOutput plugin.
-  // And cleanOutput plugin should be registered before other plugins.
-  'fileSize',
-  'cleanOutput',
-
-  // Plugins that provide basic features
-  'hmr',
-  'svg',
-  'pug',
-  'copy',
-  'font',
-  'html',
-  'image',
-  'media',
-  'moment',
-  'define',
-  'progress',
-  'minimize',
-  'manifest',
-  'moduleScopes',
-  'tsLoader',
-  'babel',
-  'tsChecker',
-  'css',
-  'sass',
-  'less',
-  'react',
-  'bundleAnalyzer',
-  'toml',
-  'yaml',
-  'splitChunks',
-  'inspector',
-  'sri',
-  'startUrl',
-  'inlineChunk',
-  'assetsRetry',
-
-  // fallback should be the last plugin
-  'fallback',
-] as const;
-
-export const applyDefaultPlugins = () => applyBuiltinPlugins(presetDefault);
+export const applyDefaultPlugins = () =>
+  awaitablePlugins([
+    ...applyMinimalPlugins(),
+    import('../plugins/fileSize').then(m => m.PluginFileSize()),
+    import('../plugins/cleanOutput').then(m => m.PluginCleanOutput()),
+    import('../plugins/hmr').then(m => m.PluginHMR()),
+    import('../plugins/svg').then(m => m.PluginSvg()),
+    import('../plugins/pug').then(m => m.PluginPug()),
+    import('../plugins/copy').then(m => m.PluginCopy()),
+    import('../plugins/font').then(m => m.PluginFont()),
+    import('../plugins/html').then(m => m.PluginHtml()),
+    import('../plugins/image').then(m => m.PluginImage()),
+    import('../plugins/media').then(m => m.PluginMedia()),
+    import('../plugins/moment').then(m => m.PluginMoment()),
+    import('../plugins/define').then(m => m.PluginDefine()),
+    import('../plugins/progress').then(m => m.PluginProgress()),
+    import('../plugins/minimize').then(m => m.PluginMinimize()),
+    import('../plugins/manifest').then(m => m.PluginManifest()),
+    import('../plugins/moduleScopes').then(m => m.PluginModuleScopes()),
+    import('../plugins/tsLoader').then(m => m.PluginTsLoader()),
+    import('../plugins/babel').then(m => m.PluginBabel()),
+    import('../plugins/tsChecker').then(m => m.PluginTsChecker()),
+    import('../plugins/css').then(m => m.PluginCss()),
+    import('../plugins/sass').then(m => m.PluginSass()),
+    import('../plugins/less').then(m => m.PluginLess()),
+    import('../plugins/react').then(m => m.PluginReact()),
+    import('../plugins/bundleAnalyzer').then(m => m.PluginBundleAnalyzer()),
+    import('../plugins/toml').then(m => m.PluginToml()),
+    import('../plugins/yaml').then(m => m.PluginYaml()),
+    import('../plugins/splitChunks').then(m => m.PluginSplitChunks()),
+    import('../plugins/inspector').then(m => m.PluginInspector()),
+    import('../plugins/sri').then(m => m.PluginSRI()),
+    import('../plugins/startUrl').then(m => m.PluginStartUrl()),
+    import('../plugins/inlineChunk').then(m => m.PluginInlineChunk()),
+    import('../plugins/assetsRetry').then(m => m.PluginAssetsRetry()),
+    import('../plugins/fallback').then(m => m.PluginFallback()),
+  ] as const);
