cbd1b0bfcde04f8bb31e2c5d4694099a1af9ba9a
Asuka109 <chenwenjie.asuka@bytedance.com>
2022-09-16 14:34:54 +0800

    feat(webpack-builder): remove `memfs` and auto set `distPath` by `webpack` option (#1705)
    * feat(webpack-builder): remove `memfs`
    
    * feat(utils): add new util `getTemplatePath`
    
    * feat(webpack-builder): no longer restrict imports in `src/stub`
    
    * feat(webpack-builder): remove the selection `in-memory` of `webpack`
    
    Set a string value to enable using webpack and specify the `output.distPath`.

--- packages/toolkit/e2e/src/server/static.ts
+++ packages/toolkit/e2e/src/server/static.ts
@@ -1,42 +1,16 @@
-import os from 'os';
 import http from 'http';
-import fs from 'fs';
 import serveStaticImpl from 'serve-static';
-import { Volume } from 'memfs/lib/volume';
-import { nanoid } from 'nanoid';
-import path from 'path';
-import { createFsFromVolume } from 'memfs';
 
 export interface ServeStaticOptions<
   R extends http.ServerResponse = http.ServerResponse,
 > extends serveStaticImpl.ServeStaticOptions<R> {
-  volume?: Volume;
 }
 
 function serveStaticMiddle<R extends http.ServerResponse>(
   root: string,
   options?: ServeStaticOptions<R>,
 ): serveStaticImpl.RequestHandler<R> {
-  const ofs = options?.volume ? createFsFromVolume(options.volume) : fs;
-  let _root = root;
-  if (ofs !== fs) {
-    const tempDir = fs.realpathSync(os.tmpdir());
-    const outputDir = path.resolve(tempDir, 'modern-js-e2e', nanoid());
-    _root = outputDir;
-    if (options?.volume) {
-      const files = options.volume.toJSON(root, undefined, true);
-      for (const [filename, data] of Object.entries(files)) {
-        if (data) {
-          const filepath = path.resolve(outputDir, filename);
-          fs.mkdirSync(path.dirname(filepath), { recursive: true });
-          fs.writeFileSync(filepath, data);
-        }
-      }
-    } else {
-      throw new Error('Unsupported filesystem.');
-    }
-  }
-  return serveStaticImpl(_root, options);
+  return serveStaticImpl(root, options);
 }
 
 export default serveStaticMiddle;
