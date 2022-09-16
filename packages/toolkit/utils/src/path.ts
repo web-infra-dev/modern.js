acc6e998426f5845dbe7d69cff73ae6f01eaf62f
Asuka109 <chenwenjie.asuka@bytedance.com>
2022-09-16 14:34:54 +0800

    feat(webpack-builder): remove `memfs` and auto set `distPath` by `webpack` option (#1705)
    * feat(webpack-builder): remove `memfs`
    
    * feat(utils): add new util `getTemplatePath`
    
    * feat(webpack-builder): no longer restrict imports in `src/stub`
    
    * feat(webpack-builder): remove the selection `in-memory` of `webpack`
    
    Set a string value to enable using webpack and specify the `output.distPath`.

--- packages/toolkit/utils/src/path.ts
+++ packages/toolkit/utils/src/path.ts
@@ -1,5 +1,7 @@
 import path from 'path';
-import { upath } from './compiled';
+import os from 'os';
+import fs from 'fs';
+import { nanoid, upath } from './compiled';
 
 export const isPathString = (test: string): boolean =>
   path.posix.basename(test) !== test || path.win32.basename(test) !== test;
@@ -12,3 +14,11 @@ export const normalizeToPosixPath = (p: string | undefined) =>
   upath
     .normalizeSafe(path.normalize(p || ''))
     .replace(/^([a-zA-Z]+):/, (_, m: string) => `/${m.toLowerCase()}`);
+
+export const getTemplatePath = (prefix?: string) => {
+  const tmpRoot = fs.realpathSync(os.tmpdir());
+  const parts = [tmpRoot];
+  prefix && parts.push(prefix);
+  parts.push(nanoid());
+  return path.resolve(...parts);
+};
