e6ad6b0593bc2dd033d36db1a499e955ad0f9f43
gaoyuan <gaoyuan.1226@bytedance.com>
2022-09-27 11:50:40 +0800

    feat: add webpack-builder-plugin-rem (#1748)
    * feat: add webpack-builder-plugin-rem
    
    * chore: dynamic import
    
    * docs: add convertToRem
    
    * docs: update convertToRem
    
    * fix: remove useless file
    
    * fix: declare defaultOptions
    
    * chore: compiled postcss-pxtorem
    
    * fix: update lock
    
    * fix: doc & comment
    
    * docs: add en convertToRem.md
    
    * chore: use webpack-chain add PxToRemPlugin
    
    * docs: add type for options
    
    * fix: add less & sass rule

--- packages/builder/webpack-builder/src/types/config/output.ts
+++ packages/builder/webpack-builder/src/types/config/output.ts
@@ -47,6 +47,44 @@ export type AssetsRetryOptions = {
   onFail?: (options: AssetsRetryHookContext) => void;
 };
 
+/**
+ * postcss-pxtorem options
+ *
+ * https://github.com/cuth/postcss-pxtorem#options
+ */
+export type PxToRemOptions = Partial<{
+  rootValue: number;
+  unitPrecision: number;
+  propList: Array<string>;
+  selectorBlackList: Array<string>;
+  replace: boolean;
+  mediaQuery: boolean;
+  minPixelValue: number;
+  exclude: string | RegExp | ((filePath: string) => boolean);
+}>;
+
+export type RemOptions = Partial<{
+  /** Runtime options */
+  /** Whether to inject runtime code into html templates。Default: true */
+  enableRuntime: boolean;
+  /** Usually, `fontSize = (clientWidth * rootFontSize) / screenWidth` */
+  screenWidth: number;
+  rootFontSize: number;
+  maxRootFontSize: number;
+  /** Get clientWidth from the url query based on widthQueryKey */
+  widthQueryKey: string;
+  /** The entries to ignore */
+  excludeEntries: Array<string>;
+  /** Use height to calculate rem in landscape。Default: false */
+  supportLandscape: boolean;
+  /**
+   * Whether to use rootFontSize when large than maxRootFontSize （scene：pc）
+   */
+  useRootFontSizeBeyondMax: boolean;
+  /** CSS (postcss-pxtorem) option */
+  pxtorem: PxToRemOptions;
+}>;
+
 export type ExternalsOptions = WebpackConfig['externals'];
 
 export interface OutputConfig {
@@ -71,5 +109,6 @@ export interface OutputConfig {
   overrideBrowserslist?: string[];
   svgDefaultExport?: 'component' | 'url';
   assetsRetry?: AssetsRetryOptions;
+  convertToRem?: boolean | RemOptions;
   externals?: ExternalsOptions;
 }
