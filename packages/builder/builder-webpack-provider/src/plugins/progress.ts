72d05010e56c66fb606cee2536bef5f7015502ad
zhushijie.jayson <zhushijie.jayson@bytedance.com>
2022-10-08 19:10:21 +0800

    feat(builder): add new webpack progress bar

--- packages/builder/builder-webpack-provider/src/plugins/progress.ts
+++ packages/builder/builder-webpack-provider/src/plugins/progress.ts
@@ -1,22 +1,46 @@
 import type { BuilderPlugin } from '../types';
+// eslint-disable-next-line @typescript-eslint/no-restricted-imports
+import { UseWebpackProgressPlugin } from '../webpackPlugins/ProgressPlugin';
 
-export const PluginProgress = (): BuilderPlugin => ({
-  name: 'builder-plugin-progress',
-
+/**
+ * @deprecated
+ * 旧版 进度条插件(已废弃)
+ */
+export const PluginProgressOld = (): BuilderPlugin => ({
+  name: 'webpackBuilderPluginProgress',
   setup(api) {
     api.modifyWebpackChain(async (chain, { isServer, CHAIN_ID }) => {
       const config = api.getBuilderConfig();
-
       if (!config.dev?.progressBar) {
         return;
       }
-
       const { default: WebpackBar } = await import('../../compiled/webpackbar');
       chain.plugin(CHAIN_ID.PLUGIN.PROGRESS).use(WebpackBar, [
         {
           name: isServer ? 'server' : 'client',
         },
       ]);
+    });
+  },
+});
+
+export const PluginProgress = (): BuilderPlugin => ({
+  name: 'webpackBuilderPluginProgress',
+  setup(api) {
+    api.modifyWebpackConfig((webpack_config, { webpack }) => {
+      const config = api.getBuilderConfig();
+      const options = config.dev?.progressBar;
+      let plugin;
+      if (!options) {
+        return;
+      } else if (options === true) {
+        plugin = UseWebpackProgressPlugin({}, webpack);
+      } else {
+        plugin = UseWebpackProgressPlugin(options, webpack);
+      }
+      if (plugin) {
+        webpack_config.plugins?.push(plugin);
+      }
     });
   },
 });
