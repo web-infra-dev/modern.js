b2c41363a712d8dafbc27e468f90caf19895814c
chenjiahan <chenjiahan.jait@bytedance.com>
2022-10-08 10:20:07 +0800

    chore: remove unused eslint

--- packages/builder/builder-shared/src/types/index.ts
+++ packages/builder/builder-shared/src/types/index.ts
@@ -1,7 +1,5 @@
-/* eslint-disable @typescript-eslint/no-restricted-imports */
 export * from './builder';
 export * from './context';
 export * from './utils';
 export * from './plugin';
 export * from './provider';
-/* eslint-enable @typescript-eslint/no-restricted-imports */
