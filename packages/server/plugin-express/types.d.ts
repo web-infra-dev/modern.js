ef1cf7e52b0c621c5bd6af5809827db4ae35b03f
zllkjc <kongjiacong@bytedance.com>
2022-10-10 17:51:48 +0800

    chore: add koa & express types for /server namespace

--- packages/server/plugin-express/types.d.ts
+++ packages/server/plugin-express/types.d.ts
@@ -22,3 +22,26 @@ declare module '@modern-js/runtime/express' {
 
   export * from '@modern-js/bff-core';
 }
+
+declare module '@modern-js/runtime/server' {
+  import { Request, Response, RequestHandler } from 'express';
+
+  type ExpressOptions = {
+    addMiddleware: (...input: RequestHandler[]) => void;
+  };
+
+  type ExpressAttacher = (options: ExpressOptions) => void;
+
+  type Context = { req: Request; res: Response };
+
+  export const Pipe: import('./src/runtime').Pipe;
+  export const Middleware: import('./src/runtime').Middleware;
+
+  export function useContext(): Context;
+
+  export function hook(attacher: ExpressAttacher): ExpressAttacher;
+
+  export type { RequestHandler };
+
+  export * from '@modern-js/bff-core';
+}
