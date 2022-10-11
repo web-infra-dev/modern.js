6fcc581cf69f2639b19a230f401d897a8d1adcda
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2022-10-11 11:56:16 +0800

    feat: support devServer.setupMiddlewares

--- packages/toolkit/types/server/devServer.d.ts
+++ packages/toolkit/types/server/devServer.d.ts
@@ -9,6 +9,13 @@ export type RequestHandler = (
   next: NextFunction,
 ) => void;
 
+export type ExposeServerApis = {
+  sockWrite: (
+    type: string,
+    data?: string | boolean | Record<string, any>,
+  ) => void;
+};
+
 export type DevServerOptions = {
   /** config of hmr client. */
   client?: {
@@ -26,6 +33,19 @@ export type DevServerOptions = {
   headers?: Record<string, string>;
   before?: RequestHandler[];
   after?: RequestHandler[];
+  /** Provides the ability to execute a custom function and apply custom middlewares */
+  setupMiddlewares?: Array<
+    (
+      /** Order: `devServer.before` => `unshift` => internal middlewares => `push` => `devServer.after` */
+      middlewares: {
+        /** Use the `unshift` method if you want to run a middleware before all other middlewares */
+        unshift: (handles: RequestHandler) => void;
+        /** Use the `push` method if you want to run a middleware after all other middlewares */
+        push: (handles: RequestHandler) => void;
+      },
+      server: ExposeServerApis,
+    ) => void
+  >;
   /** Whether to watch files change. */
   watch?: boolean;
   /** Whether to enable hot reload. */
