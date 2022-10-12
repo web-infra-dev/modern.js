8f90038db141155e7a0d9dd83e91bd3a56ef01ad
zhushijie <zsj439453290@126.com>
2022-10-12 10:29:30 +0800

    feat(builder): add new webpack progress bar (#1814)

--- packages/builder/builder-webpack-provider/tests/setup.ts
+++ packages/builder/builder-webpack-provider/tests/setup.ts
@@ -1,6 +1,10 @@
 import path from 'path';
+// eslint-disable-next-line node/prefer-global/console
+import { Console } from 'console';
 import { expect } from 'vitest';
 import { createSnapshotSerializer } from '@scripts/vitest-config';
+
+global.console.Console = Console;
 
 expect.addSnapshotSerializer(
   createSnapshotSerializer({
