12fc306e95080730bfbfa7009c896d4cfdfa420c
kyli <ldh71777@gmail.com>
2022-10-12 11:37:30 +0800

    fix: types (#1853)

--- packages/runtime/plugin-router-legacy/type.d.ts
+++
@@ -1,7 +0,0 @@
-import './dist/types/runtime';
-
-declare module '@modern-js/runtime/router' {
-  export * from './dist/types/runtime';
-}
-
-export * from './dist/types';
--- packages/runtime/plugin-runtime/lib/types.d.ts
+++ packages/runtime/plugin-runtime/lib/types.d.ts
@@ -1,16 +1,21 @@
 /// <reference path="../dist/types/index.d.ts" />
 
-import { RouterConfig, StateConfig } from "../dist/types";
+import { RouterConfig, StateConfig } from '../dist/types';
+import '../dist/types/router/index';
 
 declare module '@modern-js/core' {
-    interface RuntimeConfig {
-      router?: RouterConfig | boolean;
-      state?: StateConfig | boolean;
-    }
+  interface RuntimeConfig {
+    router?: RouterConfig | boolean;
+    state?: StateConfig | boolean;
   }
+}
 
 declare module 'http' {
   interface ServerResponse {
-    locals: Record<string, any>
+    locals: Record<string, any>;
   }
+}
+
+declare module '@modern-js/runtime/router' {
+  export * from '../dist/types/router/index';
 }
