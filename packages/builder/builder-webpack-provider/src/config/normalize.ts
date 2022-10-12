330bdba46343ff02dae67c85f0c35c3375de9c8b
Asuka109 <chenwenjie.asuka@bytedance.com>
2022-10-12 14:46:24 +0800

    feat(builder): config normalizer share default config value with `config/defaults`

--- packages/builder/builder-webpack-provider/src/config/normalize.ts
+++ packages/builder/builder-webpack-provider/src/config/normalize.ts
@@ -1,16 +1,7 @@
-import {
-  CSS_DIST_DIR,
-  FONT_DIST_DIR,
-  HTML_DIST_DIR,
-  IMAGE_DIST_DIR,
-  JS_DIST_DIR,
-  MEDIA_DIST_DIR,
-  mergeBuilderConfig,
-  ROOT_DIST_DIR,
-  SVG_DIST_DIR,
-} from '@modern-js/builder-shared';
+import { mergeBuilderConfig } from '@modern-js/builder-shared';
 import _ from '@modern-js/utils/lodash';
 import { BuilderConfig, NormalizedConfig } from '../types';
+import { createDefaultConfig } from './defaults';
 
 const createNormalizer = (normalizer: NormalizedConfig) => {
   return (config: BuilderConfig): NormalizedConfig => {
@@ -26,57 +17,4 @@ const createNormalizer = (normalizer: NormalizedConfig) => {
  * 2. Object value that should not be empty.
  * 3. Meaningful and can be filled by constant value.
  */
-export const normalizeConfig = createNormalizer({
-  dev: {
-    port: 3000,
-    assetPrefix: '/',
-    https: false,
-    startUrl: true,
-    hmr: false,
-    progressBar: true,
-  },
-  html: {},
-  tools: {
-    tsChecker: {},
-    babel: {},
-    devServer: {
-      client: {},
-      devMiddleware: {
-        writeToDisk: true,
-      },
-      hot: true,
-      liveReload: true,
-      watch: true,
-      https: false,
-    },
-  },
-  source: {
-    preEntry: [],
-    globalVars: {},
-    alias: {},
-    compileJsDataURI: false,
-  },
-  output: {
-    distPath: {
-      root: ROOT_DIST_DIR,
-      html: HTML_DIST_DIR,
-      js: JS_DIST_DIR,
-      css: CSS_DIST_DIR,
-      svg: SVG_DIST_DIR,
-      font: FONT_DIST_DIR,
-      image: IMAGE_DIST_DIR,
-      media: MEDIA_DIST_DIR,
-    },
-    filename: {},
-    polyfill: 'entry',
-    cleanDistPath: true,
-    svgDefaultExport: 'url',
-  },
-  security: { sri: false },
-  performance: {
-    removeConsole: false,
-    removeMomentLocale: false,
-    chunkSplit: { strategy: 'split-by-experience' },
-  },
-  experiments: {},
-});
+export const normalizeConfig = createNormalizer(createDefaultConfig());
