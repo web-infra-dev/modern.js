2273695a7646993c2763e0e2f36ffa6a4dd822c7
Asuka109 <chenwenjie.asuka@bytedance.com>
2022-10-12 15:08:40 +0800

    refactor(builder): better impl of `normalizeConfig`

--- packages/builder/builder-webpack-provider/src/config/normalize.ts
+++ packages/builder/builder-webpack-provider/src/config/normalize.ts
@@ -1,20 +1,21 @@
-import { mergeBuilderConfig } from '@modern-js/builder-shared';
-import _ from '@modern-js/utils/lodash';
+import { extendsType, mergeBuilderConfig } from '@modern-js/builder-shared';
 import { BuilderConfig, NormalizedConfig } from '../types';
 import { createDefaultConfig } from './defaults';
 
-const createNormalizer = (normalizer: NormalizedConfig) => {
-  return (config: BuilderConfig): NormalizedConfig => {
-    return mergeBuilderConfig<NormalizedConfig>(
-      _.cloneDeep(normalizer),
-      config as NormalizedConfig,
-    );
-  };
-};
+const defineNormalizedDefaultConfig = extendsType<NormalizedConfig>();
+
+const createNormalizedDefaultConfig = () =>
+  defineNormalizedDefaultConfig({
+    ...createDefaultConfig(),
+  });
 
 /** #__PURE__
  * 1. May used by multiple plugins.
  * 2. Object value that should not be empty.
  * 3. Meaningful and can be filled by constant value.
  */
-export const normalizeConfig = createNormalizer(createDefaultConfig());
+export const normalizeConfig = (config: BuilderConfig): NormalizedConfig =>
+  mergeBuilderConfig<NormalizedConfig>(
+    createNormalizedDefaultConfig(),
+    config as NormalizedConfig,
+  );
