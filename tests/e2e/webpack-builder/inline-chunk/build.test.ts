60ad4d92b4519b7291911f3340d44a3631f5250b
zjk <jingkai.zhao@foxmail.com>
2022-10-13 19:34:44 +0800

    fix(builder): inlined assets should be deleted after all html generated (#1881)
    * test: add failing test
    
    * fix(builder): inlined assets should be deleted after all html plugins
    
    * chore: add changeset
    
    * test: update outdated snapshots

--- tests/e2e/webpack-builder/inline-chunk/build.test.ts
+++ tests/e2e/webpack-builder/inline-chunk/build.test.ts
@@ -3,6 +3,9 @@ import { expect, test } from '@modern-js/e2e/playwright';
 import { RUNTIME_CHUNK_NAME } from '@modern-js/builder-shared';
 import { createStubBuilder } from '@modern-js/builder-webpack-provider/stub';
 
+const isRuntimeChunkInHtml = (html: string): boolean =>
+  Boolean(html.match(new RegExp(`${RUNTIME_CHUNK_NAME}\\.(.+)\\.js\\.map`)));
+
 test('inline runtime chunk by default', async () => {
   const builder = await createStubBuilder({
     webpack: true,
@@ -19,7 +22,30 @@ test('inline runtime chunk by default', async () => {
   const indexHtml =
     files[path.resolve(__dirname, './dist/html/index/index.html')];
 
+  expect(isRuntimeChunkInHtml(indexHtml)).toBeTruthy();
+});
+
+test('inline runtime chunk by default with multiple entries', async () => {
+  const builder = await createStubBuilder({
+    webpack: true,
+    entry: {
+      index: path.resolve('./src/index.js'),
+      another: path.resolve('./src/another.js'),
+    },
+  });
+  const files = await builder.unwrapOutputJSON();
+
+  // no builder-runtime file in output
   expect(
-    indexHtml.match(new RegExp(`${RUNTIME_CHUNK_NAME}\\.(.+)\\.js\\.map`)),
-  ).toBeTruthy();
+    Object.keys(files).some(fileName => fileName.includes(RUNTIME_CHUNK_NAME)),
+  ).toBe(false);
+
+  // found builder-runtime file in html
+  const indexHtml =
+    files[path.resolve(__dirname, './dist/html/index/index.html')];
+  const anotherHtml =
+    files[path.resolve(__dirname, './dist/html/another/index.html')];
+
+  expect(isRuntimeChunkInHtml(indexHtml)).toBeTruthy();
+  expect(isRuntimeChunkInHtml(anotherHtml)).toBeTruthy();
 });
