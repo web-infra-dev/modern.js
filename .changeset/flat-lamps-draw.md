a4b0e277b41b00604aba7c91ae5854469ed56dea
targeral <targeral@outlook.com>
2022-10-15 08:51:53 +0800

    feat: add log logic (#1893)

---
+++ .changeset/flat-lamps-draw.md
@@ -0,0 +1,6 @@
+---
+'@modern-js/module-tools-v2': minor
+---
+
+feat: add log logic
+feat: 添加日志逻辑
--- packages/solutions/module-tools-v2/src/constants/log.ts
+++ packages/solutions/module-tools-v2/src/constants/log.ts
@@ -3,3 +3,7 @@ export enum SectionTitleStatus {
   Fail,
   Log,
 }
+
+export const BundleDtsLogPrefix = '[Bundle:DTS]';
+export const BundlelessDtsLogPrefix = '[Bundleless:DTS]';
+export const buildSuccessText = 'Build succeed';
