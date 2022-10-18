8cd6a53962a0e1b5d717105edf9a0ff65bcd5381
yangxingyuan <yangxingyuan@bytedance.com>
2022-10-18 15:22:55 +0800

    feat: support before params in addPlugins

--- packages/builder/builder-shared/src/createPluginStore.ts
+++ packages/builder/builder-shared/src/createPluginStore.ts
@@ -4,10 +4,16 @@ import type { PluginStore, BuilderPlugin } from './types';
 export function createPluginStore(): PluginStore {
   let plugins: BuilderPlugin[] = [];
 
-  const addPlugins = (newPlugins: BuilderPlugin[]) => {
+  const addPlugins = (newPlugins: BuilderPlugin[], before?: string) => {
     newPlugins.forEach(newPlugin => {
       if (plugins.find(item => item.name === newPlugin.name)) {
         logger.warn(`Plugin "${newPlugin.name}" already exists.`);
+      } else if (before) {
+        const index = plugins.findIndex(item => item.name === before);
+        if (index === -1) {
+          logger.warn(`Plugin "${before}" does not exist.`);
+        }
+        plugins.splice(index, 0, newPlugin);
       } else {
         plugins.push(newPlugin);
       }
