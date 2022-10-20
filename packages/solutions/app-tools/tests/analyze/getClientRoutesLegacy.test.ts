32d7d7e406633b1b7e729120dc538b63bfc6968e
Ming <wangyiming.fe@bytedance.com>
2022-10-20 23:56:44 +0800

    feat: support nested routes

--- packages/solutions/app-tools/tests/analyze/getClientRoutesLegacy.test.ts
+++ packages/solutions/app-tools/tests/analyze/getClientRoutesLegacy.test.ts
@@ -1,7 +1,10 @@
 import path from 'path';
 import type { IAppContext, NormalizedConfig } from '@modern-js/core';
 import { getBundleEntry } from '../../src/analyze/getBundleEntry';
-import { getClientRoutesLegacy as getClientRoutes } from '../../src/analyze/getClientRoutes';
+import {
+  getClientRoutesLegacy as getClientRoutes,
+  getClientRoutesLegacy,
+} from '../../src/analyze/getClientRoutes';
 
 const prepareEnv = (fixturePath: string) => {
   const appContext = {
@@ -20,122 +23,134 @@ const prepareEnv = (fixturePath: string) => {
   };
 };
 
-describe('get client routes', () => {
-  test('basic usage', () => {
-    const fixturePath = path.resolve(
-      __dirname,
-      './fixtures/entries/file-system-routes',
-    );
+describe('getClientRoutesLegacy', () => {
+  test('basic usage', () => basicUsage(getClientRoutesLegacy));
 
-    const { appContext, config } = prepareEnv(fixturePath);
-
-    const entries = getBundleEntry(
-      appContext as IAppContext,
-      config as NormalizedConfig,
-    );
-
-    let routes;
-
-    for (const entrypoint of entries) {
-      routes = getClientRoutes({
-        entrypoint,
-        srcDirectory: appContext.srcDirectory,
-        srcAlias: appContext.internalSrcAlias,
-        internalDirectory: appContext.internalDirectory,
-        internalDirAlias: appContext.internalDirAlias,
-      });
-    }
-
-    expect(routes).toEqual([
-      {
-        path: '/',
-        _component: '@_modern_js_src/pages/index.ts',
-        component: 'Comp_pages_index_ts',
-        exact: true,
-        parent: undefined,
-      },
-      {
-        path: '/info',
-        _component: '@_modern_js_src/pages/info.ts',
-        component: 'Comp_pages_info_ts',
-        exact: true,
-        parent: undefined,
-      },
-      {
-        path: '/about',
-        _component: '@_modern_js_src/pages/about.ts',
-        component: 'Comp_pages_about_ts',
-        exact: true,
-        parent: undefined,
-      },
-    ]);
-  });
-
-  test('nested routes with `_app.tsx` and `_layout.tsx`', () => {
-    const fixturePath = path.resolve(
-      __dirname,
-      './fixtures/entries/file-system-routes-nested',
-    );
-
-    const { appContext, config } = prepareEnv(fixturePath);
-
-    const entries = getBundleEntry(
-      appContext as IAppContext,
-      config as NormalizedConfig,
-    );
-
-    let routes;
-
-    for (const entrypoint of entries) {
-      routes = getClientRoutes({
-        entrypoint,
-        srcDirectory: appContext.srcDirectory,
-        srcAlias: appContext.internalSrcAlias,
-        internalDirectory: appContext.internalDirectory,
-        internalDirAlias: appContext.internalDirAlias,
-      });
-    }
-
-    expect(routes).toEqual([
-      {
-        path: '/',
-        _component: '@_modern_js_src/pages/index.ts',
-        component: 'Comp_pages_index_ts',
-        exact: true,
-        parent: undefined,
-      },
-      {
-        path: '/a',
-        _component:
-          '@_modern_js_internal/main/internal_components/L_a_Comp_pages_a_index_ts.jsx',
-        component: 'L_a_Comp_pages_a_index_ts',
-        exact: true,
-        parent: undefined,
-      },
-      {
-        path: '/a/list',
-        _component:
-          '@_modern_js_internal/main/internal_components/L_a_Comp_pages_a_list_index_ts.jsx',
-        component: 'L_a_Comp_pages_a_list_index_ts',
-        exact: true,
-        parent: undefined,
-      },
-      {
-        path: '/a/about',
-        _component:
-          '@_modern_js_internal/main/internal_components/L_a_Comp_pages_a_about_index_ts.jsx',
-        component: 'L_a_Comp_pages_a_about_index_ts',
-        exact: true,
-        parent: undefined,
-      },
-      {
-        path: '/a/list/:id',
-        _component:
-          '@_modern_js_internal/main/internal_components/L_a_Comp_pages_a_list__id__index_ts.jsx',
-        component: 'L_a_Comp_pages_a_list__id__index_ts',
-        exact: true,
-        parent: undefined,
-      },
-    ]);
-  });
+  test('nested routes with `_app.tsx` and `_layout.tsx`', () =>
+    supportLayout(getClientRoutesLegacy));
 });
+
+describe('getClientRoutes', () => {
+  test('basic usage', () => basicUsage(getClientRoutes));
+
+  test('nested routes with `_app.tsx` and `_layout.tsx`', () =>
+    supportLayout(getClientRoutes));
+});
+
+function basicUsage(getClientRoutes: typeof getClientRoutesLegacy) {
+  const fixturePath = path.resolve(
+    __dirname,
+    './fixtures/entries/file-system-routes',
+  );
+
+  const { appContext, config } = prepareEnv(fixturePath);
+
+  const entries = getBundleEntry(
+    appContext as IAppContext,
+    config as NormalizedConfig,
+  );
+
+  let routes;
+
+  for (const entrypoint of entries) {
+    routes = getClientRoutes({
+      entrypoint,
+      srcDirectory: appContext.srcDirectory,
+      srcAlias: appContext.internalSrcAlias,
+      internalDirectory: appContext.internalDirectory,
+      internalDirAlias: appContext.internalDirAlias,
+    });
+  }
+
+  expect(routes).toEqual([
+    {
+      path: '/',
+      _component: '@_modern_js_src/pages/index.ts',
+      component: 'Comp_pages_index_ts',
+      exact: true,
+      parent: undefined,
+    },
+    {
+      path: '/info',
+      _component: '@_modern_js_src/pages/info.ts',
+      component: 'Comp_pages_info_ts',
+      exact: true,
+      parent: undefined,
+    },
+    {
+      path: '/about',
+      _component: '@_modern_js_src/pages/about.ts',
+      component: 'Comp_pages_about_ts',
+      exact: true,
+      parent: undefined,
+    },
+  ]);
+}
+
+function supportLayout(getClientRoutes: typeof getClientRoutesLegacy) {
+  const fixturePath = path.resolve(
+    __dirname,
+    './fixtures/entries/file-system-routes-nested',
+  );
+
+  const { appContext, config } = prepareEnv(fixturePath);
+
+  const entries = getBundleEntry(
+    appContext as IAppContext,
+    config as NormalizedConfig,
+  );
+
+  let routes;
+
+  for (const entrypoint of entries) {
+    routes = getClientRoutes({
+      entrypoint,
+      srcDirectory: appContext.srcDirectory,
+      srcAlias: appContext.internalSrcAlias,
+      internalDirectory: appContext.internalDirectory,
+      internalDirAlias: appContext.internalDirAlias,
+    });
+  }
+
+  expect(routes).toEqual([
+    {
+      path: '/',
+      _component: '@_modern_js_src/pages/index.ts',
+      component: 'Comp_pages_index_ts',
+      exact: true,
+      parent: undefined,
+    },
+    {
+      path: '/a',
+      _component:
+        '@_modern_js_internal/main/internal_components/L_a_Comp_pages_a_index_ts.jsx',
+      component: 'L_a_Comp_pages_a_index_ts',
+      exact: true,
+      parent: undefined,
+    },
+    {
+      path: '/a/list',
+      _component:
+        '@_modern_js_internal/main/internal_components/L_a_Comp_pages_a_list_index_ts.jsx',
+      component: 'L_a_Comp_pages_a_list_index_ts',
+      exact: true,
+      parent: undefined,
+    },
+    {
+      path: '/a/about',
+      _component:
+        '@_modern_js_internal/main/internal_components/L_a_Comp_pages_a_about_index_ts.jsx',
+      component: 'L_a_Comp_pages_a_about_index_ts',
+      exact: true,
+      parent: undefined,
+    },
+    {
+      path: '/a/list/:id',
+      _component:
+        '@_modern_js_internal/main/internal_components/L_a_Comp_pages_a_list__id__index_ts.jsx',
+      component: 'L_a_Comp_pages_a_list__id__index_ts',
+      exact: true,
+      parent: undefined,
+    },
+  ]);
+}
