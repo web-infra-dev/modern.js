d50dc226987d53cd218e89606c95800ae6788cd8
targeral <targeral@outlook.com>
2022-10-21 16:10:46 +0800

    test: add envVars test

--- packages/solutions/module-tools-v2/tests/constants.ts
+++ packages/solutions/module-tools-v2/tests/constants.ts
@@ -2,3 +2,6 @@ export const aliasBundleObjectDistPath = 'dist/bundle/object';
 export const aliasBundlelessObjectDistPath = 'dist/bundleless/object';
 export const aliasBundleFunctionDistPath = 'dist/bundle/function';
 export const aliasBundlelessFunctionDistPath = 'dist/bundleless/function';
+
+export const envVarsBundleDistPath = 'dist/bundle';
+export const envVarsBundlelessDistPath = 'dist/bundleless';
