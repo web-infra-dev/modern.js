c73492b7238da373f7a1a7022e5e9797d1a9ef06
clchenliang <clchenliang@hotmail.com>
2022-10-21 16:28:33 +0800

    feat: tmp

--- packages/cli/webpack/src/config/features/html.ts
+++ packages/cli/webpack/src/config/features/html.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import {
-  isProd,
+  // isProd,
   CHAIN_ID,
   findExists,
   getEntryOptions,
@@ -8,6 +8,9 @@ import {
   generateMetaTags,
 } from '@modern-js/utils';
 import webpack from 'webpack';
+// import React from 'react';
+// import ReactDomServer from 'react-dom/server';
+import { build } from 'esbuild';
 import type { Entrypoint, IAppContext } from '@modern-js/types';
 import { template as lodashTemplate } from '@modern-js/utils/lodash';
 import { BottomTemplatePlugin } from '../../plugins/bottom-template-plugin';
@@ -65,16 +68,57 @@ export function applyHTMLPlugin({
           filename: htmlFilename(entryName),
           chunks: [entryName],
           template: appContext.htmlTemplates[entryName],
-          minify: !isProd()
-            ? false
-            : {
-                collapseWhitespace: true,
-                removeComments: false,
-                removeRedundantAttributes: true,
-                removeScriptTypeAttributes: true,
-                removeStyleLinkTypeAttributes: true,
-                useShortDoctype: true,
-              },
+          minify: false,
+          templateContent: async () => {
+            // console.log('===> \n template: ', htmlWebpackPlugin);
+            // 1. 获取 rootDir
+            // 2. 获取 docuemnt 文件 & 判断要渲染哪些 document 文件
+            // 3. 拼接成 html 文件，返回
+            const appdir = path.resolve(appContext.appDirectory);
+            // todo: entryName 加入
+            const docFileIn = ['src/document'];
+            const docExt = ['.jsx', '.tsx'];
+
+            // 查找逻辑 | 全部 html 文件
+            const docFiles = docFileIn.reduce((sum, file) => {
+              for (const ext of docExt) {
+                const validateFile = path.join(
+                  appdir,
+                  `${[file, ext].join('')}`,
+                );
+                if (findExists([validateFile])) {
+                  sum.push(validateFile);
+                  break;
+                }
+              }
+              return sum;
+            }, [] as string[]);
+
+            // 编译 Document 文件
+            // console.log('\n\n===> esbuild params:\n', appdir, docFiles);
+            const res = await build({
+              entryNames: docFiles[0],
+              // outdir: path.join(appdir, '.tmp', 'document.js'),
+              outfile: `${appdir}/document.js`,
+              platform: 'node',
+              bundle: true,
+            });
+            // eslint-disable-next-line no-console
+            console.log('\n\n\n===>  res: \n', res);
+            // const { Document } = await import(`${appdir}/d.js`);
+            // // console.log('===> Document: \n', Document);
+            // const html = ReactDomServer.renderToString(
+            //   React.createElement(Document, null),
+            // );
+
+            // console.log('\n\n\n===> html string: \n:', html);
+
+            return `<html>
+              <head></head>
+              'hello'
+            </html>`;
+            // return d.replace('sorry', 'chli');
+          },
           favicon:
             getEntryOptions<string | undefined>(
               entryName,
