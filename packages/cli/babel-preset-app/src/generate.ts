6663be88f6f98b95178737b2f4fff5981a202e0d
chenjiahan <chenjiahan.jait@bytedance.com>
2022-10-25 14:13:41 +0800

    fix(babel-preset-app): remove useless plugin-transform-destructuring

--- packages/cli/babel-preset-app/src/generate.ts
+++ packages/cli/babel-preset-app/src/generate.ts
@@ -3,37 +3,6 @@ import { genCommon } from './common';
 import type { Options } from './type';
 
 export const generate = (options: Options, chain: BabelChain): BabelChain => {
-  const { useTsLoader } = options;
-
   const commonChain = genCommon(options);
-
-  if (useTsLoader) {
-    return chain.merge(commonChain);
-  }
-
-  chain.plugin('@babel/plugin-transform-destructuring').use(
-    require.resolve('@babel/plugin-transform-destructuring'),
-
-    [
-      {
-        // Use loose mode for performance:
-        // https://github.com/facebook/create-react-app/issues/5602
-        loose: false,
-        selectiveLoose: [
-          'useState',
-          'useEffect',
-          'useContext',
-          'useReducer',
-          'useCallback',
-          'useMemo',
-          'useRef',
-          'useImperativeHandle',
-          'useLayoutEffect',
-          'useDebugValue',
-        ],
-      },
-    ],
-  );
-
   return chain.merge(commonChain);
 };
