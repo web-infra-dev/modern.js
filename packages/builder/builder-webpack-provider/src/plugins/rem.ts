5e3cecd5230b3d55ed5bdcd486f6f08e95c99a0a
neverland <chenjiahan.jait@bytedance.com>
2022-10-28 15:28:50 +0800

    fix(builder): skip rem plugin when target is node or web worker (#1975)

--- packages/builder/builder-webpack-provider/src/plugins/rem.ts
+++ packages/builder/builder-webpack-provider/src/plugins/rem.ts
@@ -10,71 +10,73 @@ export const PluginRem = (): BuilderPlugin => ({
   name: 'builder-plugin-rem',
 
   setup(api) {
-    api.modifyWebpackChain(async (chain, { CHAIN_ID }) => {
-      const {
-        output: { convertToRem },
-      } = api.getNormalizedConfig();
+    api.modifyWebpackChain(
+      async (chain, { CHAIN_ID, isServer, isWebWorker }) => {
+        const {
+          output: { convertToRem },
+        } = api.getNormalizedConfig();
 
-      if (!convertToRem) {
-        return;
-      }
+        if (!convertToRem || isServer || isWebWorker) {
+          return;
+        }
 
-      const userOptions = {
-        ...defaultOptions,
-        ...(typeof convertToRem === 'boolean' ? {} : convertToRem),
-      };
+        const userOptions = {
+          ...defaultOptions,
+          ...(typeof convertToRem === 'boolean' ? {} : convertToRem),
+        };
 
-      // handle css
-      const { default: PxToRemPlugin } = (await import(
-        '../../compiled/postcss-pxtorem'
-      )) as {
-        default: (_opts: PxToRemOptions) => any;
-      };
+        // handle css
+        const { default: PxToRemPlugin } = (await import(
+          '../../compiled/postcss-pxtorem'
+        )) as {
+          default: (_opts: PxToRemOptions) => any;
+        };
 
-      const applyRules = [
-        CHAIN_ID.RULE.CSS,
-        CHAIN_ID.RULE.LESS,
-        CHAIN_ID.RULE.SASS,
-      ];
-      const getPxToRemPlugin = () =>
-        PxToRemPlugin({
-          rootValue: userOptions.rootFontSize,
-          unitPrecision: 5,
-          propList: ['*'],
-          ..._.cloneDeep(userOptions.pxtorem || {}),
+        const applyRules = [
+          CHAIN_ID.RULE.CSS,
+          CHAIN_ID.RULE.LESS,
+          CHAIN_ID.RULE.SASS,
+        ];
+        const getPxToRemPlugin = () =>
+          PxToRemPlugin({
+            rootValue: userOptions.rootFontSize,
+            unitPrecision: 5,
+            propList: ['*'],
+            ..._.cloneDeep(userOptions.pxtorem || {}),
+          });
+        // Deep copy options to prevent unexpected behavior.
+        applyRules.forEach(name => {
+          chain.module.rules.has(name) &&
+            chain.module
+              .rule(name)
+              .use(CHAIN_ID.USE.POSTCSS)
+              .tap((options = {}) => ({
+                ...options,
+                postcssOptions: {
+                  ...(options.postcssOptions || {}),
+                  plugins: [
+                    ...(options.postcssOptions?.plugins || []),
+                    getPxToRemPlugin(),
+                  ],
+                },
+              }));
         });
-      // Deep copy options to prevent unexpected behavior.
-      applyRules.forEach(name => {
-        chain.module.rules.has(name) &&
-          chain.module
-            .rule(name)
-            .use(CHAIN_ID.USE.POSTCSS)
-            .tap((options = {}) => ({
-              ...options,
-              postcssOptions: {
-                ...(options.postcssOptions || {}),
-                plugins: [
-                  ...(options.postcssOptions?.plugins || []),
-                  getPxToRemPlugin(),
-                ],
-              },
-            }));
-      });
 
-      // handle runtime (html)
-      if (!userOptions.enableRuntime) {
-        return;
-      }
+        // handle runtime (html)
+        if (!userOptions.enableRuntime) {
+          return;
+        }
 
-      const { AutoSetRootFontSizePlugin } = await import(
-        '../webpackPlugins/AutoSetRootFontSizePlugin'
-      );
+        const { AutoSetRootFontSizePlugin } = await import(
+          '../webpackPlugins/AutoSetRootFontSizePlugin'
+        );
 
-      const entries = Object.keys(chain.entryPoints.entries() || {});
+        const entries = Object.keys(chain.entryPoints.entries() || {});
 
-      chain
-        .plugin(CHAIN_ID.PLUGIN.AUTO_SET_ROOT_SIZE)
-        .use(AutoSetRootFontSizePlugin, [userOptions, entries]);
-    });
+        chain
+          .plugin(CHAIN_ID.PLUGIN.AUTO_SET_ROOT_SIZE)
+          .use(AutoSetRootFontSizePlugin, [userOptions, entries]);
+      },
+    );
   },
 });
