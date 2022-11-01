0a3c9907d943e02dca0e0e5e8ff9ea781d4bc1d5
gaoyuan <gaoyuan.1226@bytedance.com>
2022-11-01 15:55:29 +0800

    test(builder): add e2e test cases (#1968)
    * test(builder): add e2e test cases

---
+++ tests/e2e/builder/fixtures/performance/removeConsole/src/index.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable no-console */
+
+console.debug('test-console-debug');
+console.log('test-console-log');
+console.warn('test-console-warn');
+console.error('test-console-error');
---
+++ tests/e2e/builder/fixtures/source/resolve-extension-prefix/src/ex.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable no-undef */
+const testEl = document.createElement('div');
+testEl.id = 'test-el';
+testEl.innerHTML = 'aaaaa';
+document.body.appendChild(testEl);
