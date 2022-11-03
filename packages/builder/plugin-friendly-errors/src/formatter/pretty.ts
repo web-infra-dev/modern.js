006f2e64dfb83cd34fd636713d60f7ecc6d65bc8
Asuka109 <chenwenjie.asuka@bytedance.com>
2022-11-03 15:59:37 +0800

    feat(friendly-errors): better formatting style

--- packages/builder/plugin-friendly-errors/src/formatter/pretty.ts
+++ packages/builder/plugin-friendly-errors/src/formatter/pretty.ts
@@ -1,16 +1,24 @@
 import chalk from '@modern-js/utils/chalk';
+import type StackTracey from 'stacktracey';
 import { ErrorFormatter } from '../shared/types';
+
+const formatTraceEntry = (entry: StackTracey.Entry) => {
+  const { file, line, column } = entry;
+  const prompt = chalk.gray('at');
+  const filename = file;
+  const pos = chalk.gray(`:${line}:${column}`);
+  return `    ${prompt} ${filename}${pos}`;
+};
 
 export const prettyFormatter: ErrorFormatter = e => {
   const message = e.message || e;
   const name = e.name || 'Error';
 
-  const formattedStack = e.trace
-    .map(frame => {
-      const { file, line, column } = frame;
-      return `  at ${chalk.gray(file)}:${line}:${column}`;
-    })
-    .join('\n');
+  const errorSign = chalk.bgRed.bold(' ERROR ');
+  const errorName = chalk.red.bold(name);
+  const connector = chalk.gray(':');
+  const title = `${errorSign} ${errorName}${connector} ${message}`;
+  const formattedStack = e.trace.map(formatTraceEntry).join('\n');
 
-  return `${chalk.red(`${name}: ${message}`)}\n\n${formattedStack}`;
+  return `${title}\n${formattedStack}`;
 };
