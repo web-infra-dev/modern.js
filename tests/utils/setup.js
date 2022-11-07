b015dc7d8bc219e74c318764c75528a8ea06c03f
Belinda Cao <caohuilin@bytedance.com>
2022-11-07 10:34:00 +0800

    fix: puppeteer params (#2027)

--- tests/utils/setup.js
+++ tests/utils/setup.js
@@ -7,7 +7,7 @@ const puppeteer = require('puppeteer');
 const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup');
 
 module.exports = async function () {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
   // store the browser instance so we can teardown it later
   // this global is only available in the teardown but not in TestEnvironments
   global.__BROWSER_GLOBAL__ = browser;
