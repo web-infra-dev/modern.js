11d9dbbe36651f1f8f64c4b0bbb5fa1cb83905f5
fengyu <1114550440@qq.com>
2022-11-08 13:24:04 +0800

    chore: upgrade swc-plugins, update tests

--- packages/builder/plugin-swc/tests/fixtures/compat/cjs-no-preserve-dyn-import/expected.js
+++ packages/builder/plugin-swc/tests/fixtures/compat/cjs-no-preserve-dyn-import/expected.js
@@ -3,8 +3,13 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 var _interopRequireWildcard = require("@swc/helpers/lib/_interop_require_wildcard.js").default;
+<<<<<<< HEAD
 require("core-js/modules/es.object.to-string.js");
 require("core-js/modules/es.promise.js");
+=======
+require("core-js/modules/es.promise.js");
+require("core-js/modules/es.object.to-string.js");
+>>>>>>> e9b458eb6 (chore: upgrade swc-plugins, update tests)
 var a = require("foo");
 console.log(a);
 Promise.resolve().then(function() {
--- packages/builder/plugin-swc/tests/fixtures/compat/cjs-preserve-dyn-import/expected.js
+++ packages/builder/plugin-swc/tests/fixtures/compat/cjs-preserve-dyn-import/expected.js
@@ -2,8 +2,13 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
+<<<<<<< HEAD
 require("core-js/modules/es.object.to-string.js");
 require("core-js/modules/es.promise.js");
+=======
+require("core-js/modules/es.promise.js");
+require("core-js/modules/es.object.to-string.js");
+>>>>>>> e9b458eb6 (chore: upgrade swc-plugins, update tests)
 var a = require("foo");
 console.log(a);
 import("other");
