a9b38ea196c6dcc27b2571574cf85477048bf94a
Fy <1114550440@qq.com>
2022-11-11 16:15:06 +0800

    fix(builder-plugin-swc): fix minify plugin includes css (#2082)
    * chore: upgrade swc-plugins, update tests
    
    * fix: fix missing css handle, update minify plugin

---
+++ .changeset/fast-files-relax.md
@@ -0,0 +1,7 @@
+---
+'@modern-js/builder-plugin-swc': patch
+---
+
+fix(builder-plugin-swc): Fix minify plugin includes css.
+
+fix(builder-plugin-swc): 修复 minify 插件误将css文件加入minify
--- packages/builder/plugin-swc/tests/fixtures/compat/cjs-preserve-dyn-import/expected.js
+++ packages/builder/plugin-swc/tests/fixtures/compat/cjs-preserve-dyn-import/expected.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-require("core-js/modules/es.object.to-string.js");
 require("core-js/modules/es.promise.js");
+require("core-js/modules/es.object.to-string.js");
 var a = require("foo");
 console.log(a);
 import("other");
