125540fd5ad796a8143b012ed2a9d4dc2ab849da
Asuka109 <chenwenjie.asuka@bytedance.com>
2022-11-17 20:30:50 +0800

    feat(builder): export `webpackBuild` from main entry

--- packages/builder/builder-webpack-provider/src/exports/webpack.ts
+++ packages/builder/builder-webpack-provider/src/exports/webpack.ts
@@ -1,6 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-restricted-imports
 import webpack from 'webpack';
 
-export type { BuildExecuter, WebpackBuildError } from '../core/build';
-export { webpackBuild } from '../core/build';
 export default webpack;
