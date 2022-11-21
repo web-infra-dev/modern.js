c1a75de0555af3d7187f12a7c7451e7f054bbecb
Asuka109 <chenwenjie.asuka@bytedance.com>
2022-11-21 13:10:10 +0800

    build(friendly-errors): enable to use snapshot with integration tests

--- packages/builder/plugin-friendly-errors/tests/integrations/ChunkRenderError.test.ts
+++ packages/builder/plugin-friendly-errors/tests/integrations/ChunkRenderError.test.ts
@@ -1,7 +1,7 @@
 import assert from 'assert';
 import { createStubBuilder } from '@modern-js/builder-webpack-provider/stub';
 import { useFixture } from '@modern-js/e2e';
-import { test } from 'vitest';
+import { expect, test } from 'vitest';
 import { Chunk } from 'webpack';
 import ChunkRenderError from 'webpack/lib/ChunkRenderError';
 import { baseFormatter, prettyFormatter } from '@/formatter';
@@ -23,6 +23,39 @@ test('ChunkRenderError', async () => {
     new Error('foo'),
   );
   const parsed = parseError(error);
-  console.log(baseFormatter(parsed));
-  console.log(prettyFormatter(flattenErrorCauses(parsed)!));
+  expect(baseFormatter(parsed)).toMatchInlineSnapshot(`
+    "/chunkrendererror foo
+        at <ROOT>/tests/integrations/ChunkRenderError.test.ts:<POS>
+        at async runTest (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)
+        at async runSuite (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)
+        at async runFiles (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)
+        at async startTestsNode (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)
+        at async <WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/entry.mjs:<POS>
+        at async Module.withEnv (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)
+        at async run (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/entry.mjs:<POS>)
+        at async file:<WORKSPACE>/node_modules/<PNPM_INNER>/tinypool/dist/esm/worker.js:<POS>
+    Error: foo
+        at <ROOT>/tests/integrations/ChunkRenderError.test.ts:<POS>
+        at async runTest (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)
+        at async runSuite (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)
+        at async runFiles (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)
+        at async startTestsNode (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)
+        at async <WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/entry.mjs:<POS>
+        at async Module.withEnv (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)
+        at async run (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/entry.mjs:<POS>)
+        at async file:<WORKSPACE>/node_modules/<PNPM_INNER>/tinypool/dist/esm/worker.js:99:20"
+  `);
+  expect(prettyFormatter(flattenErrorCauses(parsed)!)).toMatchInlineSnapshot(`
+    "[41m[1m ERROR [22m[49m [31m[1mChunkRenderError[22m[39m[90m:[39m foo
+        [90mat[39m [90m<ROOT>/tests/integrations/ChunkRenderError.test.ts:<POS>[39m
+        [90mat[39m [90m<ROOT>/tests/integrations/ChunkRenderError.test.ts:<POS>[39m
+        [90mat[39m async runTest [90m(<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)[39m
+        [90mat[39m async runSuite [90m(<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)[39m
+        [90mat[39m async runFiles [90m(<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)[39m
+        [90mat[39m async startTestsNode [90m(<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)[39m
+        [90mat[39m [90masync <WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/entry.mjs:<POS>[39m
+        [90mat[39m async Module.withEnv [90m(<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)[39m
+        [90mat[39m async run [90m(<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/entry.mjs:<POS>)[39m
+        [90mat[39m [90masync file:<WORKSPACE>/node_modules/<PNPM_INNER>/tinypool/dist/esm/worker.js:<POS>[39m"
+  `);
 });
--- packages/builder/plugin-friendly-errors/tests/integrations/ModuleParseError.test.ts
+++ packages/builder/plugin-friendly-errors/tests/integrations/ModuleParseError.test.ts
@@ -2,7 +2,7 @@ import assert from 'assert';
 import ModuleParseError from 'webpack/lib/ModuleParseError';
 import { createStubBuilder } from '@modern-js/builder-webpack-provider/stub';
 import { useFixture } from '@modern-js/e2e';
-import { test } from 'vitest';
+import { expect, test } from 'vitest';
 import { baseFormatter, prettyFormatter } from '@/formatter';
 import { parseError } from '@/shared/utils';
 import { transformModuleParseError } from '@/transformer';
@@ -27,7 +27,61 @@ test('MissingLoader', async () => {
     'javascript/esm',
   );
   const parsed = parseError(error);
-  console.log(baseFormatter(parsed));
+  expect(baseFormatter(parsed)).toMatchInlineSnapshot(`
+    "/moduleparseerror Module parse failed: foo
+    File was processed with these loaders:
+     * bar-loader
+     * foo-loader
+    You may need an additional loader to handle the result of these loaders.
+    Error: foo
+        at <ROOT>/tests/integrations/ModuleParseError.test.ts:<POS>
+        at async runTest (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)
+        at async runSuite (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)
+        at async runFiles (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)
+        at async startTestsNode (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)
+        at async <WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/entry.mjs:<POS>
+        at async Module.withEnv (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)
+        at async run (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/entry.mjs:<POS>)
+        at async file:<WORKSPACE>/node_modules/<PNPM_INNER>/tinypool/dist/esm/worker.js:<POS>
+        at <ROOT>/tests/integrations/ModuleParseError.test.ts:<POS>
+        at async runTest (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)
+        at async runSuite (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)
+        at async runFiles (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)
+        at async startTestsNode (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)
+        at async <WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/entry.mjs:<POS>
+        at async Module.withEnv (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)
+        at async run (<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/entry.mjs:<POS>)
+        at async file:<WORKSPACE>/node_modules/<PNPM_INNER>/tinypool/dist/esm/worker.js:99:20"
+  `);
   transformModuleParseError(parsed);
-  console.log(prettyFormatter(parsed));
+  expect(prettyFormatter(parsed)).toMatchInlineSnapshot(`
+    "[41m[1m ERROR [22m[49m [31m[1mModuleParseError[22m[39m[90m:[39m Module parse failed: foo
+    File was processed with these loaders:
+     * bar-loader
+     * foo-loader
+    You may need an additional loader to handle the result of these loaders. You can try to fix it by:
+     [90m*[39m Check if the file is valid.
+     [90m*[39m Enable relational config of \`tools\`: https:/modernjs.dev/builder/en/api/config-tools.html
+     [90m*[39m Install builder plugins: https:/modernjs.dev/builder/en/plugin
+    Or you can try to configure bundler loaders manually.
+        [90mat[39m [90m<ROOT>/tests/integrations/ModuleParseError.test.ts:<POS>[39m
+        [90mat[39m async runTest [90m(<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)[39m
+        [90mat[39m async runSuite [90m(<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)[39m
+        [90mat[39m async runFiles [90m(<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)[39m
+        [90mat[39m async startTestsNode [90m(<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)[39m
+        [90mat[39m [90masync <WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/entry.mjs:<POS>[39m
+        [90mat[39m async Module.withEnv [90m(<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)[39m
+        [90mat[39m async run [90m(<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/entry.mjs:<POS>)[39m
+        [90mat[39m [90masync file:<WORKSPACE>/node_modules/<PNPM_INNER>/tinypool/dist/esm/worker.js:<POS>[39m
+    [41m[1m CAUSE [22m[49m [31m[1mError[22m[39m[90m:[39m foo
+        [90mat[39m [90m<ROOT>/tests/integrations/ModuleParseError.test.ts:<POS>[39m
+        [90mat[39m async runTest [90m(<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)[39m
+        [90mat[39m async runSuite [90m(<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)[39m
+        [90mat[39m async runFiles [90m(<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)[39m
+        [90mat[39m async startTestsNode [90m(<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)[39m
+        [90mat[39m [90masync <WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/entry.mjs:<POS>[39m
+        [90mat[39m async Module.withEnv [90m(<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/chunk-runtime-error.87a2b5a2.mjs:<POS>)[39m
+        [90mat[39m async run [90m(<WORKSPACE>/node_modules/<PNPM_INNER>/vitest/dist/entry.mjs:<POS>)[39m
+        [90mat[39m [90masync file:<WORKSPACE>/node_modules/<PNPM_INNER>/tinypool/dist/esm/worker.js:<POS>[39m"
+  `);
 });
