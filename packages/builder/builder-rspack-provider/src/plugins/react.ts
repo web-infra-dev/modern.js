b5106577c6f0f33ad798fccea27655e1e02ab763
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2022-11-21 20:36:09 +0800

    fix: hmr

--- packages/builder/builder-rspack-provider/src/plugins/react.ts
+++ packages/builder/builder-rspack-provider/src/plugins/react.ts
@@ -5,11 +5,19 @@ export const PluginReact = (): BuilderPlugin => ({
   name: 'builder-plugin-react',
 
   setup(api) {
-    api.modifyRspackConfig(async rspackConfig => {
-      // todo
+    api.modifyRspackConfig(async (rspackConfig, { isProd, target }) => {
+      const config = api.getNormalizedConfig();
+
+      const isUsingHMR =
+        !isProd &&
+        target !== 'node' &&
+        // target !== 'web-worker' &&
+        config.dev.hmr;
+
+      // todo: no type check
       _.set(rspackConfig, 'builtins.react', {
-        development: true,
-        refresh: false,
+        development: !isProd,
+        refresh: isUsingHMR,
       });
     });
   },
