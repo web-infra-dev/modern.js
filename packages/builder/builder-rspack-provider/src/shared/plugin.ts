6cc5398127a50aad899f139afa218d76f68ce64f
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2022-11-22 19:32:05 +0800

    fix: add plugin-target

--- packages/builder/builder-rspack-provider/src/shared/plugin.ts
+++ packages/builder/builder-rspack-provider/src/shared/plugin.ts
@@ -4,6 +4,7 @@ import { awaitableGetter } from '@modern-js/builder-shared';
 export const applyMinimalPlugins = () =>
   awaitableGetter<BuilderPlugin>([
     import('../plugins/entry').then(m => m.PluginEntry()),
+    import('../plugins/target').then(m => m.PluginTarget()),
     import('../plugins/define').then(m => m.PluginDefine()),
   ]);
 
