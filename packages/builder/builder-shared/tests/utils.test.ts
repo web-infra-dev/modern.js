1e70a6bc17b5e730208bc376819e735064d6e17a
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2022-11-23 10:29:45 +0800

    fix: awaitableGetter

--- packages/builder/builder-shared/tests/utils.test.ts
+++ packages/builder/builder-shared/tests/utils.test.ts
@@ -3,10 +3,7 @@ import { awaitableGetter } from '../src';
 
 describe('awaitableGetter', () => {
   it('should work', async () => {
-    const arr = [
-      new Promise(resolve => resolve('123')),
-      new Promise(resolve => resolve('456')),
-    ];
+    const arr = ['123', '456'];
     const promises = arr.map(item => Promise.resolve(item));
     const wrapped = awaitableGetter(promises);
     expect(wrapped.promises).toStrictEqual(promises);
