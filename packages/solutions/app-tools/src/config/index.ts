fa2e44716044a3b64b0e907bf18fc08cdd9c5859
GiveMe-A-Name <a455300764b@hotmail.com>
2022-11-24 16:27:04 +0800

    fix: update pnpm-lock.yaml

--- packages/solutions/app-tools/src/config/index.ts
+++ packages/solutions/app-tools/src/config/index.ts
@@ -25,7 +25,6 @@ export function createDefaultConfig(
     configDir: './config',
     globalVars: undefined,
     moduleScopes: undefined,
-    apiDir: './api',
     include: [],
     alias: defaultAlias,
   };
