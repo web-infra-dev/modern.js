2b110039abd476d42b096364704c237d00a2b1db
Asuka109 <chenwenjie.asuka@bytedance.com>
2022-11-25 18:09:12 +0800

    feat(image-compress): organize dependencies

--- packages/builder/plugin-image-compress/tests/index.test.ts
+++ packages/builder/plugin-image-compress/tests/index.test.ts
@@ -1,11 +1,11 @@
 import { it, expect, describe } from 'vitest';
-import { createStubBuilder } from '@modern-js/webpack-builder/stub';
-import { PluginImage } from '@modern-js/webpack-builder/plugins/image';
+import { createStubBuilder } from '@modern-js/builder-webpack-provider/stub';
+import { PluginImage } from '@modern-js/builder-webpack-provider/plugins/image';
 import { PluginImageCompress } from '../src';
 
 describe('plugin/image-compress', () => {
   it('should generate correct options', async () => {
-    const builder = createStubBuilder({
+    const builder = await createStubBuilder({
       plugins: [PluginImage(), PluginImageCompress()],
     });
     expect(await builder.unwrapWebpackConfig()).toMatchSnapshot();
