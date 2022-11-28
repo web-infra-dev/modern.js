083a4057078ce054b0f5dac8ea4ed0e77d31634b
chenjiahan <chenjiahan.jait@bytedance.com>
2022-11-28 15:27:07 +0800

    fix(builder): invalidate cache when buildDependencies changed

--- packages/builder/builder-webpack-provider/src/plugins/cache.ts
+++ packages/builder/builder-webpack-provider/src/plugins/cache.ts
@@ -1,23 +1,66 @@
-import { join } from 'path';
-import { isFileExists } from '@modern-js/builder-shared';
+import { isAbsolute, join } from 'path';
+import {
+  BuildCacheOptions,
+  BuilderContext,
+  isFileExists,
+} from '@modern-js/builder-shared';
 import type { BuilderPlugin } from '../types';
+
+async function validateCache(
+  cacheDirectory: string,
+  buildDependencies: Record<string, string[]>,
+) {
+  const { fs } = await import('@modern-js/utils');
+  const configFile = join(cacheDirectory, 'buildDependencies.json');
+
+  if (await isFileExists(configFile)) {
+    const prevBuildDependencies = await fs.readJSON(configFile);
+
+    if (
+      JSON.stringify(prevBuildDependencies) ===
+      JSON.stringify(buildDependencies)
+    ) {
+      return;
+    }
+
+    /**
+     * If the filenames in the buildDependencies are changed, webpack will not invalidate the previous cache.
+     * So we need to remove the cache directory to make sure the cache is invalidated.
+     */
+    await fs.remove(cacheDirectory);
+  }
+
+  await fs.outputJSON(configFile, buildDependencies);
+}
+
+function getCacheDirectory(
+  { cacheDirectory }: BuildCacheOptions,
+  context: BuilderContext,
+) {
+  if (cacheDirectory) {
+    return isAbsolute(cacheDirectory)
+      ? cacheDirectory
+      : join(context.rootPath, cacheDirectory);
+  }
+  return join(context.cachePath, 'webpack');
+}
 
 export const PluginCache = (): BuilderPlugin => ({
   name: 'builder-plugin-cache',
 
   setup(api) {
     api.modifyWebpackChain(async (chain, { target, env }) => {
-      const buildCacheOption = api.getNormalizedConfig().performance.buildCache;
-      if (buildCacheOption === false) {
+      const { buildCache } = api.getNormalizedConfig().performance;
+
+      if (buildCache === false) {
         chain.cache(false);
         return;
       }
-      const buildCacheConfig =
-        typeof buildCacheOption === 'boolean' ? {} : buildCacheOption;
-      const { context } = api;
 
-      const cacheDirectory =
-        buildCacheConfig.cacheDirectory || join(context.cachePath, 'webpack');
+      const { context } = api;
+      const cacheConfig = typeof buildCache === 'boolean' ? {} : buildCache;
+
+      const cacheDirectory = getCacheDirectory(cacheConfig, context);
       const rootPackageJson = join(context.rootPath, 'package.json');
       const browserslistConfig = join(context.rootPath, '.browserslistrc');
 
@@ -37,6 +80,8 @@ export const PluginCache = (): BuilderPlugin => ({
       if (await isFileExists(browserslistConfig)) {
         buildDependencies.browserslistrc = [browserslistConfig];
       }
+
+      await validateCache(cacheDirectory, buildDependencies);
 
       chain.cache({
         // The default cache name of webpack is '${name}-${env}', and the `name` is `default` by default.
