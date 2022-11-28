26d61763628ca4856464913e81698235619f3598
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2022-11-28 18:09:18 +0800

    fix: add output plugin into applyDefaultPlugins

--- packages/builder/builder-rspack-provider/src/shared/plugin.ts
+++ packages/builder/builder-rspack-provider/src/shared/plugin.ts
@@ -5,7 +5,9 @@ export const applyMinimalPlugins = () =>
   awaitableGetter<BuilderPlugin>([
     import('../plugins/basic').then(m => m.PluginBasic()),
     import('../plugins/entry').then(m => m.PluginEntry()),
-    import('../plugins/target').then(m => m.PluginTarget()),
+    // todo: module import error when target is esX
+    // import('../plugins/target').then(m => m.PluginTarget()),
+    import('../plugins/output').then(m => m.PluginOutput()),
     import('../plugins/resolve').then(m => m.PluginResolve()),
     import('../plugins/define').then(m => m.PluginDefine()),
   ]);
