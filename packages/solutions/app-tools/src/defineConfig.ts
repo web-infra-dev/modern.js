14f698e5118d9beb41fd4abfa7eae7fcecf73309
GiveMe-A-Name <a455300764b@hotmail.com>
2022-11-29 16:47:34 +0800

    fix: speficy the type

--- packages/solutions/app-tools/src/defineConfig.ts
+++ packages/solutions/app-tools/src/defineConfig.ts
@@ -6,7 +6,9 @@ export const defineConfig = (config: AppUserConfig) => config;
  * @deprecated
  * Using defineConfig first.
  */
-export const defineLegacyConfig = (config: AppLegacyUserConfig) => ({
+export const defineLegacyConfig = (
+  config: AppLegacyUserConfig,
+): AppLegacyUserConfig => ({
   ...config,
   legacy: true,
 });
