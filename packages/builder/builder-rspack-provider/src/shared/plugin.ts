b9407a80356d0b87b07f3c5877857949e6e72539
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2022-11-29 19:21:19 +0800

    feat: add dev plugins

--- packages/builder/builder-rspack-provider/src/shared/plugin.ts
+++ packages/builder/builder-rspack-provider/src/shared/plugin.ts
@@ -8,6 +8,7 @@ export const applyMinimalPlugins = () =>
     // todo: module import error when target is esX
     // import('../plugins/target').then(m => m.PluginTarget()),
     import('../plugins/output').then(m => m.PluginOutput()),
+    import('../plugins/devtool').then(m => m.PluginDevtool()),
     import('../plugins/resolve').then(m => m.PluginResolve()),
     import('../plugins/define').then(m => m.PluginDefine()),
   ]);
@@ -15,5 +16,7 @@ export const applyMinimalPlugins = () =>
 export const applyDefaultPlugins = () =>
   awaitableGetter<BuilderPlugin>([
     ...applyMinimalPlugins().promises,
+    import('../plugins/hmr').then(m => m.PluginHMR()),
     import('../plugins/progress').then(m => m.PluginProgress()),
+    import('../plugins/react').then(m => m.PluginReact()),
   ]);
