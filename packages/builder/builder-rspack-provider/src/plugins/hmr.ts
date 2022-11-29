df01165341eaef8ea7645444601933a0db4c97d8
gaoyuan <gaoyuan.1226@bytedance.com>
2022-11-29 19:35:54 +0800

    feat(rspack-provider): add dev plugins (#2241)
    feat(rspack-provider): add rspack some dev plugins

---
+++ packages/builder/builder-rspack-provider/src/plugins/hmr.ts
@@ -0,0 +1,25 @@
+import type {
+  BuilderPlugin,
+  NormalizedConfig,
+  ModifyRspackConfigUtils,
+} from '../types';
+import { setConfig } from '@modern-js/builder-shared';
+
+export const isUsingHMR = (
+  config: NormalizedConfig,
+  { isProd, target }: ModifyRspackConfigUtils,
+) => !isProd && target !== 'node' && target !== 'web-worker' && config.dev.hmr;
+
+export const PluginHMR = (): BuilderPlugin => ({
+  name: 'builder-plugin-hmr',
+
+  setup(api) {
+    api.modifyRspackConfig((rspackConfig, utils) => {
+      const config = api.getNormalizedConfig();
+
+      const usingHMR = isUsingHMR(config, utils);
+
+      setConfig(rspackConfig, 'devServer.hot', usingHMR);
+    });
+  },
+});
---
+++ packages/builder/builder-rspack-provider/src/plugins/react.ts
@@ -0,0 +1,20 @@
+import type { BuilderPlugin } from '../types';
+import { setConfig } from '@modern-js/builder-shared';
+import { isUsingHMR } from './hmr';
+
+export const PluginReact = (): BuilderPlugin => ({
+  name: 'builder-plugin-react',
+
+  setup(api) {
+    api.modifyRspackConfig(async (rspackConfig, utils) => {
+      const config = api.getNormalizedConfig();
+
+      const usingHMR = isUsingHMR(config, utils);
+
+      setConfig(rspackConfig, 'builtins.react', {
+        development: !utils.isProd,
+        refresh: usingHMR,
+      });
+    });
+  },
+});
