a4723a36a6de83334a4c9e0981a53cc46980d387
Asuka109 <chenwenjie.asuka@bytedance.com>
2022-11-30 17:46:00 +0800

    test: setup vitest path snapshot serializer

--- packages/builder/plugin-image-compress/vitest.config.ts
+++ packages/builder/plugin-image-compress/vitest.config.ts
@@ -1,10 +1,18 @@
+import path from 'path';
 import { defineConfig } from 'vitest/config';
 import { withTestPreset } from '@scripts/vitest-config';
 
 const config = defineConfig({
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname),
+      '@': path.resolve(__dirname, 'src'),
+    }
+  },
   test: {
     root: __dirname,
     environment: 'node',
+    setupFiles: ['./tests/setup.ts'],
   },
 });
 
