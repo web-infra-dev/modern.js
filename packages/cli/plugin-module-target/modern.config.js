7147a80ddfd9c7245afb89381fbbf5fd25bf3bf3
targeral <targeral@outlook.com>
2022-12-01 17:27:43 +0800

    chore: modify modern.config for cli packages

--- packages/cli/plugin-module-target/modern.config.js
+++ packages/cli/plugin-module-target/modern.config.js
@@ -1,18 +1,9 @@
 module.exports = {
-  output: {
-    buildConfig: [
-      {
-        format: 'cjs',
-        bundleOptions: {
-          skipDeps: false,
-        },
-        target: 'es6',
-        sourceMap: true,
-      },
-      {
-        enableDts: true,
-        dtsOnly: true,
-      },
-    ],
-  },
+  buildConfig: [
+    {
+      format: 'cjs',
+      target: 'es6',
+      sourceMap: true,
+    },
+  ],
 };
