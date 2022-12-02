3e8260d3865580345899263f7a2392b99c6e8851
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2022-12-02 15:55:41 +0800

    feat: support tools.rspack

--- packages/builder/builder-rspack-provider/src/core/rspackConfig.ts
+++ packages/builder/builder-rspack-provider/src/core/rspackConfig.ts
@@ -3,6 +3,7 @@ import {
   type NodeEnv,
   type BuilderTarget,
 } from '@modern-js/builder-shared';
+import { castArray } from '@modern-js/utils/lodash';
 import { getCompiledPath } from '../shared';
 import type { Context, RspackConfig, ModifyRspackConfigUtils } from '../types';
 
@@ -12,16 +13,73 @@ async function modifyRspackConfig(
   utils: ModifyRspackConfigUtils,
 ) {
   debug('modify rspack config');
-  const [modifiedConfig] = await context.hooks.modifyRspackConfigHook.call(
+  let [modifiedConfig] = await context.hooks.modifyRspackConfigHook.call(
     rspackConfig,
     utils,
   );
+
+  if (context.config.tools?.rspack) {
+    const { applyOptionsChain } = await import('@modern-js/utils');
+    const { merge } = await import('../../compiled/webpack-merge');
+
+    modifiedConfig = applyOptionsChain(
+      modifiedConfig,
+      context.config.tools.rspack,
+      utils,
+      merge,
+    );
+  }
 
   debug('modify rspack config done');
   return modifiedConfig;
 }
 
-function getConfigUtils(target: BuilderTarget): ModifyRspackConfigUtils {
+function getRspackConfigUtils(
+  config: RspackConfig,
+): Pick<
+  ModifyRspackConfigUtils,
+  'addRules' | 'prependPlugins' | 'appendPlugins' | 'removePlugin'
+> {
+  return {
+    addRules(rules) {
+      const ruleArr = castArray(rules);
+      if (!config.module) {
+        config.module = {};
+      }
+      if (!config.module.rules) {
+        config.module.rules = [];
+      }
+      config.module.rules.unshift(...ruleArr);
+    },
+
+    prependPlugins(plugins) {
+      const pluginArr = castArray(plugins);
+      if (!config.plugins) {
+        config.plugins = [];
+      }
+      config.plugins.unshift(...pluginArr);
+    },
+
+    appendPlugins(plugins) {
+      const pluginArr = castArray(plugins);
+      if (!config.plugins) {
+        config.plugins = [];
+      }
+      config.plugins.push(...pluginArr);
+    },
+
+    removePlugin(pluginName) {
+      if (config.plugins) {
+        config.plugins = config.plugins.filter(p => p.name !== pluginName);
+      }
+    },
+  };
+}
+
+function getConfigUtils(
+  target: BuilderTarget,
+  config: RspackConfig,
+): ModifyRspackConfigUtils {
   const nodeEnv = process.env.NODE_ENV as NodeEnv;
 
   return {
@@ -31,6 +89,7 @@ function getConfigUtils(target: BuilderTarget): ModifyRspackConfigUtils {
     isServer: target === 'node',
     isWebWorker: target === 'web-worker',
     getCompiledPath,
+    ...getRspackConfigUtils(config),
   };
 }
 
@@ -41,10 +100,12 @@ export async function generateRspackConfig({
   target: BuilderTarget;
   context: Context;
 }) {
-  const utils = getConfigUtils(target);
+  let rspackConfig = {};
+
+  const utils = getConfigUtils(target, rspackConfig);
 
   /** not set rspack default config here, the default value is configured in the corresponding plugin */
-  const rspackConfig = await modifyRspackConfig(context, {}, utils);
+  rspackConfig = await modifyRspackConfig(context, rspackConfig, utils);
 
   return rspackConfig;
 }
