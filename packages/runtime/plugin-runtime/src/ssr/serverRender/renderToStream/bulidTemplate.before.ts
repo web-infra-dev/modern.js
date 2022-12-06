84e0f7213b37a72e5562caaeb6dcf24f8a4475e1
Ming <wangyiming.fe@bytedance.com>
2022-12-06 17:27:35 +0800

    fix: root layout css chunks should't be loaded

--- packages/runtime/plugin-runtime/src/ssr/serverRender/renderToStream/bulidTemplate.before.ts
+++ packages/runtime/plugin-runtime/src/ssr/serverRender/renderToStream/bulidTemplate.before.ts
@@ -9,6 +9,7 @@ import {
 } from './buildTemplate.share';
 
 const CSS_CHUNKS_PLACEHOLDER = '<!--<?- chunksMap.css ?>-->';
+const ROUTE_LAYOUT = 'layout';
 
 // build head template
 function getHeadTemplate(beforeEntryTemplate: string, context: RuntimeContext) {
@@ -45,7 +46,7 @@ function getHeadTemplate(beforeEntryTemplate: string, context: RuntimeContext) {
       matches?.forEach(match => {
         const routeId = match.route.id;
 
-        if (routeId) {
+        if (routeId && routeId !== ROUTE_LAYOUT) {
           const { assets = [] } = routeAssets[routeId];
           const _cssChunks = assets.filter((asset?: string) =>
             asset?.endsWith('.css'),
