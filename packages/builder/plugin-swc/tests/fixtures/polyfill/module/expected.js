7b7d12cf8f5fc1de0486e335c873d10a0b444320
Fy <1114550440@qq.com>
2022-12-13 10:22:44 +0800

    fix(builder-plugin-swc): correct corejs version (#2358)
    * fix(builder-plugin-swc): correct corejs version
    
    * fix(builder-plugin-swc): correct corejs version

--- packages/builder/plugin-swc/tests/fixtures/polyfill/module/expected.js
+++
@@ -1,7 +0,0 @@
-import _object_spread from "@swc/helpers/src/_object_spread.mjs";
-import p from "path";
-if (Math.random() === 1.2) {
-    console.log(_object_spread({}, require("foo")), p);
-} else {
-    require("bar");
-}
