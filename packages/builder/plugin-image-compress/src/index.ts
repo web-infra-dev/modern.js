179b86fc66ea4f660423cf0e8af728f2901cab45
Asuka109 <chenwenjie.asuka@bytedance.com>
2022-12-14 17:43:26 +0800

    feat: support svgo to compress svg files

--- packages/builder/plugin-image-compress/src/index.ts
+++ packages/builder/plugin-image-compress/src/index.ts
@@ -2,12 +2,48 @@ import _ from '@modern-js/utils/lodash';
 import type { BuilderPlugin } from '@modern-js/builder-webpack-provider/types';
 import path from 'path';
 import { withDefaultOptions } from './shared/utils';
-import { Codecs, Options } from './types';
+import { Codecs, FinalOptions, Options } from './types';
+import { CHAIN_ID } from '@modern-js/utils';
+import ChainedConfig from '@modern-js/builder-webpack-provider/compiled/webpack-5-chain';
 
-export const loaderPath = path.resolve(__dirname, 'loader.js');
+export const LOADER_PATH = path.resolve(__dirname, 'loader.js');
 export const DEFAULT_OPTIONS: Codecs[] = ['jpeg', 'png', 'ico'];
 
 export type PluginImageCompressOptions = Options[];
+
+export const applyImageLoaderChain = (
+  chain: ChainedConfig,
+  opt: FinalOptions,
+) => {
+  chain.module
+    .rule(CHAIN_ID.RULE.IMAGE)
+    .oneOf(opt.use)
+    .test(opt.test)
+    .use(`${CHAIN_ID.USE.IMAGE_COMPRESS}-${opt.use}`)
+    .loader(path.resolve(__dirname, './loader'))
+    .options(opt);
+};
+
+export const CHAIN_ONE_OF_SVG = [
+  CHAIN_ID.ONE_OF.SVG,
+  CHAIN_ID.ONE_OF.SVG_ASSET,
+  CHAIN_ID.ONE_OF.SVG_INLINE,
+  CHAIN_ID.ONE_OF.SVG_URL,
+] as const;
+
+export const applySvgLoaderChain = (
+  chain: ChainedConfig,
+  opt: FinalOptions,
+) => {
+  const svgRule = chain.module.rule(CHAIN_ID.RULE.SVG);
+  for (const ONE_OF of CHAIN_ONE_OF_SVG) {
+    svgRule
+      .oneOf(ONE_OF)
+      .use(CHAIN_ID.USE.SVGO)
+      .loader(require.resolve('svgo-loader'))
+      .options(opt);
+  }
+};
 
 /** Options enable by default: {@link DEFAULT_OPTIONS} */
 export const PluginImageCompress = (...options: Options[]): BuilderPlugin => ({
@@ -15,18 +51,16 @@ export const PluginImageCompress = (...options: Options[]): BuilderPlugin => ({
   setup(api) {
     const optsWithDefault = options.length ? options : DEFAULT_OPTIONS;
     const opts = optsWithDefault.map(opt => withDefaultOptions(opt));
-    api.modifyWebpackChain((chain, { CHAIN_ID, env }) => {
+    api.modifyWebpackChain((chain, { env }) => {
       if (env !== 'production') {
         return;
       }
-      const rule = chain.module.rule(CHAIN_ID.RULE.IMAGE);
       _.each(opts, opt => {
-        rule
-          .oneOf(opt.use)
-          .test(opt.test)
-          .use(`${CHAIN_ID.USE.IMAGE_COMPRESS}-${opt.use}`)
-          .loader(path.resolve(__dirname, './loader'))
-          .options(opt);
+        if (opt.use === 'svg') {
+          applySvgLoaderChain(chain, opt);
+        } else {
+          applyImageLoaderChain(chain, opt);
+        }
       });
     });
   },
--- packages/builder/plugin-image-compress/src/shared/utils.ts
+++ packages/builder/plugin-image-compress/src/shared/utils.ts
@@ -2,6 +2,8 @@ import assert from 'assert';
 import _ from '@modern-js/utils/lodash';
 import { FinalOptions, Options } from '../types';
 import codecs from './codecs';
+import type { WebpackChain } from '@modern-js/utils';
+import type { ModifyWebpackChainUtils } from '@modern-js/builder-webpack-provider/types';
 
 export const withDefaultOptions = (opt: Options): FinalOptions => {
   const options = _.isString(opt) ? { use: opt } : opt;
@@ -9,4 +11,13 @@ export const withDefaultOptions = (opt: Options): FinalOptions => {
   const ret = { ...defaultOptions, ...options };
   assert('test' in ret);
   return ret;
+};
+
+export const applyChainSVGO = async (
+  chain: WebpackChain,
+  options: any,
+  { CHAIN_ID }: ModifyWebpackChainUtils,
+): Promise<void> => {
+  const rule = chain.module.rule(CHAIN_ID.RULE.SVG);
+  require('@svgr/webpack').default(rule, options);
 };
