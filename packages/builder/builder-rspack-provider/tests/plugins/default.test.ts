c34c1d5eb31bb9aa89f417a3a7372cb5e1ba434e
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2022-12-14 11:40:53 +0800

    fix: updata snapshot

--- packages/builder/builder-rspack-provider/tests/plugins/default.test.ts
+++ packages/builder/builder-rspack-provider/tests/plugins/default.test.ts
@@ -3,12 +3,16 @@ import { createBuilder } from '../helper';
 
 describe('applyDefaultPlugins', () => {
   it('should apply default plugins correctly', async () => {
+    const { NODE_ENV } = process.env;
+    process.env.NODE_ENV = 'development';
     const builder = await createBuilder({});
 
     const {
       origin: { bundlerConfigs },
     } = await builder.inspectConfig();
     expect(bundlerConfigs[0]).toMatchSnapshot();
+
+    process.env.NODE_ENV = NODE_ENV;
   });
 
   it('should apply default plugins correctly when prod', async () => {
@@ -28,6 +32,9 @@ describe('applyDefaultPlugins', () => {
 
 describe('tools.rspack', () => {
   it('should match snapshot', async () => {
+    const { NODE_ENV } = process.env;
+    process.env.NODE_ENV = 'development';
+
     class TestPlugin {
       readonly name: string = 'TestPlugin';
 
@@ -57,5 +64,7 @@ describe('tools.rspack', () => {
     } = await builder.inspectConfig();
 
     expect(bundlerConfigs[0]).toMatchSnapshot();
+
+    process.env.NODE_ENV = NODE_ENV;
   });
 });
