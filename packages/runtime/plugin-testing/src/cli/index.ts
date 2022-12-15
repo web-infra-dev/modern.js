ba5eedb5fb43ac8fddd9525dc2967200244a02f2
targeral <targeral@outlook.com>
2022-12-15 12:53:10 +0800

    fix: testing alias

--- packages/runtime/plugin-testing/src/cli/index.ts
+++ packages/runtime/plugin-testing/src/cli/index.ts
@@ -72,6 +72,9 @@ export default (): CliPlugin<{
           return {
             source: {
               alias: {
+                // The module-tools alias configuration is different and more specific than app-tools.
+                // So for the time being, the @ alias is configured here.
+                '@': appContext.appDirectory,
                 '@modern-js/runtime/testing': testingExportsUtils.getPath(),
               },
             },
