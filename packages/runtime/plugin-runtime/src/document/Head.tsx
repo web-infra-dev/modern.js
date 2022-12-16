81ba40b798e8d0b0616ff0952aa03613a68ad262
程成 <holynewbie@users.noreply.github.com>
2022-12-16 16:26:55 +0800

    feat: document.tsx add Links

--- packages/runtime/plugin-runtime/src/document/Head.tsx
+++ packages/runtime/plugin-runtime/src/document/Head.tsx
@@ -2,16 +2,18 @@
 import React, { useContext } from 'react';
 import { DocumentStructrueContext } from './DocumentStructrueContext';
 import { Scripts } from './Scripts';
+import { Links } from './Links';
 import { DOCUMENT_META_PLACEHOLDER } from './constants';
 
 export function Head(props: { children?: any }) {
-  const { hasSetScripts } = useContext(DocumentStructrueContext);
+  const { hasSetScripts, hasSetLinks } = useContext(DocumentStructrueContext);
   const { children } = props;
   // todo: verify the children
   return (
     <head>
       {/* configuration by config.output.meta */}
       {`${DOCUMENT_META_PLACEHOLDER}`}
+      {!hasSetLinks && <Links />}
       {/* Scripts must have as default. If not, place in Head */}
       {!hasSetScripts && <Scripts />}
       {children}
