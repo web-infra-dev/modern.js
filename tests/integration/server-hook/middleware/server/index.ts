471aa6bfc1904c427534d300f95d1207be8ed5bf
kongjiacong <kongjiacong@bytedance.com>
2022-12-18 17:07:13 +0800

    fix: rewrite server error catch logic

--- tests/integration/server-hook/middleware/server/index.ts
+++ tests/integration/server-hook/middleware/server/index.ts
@@ -1,7 +1,5 @@
 import { Middleware } from '@modern-js/runtime/server';
 
 export const middleware: Middleware = (ctx, next) => {
-  const { res } = ctx.source;
-  res.setHeader('x-index-middleware', 'true');
-  next();
+  throw new Error(1);
 };
