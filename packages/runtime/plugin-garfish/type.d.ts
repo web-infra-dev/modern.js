a032a19580065ce65c7cb462551e139ea3658276
Belinda Cao <caohuilin@bytedance.com>
2022-12-21 21:04:12 +0800

    fix: garfish types (#2486)

--- packages/runtime/plugin-garfish/type.d.ts
+++ packages/runtime/plugin-garfish/type.d.ts
@@ -1,6 +1,12 @@
 import '@modern-js/plugin-router-v5';
-
-declare module '@modern-js/runtime' {
+import { Config } from './dist/types';
+declare module '@modern-js/runtime/garfish' {
   export const useModuleApp: typeof import('./dist/types/runtime').useModuleApp;
   export const useModuleApps: typeof import('./dist/types/runtime').useModuleApps;
+}
+
+declare module '@modern-js/runtime' {
+  interface AppConfig {
+    masterApp?: Config;
+  }
 }
