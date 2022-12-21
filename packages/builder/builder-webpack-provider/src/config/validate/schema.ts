0c6246acc9d952efc9955166ad273844b06316b7
neverland <chenjiahan.jait@bytedance.com>
2022-12-21 22:39:51 +0800

    fix(Builder): remove some invalid schema (#2488)

--- packages/builder/builder-webpack-provider/src/config/validate/schema.ts
+++ packages/builder/builder-webpack-provider/src/config/validate/schema.ts
@@ -11,7 +11,6 @@ import {
 import type {
   DevConfig,
   HtmlConfig,
-  ToolsConfig,
   OutputConfig,
   SourceConfig,
   BuilderConfig,
@@ -20,16 +19,8 @@ import type {
   PerformanceConfig,
 } from '../../types';
 
-const sourceConfigSchema: ConfigSchema<SourceConfig> = {
-  type: 'object',
-  properties: {
-    ...sharedSourceConfigSchema.properties,
-    moduleScopes: {
-      instanceof: ['Array', 'Function'],
-    },
-  },
-};
-
+const sourceConfigSchema: ConfigSchema<Omit<SourceConfig, 'moduleScopes'>> =
+  sharedSourceConfigSchema;
 const devConfigSchema: ConfigSchema<DevConfig> = sharedDevConfigSchema;
 const htmlConfigSchema: ConfigSchema<HtmlConfig> = sharedHtmlConfigSchema;
 const securityConfigSchema: ConfigSchema<SecurityConfig> =
@@ -37,7 +28,7 @@ const securityConfigSchema: ConfigSchema<SecurityConfig> =
 const experimentsConfigSchema: ConfigSchema<ExperimentsConfig> =
   sharedExperimentsConfigSchema;
 
-const outputConfigSchema: ConfigSchema<OutputConfig> = {
+const outputConfigSchema: ConfigSchema<Omit<OutputConfig, 'externals'>> = {
   type: 'object',
   properties: {
     ...sharedOutputConfigSchema.properties,
@@ -46,9 +37,6 @@ const outputConfigSchema: ConfigSchema<OutputConfig> = {
     },
     convertToRem: {
       type: ['boolean', 'object'],
-    },
-    externals: {
-      instanceof: ['String', 'Function', 'Object', 'RegExp'],
     },
   },
 };
@@ -66,67 +54,9 @@ const performanceConfigSchema: ConfigSchema<PerformanceConfig> = {
   },
 };
 
-const toolsConfigSchema: ConfigSchema<ToolsConfig> = {
+const toolsConfigSchema: ConfigSchema<Record<string, unknown>> = {
   type: 'object',
-  properties: {
-    pug: {
-      typeof: ['boolean', 'object', 'function'],
-    },
-    sass: {
-      typeof: ['object', 'function'],
-    },
-    less: {
-      typeof: ['object', 'function'],
-    },
-    babel: {
-      typeof: ['object', 'function'],
-    },
-    terser: {
-      typeof: ['object', 'function'],
-    },
-    tsLoader: {
-      typeof: ['object', 'function'],
-    },
-    tsChecker: {
-      typeof: ['object', 'function'],
-    },
-    devServer: {
-      typeof: 'object',
-    },
-    minifyCss: {
-      typeof: ['object', 'function'],
-    },
-    htmlPlugin: {
-      typeof: ['boolean', 'object', 'function'],
-    },
-    styledComponents: {
-      typeof: ['object', 'function'],
-    },
-    cssLoader: {
-      typeof: ['object', 'function'],
-    },
-    styleLoader: {
-      typeof: ['object', 'function'],
-    },
-    cssExtract: {
-      typeof: 'object',
-    },
-    postcss: {
-      typeof: ['object', 'function'],
-    },
-    autoprefixer: {
-      typeof: ['object', 'function'],
-    },
-    webpack: {
-      typeof: ['object', 'function'],
-    },
-    webpackChain: {
-      typeof: 'function',
-    },
-    inspector: {
-      typeof: ['object', 'function'],
-    },
-  },
+  properties: {},
 };
 
 export const configSchema: ConfigSchema<BuilderConfig> = {
