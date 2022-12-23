a14e45323b47322b3e4b7e18fd92391ecb35c037
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2022-12-23 14:44:58 +0800

    fix: webpack entry plugin

--- packages/builder/builder-webpack-provider/src/stub/builder.ts
+++ packages/builder/builder-webpack-provider/src/stub/builder.ts
@@ -6,6 +6,11 @@ import {
   type CreateBuilderOptions,
   type PluginStore,
 } from '@modern-js/builder-shared';
+/**
+ * provider shouldn't dependency builder, so use /test-stub as placeholder
+ *
+ * real import by: /tests/setup.ts
+ */
 import { mockBuilderPlugins } from '@modern-js/builder-shared/test-stub';
 import type * as playwright from '@modern-js/e2e/playwright';
 import { getTemplatePath } from '@modern-js/utils';
