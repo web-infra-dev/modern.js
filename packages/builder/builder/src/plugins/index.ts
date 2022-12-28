f6a96cdc188ad12f9d526bd6121f22b15159f73e
gaoyuan <gaoyuan.1226@bytedance.com>
2022-12-28 10:28:35 +0800

    feat(builder): reuse cache plugin between webpack and rspack (#2510)
    * feat: add bundlerType in chainUtils
    
    * feat: reuse cache plugin
    
    * fix: move bundlerType from utils to context
    
    * fix: make BundlerType enumerable

--- packages/builder/builder/src/plugins/index.ts
+++ packages/builder/builder/src/plugins/index.ts
@@ -5,4 +5,5 @@ export const plugins = {
   devtool: () => import('./devtool').then(m => m.PluginDevtool()),
   target: () => import('./target').then(m => m.PluginTarget()),
   entry: () => import('./entry').then(m => m.PluginEntry()),
+  cache: () => import('./cache').then(m => m.PluginCache()),
 };
