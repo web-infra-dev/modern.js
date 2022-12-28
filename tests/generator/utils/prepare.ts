5b256a428c9a8462cfad86a70cbaa7f90b3ab202
caohuilin <caohuilin@bytedance.com>
2022-12-28 22:32:55 +0800

    fix: generator test

--- tests/generator/utils/prepare.ts
+++ tests/generator/utils/prepare.ts
@@ -19,6 +19,7 @@ async function addNewActionDevDependence(repoCwd: string) {
     '@modern-js/repo-generator': 'workspace:*',
     '@modern-js/tailwindcss-generator': 'workspace:*',
     '@modern-js/storybook-generator': 'workspace:*',
+    '@modern-js/ssg-generator': 'workspace:*',
   };
   await fs.writeFile(
     actionPath,
