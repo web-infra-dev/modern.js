226005d6a63cca3085a3611904e3b7070e8fa0e3
Zhou xiao <zhouxiao.shaw@bytedance.com>
2022-12-28 11:37:19 +0800

    chore(plugin-garfish): compatible react-router-v6 (#2484)

--- packages/runtime/plugin-garfish/tests/mapp.test.tsx
+++ packages/runtime/plugin-garfish/tests/mapp.test.tsx
@@ -1,35 +1,28 @@
 import React from 'react';
 import { act, render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { createApp } from '@modern-js/runtime';
 import fetchMock from 'jest-fetch-mock';
 import '@testing-library/jest-dom';
 // https://stackoverflow.com/questions/49034907/fetch-mock-does-not-mock-my-fetch
-import {
-  Link,
-  Route,
-  Switch,
-  useLocation,
-  MemoryRouter,
-} from '@modern-js/plugin-router-v5/runtime';
-import ModernGarfishPlugin from '../src/runtime';
+import { Link, MemoryRouter } from '@modern-js/plugin-router-v5/runtime';
+import ModernGarfishPlugin, { useModuleApp } from '../src/runtime';
 import { useModuleApps } from '../src';
 import {
   TABLE_LIST_ESCAPE_NODE,
   TABLE_LIST_HTML,
   TABLE_LIST_ROOT_NODE,
 } from './resource/table';
+import { DASHBOARD_HTML } from './resource/dashboard';
 import {
-  DASHBOARD_ESCAPE_NODE,
-  DASHBOARD_HTML,
-  DASHBOARD_ROOT_NODE,
-} from './resource/dashboard';
-import { USER_INFO_HTML } from './resource/userInfo';
+  USER_INFO_ESCAPE_NODE,
+  USER_INFO_HTML,
+  USER_INFO_ROOT_NODE,
+} from './resource/userInfo';
 
 global.React = React;
 global.fetch = fetchMock as any;
 
-const dashboardPath = 'http://garfish-mock.com/dashboard';
+
 const tableListPath = 'http://garfish-mock.com/table-list';
 const userInfoPath = 'http://garfish-mock.com/user-info';
 
@@ -69,41 +62,29 @@ describe('plugin-garfish', () => {
     fetchMock.doMock();
   });
 
-  // testing-library.com/docs/example-react-router/
-  test('useModuleApps hooks', async () => {
-    const dashBoardModuleInfo = {
-      name: 'Dashboard',
-      entry: dashboardPath,
-    };
-    const tableListModuleInfo = {
-      name: 'TableList',
-      entry: tableListPath,
-    };
+  test('legacyModule hooks MApp', async () => {
     const userInfo = {
-      name: 'UserInfo',
-      activeWhen: '/user-info',
+      name: 'UserInfo2',
+      activeWhen: '/legacy-user-info',
       entry: userInfoPath,
+    };
+    const tableList2 = {
+      name: 'tablist2',
+      activeWhen: '/legacy-table-list',
+      entry: tableListPath,
     };
 
     const microFrontendConfig = {
-      apps: [tableListModuleInfo, dashBoardModuleInfo, userInfo],
-      manifest: {
-        loadable: {
-          loading: () => {
-            return <div data-testid="loading-id">loading</div>;
-          },
-        },
+      apps: [userInfo, tableList2],
+      LoadingComponent() {
+        return <div data-testid="loading-id">loading</div>;
       },
     };
 
     const App = () => {
-      const HomeTitle = 'Micro home page';
-      const Home = () => <div data-testid="home-title">{HomeTitle}</div>;
-      const { Dashboard, TableList } = useModuleApps();
-      const LocationDisplay = () => {
-        const location = useLocation();
-        return <div data-testid="location-display">{location.pathname}</div>;
-      };
+      const { MApp } = useModuleApps();
+      const NMApp = useModuleApp();
+      expect(MApp).toBe(NMApp);
 
       return (
         <MemoryRouter>
@@ -112,24 +93,13 @@ describe('plugin-garfish', () => {
             <Link data-testid="dashboard-link" to="/dashboard">
               dashboard
             </Link>
-            <Link data-testid="table-list-link" to="/table-list">
+            <Link data-testid="table-list-link" to="/legacy-table-list">
               table-list
             </Link>
-            <Link data-testid="user-info-link" to="/user-info">
+            <Link data-testid="user-info-link" to="/legacy-user-info">
               user-info
             </Link>
-            <Switch>
-              <Route exact={true} path="/">
-                <Home />
-              </Route>
-              <Route path="/dashboard">
-                <Dashboard />
-              </Route>
-              <Route path="/table-list">
-                <TableList />
-              </Route>
-              <LocationDisplay />
-            </Switch>
+            <MApp />
           </div>
         </MemoryRouter>
       );
@@ -143,26 +113,22 @@ describe('plugin-garfish', () => {
       const res = render(<AppWrapper />);
       unmount = res.unmount;
     });
-
-    expect(screen.getByTestId('home-title')).toBeInTheDocument();
     await act(async () => {
-      userEvent.click(screen.getByTestId('dashboard-link'));
+      history.pushState(null, '', '/legacy-user-info');
     });
-    // expect(await screen.findByText('loading')).toBeInTheDocument();
     expect(
-      await screen.findByText(DASHBOARD_ROOT_NODE.text),
+      await screen.findByText(USER_INFO_ROOT_NODE.text),
     ).toBeInTheDocument();
     expect(
-      await screen.findByText(DASHBOARD_ESCAPE_NODE.text),
+      await screen.findByText(USER_INFO_ESCAPE_NODE.text),
     ).toBeInTheDocument();
 
     await act(async () => {
-      userEvent.click(screen.getByTestId('table-list-link'));
+      history.pushState(null, '', '/legacy-table-list');
     });
+    expect(screen.queryByText(USER_INFO_ROOT_NODE.text)).toBeNull();
+    expect(screen.queryByText(USER_INFO_ESCAPE_NODE.text)).toBeNull();
 
-    // expect(screen.queryByText(DASHBOARD_ROOT_NODE.text)).toBeNull();
-    // expect(screen.queryByText(DASHBOARD_ESCAPE_NODE.text)).toBeNull();
-    // expect(await screen.findByText('loading')).toBeInTheDocument();
     expect(
       await screen.findByText(TABLE_LIST_ROOT_NODE.text),
     ).toBeInTheDocument();
