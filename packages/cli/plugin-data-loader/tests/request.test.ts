49f4fd8bfd132d9032dfb5f5260edc16ceb5da86
wangyiming.fe <wangyiming.fe@bytedance.com>
2023-01-04 11:36:12 +0800

    feat: rename request param for loader

--- packages/cli/plugin-data-loader/tests/request.test.ts
+++ packages/cli/plugin-data-loader/tests/request.test.ts
@@ -3,6 +3,7 @@
  */
 import 'isomorphic-fetch';
 import { getRequestUrl } from '../src/cli/create-request';
+import { DIRECT_PARAM, LOADER_ID_PARAM } from '../src/common/constants';
 
 describe('getRequestUrl', () => {
   test('support params', () => {
@@ -13,9 +14,9 @@ describe('getRequestUrl', () => {
     };
     const url = getRequestUrl({ params, request, routeId });
     expect(url.href).toEqual(
-      `http://localhost:8080/three/1234/profile?_loader=${encodeURIComponent(
+      `http://localhost:8080/three/1234/profile?${LOADER_ID_PARAM}=${encodeURIComponent(
         'user/profile/layout',
-      )}`,
+      )}&${DIRECT_PARAM}=true`,
     );
   });
 });
