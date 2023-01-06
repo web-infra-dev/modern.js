3b6e29b7f574bb9b0cf97ea4640312f1d9f9a867
wangyiming.fe <wangyiming.fe@bytedance.com>
2023-01-06 13:21:02 +0800

    fix: static files should be handled correctly for data loader

--- packages/solutions/app-tools/jest.config.js
+++ packages/solutions/app-tools/jest.config.js
@@ -3,5 +3,6 @@ const sharedConfig = require('@scripts/jest-config');
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   ...sharedConfig,
+  testEnvironment: 'node',
   rootDir: __dirname,
 };
