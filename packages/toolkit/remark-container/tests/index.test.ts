880bec131be004fab0bf0ad7991c19560e757dd6
yangxingyuan <39261479+sanyuan0704@users.noreply.github.com>
2023-01-09 10:28:24 +0800

    fix: path error when change language (#2628)

--- packages/toolkit/remark-container/tests/index.test.ts
+++ packages/toolkit/remark-container/tests/index.test.ts
@@ -13,87 +13,107 @@ describe('remark-container', () => {
     .use(rehypeStringify);
   test('No newline', () => {
     const result = processor.processSync(`
-:::tip
-This is a tip
-:::
+  :::tip
+  This is a tip
+  :::
 
-12312
-`);
+  12312
+  `);
     expect(result.value).toMatchSnapshot();
   });
 
   test('With a new line after the start position of container', () => {
     const result = processor.processSync(`
-:::tip
+  :::tip
 
-This is a tip
-:::`);
+  This is a tip
+  :::`);
     expect(result.value).toMatchSnapshot();
   });
 
   test('With new line before the end position of container', () => {
     const result = processor.processSync(`
-:::tip
-This is a tip
+  :::tip
+  This is a tip
 
-:::`);
+  :::`);
     expect(result.value).toMatchSnapshot();
   });
 
   test('With new line in all case', () => {
     const result = processor.processSync(`
-:::tip
+  :::tip
 
-This is a tip
+  This is a tip
 
-:::`);
+  :::`);
     expect(result.value).toMatchSnapshot();
   });
 
   test('With code block in container', () => {
     const result = processor.processSync(`
-:::tip
+  :::tip
 
-This is a tip with \`code\` and [link](xxx) some text
+  This is a tip with \`code\` and [link](xxx) some text
 
-\`\`\`js
-const a = 1;
-\`\`\`
+  \`\`\`js
+  const a = 1;
+  \`\`\`
 
-:::`);
+  :::`);
     expect(result.value).toMatchSnapshot();
   });
 
   test('With block quote in container', () => {
     const result = processor.processSync(`
-:::tip
+  :::tip
 
-This is a tip with \`code\` and [link](xxx) some text
+  This is a tip with \`code\` and [link](xxx) some text
 
-> This is a quote
+  > This is a quote
 
-:::`);
+  :::`);
     expect(result.value).toMatchSnapshot();
   });
 
   test('With link, inlineCode, img, list in container', () => {
     const result = processor.processSync(`
-:::tip
+  :::tip
 
-This is a tip with \`code\` and [link](xxx) some text
+  This is a tip with \`code\` and [link](xxx) some text
 
-- list 1
-- list 2
+  - list 1
+  - list 2
 
-![img](xxx)
+  ![img](xxx)
 
-:::`);
+  :::`);
     expect(result.value).toMatchSnapshot();
   });
 
   test('Has space before type', () => {
     const result = processor.processSync(`
-::: tip
+  ::: tip
+
+  This is a tip.
+
+  :::`);
+    expect(result.value).toMatchSnapshot();
+  });
+
+  test('Use {title="xxx"} as title', () => {
+    const result = processor.processSync(`
+  ::: tip{title="Cutsom title"}
+
+  This is a tip.
+
+  :::`);
+    expect(result.value).toMatchSnapshot();
+  });
+
+  test("Use {title='xxx'} as title", () => {
+    const result = processor.processSync(`
+::: tip{title='Cutsom title'}
 
 This is a tip.
 
@@ -101,13 +121,21 @@ This is a tip.
     expect(result.value).toMatchSnapshot();
   });
 
-  test('Use {title="xxx"} as title', () => {
+  test('Use {title=xxx} as title', () => {
     const result = processor.processSync(`
-::: tip{title="Cutsom title"}
+::: tip{title=Cutsom title}
 
 This is a tip.
 
 :::`);
+    expect(result.value).toMatchSnapshot();
+  });
+
+  test('Has space before :::', () => {
+    const result = processor.processSync(`
+::: tip title
+sss
+  :::`);
     expect(result.value).toMatchSnapshot();
   });
 });
