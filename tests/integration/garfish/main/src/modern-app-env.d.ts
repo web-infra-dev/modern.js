a12b42ac134d94d1e89396a39e29fa7f5b7346b8
Zhou xiao <zhouxiao.shaw@bytedance.com>
2023-01-09 16:16:08 +0800

    fix: e2e test (#2632)

--- tests/integration/garfish/main/src/modern-app-env.d.ts
+++ tests/integration/garfish/main/src/modern-app-env.d.ts
@@ -4,3 +4,8 @@
 /// <reference types='@modern-js/plugin-express/types' />
 /// <reference types='@modern-js/plugin-koa/types' />
 /// <reference types='@modern-js/plugin-router-v5/types' />
+
+declare module 'dashboardApp/share-button' {
+  const Button = () => JSX.Element;
+  export default Button;
+}
