4eb78bd79dec8792c436d4f8330098626af8d3b4
yangxingyuan <39261479+sanyuan0704@users.noreply.github.com>
2023-01-10 14:57:46 +0800

    fix: remark container plugin title (#2638)

--- packages/toolkit/remark-container/src/index.ts
+++ packages/toolkit/remark-container/src/index.ts
@@ -20,6 +20,7 @@ import {
   BlockContent,
   PhrasingContent,
   Paragraph,
+  Literal,
 } from 'mdast';
 
 export const DIRECTIVE_TYPES: string[] = [
@@ -32,7 +33,17 @@ export const DIRECTIVE_TYPES: string[] = [
 ];
 export const REGEX_BEGIN = /^\s*:::\s*(\w+)\s*(.*)?/;
 export const REGEX_END = /\s*:::$/;
-export const TITLE_REGEX = /{\s*title=["']?(.+?)['"]?}\s*/;
+export const TITLE_REGEX_IN_MD = /{\s*title=["']?(.+)}\s*/;
+export const TITLE_REGEX_IN_MDX = /\s*title=["']?(.+)\s*/;
+
+const trimTailingQuote = (str: string) => str.replace(/['"]$/g, '');
+
+const parseTitle = (rawTitle = '', isMDX = false) => {
+  const matched = rawTitle?.match(
+    isMDX ? TITLE_REGEX_IN_MDX : TITLE_REGEX_IN_MD,
+  );
+  return trimTailingQuote(matched?.[1] || rawTitle);
+};
 
 /**
  * Construct the DOM structure of the container directive.
@@ -93,140 +104,105 @@ const createContainer = (
  */
 function transformer(tree: Root) {
   let i = 0;
-  while (i < tree.children.length) {
-    const node = tree.children[i];
-    if (node.type !== 'paragraph') {
-      i++;
-      continue;
-    }
-    // 1. We get the paragraph and check if it is a container directive
-    if (node.children[0].type !== 'text') {
-      i++;
-      continue;
-    }
-    const firstTextNode = node.children[0];
-    const text = firstTextNode.value;
-    const metaText = text.split('\n')[0];
-    const content = text.slice(metaText.length);
-    const match = metaText.match(REGEX_BEGIN);
-    if (!match) {
-      i++;
-      continue;
-    }
-    const [, type, rawTitle] = match;
-    const title = rawTitle?.match(TITLE_REGEX)?.[1] || rawTitle;
-    if (!DIRECTIVE_TYPES.includes(type)) {
-      i++;
-      continue;
-    }
-    // 2. If it is, we remove the paragraph and create a container directive
-    const wrappedChildren: BlockContent[] = [];
-    // 2.1 case: with no newline between `:::` and `:::`, for example
-    // ::: tip
-    // This is a tip
-    // :::
-    // Here the content is `::: tip\nThis is a tip\n:::`
-    if (content?.endsWith(':::')) {
-      wrappedChildren.push({
-        type: 'paragraph',
-        children: [
-          {
-            type: 'text',
-            value: content.replace(REGEX_END, ''),
-          },
-        ],
-      });
-      const newChild = createContainer(type, title, wrappedChildren);
-      tree.children.splice(i, 1, newChild as Content);
-    } else {
-      // 2.2 case: with newline before the end of container, for example:
-      // ::: tip
-      // This is a tip
-      //
-      // :::
-      // Here the content is `::: tip\nThis is a tip`
-      const paragraphChild: Paragraph = {
-        type: 'paragraph',
-        children: [] as PhrasingContent[],
-      };
-      wrappedChildren.push(paragraphChild);
-      if (content.length) {
-        paragraphChild.children.push({
-          type: 'text',
-          value: content,
-        });
-      }
-      paragraphChild.children.push(...node.children.slice(1, -1));
-      // If the inserted paragraph is empty, we remove it
-      if (paragraphChild.children.length === 0) {
-        wrappedChildren.pop();
-      }
-      const lastChildInNode = node.children[node.children.length - 1];
-      // We find the end of the container directive in current paragraph
-      if (
-        lastChildInNode.type === 'text' &&
-        REGEX_END.test(lastChildInNode.value)
-      ) {
-        const lastChildInNodeText = lastChildInNode.value;
-        const matchedEndContent = lastChildInNodeText.slice(0, -3).trim();
-        if (matchedEndContent) {
-          (wrappedChildren[0] as Paragraph).children.push({
-            type: 'text',
-            value: matchedEndContent,
-          });
-        }
-        const newChild = createContainer(type, title, wrappedChildren);
-        tree.children.splice(i, 1, newChild as Content);
+  try {
+    while (i < tree.children.length) {
+      const node = tree.children[i];
+      if (node.type !== 'paragraph') {
         i++;
         continue;
-      } else if (lastChildInNode !== firstTextNode) {
-        // We don't find the end of the container directive in current paragraph
-        (wrappedChildren[0] as Paragraph).children.push(lastChildInNode);
       }
-      // 2.3 The final case: has newline after the start of container, for example:
+      // 1. We get the paragraph and check if it is a container directive
+      if (node.children[0].type !== 'text') {
+        i++;
+        continue;
+      }
+      const firstTextNode = node.children[0];
+      const text = firstTextNode.value;
+      const metaText = text.split('\n')[0];
+      const content = text.slice(metaText.length);
+      const match = metaText.match(REGEX_BEGIN);
+      if (!match) {
+        i++;
+        continue;
+      }
+      const [, type, rawTitle] = match;
+      // In .md, we can get :::tip{title="xxx"} in the first text node
+      // In .mdx, we get :::tip in first node and {title="xxx"} in second node
+      let title = parseTitle(rawTitle);
+      // :::tip{title="xxx"}
+      const titleExpressionNode =
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error mdxTextExpression is not defined in mdast
+        node.children[1] && node.children[1].type === 'mdxTextExpression'
+          ? node.children[1]
+          : null;
+      // Handle the case of `::: tip {title="xxx"}`
+      if (titleExpressionNode) {
+        title = parseTitle((titleExpressionNode as Literal).value, true);
+        // {title="xxx"} is not a part of the content, So we need to remove it
+        node.children.splice(1, 1);
+      }
+      if (!DIRECTIVE_TYPES.includes(type)) {
+        i++;
+        continue;
+      }
+      // 2. If it is, we remove the paragraph and create a container directive
+      const wrappedChildren: BlockContent[] = [];
+      // 2.1 case: with no newline between `:::` and `:::`, for example
       // ::: tip
-      //
       // This is a tip
       // :::
-
-      // All of the above cases need to crawl the children of the container directive node.
-      // In other word, We look for the next paragraph nodes and collect all the content until we find the end of the container directive
-      let j = i + 1;
-      while (j < tree.children.length) {
-        const currentParagraph = tree.children[j];
-        if (currentParagraph.type !== 'paragraph') {
-          wrappedChildren.push(currentParagraph as BlockContent);
-          j++;
-          continue;
-        }
-        const lastChild =
-          currentParagraph.children[currentParagraph.children.length - 1];
-        // The whole paragrph doesn't arrive at the end of the container directive, we collect the whole paragraph
-        if (
-          lastChild !== firstTextNode &&
-          (lastChild.type !== 'text' || !REGEX_END.test(lastChild.value))
-        ) {
-          wrappedChildren.push({
-            ...currentParagraph,
-            children: currentParagraph.children.filter(
-              child => child !== firstTextNode,
-            ),
+      // Here the content is `::: tip\nThis is a tip\n:::`
+      if (content?.endsWith(':::')) {
+        wrappedChildren.push({
+          type: 'paragraph',
+          children: [
+            {
+              type: 'text',
+              value: content.replace(REGEX_END, ''),
+            },
+          ],
+        });
+        const newChild = createContainer(type, title, wrappedChildren);
+        tree.children.splice(i, 1, newChild as Content);
+      } else {
+        // 2.2 case: with newline before the end of container, for example:
+        // ::: tip
+        // This is a tip
+        //
+        // :::
+        // Here the content is `::: tip\nThis is a tip`
+        const paragraphChild: Paragraph = {
+          type: 'paragraph',
+          children: [] as PhrasingContent[],
+        };
+        wrappedChildren.push(paragraphChild);
+        if (content.length) {
+          paragraphChild.children.push({
+            type: 'text',
+            value: content,
           });
-          j++;
-          continue;
-        } else {
-          // 3. We find the end of the container directive
-          // Then create the container directive, and remove the original paragraphs
-          // Finally, we insert the new container directive and break the loop
-          const lastChildText = lastChild.value;
-          const matchedEndContent = lastChildText.slice(0, -3).trim();
-          wrappedChildren.push(
-            ...(currentParagraph.children.filter(
-              child => child !== firstTextNode && child !== lastChild,
-            ) as BlockContent[]),
-          );
-
-          if (matchedEndContent) {
+        }
+        paragraphChild.children.push(...node.children.slice(1, -1));
+        // If the inserted paragraph is empty, we remove it
+        if (paragraphChild.children.length === 0) {
+          wrappedChildren.pop();
+        }
+        const lastChildInNode = node.children[node.children.length - 1];
+        // We find the end of the container directive in current paragraph
+        if (
+          lastChildInNode.type === 'text' &&
+          REGEX_END.test(lastChildInNode.value)
+        ) {
+          const lastChildInNodeText = lastChildInNode.value;
+          const matchedEndContent = lastChildInNodeText.slice(0, -3).trim();
+          // eslint-disable-next-line max-depth
+          if (wrappedChildren.length) {
+            (wrappedChildren[0] as Paragraph).children.push({
+              type: 'text',
+              value: matchedEndContent,
+            });
+          } else if (matchedEndContent) {
             wrappedChildren.push({
               type: 'paragraph',
               children: [
@@ -238,12 +214,82 @@ function transformer(tree: Root) {
             });
           }
           const newChild = createContainer(type, title, wrappedChildren);
-          tree.children.splice(i, j - i + 1, newChild as Content);
-          break;
+          tree.children.splice(i, 1, newChild as Content);
+          i++;
+          continue;
+        } else if (
+          lastChildInNode !== firstTextNode &&
+          wrappedChildren.length
+        ) {
+          // We don't find the end of the container directive in current paragraph
+          (wrappedChildren[0] as Paragraph).children.push(lastChildInNode);
+        }
+        // 2.3 The final case: has newline after the start of container, for example:
+        // ::: tip
+        //
+        // This is a tip
+        // :::
+
+        // All of the above cases need to crawl the children of the container directive node.
+        // In other word, We look for the next paragraph nodes and collect all the content until we find the end of the container directive
+        let j = i + 1;
+        while (j < tree.children.length) {
+          const currentParagraph = tree.children[j];
+          if (currentParagraph.type !== 'paragraph') {
+            wrappedChildren.push(currentParagraph as BlockContent);
+            j++;
+            continue;
+          }
+          const lastChild =
+            currentParagraph.children[currentParagraph.children.length - 1];
+          // The whole paragrph doesn't arrive at the end of the container directive, we collect the whole paragraph
+          if (
+            lastChild !== firstTextNode &&
+            (lastChild.type !== 'text' || !REGEX_END.test(lastChild.value))
+          ) {
+            wrappedChildren.push({
+              ...currentParagraph,
+              children: currentParagraph.children.filter(
+                child => child !== firstTextNode,
+              ),
+            });
+            j++;
+            continue;
+          } else {
+            // 3. We find the end of the container directive
+            // Then create the container directive, and remove the original paragraphs
+            // Finally, we insert the new container directive and break the loop
+            const lastChildText = lastChild.value;
+            const matchedEndContent = lastChildText.slice(0, -3).trim();
+            wrappedChildren.push(
+              ...(currentParagraph.children.filter(
+                child => child !== firstTextNode && child !== lastChild,
+              ) as BlockContent[]),
+            );
+
+            if (matchedEndContent) {
+              wrappedChildren.push({
+                type: 'paragraph',
+                children: [
+                  {
+                    type: 'text',
+                    value: matchedEndContent,
+                  },
+                ],
+              });
+            }
+            const newChild = createContainer(type, title, wrappedChildren);
+            tree.children.splice(i, j - i + 1, newChild as Content);
+            break;
+          }
         }
       }
+      i++;
     }
-    i++;
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e);
+    throw e;
   }
 }
 
