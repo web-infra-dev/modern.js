6750aa1807a9a487e55a693b5d6c013b2b871faa
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2023-01-11 16:33:06 +0800

    test: add assets e2e test case

---
+++ tests/e2e/builder/fixtures/output/assets-no-inline/src/App.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import img from './icon.png?__inline=false';
+
+function App() {
+  return (
+    <div>
+      <div id="test">Hello Builder!</div>
+      <img id="test-img" src={img} />
+    </div>
+  );
+}
+
+export default App;
