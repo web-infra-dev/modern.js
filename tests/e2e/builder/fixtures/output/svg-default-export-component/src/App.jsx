add19b01ba4ac0e06ee4918134b5d53186e23b1f
gaoyuan <gaoyuan.1226@bytedance.com>
2023-01-12 16:05:04 +0800

    e2e: add svg e2e test case (#2674)
    * test: add svg e2e test case
    
    * test: svg url

---
+++ tests/e2e/builder/fixtures/output/svg-default-export-component/src/App.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import Logo from './app.svg';
+
+function App() {
+  return (
+    <div>
+      <div id="test">Hello Builder!</div>
+      <Logo id="test-svg" />
+    </div>
+  );
+}
+
+export default App;
