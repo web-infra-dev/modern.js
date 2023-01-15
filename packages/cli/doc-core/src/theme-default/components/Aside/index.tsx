803b21693d8e693da88d750d2b30f7efee8ec252
yangxingyuan <yangxingyuan@bytedance.com>
2023-01-15 19:27:03 +0800

    feat: doc layout optimize

--- packages/cli/doc-core/src/theme-default/components/Aside/index.tsx
+++ packages/cli/doc-core/src/theme-default/components/Aside/index.tsx
@@ -54,6 +54,7 @@ export function Aside(props: { headers: Header[]; outlineTitle: string }) {
           }}
           onClick={e => {
             e.preventDefault();
+            window.location.hash = header.id;
             const target = document.getElementById(header.id);
             if (target) {
               scrollToTarget(target, false);
@@ -67,7 +68,7 @@ export function Aside(props: { headers: Header[]; outlineTitle: string }) {
   };
 
   return (
-    <div flex="~ col" className="max-w-256px">
+    <div flex="~ col">
       <div className={hasOutline ? `<lg:hidden` : 'hidden'}>
         <div
           p="l-4"
