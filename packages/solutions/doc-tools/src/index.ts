eacabda8cbf7d86f9691a920ec329aec0399d4da
yangxingyuan <yangxingyuan@bytedance.com>
2023-01-30 17:09:01 +0800

    feat: doc-tools config hmr

--- packages/solutions/doc-tools/src/index.ts
+++ packages/solutions/doc-tools/src/index.ts
@@ -4,21 +4,47 @@ import { schema } from './config/schema';
 
 export type { CliPlugin, Sidebar, NavItem, UserConfig };
 
+const MODERN_CONFIG_FILE = 'modern.config.ts';
+
+interface ServerInstance {
+  close: () => Promise<void>;
+}
+
 export default (): CliPlugin => ({
   name: '@modern-js/doc-tools',
   setup: async api => {
     const { dev, build } = await import('@modern-js/doc-core');
+    let server: ServerInstance | undefined;
+    let restartServer: (() => Promise<void>) | undefined;
     return {
       validateSchema: () => {
         return schema;
+      },
+      watchFiles() {
+        const config = api.useAppContext();
+        return [config.configFile];
+      },
+      async fileChange({ filename, eventType }) {
+        if (
+          filename.endsWith(MODERN_CONFIG_FILE) &&
+          eventType === 'change' &&
+          server
+        ) {
+          // Config file HMR in devepment mode
+          await server.close();
+          await restartServer!();
+        }
       },
       commands({ program }) {
         program
           .command('dev [root]')
           .description('start dev server')
           .action(async (root?: string) => {
-            const config = api.useConfigContext() as UserConfig;
-            await dev(root || '', config);
+            restartServer = async () => {
+              const config = api.useConfigContext() as UserConfig;
+              server = await dev(root || '', config);
+            };
+            await restartServer();
           });
 
         program
