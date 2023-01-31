8e17ea7d4b7ade6e36084c9e35848ff4d6ceac0e
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2023-01-31 11:52:25 +0800

    fix: removeConsole merge

--- packages/builder/builder-shared/src/mergeBuilderConfig.ts
+++ packages/builder/builder-shared/src/mergeBuilderConfig.ts
@@ -1,19 +1,29 @@
 import _ from '@modern-js/utils/lodash';
 
 export const mergeBuilderConfig = <T>(...configs: T[]): T =>
-  _.mergeWith({}, ...configs, (target: unknown, source: unknown) => {
-    const pair = [target, source];
-    if (pair.some(_.isUndefined)) {
+  _.mergeWith(
+    {},
+    ...configs,
+    (target: unknown, source: unknown, key: string) => {
+      const pair = [target, source];
+      if (pair.some(_.isUndefined)) {
+        // fallback to lodash default merge behavior
+        return undefined;
+      }
+
+      // target maybe array，should not merge source & target
+      if (key === 'removeConsole') {
+        return source ?? target;
+      }
+
+      if (pair.some(_.isArray)) {
+        return [..._.castArray(target), ..._.castArray(source)];
+      }
+      // convert function to chained function
+      if (pair.some(_.isFunction)) {
+        return [target, source];
+      }
       // fallback to lodash default merge behavior
       return undefined;
-    }
-    if (pair.some(_.isArray)) {
-      return [..._.castArray(target), ..._.castArray(source)];
-    }
-    // convert function to chained function
-    if (pair.some(_.isFunction)) {
-      return [target, source];
-    }
-    // fallback to lodash default merge behavior
-    return undefined;
-  });
+    },
+  );
