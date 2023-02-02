33083311160e39099b314bb10935c502156aa003
chenjiahan <chenjiahan.jait@bytedance.com>
2023-02-02 13:09:56 +0800

    chore: add some test integrations

--- tests/integration/load-config/test/index.test.ts
+++ tests/integration/load-config/test/index.test.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import { existsSync } from 'fs';
+import { existsSync, readFileSync } from 'fs';
 import {
   launchApp,
   killApp,
@@ -38,5 +38,44 @@ describe('local config', () => {
     ).toBeTruthy();
 
     killApp(app);
+  });
+
+  it(`should passing correct config params when running dev command`, async () => {
+    const appDir = path.resolve(fixtures, 'config-function-params');
+    const app = await launchApp(appDir);
+
+    const rawParams = readFileSync(
+      path.join(appDir, 'dist/params.json'),
+      'utf-8',
+    );
+    expect(JSON.parse(rawParams)).toEqual({
+      env: 'development',
+      command: 'dev',
+    });
+
+    killApp(app);
+  });
+
+  it(`should passing correct config params when running build command`, async () => {
+    const appDir = path.resolve(fixtures, 'config-function-params');
+    await modernBuild(appDir);
+
+    const rawParams = readFileSync(
+      path.join(appDir, 'dist/params.json'),
+      'utf-8',
+    );
+    expect(JSON.parse(rawParams)).toEqual({
+      env: 'production',
+      command: 'build',
+    });
+  });
+
+  it(`should allow config file to export an async function`, async () => {
+    const appDir = path.resolve(fixtures, 'async-config-function');
+    await modernBuild(appDir);
+
+    expect(
+      existsSync(path.join(appDir, 'dist/foo/html/main/index.html')),
+    ).toBeTruthy();
   });
 });
