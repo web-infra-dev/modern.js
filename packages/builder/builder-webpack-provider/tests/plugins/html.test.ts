a5572b847b9267f29da8eb726ef3edcabfda8eaf
qixuan <58852732+GiveMe-A-Name@users.noreply.github.com>
2023-02-03 20:52:24 +0800

    feat(builder-shared): more plugin sink in shared (#2786)
    * feat(builder-shared): more plugin sink in shared
    
    * feat: sink output plugin into bulider-shared

--- packages/builder/builder-webpack-provider/tests/plugins/html.test.ts
+++ packages/builder/builder-webpack-provider/tests/plugins/html.test.ts
@@ -1,8 +1,7 @@
 import { describe, expect, it } from 'vitest';
 import { builderPluginEntry } from '@builder/plugins/entry';
-import { getTemplatePath, builderPluginHtml } from '@/plugins/html';
+import { builderPluginHtml } from '@/plugins/html';
 import { createStubBuilder } from '@/stub';
-import type { BuilderConfig, NormalizedConfig } from '@/types';
 
 describe('plugins/html', () => {
   it('should register html plugin correctly', async () => {
@@ -163,15 +162,6 @@ describe('plugins/html', () => {
     });
 
     expect(await builder.matchWebpackPlugin('HtmlWebpackPlugin')).toBeFalsy();
-  });
-
-  it.each<[string, string, BuilderConfig['html']]>([
-    ['main', 'foo', { template: 'foo' }],
-    ['main', 'foo', { templateByEntries: { main: 'foo' } }],
-    ['other', 'bar', { template: 'bar', templateByEntries: { main: 'foo' } }],
-  ])(`should get template path for %s`, async (entry, expected, html) => {
-    const templatePath = getTemplatePath(entry, { html } as NormalizedConfig);
-    expect(templatePath).toEqual(expected);
   });
 
   it('should support multi entry', async () => {
