808e3b9b2b26ec110023f2365b64b39ef8333a1f
caohuilin <caohuilin@bytedance.com>
2023-02-04 10:11:30 +0800

    feat: handle time

--- packages/runtime/plugin-runtime/src/ssr/serverRender/time.worker.ts
+++ packages/runtime/plugin-runtime/src/ssr/serverRender/time.worker.ts
@@ -1,6 +1,29 @@
-/** work not support colletc system time */
+// generate timestamp or delta
+function processHrtime(previousTimestamp?: [number, number]): [number, number] {
+  const now = new Date().getTime();
+  const clocktime = now * 1e-3;
+  let seconds = Math.floor(clocktime);
+  let nanoseconds = Math.floor((clocktime % 1) * 1e9);
+  if (previousTimestamp) {
+    seconds -= previousTimestamp[0];
+    nanoseconds -= previousTimestamp[1];
+    if (nanoseconds < 0) {
+      seconds--;
+      nanoseconds += 1e9;
+    }
+  }
+  return [seconds, nanoseconds];
+}
+
+const getLatency = (hrtime: [number, number]) => {
+  const [s, ns] = processHrtime(hrtime);
+  return s * 1e3 + ns / 1e6;
+};
+
 export const time = () => {
+  const hrtime = processHrtime();
+
   return () => {
-    return 0;
+    return getLatency(hrtime);
   };
 };
