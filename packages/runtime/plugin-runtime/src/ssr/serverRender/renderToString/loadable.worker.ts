284cccbbffbe34fa8e3ef66e179a93959a53ff8a
caohuilin <caohuilin@bytedance.com>
2023-02-06 18:23:35 +0800

    fix: toHtml

--- packages/runtime/plugin-runtime/src/ssr/serverRender/renderToString/loadable.worker.ts
+++ packages/runtime/plugin-runtime/src/ssr/serverRender/renderToString/loadable.worker.ts
@@ -1,34 +1,5 @@
 import { RenderHandler } from './type';
 
-export const toHtml: RenderHandler = (jsx, renderer, next) => {
+export const toHtml: RenderHandler = (jsx, _renderer, next) => {
   return next(jsx);
-
-  // const extractor = new ChunkExtractor({
-  //   stats,
-  //   entrypoints: [renderer.entryName],
-  // });
-
-  // const html = next(extractor.collectChunks(jsx));
-  // const chunks = extractor.getChunkAssets(extractor.chunks);
-
-  // chunksMap.js = (chunksMap.js || '') + getLoadableScripts(extractor);
-
-  // for (const v of chunks) {
-  //   const fileType = extname(v.url!).slice(1);
-
-  //   if (fileType === 'js') {
-  //     const props = [];
-  //     const { crossorigin } = config;
-  //     if (crossorigin && isCrossOrigin(v.url, host)) {
-  //       props.push(
-  //         `crossorigin="${crossorigin === true ? 'anonymous' : crossorigin}"`,
-  //       );
-  //     }
-  //     chunksMap[fileType] += `<script src="${v.url}" ${props.join(
-  //       ' ',
-  //     )}></script>`;
-  //   } else if (fileType === 'css') {
-  //     chunksMap[fileType] += `<link href="${v.url}" rel="stylesheet" />`;
-  //   }
-  // }
 };
