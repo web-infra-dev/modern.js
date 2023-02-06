f42fede76d974769fa318252e18040242728d0f0
caohuilin <caohuilin@bytedance.com>
2023-02-06 12:56:22 +0800

    feat: fix tootip marks

--- packages/generator/generators/router-v5-generator/src/locale/en.ts
+++ packages/generator/generators/router-v5-generator/src/locale/en.ts
@@ -1,4 +1,4 @@
 export const EN_LOCALE = {
   success: `Plugin dependency installed successfully! Please add the following code to`,
-  successTooltip: `After add code, you will use React Router v5 in your project, please use React Router v5 API from '@modern-js/runtime/router-v5'`,
+  successTooltip: `After add code, you will use React Router v5 in your project, please use React Router v5 API from '@modern-js/runtime/router-v5'.`,
 };
