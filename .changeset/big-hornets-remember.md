59172e9c39286a7de3ba7b6501b29721c9259fb6
10Derozan <fupeng.antik@gmail.com>
2023-02-09 20:55:20 +0800

    feat(module-tools): report size and info after build (#2843)
    * feat(module-tools): report size and info after build
    
    * fix: fix windows path

---
+++ .changeset/big-hornets-remember.md
@@ -0,0 +1,6 @@
+---
+'@modern-js/module-tools': patch
+---
+
+feat: report size and info after build
+feat: 构建后报告文件大小和信息
--- packages/solutions/module-tools/src/constants/log.ts
+++ packages/solutions/module-tools/src/constants/log.ts
@@ -7,3 +7,6 @@ export enum SectionTitleStatus {
 export const BundleDtsLogPrefix = '[Bundle:DTS]';
 export const BundlelessDtsLogPrefix = '[Bundleless:DTS]';
 export const buildSuccessText = 'Build succeed';
+
+export const reportFile1LineText = 'Bundle Files';
+export const reportFile2LineText = 'Size';
