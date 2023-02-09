e0140ff8446753b27d04aaeb8f501cbf5bb35bc1
caohuilin <caohuilin@bytedance.com>
2023-02-09 19:05:18 +0800

    fix: worker code

--- packages/server/plugin-worker/src/code.ts
+++ packages/server/plugin-worker/src/code.ts
@@ -1,12 +1,19 @@
 export const worker = () => `
 // work entry code
 import { createHandler, handleUrl } from "@modern-js/prod-server/worker";
+import loadableStats from "../loadable-stats.json";
+import routeManifest from "../routes-manifest.json";
 import { manifest } from "./manifest";
 async function handleRequest(request) {
   const context = {
-    request,
+    request: {
+      ...request,
+      url: request.url,
+    },
     url: handleUrl(request.url),
     body: null,
+    loadableStats,
+    routeManifest,
   };
   const handler = createHandler(manifest);
   await handler(context);
