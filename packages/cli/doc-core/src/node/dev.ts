382ea0de4879449060ea4f8fb329a5a6ee86a4d4
10Derozan <fupeng.antik@gmail.com>
2023-02-13 15:55:51 +0800

    feat: add beforeBuild and afterBuild in doc plugin (#2861)
    * feat: add beforeBuild and afterBuild in doc plugin
    
    * fix: use promise all in build hooks
    
    * fix: support hooks in dev

--- packages/cli/doc-core/src/node/dev.ts
+++ packages/cli/doc-core/src/node/dev.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { UserConfig } from 'shared/types';
 import { createModernBuilder } from './createBuilder';
 import { writeSearchIndex } from './searchIndex';
+import { modifyConfig, beforeBuild, afterBuild } from './hooks';
 
 interface ServerInstance {
   close: () => Promise<void>;
@@ -11,8 +12,25 @@ export async function dev(
   rootDir: string,
   config: UserConfig,
 ): Promise<ServerInstance> {
-  const builder = await createModernBuilder(rootDir, config);
+  const docPlugins = [...(config.doc?.plugins ?? [])];
+  const isProd = false;
+  const modifiedConfig = await modifyConfig({
+    config,
+    docPlugins,
+  });
+  await beforeBuild({
+    config: modifiedConfig,
+    docPlugins,
+    isProd,
+  });
+  const builder = await createModernBuilder(rootDir, modifiedConfig);
   const { server } = await builder.startDevServer();
+  await afterBuild({
+    config: modifiedConfig,
+    docPlugins,
+    isProd,
+  });
+
   const userRoot = path.resolve(rootDir || config.doc?.root || process.cwd());
   await writeSearchIndex(userRoot, config);
   return server;
