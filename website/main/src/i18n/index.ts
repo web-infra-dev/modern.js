9ada4aa70b5a4007d6108c2dd557e5cd7a378e7b
neverland <chenjiahan.jait@bytedance.com>
2023-02-15 14:57:12 +0800

    docs(builder): fix broken links in Step component (#2898)

--- website/main/src/i18n/index.ts
+++ website/main/src/i18n/index.ts
@@ -1,4 +1,4 @@
-import { useLocation, withBase } from '@modern-js/doc-tools/runtime';
+import { withBase, useLang } from '@modern-js/doc-tools/runtime';
 import { EN_US } from './en-US';
 import { ZH_CN } from './zh-CN';
 
@@ -7,23 +7,12 @@ const translations = {
   zh: ZH_CN,
 } as const;
 
-const removeTrailingSlash = (path: string) => {
-  return path.endsWith('/') ? path.slice(0, -1) : path;
-};
-
-export function useLang() {
-  const location = useLocation();
-  const langPrefix =
-    location.pathname === '/' ? '' : removeTrailingSlash(location.pathname);
-  return langPrefix.includes('en') ? 'en' : 'zh';
-}
-
 export function useUrl(url: string) {
   const lang = useLang();
   return withBase(lang === 'zh' ? url : `/en${url}`);
 }
 
 export function useI18n() {
-  const lang = useLang();
+  const lang = useLang() as keyof typeof translations;
   return (key: keyof typeof EN_US) => translations[lang][key];
 }
