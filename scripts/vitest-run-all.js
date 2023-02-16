3382fe2c72fd53bf1558a72becc12888659355cb
chenjiahan <chenjiahan.jait@bytedance.com>
2023-02-16 16:49:26 +0800

    chore: update vitest script

--- scripts/vitest-run-all.js
+++ scripts/vitest-run-all.js
@@ -13,16 +13,7 @@ const SHELL = process.env.SHELL || true;
   });
   const directories = configs.map(config => path.dirname(config));
 
-  const pnpmFilters = directories
-    .map(dir => `--filter "{${dir}}..."`)
-    .join(' ');
-  const buildCmd = `pnpm run ${pnpmFilters} build`;
-  // eslint-disable-next-line no-console
-  console.log('>', buildCmd);
-
   try {
-    await execa(buildCmd, { shell: SHELL, stdio: 'inherit' });
-
     for (const cwd of directories) {
       const cmd = 'pnpm run test';
       await execa(cmd, { shell: SHELL, stdio: 'inherit', cwd });
