6405f3cb5875cf221d942e0cb7bfc4712cca9250
chenjiahan <chenjiahan.jait@bytedance.com>
2023-02-16 17:56:13 +0800

    chore: run vitest parallelly

--- scripts/vitest-run-all.js
+++ scripts/vitest-run-all.js
@@ -26,10 +26,11 @@ const SHELL = process.env.SHELL || true;
       stdio: 'inherit',
     });
 
-    for (const cwd of directories) {
-      const cmd = 'pnpm run test';
-      await execa(cmd, { shell: SHELL, stdio: 'inherit', cwd });
-    }
+    await Promise.all(
+      directories.map(cwd =>
+        execa('pnpm run test', { shell: SHELL, stdio: 'inherit', cwd }),
+      ),
+    );
   } catch (err) {
     console.error(err);
     // eslint-disable-next-line no-process-exit
