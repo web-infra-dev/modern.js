c608abd2bf106f422b5e1eb533ca1eddc759b5d0
yangxingyuan <39261479+sanyuan0704@users.noreply.github.com>
2023-02-16 15:03:51 +0800

    fix(doc-core): home page title (#2915)

--- packages/cli/doc-core/src/theme-default/layout/Layout/index.tsx
+++ packages/cli/doc-core/src/theme-default/layout/Layout/index.tsx
@@ -51,7 +51,7 @@ export const Layout: React.FC<LayoutProps> = props => {
   let title = frontmatter?.title ?? articleTitle;
   const mainTitle = siteData.title || localesData.title;
 
-  if (title) {
+  if (title && pageType === 'doc') {
     // append main title as a suffix
     title = `${title} - ${mainTitle}`;
   } else {
