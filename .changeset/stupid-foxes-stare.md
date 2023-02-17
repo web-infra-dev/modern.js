eb2a52eb61a02a5928f2cb7f6705d10b8e691200
targeral <targeral@outlook.com>
2023-02-17 16:58:57 +0800

    fix(plugin-storybook): fix template path

---
+++ .changeset/stupid-foxes-stare.md
@@ -0,0 +1,6 @@
+---
+'@modern-js/plugin-storybook': patch
+---
+
+fix(storybook): remove `require` and fix dist/template path
+fix(storybook): 移除 require 代码以及修复 dist/template 的路径问题
--- packages/cli/plugin-storybook/modern.config.js
+++ packages/cli/plugin-storybook/modern.config.js
@@ -11,7 +11,7 @@ module.exports = {
           {
             from: './template',
             context: __dirname,
-            to: './js/template',
+            to: './template',
           },
         ],
       },
