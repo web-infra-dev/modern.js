d06e172b860c7382c7e02f01aa685716c8388f7d
10Derozan <fupeng.antik@gmail.com>
2023-02-20 21:12:30 +0800

    fix: change test env

--- packages/solutions/module-tools/jest.config.js
+++ packages/solutions/module-tools/jest.config.js
@@ -4,6 +4,7 @@ const sharedConfig = require('@scripts/jest-config');
 module.exports = {
   ...sharedConfig,
   rootDir: __dirname,
+  testEnvironment: 'node',
   testMatch: ['<rootDir>/tests/**/*.test.[jt]s?(x)'],
   testPathIgnorePatterns: ['buildWatch.test.ts'],
   coveragePathIgnorePatterns: ['onExit.ts', './src/constants/*'],
