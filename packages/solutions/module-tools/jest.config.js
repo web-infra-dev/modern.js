717d6d00a59711543d4aabfcd997ea4c6183fbb7
10Derozan <fupeng.antik@gmail.com>
2023-02-20 17:31:57 +0800

    fix: fix jest environment

--- packages/solutions/module-tools/jest.config.js
+++ packages/solutions/module-tools/jest.config.js
@@ -4,6 +4,10 @@ const sharedConfig = require('@scripts/jest-config');
 module.exports = {
   ...sharedConfig,
   rootDir: __dirname,
+  globals: {
+    Uint8Array,
+    ArrayBuffer,
+  },
   testMatch: ['<rootDir>/tests/**/*.test.[jt]s?(x)'],
   testPathIgnorePatterns: ['buildWatch.test.ts'],
   coveragePathIgnorePatterns: ['onExit.ts', './src/constants/*'],
