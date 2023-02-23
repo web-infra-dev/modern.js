4491e07e69f93bb3cc2231049615a44826340d95
jason.xu <jason.xu@bytedance.com>
2023-02-23 14:43:31 +0800

    fix: fix server-loader bundle not exsited

--- packages/cli/plugin-data-loader/src/server/index.ts
+++ packages/cli/plugin-data-loader/src/server/index.ts
@@ -1,5 +1,6 @@
 /* eslint-disable consistent-return */
 import path from 'path';
+import fs from 'fs';
 import type { ServerPlugin } from '@modern-js/server-core';
 import type { ServerRoute } from '@modern-js/types';
 import { MAIN_ENTRY_NAME, SERVER_BUNDLE_DIRECTORY } from '@modern-js/utils';
@@ -24,8 +25,14 @@ export default (): ServerPlugin => ({
         const routesPath = path.join(
           distDir,
           SERVER_BUNDLE_DIRECTORY,
-          `${entry.entryName || MAIN_ENTRY_NAME}-server-loaders`,
+          `${entry.entryName || MAIN_ENTRY_NAME}-server-loaders.js`,
         );
+
+        // legacy ssr does not contain server loaders
+        if (!fs.existsSync(routesPath)) {
+          return;
+        }
+
         const { routes, handleRequest } = await import(routesPath);
 
         if (!routes) {
