5f899af53af143842ba4adc66c516369ae319211
gaoyuan <gaoyuan.1226@bytedance.com>
2023-02-28 17:15:35 +0800

    feat(builder): support output.enableAssetFallback in rspack-provider (#3019)
    * feat: support fallback plugin in rspack-provider
    
    * fix: update doc

---
+++ packages/builder/builder-shared/src/fallback.ts
@@ -0,0 +1,46 @@
+import { JS_REGEX, TS_REGEX } from './constants';
+import type { RuleSetRule } from 'webpack';
+
+export const resourceRuleFallback = (
+  rules: Array<RuleSetRule | '...'> = [],
+): Array<RuleSetRule | '...'> => {
+  const innerRules: Array<RuleSetRule> = [];
+  const outerRules: Array<RuleSetRule | '...'> = [];
+
+  for (const rule of rules) {
+    if (
+      // "..." refers to the webpack defaults
+      rule === '...' ||
+      // this is a special case, put the mjs fullySpecified rule in the outside
+      (rule.resolve && !rule.mimetype)
+    ) {
+      outerRules.push(rule);
+    } else if (
+      rule.oneOf &&
+      !(
+        rule.test ||
+        rule.exclude ||
+        rule.resource ||
+        rule.issuer ||
+        rule.mimetype
+      )
+    ) {
+      rule.oneOf.forEach(r => innerRules.push(r));
+    } else {
+      innerRules.push(rule);
+    }
+  }
+
+  const fileLoader = {
+    exclude: [
+      JS_REGEX,
+      TS_REGEX,
+      // exclude `html` and `json`, they get processed by webpack internal loaders.
+      /\.html$/,
+      /\.json$/,
+    ],
+    type: 'asset/resource',
+  };
+
+  return [...outerRules, { oneOf: [...innerRules, fileLoader] }];
+};
