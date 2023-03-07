41bf78cf3cb4489a3231ee2796c500be57c318e2
targeral <targeral@outlook.com>
2023-03-07 17:08:14 +0800

    feat: add plugin-module-import

---
+++ packages/cli/plugin-module-import/src/index.ts
@@ -0,0 +1,32 @@
+import type { CliPlugin, ModuleTools } from '@modern-js/module-tools';
+import type { ImportItem } from '@modern-js/libuild-plugin-swc';
+
+export default (options: {
+  pluginImport?: ImportItem[];
+}): CliPlugin<ModuleTools> => ({
+  name: '@modern-js/plugin-module-import',
+  setup: () => ({
+    async modifyLibuild(config) {
+      if (!options.pluginImport || options.pluginImport.length === 0) {
+        return config;
+      }
+
+      const { transformPlugin } = await import('@modern-js/libuild-plugin-swc');
+      config.plugins?.push(
+        transformPlugin({
+          jsc: {
+            parser: {
+              syntax: 'ecmascript',
+              jsx: true,
+            },
+            target: 'es2022',
+          },
+          extensions: {
+            pluginImport: options.pluginImport ?? [],
+          },
+        }),
+      );
+      return config;
+    },
+  }),
+});
