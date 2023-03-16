9bc2706d554830b24103aba0c182269478df7154
Belinda Cao <caohuilin@bytedance.com>
2023-03-16 10:54:12 +0800

    Revert "chore: upgrade jest and puppeteer (#3163)" (#3166)
    This reverts commit fca9731f69725b2dfc508c97660974f43189bd72.

--- jestEnv.js
+++ jestEnv.js
@@ -1,22 +1,16 @@
-const { TestEnvironment } = require('jest-environment-jsdom');
+const JsdomEnvironment = require('jest-environment-jsdom');
 
 // https://github.com/facebook/jest/issues/4422
-class JestEnvironment extends TestEnvironment {
-  constructor(config, testEnvironmentContext) {
-    super(
-      {
-        ...config,
-        projectConfig: {
-          ...config.projectConfig,
-          globals: {
-            ...config.projectConfig.globals,
-            Uint8Array,
-            ArrayBuffer,
-          },
-        },
+class JestEnvironment extends JsdomEnvironment {
+  constructor(config) {
+    super({
+      ...config,
+      globals: {
+        ...config.globals,
+        Uint8Array,
+        ArrayBuffer,
       },
-      testEnvironmentContext,
-    );
+    });
   }
 }
 
