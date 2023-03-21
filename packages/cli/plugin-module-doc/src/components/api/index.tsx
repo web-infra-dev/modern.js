712f891cafd5959bc7f705cada25030ec3d4f596
yangxingyuan <yangxingyuan@bytedance.com>
2023-03-21 11:38:48 +0800

    fix: table style

--- packages/cli/plugin-module-doc/src/components/api/index.tsx
+++ packages/cli/plugin-module-doc/src/components/api/index.tsx
@@ -1,9 +1,13 @@
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import { getCustomMDXComponent } from '@modern-js/doc-core/theme';
 
 export default (props: { moduleName: string }) => {
   return (
-    <ReactMarkdown remarkPlugins={[remarkGfm]}>
+    <ReactMarkdown
+      remarkPlugins={[remarkGfm]}
+      components={getCustomMDXComponent()}
+    >
       {props.moduleName}
     </ReactMarkdown>
   );
