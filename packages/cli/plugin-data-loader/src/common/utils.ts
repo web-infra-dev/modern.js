d6b6e29bb429b0db1453bc54b584cfdfd6a664c5
zero <68810266+zllkjc@users.noreply.github.com>
2023-03-22 21:25:39 +0800

    fix: split server and runtime code in plugin-data-loader (#3224)
    * fix: split server and runtime code in plugin-data-loader
    
    * docs: add changeset
    
    * chore: update lock file

---
+++ packages/cli/plugin-data-loader/src/common/utils.ts
@@ -0,0 +1,37 @@
+import { ModernServerContext, ServerRoute } from '@modern-js/types';
+
+export type ServerContext = Pick<
+  ModernServerContext,
+  | 'req'
+  | 'res'
+  | 'params'
+  | 'headers'
+  | 'method'
+  | 'url'
+  | 'host'
+  | 'protocol'
+  | 'origin'
+  | 'href'
+  | 'path'
+  | 'query'
+>;
+
+function sortByUrlPath(entries: ServerRoute[]): ServerRoute[] {
+  entries.sort(function (a, b) {
+    const length1 = a.urlPath.length;
+    const length2 = b.urlPath.length;
+    if (length1 < length2) {
+      return 1;
+    }
+    if (length1 > length2) {
+      return -1;
+    }
+    return 0;
+  });
+  return entries;
+}
+
+export const matchEntry = (pathname: string, entries: ServerRoute[]) => {
+  sortByUrlPath(entries);
+  return entries.find(entry => pathname.startsWith(entry.urlPath));
+};
