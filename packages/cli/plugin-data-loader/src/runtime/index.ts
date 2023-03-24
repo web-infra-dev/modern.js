e2466a10713b023979e17b6c61fd837bf84362ca
Ming <wangyiming.fe@bytedance.com>
2023-03-24 11:46:56 +0800

    fix: remove nestedRoutes file from @modern-js/utils (#3242)
    * fix: remove nestedRoutes file from @modern-js/utils
    
    * fix: remove nestedRoutes file from @modern-js/utils
    
    * fix: remove nestedRoutes file from @modern-js/utils
    
    * fix: remove nestedRoutes file from @modern-js/utils
    
    * fix: remove nestedRoutes file from @modern-js/utils

--- packages/cli/plugin-data-loader/src/runtime/index.ts
+++ packages/cli/plugin-data-loader/src/runtime/index.ts
@@ -5,7 +5,6 @@ import {
   writeReadableStreamToWritable,
   Response as NodeResponse,
 } from '@remix-run/node';
-import { transformNestedRoutes } from '@modern-js/utils/nestedRoutes';
 import {
   createStaticHandler,
   ErrorResponse,
@@ -15,6 +14,7 @@ import {
 import { isPlainObject } from '@modern-js/utils/lodash';
 import { CONTENT_TYPE_DEFERRED, LOADER_ID_PARAM } from '../common/constants';
 import { matchEntry, ServerContext } from '../common/utils';
+import { transformNestedRoutes } from './nestedRoutes';
 import { createDeferredReadableStream } from './response';
 
 // Polyfill Web Fetch API
