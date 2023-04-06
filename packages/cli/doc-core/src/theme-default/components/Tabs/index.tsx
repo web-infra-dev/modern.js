236d49b9dfae477074b08577bbb525548f4fa046
10Derozan <fupeng.antik@gmail.com>
2023-04-06 11:52:14 +0800

    fix(doc-core): fix Tabs props type

--- packages/cli/doc-core/src/theme-default/components/Tabs/index.tsx
+++ packages/cli/doc-core/src/theme-default/components/Tabs/index.tsx
@@ -58,7 +58,7 @@ export function Tabs(props: TabsProps): ReactElement {
   let defaultIndex = 0;
   const needSync = groupId && tabData[groupId] !== undefined;
   if (needSync) {
-    defaultIndex = tabData[groupId] as number;
+    defaultIndex = tabData[groupId];
   } else if (defaultValue) {
     defaultIndex = tabValues.findIndex(item => {
       if (typeof item === 'string') {
