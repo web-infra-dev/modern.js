69eed513158cc2e204fbe067a5bc1b27c0b0e3dc
qixuan <58852732+GiveMe-A-Name@users.noreply.github.com>
2023-04-07 11:23:44 +0800

    refactor: move add builder-plugin-node-polyfill action to builder-generator (#3366)

--- packages/solutions/app-tools/src/builder/builder-rspack/index.ts
+++ packages/solutions/app-tools/src/builder/builder-rspack/index.ts
@@ -1,5 +1,4 @@
 import { builderRspackProvider } from '@modern-js/builder-rspack-provider';
-import { BuilderInstance } from '@modern-js/builder-shared';
 import { BuilderOptions } from '../shared';
 import { generateBuilder } from '../generator';
 
@@ -7,23 +6,5 @@ export function createRspackBuilderForModern(
   options: BuilderOptions<'rspack'>,
 ) {
   // Todo Copy config/upload
-  return generateBuilder(options, builderRspackProvider, {
-    async modifyBuilderInstance(builder) {
-      applyBuilderPlugins(builder, options);
-    },
-  });
-}
-
-/** register builder Plugin by condition */
-async function applyBuilderPlugins(
-  builder: BuilderInstance,
-  options: BuilderOptions<'rspack'>,
-) {
-  const { normalizedConfig } = options;
-  if (!normalizedConfig.output.disableNodePolyfill) {
-    const { builderPluginNodePolyfill } = await import(
-      '@modern-js/builder-plugin-node-polyfill'
-    );
-    builder.addPlugins([builderPluginNodePolyfill()]);
-  }
+  return generateBuilder(options, builderRspackProvider);
 }
