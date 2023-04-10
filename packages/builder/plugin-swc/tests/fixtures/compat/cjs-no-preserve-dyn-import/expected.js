5deb1fb8fe397c05cfa1bce9221cbfc2923a20aa
Fy <1114550440@qq.com>
2023-04-10 19:20:07 +0800

    chore: bump core-js and swc (#3390)

--- packages/builder/plugin-swc/tests/fixtures/compat/cjs-no-preserve-dyn-import/expected.js
+++ packages/builder/plugin-swc/tests/fixtures/compat/cjs-no-preserve-dyn-import/expected.js
@@ -1,9 +1,9 @@
 "use strict";
-var _interopRequireWildcard = require("<SWC_HELPER>/lib/_interop_require_wildcard.js").default;
+var _interop_require_wildcard = require("<SWC_HELPER>/_/_interop_require_wildcard");
 require("<CORE_JS>/modules/es.promise.js");
 require("<CORE_JS>/modules/es.object.to-string.js");
 var a = require("foo");
 console.log(a);
 Promise.resolve().then(function() {
-    return _interopRequireWildcard(require("other"));
+    return _interop_require_wildcard._(require("other"));
 }) /*#__PURE__*/ ;
