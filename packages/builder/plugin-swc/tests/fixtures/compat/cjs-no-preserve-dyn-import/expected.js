04f4ba5db7a493db9e7fd06f35fc4470f3429d77
jserfeng <1114550440@qq.com>
2023-04-10 20:14:40 +0800

    fix(builder-plugin-swc): remove default env.mode

--- packages/builder/plugin-swc/tests/fixtures/compat/cjs-no-preserve-dyn-import/expected.js
+++ packages/builder/plugin-swc/tests/fixtures/compat/cjs-no-preserve-dyn-import/expected.js
@@ -1,7 +1,5 @@
 "use strict";
 var _interop_require_wildcard = require("<SWC_HELPER>/_/_interop_require_wildcard");
-require("<CORE_JS>/modules/es.promise.js");
-require("<CORE_JS>/modules/es.object.to-string.js");
 var a = require("foo");
 console.log(a);
 Promise.resolve().then(function() {
