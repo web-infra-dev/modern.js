72b71b5b4e04bdfaf03a68778da65f1c64194ad8
yangxingyuan <39261479+sanyuan0704@users.noreply.github.com>
2023-04-12 20:33:58 +0800

    feat(doc-core): normalize image link (#3402)
    * feat: normalize image link in mdx-rs
    
    * fix: img asset in mdx-js
    
    * chore: upgrade mdx-rs-binding
    
    * fix: dead links
    
    * chore: style

---
+++ .changeset/wet-crabs-hammer.md
@@ -0,0 +1,7 @@
+---
+'@modern-js/doc-core': patch
+---
+
+feat: normalize link and image in mdx-rs
+
+feat: mdx-rs 中支持 link 和 图片的规范化
--- packages/cli/babel-preset-app/tests/utils.test.ts
+++ packages/cli/babel-preset-app/tests/utils.test.ts
@@ -3,7 +3,7 @@ import { getCoreJsVersion } from '@modern-js/utils';
 describe('getCoreJsVersion', () => {
   it('should get correct core-js version', () => {
     expect(getCoreJsVersion(require.resolve('core-js/package.json'))).toEqual(
-      '3.27',
+      '3.30',
     );
   });
 });
