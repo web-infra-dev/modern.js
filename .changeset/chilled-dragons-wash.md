3ff980a88d6bea9f7783c594dbd96f126d86fd81
wangyiming.fe <wangyiming.fe@bytedance.com>
2023-04-13 14:42:06 +0800

    fix(utils): the esm format files should be generate for the client

---
+++ .changeset/chilled-dragons-wash.md
@@ -0,0 +1,8 @@
+---
+'@modern-js/server': patch
+'@modern-js/utils': patch
+---
+
+fix(utils): the esm format files should be generate for the client
+
+fix(utils): 应该为客户端使用，生成 esm 格式的文件
--- packages/toolkit/utils/modern.config.js
+++ packages/toolkit/utils/modern.config.js
@@ -1,5 +1,34 @@
-export default {
-  buildConfig: {
-    buildType: 'bundleless',
-  },
+module.exports = {
+  buildConfig: [
+    {
+      buildType: 'bundleless',
+      format: 'cjs',
+      target: 'es2019',
+      dts: false,
+      outDir: './dist/cjs',
+      copy: {
+        patterns: [
+          {
+            from: './compiled',
+            context: __dirname,
+            to: '../compiled',
+          },
+        ],
+      },
+    },
+    {
+      buildType: 'bundleless',
+      format: 'esm',
+      target: 'es2019',
+      dts: false,
+      outDir: './dist/esm',
+    },
+    {
+      buildType: 'bundleless',
+      dts: {
+        only: true,
+      },
+      outDir: './dist/types',
+    },
+  ],
 };
