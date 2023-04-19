52d0cb168728d3d2b18f05d4e5635f974ee1b056
xuchaobei <jason.xu@bytedance.com>
2023-04-19 12:09:02 +0800

    feat: support config handle of Route (#3475)
    * feat: support route handle
    
    * chore: changeset

---
+++ tests/integration/routes/src/three/routes/user.profile.name/layout.config.ts
@@ -0,0 +1,3 @@
+export const handle = {
+  crumbs: 'user.profile.name.layout',
+};
--- tests/integration/routes/src/three/routes/user.profile.name/page.tsx
+++ tests/integration/routes/src/three/routes/user.profile.name/page.tsx
@@ -1,3 +1,16 @@
+import { useMatches } from '@modern-js/runtime/router';
+
 export default function Page() {
-  return <div>profile name page</div>;
+  const matches = useMatches();
+
+  const renderCrumbs = () => {
+    const crumbs = matches.map(item => (item.handle as any).crumbs);
+    return crumbs.join('/');
+  };
+
+  return (
+    <div>
+      profile name page<div>{renderCrumbs()}</div>
+    </div>
+  );
 }
