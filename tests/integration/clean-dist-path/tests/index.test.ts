d0d626a8dae3ae777994fe2c00e5e4f089f610c2
chenjiahan <chenjiahan.jait@bytedance.com>
2023-04-19 10:27:55 +0800

    chore: update test case

--- tests/integration/clean-dist-path/tests/index.test.ts
+++ tests/integration/clean-dist-path/tests/index.test.ts
@@ -1,13 +1,15 @@
 import path from 'path';
-import { writeFileSync, existsSync } from 'fs';
+import { fs } from '@modern-js/utils';
 import { modernBuild } from '../../../utils/modernTestUtils';
 
 describe('clean dist path', () => {
   it(`should not clean dist path when output.cleanDistPath is false`, async () => {
     const appDir = path.resolve(__dirname, '..');
-    const tempFile = path.join(appDir, 'dist', 'foo.txt');
-    writeFileSync(tempFile, 'foo');
+    const tempFile = path.join(appDir, 'dist/foo.txt');
+    const htmlFile = path.join(appDir, 'dist/html/main/index.html');
+    fs.outputFileSync(tempFile, 'foo');
     await modernBuild(appDir);
-    expect(existsSync(tempFile)).toBeTruthy();
+    expect(fs.existsSync(tempFile)).toBeTruthy();
+    expect(fs.existsSync(htmlFile)).toBeTruthy();
   });
 });
