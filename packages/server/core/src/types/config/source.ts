bfb060fc91f8b642e8c84d537a49533b6d0c7264
chenjiahan <chenjiahan.jait@bytedance.com>
2023-04-19 16:44:12 +0800

    chore(server): no longer replace global vars when compiler is babel

--- packages/server/core/src/types/config/source.ts
+++ packages/server/core/src/types/config/source.ts
@@ -1,18 +1,8 @@
 import type { Alias } from '@modern-js/utils';
 import type { ChainedConfig } from './share';
 
-type JSONPrimitive = string | number | boolean | null | undefined;
-
-type JSONArray = Array<JSONValue>;
-
-type JSONObject = { [key: string]: JSONValue };
-
-type JSONValue = JSONPrimitive | JSONObject | JSONArray;
-
 export interface SourceUserConfig {
   alias?: ChainedConfig<Alias>;
-  define?: Record<string, any>;
-  globalVars?: Record<string, JSONValue>;
 }
 
 export type SourceNormalizedConfig = SourceUserConfig;
