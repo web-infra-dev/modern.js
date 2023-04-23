9d2a2d028e7f51e725ce9c349d79d709a3816ded
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2023-04-23 14:34:28 +0800

    feat: support stylus plugin in rspack build

--- packages/builder/plugin-stylus/vitest.config.ts
+++ packages/builder/plugin-stylus/vitest.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vitest/config';
 import { withTestPreset } from '@scripts/vitest-config';
+import path from 'path';
 
 const config = defineConfig({
   test: {
     root: __dirname,
     environment: 'node',
     setupFiles: ['./tests/setup.ts'],
+  },
+  resolve: {
+    alias: {
+      '@rspack-builder/tests': path.resolve(__dirname, '../builder-rspack-provider/tests'),
+    }
   },
 });
 
--- tests/e2e/builder/cases/stylus/index.test.ts
+++ tests/e2e/builder/cases/stylus/index.test.ts
@@ -1,11 +1,11 @@
 import path from 'path';
 import { expect } from '@modern-js/e2e/playwright';
-import { webpackOnlyTest } from '@scripts/helper';
+import { allProviderTest } from '@scripts/helper';
 import { build } from '@scripts/shared';
 
 import { builderPluginStylus } from '@modern-js/builder-plugin-stylus';
 
-webpackOnlyTest('should compile stylus correctly', async () => {
+allProviderTest('should compile stylus correctly', async () => {
   const builder = await build({
     cwd: __dirname,
     entry: { index: path.resolve(__dirname, './src/index.js') },
@@ -16,7 +16,13 @@ webpackOnlyTest('should compile stylus correctly', async () => {
   const content =
     files[Object.keys(files).find(file => file.endsWith('.css'))!];
 
-  expect(content).toEqual(
-    'body{color:red;font:14px Arial,sans-serif}.XQprm{font-size:14px}',
-  );
+  if (builder.providerType === 'rspack') {
+    expect(content).toEqual(
+      'body{color:#f00;font:14px Arial,sans-serif}.KPtXW{font-size:14px}',
+    );
+  } else {
+    expect(content).toEqual(
+      'body{color:red;font:14px Arial,sans-serif}.XQprm{font-size:14px}',
+    );
+  }
 });
