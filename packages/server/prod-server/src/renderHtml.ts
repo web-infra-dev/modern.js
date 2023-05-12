9845fcbf57bc60803da5ced0c23e608b7d8510ae
GiveMe-A-Name <a455300764b@hotmail.com>
2023-05-12 11:08:24 +0800

    feat: support custom render html

---
+++ packages/server/prod-server/src/renderHtml.ts
@@ -0,0 +1,117 @@
+import {
+  IncomingMessage,
+  ServerResponse,
+  IncomingHttpHeaders,
+  OutgoingMessage,
+} from 'http';
+import { Readable } from 'stream';
+import { Socket } from 'net';
+import { Server } from './server';
+import { ModernServerOptions } from './type';
+
+class IncomingMessageLike extends Readable {
+  headers: IncomingHttpHeaders;
+
+  method?: string | undefined;
+
+  url?: string | undefined;
+
+  socket: Socket;
+
+  constructor({
+    method,
+    url,
+    headers,
+  }: {
+    method?: string;
+    url?: string;
+    headers?: IncomingHttpHeaders;
+  }) {
+    super();
+    this.socket = new Socket();
+    this.headers = headers || {};
+    this.method = method || 'get';
+    this.url = url;
+  }
+}
+
+class ServerResponseLike extends OutgoingMessage {
+  public data: string[];
+
+  constructor() {
+    super();
+    this.data = [];
+  }
+
+  end(cb?: (() => void) | undefined): this;
+  end(chunk: any, cb?: (() => void) | undefined): this;
+  end(
+    chunk: any,
+    encoding: BufferEncoding,
+    cb?: (() => void) | undefined,
+  ): this;
+  end(chunk?: unknown, _encoding?: unknown, cb?: unknown): this {
+    this.data.push((chunk as any).toString());
+
+    cb && (cb as () => void)();
+    this.emit('finish');
+
+    return this;
+  }
+}
+
+class CustomServer extends Server {
+  public async render(
+    req: IncomingMessageLike,
+    res: ServerResponseLike | unknown,
+    _url?: string | undefined,
+  ): Promise<string | Readable | null> {
+    const handler = this.getRequestHandler();
+    handler(req as unknown as IncomingMessage, res as ServerResponse);
+    return null;
+  }
+}
+
+interface RenderHtmlOptions {
+  /** request url */
+  url: string;
+
+  /** request method */
+  method?: string;
+
+  /** request headers  */
+  headers?: IncomingHttpHeaders;
+
+  serverOptions: ModernServerOptions;
+}
+
+async function renderHtml({
+  url,
+  method,
+  headers,
+  serverOptions,
+}: RenderHtmlOptions): Promise<string> {
+  const req = new IncomingMessageLike({
+    method,
+    url,
+    headers,
+  });
+
+  const res = new ServerResponseLike();
+
+  const customServer = new CustomServer(serverOptions);
+
+  await customServer.init({
+    disableHttpServer: true,
+  });
+
+  customServer.render(req, res);
+
+  return new Promise(resolve => {
+    res.addListener('finish', () => {
+      resolve(res.data.join(''));
+    });
+  });
+}
+
+module.exports = renderHtml;
