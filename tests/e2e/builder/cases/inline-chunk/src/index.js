8368be13be42a8f6bf0443810f9970b2a3cb7c0b
gaoyuan <gaoyuan.1226@bytedance.com>
2023-05-18 10:21:56 +0800

    test(e2e): add enableCssModuleTSDeclaration and disableCssExtract e2e cases (#3704)
    * fix: add enableCssModuleTSDeclaration and disableCssExtract test case
    
    * fix: migrate some css test case from tests/integration to  e2e/builder
    
    * fix: remove repeated test case

--- tests/e2e/builder/cases/inline-chunk/src/index.js
+++ tests/e2e/builder/cases/inline-chunk/src/index.js
@@ -1,5 +1,8 @@
 import './style.css';
 
+// eslint-disable-next-line no-undef
+window.test = 'aaaa';
+
 import(
   /* webpackChunkName: "foo" */
   './foo'
--- tests/integration/css/fixtures/single-sass/src/App.jsx
+++
@@ -1,3 +0,0 @@
-import './app.scss';
-
-export default () => 'single-scss';
