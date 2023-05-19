056627fa8ab52e3e900b4f8c4c20782b7dac6bc5
Asuka109 <chenwenjie.asuka@bytedance.com>
2023-05-19 15:43:50 +0800

    fix(plugin-sass): pollute the `global.location` object (#3712)
    * feat(plugin-sass): add `patchGlobalLocation` & `unpatchGlobalLocation` to `@modern-js/utils`
    
    * fix(plugin-sass): `global.location` object will crash SSR server
    
    * feat: move `patch` to `@modern-js/builder-shared`
    
    * fix: can't set `GLOBAL_PATCHED_SYMBOL` as prop of a string
    
    * feat: add new function `patchCompilerGlobalLocation`
    
    * feat: only create href and location variables if `global.location` is not defined
    
    * feat: remove assert

---
+++ packages/builder/builder-shared/src/patch.ts
@@ -0,0 +1,48 @@
+import path from 'path';
+import { pathToFileURL } from 'url';
+import type {
+  Compiler as WebpackCompiler,
+  MultiCompiler as WebpackMultiCompiler,
+} from 'webpack';
+import type {
+  Compiler as RspackCompiler,
+  MultiCompiler as RspackMultiCompiler,
+} from '@rspack/core';
+
+const GLOBAL_PATCHED_SYMBOL: unique symbol = Symbol('GLOBAL_PATCHED_SYMBOL');
+
+declare global {
+  interface Location {
+    [GLOBAL_PATCHED_SYMBOL]?: true;
+  }
+}
+
+/** fix issue about dart2js: https://github.com/dart-lang/sdk/issues/27979 */
+export function patchGlobalLocation() {
+  if (!global.location) {
+    const href = pathToFileURL(process.cwd()).href + path.sep;
+    const location = Object.freeze({ [GLOBAL_PATCHED_SYMBOL]: true, href });
+    global.location = location as unknown as Location;
+  }
+}
+
+export function unpatchGlobalLocation() {
+  if (global.location?.[GLOBAL_PATCHED_SYMBOL]) {
+    // @ts-expect-error
+    delete global.location;
+  }
+}
+
+export function patchCompilerGlobalLocation(
+  compiler:
+    | WebpackCompiler
+    | RspackCompiler
+    | WebpackMultiCompiler
+    | RspackMultiCompiler,
+) {
+  // https://github.com/webpack/webpack/blob/136b723023f8f26d71eabdd16badf04c1c8554e4/lib/MultiCompiler.js#L64
+  compiler.hooks.run.tap('PatchGlobalLocation', patchGlobalLocation);
+  compiler.hooks.watchRun.tap('PatchGlobalLocation', patchGlobalLocation);
+  compiler.hooks.watchClose.tap('PatchGlobalLocation', unpatchGlobalLocation);
+  compiler.hooks.done.tap('PatchGlobalLocation', unpatchGlobalLocation);
+}
