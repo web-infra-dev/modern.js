e7ac790646bef8f341b76794de83fd53cbddc806
yangxingyuan <yangxingyuan@bytedance.com>
2023-05-22 18:08:39 +0800

    fix: e2e error

--- packages/cli/doc-plugin-medium-zoom/src/index.ts
+++ packages/cli/doc-plugin-medium-zoom/src/index.ts
@@ -7,11 +7,14 @@ import type { DocPlugin } from '../../doc-core';
  * The plugin is used to add medium-zoom to the doc site.
  */
 export function pluginMediumZoom(): DocPlugin {
-  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const __dirname = path
+    .dirname(fileURLToPath(import.meta.url))
+    // Fix: adapt windows
+    .replace(/\\/g, '/');
   return {
     name: '@modern-js/doc-plugin-medium-zoom',
     globalUIComponents: [
-      path.join(__dirname, '../src/components/MediumZoom.tsx'),
+      path.posix.join(__dirname, '../src/components/MediumZoom.tsx'),
     ],
   };
 }
