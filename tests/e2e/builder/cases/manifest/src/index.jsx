1e5eae89db27b0b0d22723d1d228afefbf1fb9b6
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2023-05-22 16:03:28 +0800

    fix: add enableAssetManifest test case & update doc

---
+++ tests/e2e/builder/cases/manifest/src/index.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable no-undef */
+import React from 'react';
+import { render } from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <div id="test">Hello Builder!</div>
+    </div>
+  );
+}
+
+render(React.createElement(App), document.getElementById('root'));
