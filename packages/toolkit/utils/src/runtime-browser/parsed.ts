0063e187c11914b644b65693a39429cb3ebc7e22
kongjiacong <kongjiacong@bytedance.com>
2023-05-29 14:23:49 +0800

    fix: browser should get last element that server insert to html for baseUrl

--- packages/toolkit/utils/src/runtime-browser/parsed.ts
+++ packages/toolkit/utils/src/runtime-browser/parsed.ts
@@ -1,5 +1,10 @@
 export const parsedJSONFromElement = (id: string) => {
-  const element = document.getElementById(id);
+  const elements = document.querySelectorAll(`#${id}`);
+  if (elements.length === 0) {
+    return undefined;
+  }
+  const element = elements[elements.length - 1];
+
   if (element) {
     try {
       const parsed = JSON.parse(element?.textContent || '');
---
+++ tests/integration/ssg/fixtures/nested-routes/src/routes/user/page.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="text-center" id="data">
+      Hello, User
+    </div>
+  );
+};
+
+export default App;
