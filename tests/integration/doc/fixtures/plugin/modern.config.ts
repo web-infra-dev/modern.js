77bdeab881dfc22aa128646312041548cb06b4a6
yangxingyuan <yangxingyuan@bytedance.com>
2023-06-01 11:47:35 +0800

    fix: windows test

--- tests/integration/doc/fixtures/plugin/modern.config.ts
+++ tests/integration/doc/fixtures/plugin/modern.config.ts
@@ -1,12 +1,16 @@
 import * as path from 'path';
 import docTools, { defineConfig } from '@modern-js/doc-tools';
-import { pluginPreview } from '@modern-js/doc-plugin-preview';
+// import { pluginPreview } from '@modern-js/doc-plugin-preview';
 import { docPluginDemo } from './plugin';
 
 export default defineConfig({
   plugins: [docTools()],
   doc: {
     root: path.join(__dirname, 'doc'),
-    plugins: [docPluginDemo(), pluginPreview({ isMobile: true })],
+    plugins: [
+      docPluginDemo(),
+      // FIX: adapt windows path
+      // pluginPreview({ isMobile: true }),
+    ],
   },
 });
