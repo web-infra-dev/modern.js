271f2c1cdd333910f6a33c54a2a0c2c557b55e87
yangxingyuan <39261479+sanyuan0704@users.noreply.github.com>
2023-06-01 10:23:13 +0800

    fix(doc-plugin-preview): build error in first time (#3820)
    * fix: preview plugin build error
    
    * feat: optimize loader impl
    
    * chore: comment
    
    * feat: optimize demoMeta

--- tests/integration/doc/fixtures/plugin/modern.config.ts
+++ tests/integration/doc/fixtures/plugin/modern.config.ts
@@ -1,16 +1,12 @@
 import * as path from 'path';
 import docTools, { defineConfig } from '@modern-js/doc-tools';
-// import { pluginPreview } from '@modern-js/doc-plugin-preview';
+import { pluginPreview } from '@modern-js/doc-plugin-preview';
 import { docPluginDemo } from './plugin';
 
 export default defineConfig({
   plugins: [docTools()],
   doc: {
     root: path.join(__dirname, 'doc'),
-    plugins: [
-      docPluginDemo(),
-      // FIXME: demo not found error
-      // pluginPreview({ isMobile: true })
-    ],
+    plugins: [docPluginDemo(), pluginPreview({ isMobile: true })],
   },
 });
