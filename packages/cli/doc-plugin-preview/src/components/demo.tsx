42d9e6b11b0ebd0f983cc4ac1752c61c55402fbd
10Derozan <fupeng.antik@gmail.com>
2023-06-05 16:25:14 +0800

    fix(doc-plugin-preview): 404 error caused by not validate id in demo component

--- packages/cli/doc-plugin-preview/src/components/demo.tsx
+++ packages/cli/doc-plugin-preview/src/components/demo.tsx
@@ -1,13 +1,15 @@
 import { demos } from 'virtual-meta';
 import { createElement } from 'react';
 import { NoSSR, useLocation } from '@modern-js/doc-core/runtime';
+import { toValidVarName } from '../utils';
 
 export default function Demo() {
   // get the id from the pathname
   const { pathname } = useLocation();
   const id = pathname.split('/').pop();
+  const validId = toValidVarName(id || '');
   // get component from virtual-meta
-  const result = demos.flat().find(item => item.id === id);
+  const result = demos.flat().find(item => item.id === validId);
   if (result) {
     return <NoSSR>{createElement(result.component)}</NoSSR>;
   } else {
