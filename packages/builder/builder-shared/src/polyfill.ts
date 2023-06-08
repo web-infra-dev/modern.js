d4e85c1aed94f1247a365f5dddefe071662c5191
neverland <chenjiahan.jait@bytedance.com>
2023-06-08 18:09:36 +0800

    fix(rspack-provider): should inject polyfill for web-worker target (#3896)

--- packages/builder/builder-shared/src/polyfill.ts
+++ packages/builder/builder-shared/src/polyfill.ts
@@ -1,26 +1,22 @@
-import { createVirtualModule } from './utils';
-import type { BundlerChain, SharedNormalizedConfig } from './types';
+import { createVirtualModule, isWebTarget } from './utils';
+import type {
+  BuilderTarget,
+  BundlerChain,
+  SharedNormalizedConfig,
+} from './types';
 import type WebpackChain from '../compiled/webpack-5-chain';
-
-const enableCoreJsEntry = (
-  config: SharedNormalizedConfig,
-  isServer: boolean,
-  isServiceWorker: boolean,
-) => config.output.polyfill === 'entry' && !isServer && !isServiceWorker;
 
 /** Add core-js-entry to every entries. */
 export function addCoreJsEntry({
   chain,
   config,
-  isServer,
-  isServiceWorker,
+  target,
 }: {
   chain: BundlerChain | WebpackChain;
   config: SharedNormalizedConfig;
-  isServer: boolean;
-  isServiceWorker: boolean;
+  target: BuilderTarget;
 }) {
-  if (enableCoreJsEntry(config, isServer, isServiceWorker)) {
+  if (config.output.polyfill === 'entry' && isWebTarget(target)) {
     const entryPoints = Object.keys(chain.entryPoints.entries() || {});
     const coreJsEntry = createVirtualModule('import "core-js";');
 
