362678dc29c75f712e514c89b57bc1bf892a0011
chenjiahan <chenjiahan.jait@bytedance.com>
2023-06-09 11:02:49 +0800

    chore: revert some changes

--- packages/cli/plugin-bff/tests/loader.test.ts
+++ packages/cli/plugin-bff/tests/loader.test.ts
@@ -1,11 +1,27 @@
 import path from 'path';
-import { initSnapshotSerializer } from '@scripts/jest-config/utils';
 import { compiler } from './compiler';
 
 const apiDir = path.resolve(__dirname, './fixtures/function/api');
 const filepath = path.resolve(__dirname, './fixtures/function/api/hello.ts');
 
-initSnapshotSerializer({ cwd: __dirname });
+const root = path.resolve(__dirname, '../../../../');
+expect.addSnapshotSerializer({
+  test: val =>
+    typeof val === 'string' &&
+    (val.includes('modern.js') ||
+      val.includes('node_modules') ||
+      val.includes(root)),
+  print: val =>
+    // eslint-disable-next-line no-nested-ternary
+    typeof val === 'string'
+      ? // eslint-disable-next-line no-nested-ternary
+        val.includes('node_modules')
+        ? `"${val.replace(/'.+node_modules/, `'`)}"`
+        : val.includes('modern.js')
+        ? `"${val.replace(/'.+modern\.js/, `'`)}"`
+        : `"${val.replace(root, '')}"`
+      : (val as string),
+});
 
 describe('bff loader', () => {
   it('should works well', async () => {
--- packages/cli/plugin-data-loader/tests/loader.test.ts
+++ packages/cli/plugin-data-loader/tests/loader.test.ts
@@ -1,6 +1,5 @@
 import path from 'path';
 import { fs } from '@modern-js/utils';
-import { initSnapshotSerializer } from '@scripts/jest-config/utils';
 import { compiler } from './compiler';
 
 // compiler needs setImmediate
@@ -8,7 +7,24 @@ import { compiler } from './compiler';
 // @ts-expect-error
 global.setImmediate = setTimeout;
 
-initSnapshotSerializer({ cwd: __dirname });
+const root = path.resolve(__dirname, '../../../../');
+expect.addSnapshotSerializer({
+  test: val =>
+    typeof val === 'string' &&
+    (val.includes('modern.js') ||
+      val.includes('node_modules') ||
+      val.includes(root)),
+  print: val =>
+    // eslint-disable-next-line no-nested-ternary
+    typeof val === 'string'
+      ? // eslint-disable-next-line no-nested-ternary
+        val.includes('node_modules')
+        ? `"${val.replace(/'.+node_modules/, `'`)}"`
+        : val.includes('modern.js')
+        ? `"${val.replace(/'.+modern\.js/, `'`)}"`
+        : `"${val.replace(root, '')}"`
+      : (val as string),
+});
 
 describe('data loader', () => {
   let id = 0;
@@ -16,7 +32,10 @@ describe('data loader', () => {
   const mapFile = path.join(__dirname, 'fixtures/loader/map.json');
   const dataLoaderPath = `${path.resolve(__dirname, '../src/cli/loader.ts')}!`;
 
-  const loaderPath = `${dataLoaderPath}./loader?mapFile=${mapFile}`;
+  const loaderPath = `${dataLoaderPath}./loader?mapFile=${mapFile}`.replace(
+    /\\/g,
+    '/',
+  );
 
   const code = `
   import loader from '${loaderPath}';
