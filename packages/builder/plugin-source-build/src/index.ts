d41d16a0f5638849076e5dcea2ec732e51968101
targeral <targeral@outlook.com>
2023-06-12 21:16:04 +0800

    feat: support source code build
    1. Add monorepo-utils and get dependency project list with 'source' field.
    2. Create source-build builder plugin
    3. Add loadClosestTsconfig feature for TsConfigPathsPlugin and update resolve plugin config
    4. Bootstrap builder-plugin-source-build in app-tools

---
+++ packages/builder/plugin-source-build/src/index.ts
@@ -0,0 +1,108 @@
+import { setConfig } from '@modern-js/builder-shared';
+import type { BuilderPlugin } from '@modern-js/builder';
+import type { BuilderPluginAPI as WebpackBuilderPluginAPI } from '@modern-js/builder-webpack-provider';
+import type { BuilderPluginAPI as RspackBuilderPluginAPI } from '@modern-js/builder-rspack-provider';
+import {
+  type ExtraMonorepoStrategies,
+  getDependentProjects,
+  filterByField,
+} from '@modern-js/monorepo-utils';
+
+export const getSourceInclude = async (options: {
+  projectNameOrRootPath: string;
+  findMonorepoStartPath: string;
+  sourceField: string;
+  extraMonorepoStrategies?: ExtraMonorepoStrategies;
+}) => {
+  const {
+    projectNameOrRootPath,
+    sourceField,
+    extraMonorepoStrategies,
+    findMonorepoStartPath,
+  } = options;
+  const projects = await getDependentProjects(projectNameOrRootPath, {
+    cwd: findMonorepoStartPath,
+    recursive: true,
+    filter: filterByField(sourceField),
+    extraMonorepoStrategies,
+  });
+
+  const includes = [];
+  for (const project of projects) {
+    includes.push(...project.getSourceEntryPaths());
+  }
+
+  return includes;
+};
+
+export interface PluginSourceBuildOptions {
+  projectName?: string;
+  sourceField?: string;
+  extraMonorepoStrategies?: ExtraMonorepoStrategies;
+}
+
+/**
+ * Usage:
+ *
+ *   const { builderPluginSourceBuild } = await import('@modern-js/builder-plugin-source-build');
+ *
+ *   builder.addPlugins([ builderPluginSourceBuild() ]);
+ */
+export function builderPluginSourceBuild(
+  options?: PluginSourceBuildOptions,
+): BuilderPlugin<WebpackBuilderPluginAPI | RspackBuilderPluginAPI> {
+  const {
+    projectName,
+    sourceField = 'source',
+    extraMonorepoStrategies,
+  } = options ?? {};
+  return {
+    name: 'builder-plugin-source-build',
+
+    async setup(api) {
+      const projectRootPath = api.context.rootPath;
+      api.modifyBuilderConfig(async config => {
+        const includes = await getSourceInclude({
+          projectNameOrRootPath: projectName ?? projectRootPath,
+          sourceField,
+          findMonorepoStartPath: projectRootPath,
+          extraMonorepoStrategies,
+        });
+        config.source = config.source ?? {};
+        config.source.include = [...(config.source.include ?? []), ...includes];
+      });
+
+      api.modifyBundlerChain(chain => {
+        // Now not support chain.resolve.conditionNames API
+        // chain.resolve.conditionNames.prepend(sourcePkgField);
+
+        // when user not config source.resolveMainFields, mainFields is empty array
+        if (chain.resolve.mainFields.values().length === 0) {
+          // "..." is special syntax,it will retain the original value
+          chain.resolve.mainFields.prepend('...');
+          chain.resolve.mainFields.prepend(sourceField);
+        }
+      });
+
+      // when support chain.resolve.conditionNames API, remove this logic
+      if (api.context.bundlerType === 'rspack') {
+        (api as RspackBuilderPluginAPI).modifyRspackConfig(async config => {
+          setConfig(config, 'resolve.conditionNames', [
+            '...', // Special syntax: retain the original value
+            sourceField,
+            ...(config.resolve?.conditionNames ?? []),
+          ]);
+        });
+      } else {
+        (api as WebpackBuilderPluginAPI).modifyWebpackConfig(async config => {
+          config.resolve = config.resolve ?? {};
+          config.resolve.conditionNames = [
+            '...', // Special syntax: retain the original value
+            sourceField,
+            ...(config.resolve.conditionNames ?? []),
+          ];
+        });
+      }
+    },
+  };
+}
---
+++ packages/toolkit/monorepo-utils/src/common/index.ts
@@ -0,0 +1,9 @@
+export * from './getBaseData';
+export * from './isMonorepo';
+
+export { getMonorepoSubProjects } from './getProjects';
+export { getProjects as getPnpmMonorepoSubProjects } from './pnpm';
+export { getProjects as getRushMonorepoSubProjects } from './rush';
+
+export type { GetProjectsFunc } from './getProjects';
+export * from './types';
