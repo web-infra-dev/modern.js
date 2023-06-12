34bb8d3174845db7d6a60af4ad1b8af9f5851855
chenjiahan <chenjiahan.jait@bytedance.com>
2023-06-12 14:09:49 +0800

    fix(builder): single-vendor chunkSplit not work as expected

--- tests/e2e/builder/cases/performance/basic/src/index.ts
+++ tests/e2e/builder/cases/performance/basic/src/index.ts
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
