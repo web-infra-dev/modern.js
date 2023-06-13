38e76c1d36e29e5ffc546fdf745df0064268cb2b
chenjiahan <chenjiahan.jait@bytedance.com>
2023-06-13 15:21:08 +0800

    fix: clean

--- tests/e2e/builder/cases/cache/index.test.ts
+++ tests/e2e/builder/cases/cache/index.test.ts
@@ -7,6 +7,13 @@ import { fs } from '@modern-js/utils';
 webpackOnlyTest(
   'should save the buildDependencies to cache directory',
   async () => {
+    const configFile = path.resolve(
+      __dirname,
+      './node_modules/.cache/webpack/buildDependencies.json',
+    );
+
+    fs.removeSync(configFile);
+
     await build({
       cwd: __dirname,
       entry: { index: path.resolve(__dirname, './src/index.js') },
@@ -18,10 +25,6 @@ webpackOnlyTest(
       },
     });
 
-    const configFile = path.resolve(
-      __dirname,
-      './node_modules/.cache/webpack/buildDependencies.json',
-    );
     const buildDependencies = await fs.readJSON(configFile);
     expect(Object.keys(buildDependencies)).toEqual([
       'packageJson',
