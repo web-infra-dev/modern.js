405c4ea7005fa58a71f4b9ba9fab74cb43434a90
GiveMe-A-Name <a455300764b@hotmail.com>
2023-06-14 10:29:02 +0800

    refactor: add a factory in plugin-swc

--- packages/cli/plugin-swc/src/index.ts
+++ packages/cli/plugin-swc/src/index.ts
@@ -6,46 +6,56 @@ import {
 } from '@modern-js/builder-plugin-swc';
 import { logger } from '@modern-js/utils/logger';
 
-export default (): CliPlugin<AppTools> => ({
-  name: '@modern-js/plugin-swc',
+export function factory(
+  name: string,
+  modifySwcOptions: (options: PluginSwcOptions) => PluginSwcOptions,
+): () => CliPlugin<AppTools> {
+  return () => ({
+    name,
+    setup: api => ({
+      validateSchema() {
+        return PLUGIN_SCHEMAS['@modern-js/plugin-swc'];
+      },
 
-  setup: api => ({
-    validateSchema() {
-      return PLUGIN_SCHEMAS['@modern-js/plugin-swc'];
-    },
-
-    prepare() {
-      const context = api.useAppContext();
-      if (!context.builder) {
-        return;
-      }
-
-      const config = api.useResolvedConfigContext();
-      const { esbuild, swc = {} } = config.tools;
-
-      // for useloader api use
-      (swc as PluginSwcOptions).extensions = {
-        ...(swc.extensions || {}),
-        ssrLoaderId: {
-          runtimePackageName: '@modern-js/runtime',
-          functionUseLoaderName: 'useloader',
-        },
-      };
-
-      context.builder.addPlugins([builderPluginSwc(swc)]);
-
-      if (esbuild) {
-        if (swc.jsMinify !== false && esbuild.minimize !== false) {
-          logger.warn(
-            'You have enabled both esbuild minimizer and SWC minimizer, which will cause conflicts. Please remove `tools.esbuild` config and only use SWC to minimize your code.',
-          );
+      prepare() {
+        const context = api.useAppContext();
+        if (!context.builder) {
+          return;
         }
-        if (esbuild.loader !== false) {
-          logger.warn(
-            'You have enabled both esbuild loader and SWC loader, which will cause conflicts. Please remove `tools.esbuild` config and only use SWC to transform your code.',
-          );
+
+        const config = api.useResolvedConfigContext();
+        const { esbuild, swc = {} } = config.tools;
+
+        // for useloader api use
+        const swcOptions = modifySwcOptions(swc);
+
+        context.builder.addPlugins([builderPluginSwc(swcOptions)]);
+
+        if (esbuild) {
+          if (swc.jsMinify !== false && esbuild.minimize !== false) {
+            logger.warn(
+              'You have enabled both esbuild minimizer and SWC minimizer, which will cause conflicts. Please remove `tools.esbuild` config and only use SWC to minimize your code.',
+            );
+          }
+          if (esbuild.loader !== false) {
+            logger.warn(
+              'You have enabled both esbuild loader and SWC loader, which will cause conflicts. Please remove `tools.esbuild` config and only use SWC to transform your code.',
+            );
+          }
         }
-      }
+      },
+    }),
+  });
+}
+
+const PLUGIN_NAME = '@modern-js/plugin-swc';
+export default factory(PLUGIN_NAME, swcOptions => {
+  swcOptions.extensions = {
+    ...(swcOptions.extensions || {}),
+    ssrLoaderId: {
+      runtimePackageName: '@modern-js/runtime',
+      functionUseLoaderName: 'useloader',
     },
-  }),
+  };
+  return swcOptions;
 });
