4715b03d623b9b63d13ce72358f56f4e7fac6a2b
chenjiahan <chenjiahan.jait@bytedance.com>
2023-06-15 13:22:18 +0800

    fix(builder): can't use new React JSX with parent's package.json

--- tests/e2e/builder/cases/dev/hmr/src/App.tsx
+++ tests/e2e/builder/cases/dev/hmr/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './App.css';
 
 const App = () => <div id="test">Hello Builder!</div>;
--- tests/e2e/builder/cases/output/externals/src/App.jsx
+++ tests/e2e/builder/cases/output/externals/src/App.jsx
@@ -1,5 +1,3 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
 import { a } from './aaa';
 
 function App() {
