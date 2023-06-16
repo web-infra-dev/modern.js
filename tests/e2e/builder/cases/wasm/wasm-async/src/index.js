4ba184ccb5620a7b8692972bc33b24553c9ccb64
chenjiahan <chenjiahan.jait@bytedance.com>
2023-06-16 15:22:23 +0800

    chore: update

--- tests/e2e/builder/cases/wasm/wasm-async/src/index.js
+++ tests/e2e/builder/cases/wasm/wasm-async/src/index.js
@@ -1,6 +1,4 @@
 import('./factorial.wasm').then(({ _Z4facti: AsyncFactorial }) => {
-  console.log('---- Async Wasm Module');
-  console.log(AsyncFactorial); // [native code]
   console.log(AsyncFactorial(1));
   console.log(AsyncFactorial(2));
   console.log(AsyncFactorial(3));
