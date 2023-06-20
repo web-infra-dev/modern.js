1911e00657b16fc854b9b659da04c7a166612eb3
caohuilin <caohuilin@bytedance.com>
2023-06-20 15:03:24 +0800

    feat: add test case

--- packages/solutions/app-tools/src/builder/shared/builderPlugins/adapterHtml.ts
+++ packages/solutions/app-tools/src/builder/shared/builderPlugins/adapterHtml.ts
@@ -6,6 +6,7 @@ import {
 } from '@modern-js/builder-shared';
 import {
   ChainIdentifier,
+  MAIN_ENTRY_NAME,
   getEntryOptions,
   removeTailSlash,
 } from '@modern-js/utils';
@@ -89,14 +90,17 @@ function applyBottomHtmlPlugin<B extends Bundler>({
 }) {
   const { normalizedConfig: modernConfig, appContext } = options;
   // inject bottomTemplate into html-webpack-plugin
-  console.info('api.context.entry', api.context.entry);
   for (const entryName of Object.keys(api.context.entry)) {
+    const {
+      source: { mainEntryName },
+    } = modernConfig;
+    const isMainEntry = entryName === (mainEntryName || MAIN_ENTRY_NAME);
     // FIXME: the only need necessary
     const baseTemplateParams = {
       entryName,
       title: getEntryOptions<string | undefined>(
         entryName,
-        false, // TODO check
+        isMainEntry,
         modernConfig.html.title,
         modernConfig.html.titleByEntries,
         appContext.packageName,
@@ -104,7 +108,7 @@ function applyBottomHtmlPlugin<B extends Bundler>({
       mountId: modernConfig.html.mountId,
       ...getEntryOptions<any>(
         entryName,
-        false, // TODO check
+        isMainEntry,
         modernConfig.html.templateParameters,
         modernConfig.html.templateParametersByEntries,
         appContext.packageName,
