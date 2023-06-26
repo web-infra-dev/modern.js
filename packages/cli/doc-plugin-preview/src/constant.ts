a71a394e091ebe47cbd8a2b96f2861779f3af221
10Derozan <fupeng.antik@gmail.com>
2023-06-26 15:49:57 +0800

    feat(doc-plugin-preview): add DemoBlock for preview in mobile mode (#4052)

---
+++ packages/cli/doc-plugin-preview/src/constant.ts
@@ -0,0 +1,16 @@
+import path from 'path';
+
+export const demoComponentPath = path.join(
+  __dirname,
+  '..',
+  'dist',
+  'virtual-demo.js',
+);
+
+export const demoBlockComponentPath = path.join(
+  __dirname,
+  '..',
+  'static',
+  'global-components',
+  'DemoBlock.tsx',
+);
