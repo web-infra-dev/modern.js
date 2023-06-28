841ac8e92fa938ef6cbc0e79eaf7e69bf5b6eb90
neverland <chenjiahan.jait@bytedance.com>
2023-06-28 11:16:49 +0800

    test(builder): add more e2e cases for Vue 3 sfc (#4072)

---
+++ tests/e2e/builder/cases/vue/sfc-lang-ts/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button id="button" @click="count++">count: {{ count }} foo: {{ props.foo }}</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps<{
+  foo: string
+}>()
+
+const count = ref<number>(0);
+</script>
