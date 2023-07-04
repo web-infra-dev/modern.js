8f192fa9eb8f9baf050e769a49d7d4eaabb425a1
neverland <chenjiahan.jait@bytedance.com>
2023-07-04 17:50:39 +0800

    feat(builder-cli): support register builder plugins (#4123)
    * feat(builder-cli): support register builder plugins
    
    * chore: preserve jsx

---
+++ tests/e2e/builder/cases/builder-cli/builder-cli-vue/src/A.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>The count is {{ count }}</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const count = ref(0);
+
+    return {
+      count,
+    };
+  },
+};
+</script>
