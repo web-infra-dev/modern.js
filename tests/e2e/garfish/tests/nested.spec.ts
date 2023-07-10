494af9e19e4345bbff82d7c28b6eb770cfb18f40
neverland <chenjiahan.jait@bytedance.com>
2023-07-10 15:25:19 +0800

    test(e2e): improve the perf and stability of garfish e2e cases (#4169)
    * test(e2e): improve the perf and stability of garfish e2e cases
    
    * chore: fix lock file

--- tests/e2e/garfish/tests/nested.spec.ts
+++ tests/e2e/garfish/tests/nested.spec.ts
@@ -7,20 +7,22 @@ let app: unknown;
 let subApp1: unknown;
 let subApp2: unknown;
 
-test.afterAll(async () => {
+test.beforeAll(async () => {
   test.setTimeout(90 * 1000);
+  [app, subApp1, subApp2] = await Promise.all([
+    launchApp(join(__dirname, '../fixtures/main-router-v6')),
+    launchApp(join(__dirname, '../fixtures/dashboard-router-v6')),
+    launchApp(join(__dirname, '../fixtures/table')),
+  ]);
+});
+
+test.afterAll(async () => {
   await killApp(app);
   await killApp(subApp1);
   await killApp(subApp2);
 });
 
 test('render sub app', async ({ page }) => {
-  [app, subApp1, subApp2] = await Promise.all([
-    launchApp(join(__dirname, '../fixtures/main-router-v6')),
-    launchApp(join(__dirname, '../fixtures/dashboard-router-v6')),
-    launchApp(join(__dirname, '../fixtures/table')),
-  ]);
-
   await page.goto(getPublicPath('@e2e/garfish-main-router-v6'));
 
   await page.waitForFunction(() => {
