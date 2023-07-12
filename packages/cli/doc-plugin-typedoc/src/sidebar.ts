671bf9f7b7fe90ecfdf2c738804d1512437f3746
yangxingyuan <39261479+sanyuan0704@users.noreply.github.com>
2023-07-12 09:57:20 +0800

    feat(doc-core): upgrade typedoc to 0.24.8 (#4187)
    * feat: add typedoc plugin
    
    * feat: optimzize the file structure
    
    * fix: doc
    
    * feat: optimize error log while \_meta.json is not correct
    
    * feat(doc-tools): upgrade typedoc to 0.24.8

--- packages/cli/doc-plugin-typedoc/src/sidebar.ts
+++ packages/cli/doc-plugin-typedoc/src/sidebar.ts
@@ -8,56 +8,67 @@ import {
   getFunctionPath,
 } from './utils';
 
+interface ModuleItem {
+  id: number;
+  name: string;
+  children: {
+    id: number;
+    name: string;
+  }[];
+  groups: {
+    title: string;
+    children: number[];
+  }[];
+}
+
 export async function resolveSidebar(jsonDir: string): Promise<SidebarGroup[]> {
   const result: SidebarGroup[] = [];
   const data = JSON.parse(await fs.readFile(jsonDir, 'utf-8'));
   if (!data.children || data.children.length <= 0) {
     return result;
   }
-  data.children.forEach(
-    (module: {
-      name: string;
-      children: {
-        name: string;
-        kindString: 'Class' | 'Interface' | 'Function';
-      }[];
-    }) => {
-      const moduleNames = module.name.split('/');
-      const name = moduleNames[moduleNames.length - 1];
-      const moduleConfig = {
-        text: `Module:${name}`,
-        items: [{ text: 'Overview', link: getModulePath(module.name) }],
-      };
-      if (!module.children || module.children.length <= 0) {
+  data.children.forEach((module: ModuleItem) => {
+    const moduleNames = module.name.split('/');
+    const name = moduleNames[moduleNames.length - 1];
+    const moduleConfig = {
+      text: `Module:${name}`,
+      items: [{ text: 'Overview', link: getModulePath(module.name) }],
+    };
+    if (!module.children || module.children.length <= 0) {
+      return;
+    }
+    module.children.forEach(sub => {
+      const kindString = module.groups
+        .find(item => item.children.includes(sub.id))
+        ?.title.slice(0, -1);
+      if (!kindString) {
         return;
       }
-      module.children.forEach(sub => {
-        switch (sub.kindString) {
-          case 'Class':
-            moduleConfig.items.push({
-              text: `Class:${sub.name}`,
-              link: getClassPath(module.name, sub.name),
-            });
-            break;
-          case 'Interface':
-            moduleConfig.items.push({
-              text: `Interface:${sub.name}`,
-              link: getInterfacePath(module.name, sub.name),
-            });
-            break;
-          case 'Function':
-            moduleConfig.items.push({
-              text: `Function:${sub.name}`,
-              link: getFunctionPath(module.name, sub.name),
-            });
-            break;
-          default:
-            break;
-        }
-      });
-      result.push(moduleConfig);
-    },
-  );
+      switch (kindString) {
+        case 'Class':
+          moduleConfig.items.push({
+            text: `Class:${sub.name}`,
+            link: getClassPath(module.name, sub.name),
+          });
+          break;
+        case 'Interface':
+          moduleConfig.items.push({
+            text: `Interface:${sub.name}`,
+            link: getInterfacePath(module.name, sub.name),
+          });
+          break;
+        case 'Function':
+          moduleConfig.items.push({
+            text: `Function:${sub.name}`,
+            link: getFunctionPath(module.name, sub.name),
+          });
+          break;
+        default:
+          break;
+      }
+    });
+    result.push(moduleConfig);
+  });
   // Patch /api/README.md
   const readmeFile = path.join(path.dirname(jsonDir), 'README.md');
   const readme = await fs.readFile(readmeFile, 'utf-8');
