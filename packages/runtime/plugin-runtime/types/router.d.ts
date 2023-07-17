dd82d645bf96bb636ae74dc17b04abdd83b03ab5
jason.xu <jason.xu@bytedance.com>
2023-07-17 17:56:09 +0800

    chore: remove unnecessary type override

--- packages/runtime/plugin-runtime/types/router.d.ts
+++ packages/runtime/plugin-runtime/types/router.d.ts
@@ -9,12 +9,6 @@ declare module '@modern-js/app-tools' {
 
 declare module '@modern-js/module-tools' {
   interface RuntimeUserConfig {
-    router?: RouterConfig | boolean;
-  }
-}
-
-declare module '@modern-js/runtime' {
-  interface AppConfig {
-    router?: RouterConfig | boolean;
+    router?: Partial<RouterConfig> | boolean;
   }
 }
