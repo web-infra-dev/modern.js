be780c693ff6e3e0b2adca2cf1becbf15ffd8460
targeral <targeral@outlook.com>
2023-07-26 21:11:49 +0800

    test: remove jest mock

--- tests/integration/module/src/inputFilter.test.ts
+++ tests/integration/module/src/inputFilter.test.ts
@@ -4,15 +4,6 @@ import { runCli, initBeforeTest } from './utils';
 
 initBeforeTest();
 
-beforeAll(() => {
-  jest.mock('../src/utils/onExit.ts', () => {
-    return {
-      __esModule: true,
-      addExitListener: jest.fn(() => 'mocked'),
-    };
-  });
-});
-
 describe('input filter', () => {
   const fixtureDir = path.join(__dirname, './fixtures/inputFilter');
 
