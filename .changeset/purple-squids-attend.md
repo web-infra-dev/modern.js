c34c299d59212aaa3198b31f302a7ba7f8921bde
GiveMe-A-Name <a455300764b@hotmail.com>
2023-08-02 12:14:40 +0800

    fix(plugin-runtime): ssr reporter some problems

---
+++ .changeset/purple-squids-attend.md
@@ -0,0 +1,6 @@
+---
+'@modern-js/runtime': patch
+---
+
+fix(plugin-runtime): ssr reporter some problems
+fix(plugin-runtime): ssr reporter 小问题
---
+++ packages/runtime/plugin-runtime/src/ssr/serverRender/constants.ts
@@ -0,0 +1,5 @@
+export enum ServerTimingNames {
+  SSR_RENDER_TOTAL = 'ssr-render-total',
+  SSR_PREFETCH = 'ssr-prefetch',
+  SSR_RENDER_HTML = 'ssr-render-html',
+}
