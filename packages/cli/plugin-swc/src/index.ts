343486470416e4427f901fc1a482e03235bcbf71
jserfeng <1114550440@qq.com>
2023-08-03 20:21:12 +0800

    fix(plugin-swc): correct scheme validation

--- packages/cli/plugin-swc/src/index.ts
+++ packages/cli/plugin-swc/src/index.ts
@@ -19,7 +19,7 @@ export function factory(
         return [
           {
             target: 'tools.swc',
-            schema: { typeof: ['object'] },
+            schema: { typeof: ['object', 'function'] },
           },
         ];
       },
