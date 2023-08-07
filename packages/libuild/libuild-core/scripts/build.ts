da966076f9fb9fb5191e0df769f2744a57226b36
10Derozan <fupeng.antik@gmail.com>
2023-08-07 11:49:38 +0800

    chore(libuild): remove unuse dep and temp dir (#4386)

--- packages/libuild/libuild-core/scripts/build.ts
+++ packages/libuild/libuild-core/scripts/build.ts
@@ -7,7 +7,6 @@ import fse from 'fs-extra';
 import { resolvePathAndQuery } from '@modern-js/libuild-utils';
 import { Extractor, ExtractorConfig } from '@microsoft/api-extractor';
 import remapping from '@ampproject/remapping';
-import esbuildPluginLicense from 'esbuild-plugin-license';
 
 let rebuildCnt = 0;
 
@@ -207,35 +206,6 @@ async function run() {
       workerPlugin(),
       rawPlugin(),
       enhancedResolvePlugin(),
-      !args.watch &&
-        esbuildPluginLicense({
-          thirdParty: {
-            output: {
-              file: '../LICENSE.md',
-              template(dependencies, self) {
-                return `
-# Libuild license
-
-Libuild is released under the MIT license:
-
-The MIT License (MIT)
-
-Copyright (c) 2022 [Libuild](https://github.com/modern-js/libuild)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-## Licenses of bundled dependencies
-
-${dependencies.map((dependency) => `- ${dependency.packageJson.name} -- ${dependency.packageJson.license}`).join('\n')}
-`.trimStart();
-              },
-            },
-          },
-        }),
     ].filter(Boolean) as Plugin[],
   };
 
