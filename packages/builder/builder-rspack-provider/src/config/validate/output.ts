d9ae34f93e6f62e8896245ea01efe721d372e82c
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2023-08-11 11:14:52 +0800

    feat(builder): align minify behavior in rspack-provider

--- packages/builder/builder-rspack-provider/src/config/validate/output.ts
+++ packages/builder/builder-rspack-provider/src/config/validate/output.ts
@@ -4,7 +4,6 @@ import type { OutputConfig } from '../../types';
 export const outputConfigSchema: z.ZodType<OutputConfig> =
   sharedOutputConfigSchema
     .extend({
-      legalComments: z.enum(['none', 'linked']),
       polyfill: z.enum(['entry', 'ua', 'off']),
     })
     .partial();
