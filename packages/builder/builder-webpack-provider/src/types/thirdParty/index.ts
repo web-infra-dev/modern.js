1c838da86386e7cacb65b4196420f635a4befb31
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2023-08-16 15:44:41 +0800

    fix: reuse getJSMinifyOptions

--- packages/builder/builder-webpack-provider/src/types/thirdParty/index.ts
+++ packages/builder/builder-webpack-provider/src/types/thirdParty/index.ts
@@ -1,7 +1,6 @@
 import type WebpackChain from '@modern-js/builder-shared/webpack-5-chain';
 import type webpack from 'webpack';
 import type { Configuration as WebpackConfig } from 'webpack';
-import type TerserPlugin from 'terser-webpack-plugin';
 import type CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
 import type { Options as RawTSLoaderOptions } from 'ts-loader';
 
@@ -10,9 +9,6 @@ export type { InspectorPluginOptions } from '@modern-js/inspector-webpack-plugin
 export type { CopyPluginOptions } from './CopyWebpackPlugin';
 export type { Options as HTMLPluginOptions } from 'html-webpack-plugin';
 export type { SubresourceIntegrityPluginOptions as SubresourceIntegrityOptions } from 'webpack-subresource-integrity';
-
-export type TerserPluginOptions = TerserPlugin.BasePluginOptions &
-  TerserPlugin.DefinedDefaultMinimizerAndOptions<TerserPlugin.TerserOptions>;
 
 export type CssMinimizerPluginOptions = CssMinimizerPlugin.BasePluginOptions &
   CssMinimizerPlugin.DefinedDefaultMinimizerAndOptions<CssMinimizerPlugin.CssNanoOptionsExtended>;
