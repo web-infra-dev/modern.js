1ff80727a7123abe42433a579537495460d2d872
chenwenjie <chenwenjie.asuka@bytedance.com>
2023-08-16 14:08:39 +0800

    feat(client): sticky navbar

--- packages/devtools/client/src/routes/layout.tsx
+++ packages/devtools/client/src/routes/layout.tsx
@@ -1,16 +1,21 @@
 import { Outlet } from '@modern-js/runtime/router';
 import { Suspense } from 'react';
 import SidePanel from './SidePanel';
+import styles from './layout.module.scss';
 import { StoreContextProvider } from '@/stores';
 
 export default function Layout() {
   return (
     <StoreContextProvider>
       <div>
-        <SidePanel />
-        <Suspense fallback={<div>loading...</div>}>
-          <Outlet />
-        </Suspense>
+        <div className={styles.nav}>
+          <SidePanel />
+        </div>
+        <div className={styles.outletContainer}>
+          <Suspense fallback={<div>loading...</div>}>
+            <Outlet />
+          </Suspense>
+        </div>
       </div>
     </StoreContextProvider>
   );
--- packages/devtools/client/src/routes/router/page.tsx
+++ packages/devtools/client/src/routes/router/page.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { useSnapshot } from 'valtio';
-// @ts-expect-error
-import { routes } from '@_modern_js_internal/main/routes';
 import { useStore } from '@/stores';
 
 const Page: React.FC = () => {
@@ -11,7 +9,6 @@ const Page: React.FC = () => {
   return (
     <div>
       <pre>{JSON.stringify(serverRoutes, null, 2)}</pre>
-      <pre>{JSON.stringify(routes, null, 2)}</pre>
     </div>
   );
 };
