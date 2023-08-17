fe70fdfa18dc3d4bd99c8e1979e1081d27608639
gaoyuan <gaoyuan.1226@bytedance.com>
2023-08-17 19:34:56 +0800

    fix(builder): temporarily disable asciiOnly in rspack (#4459)

--- tests/e2e/builder/cases/output/ascii/src/index.js
+++ tests/e2e/builder/cases/output/ascii/src/index.js
@@ -1,2 +1,13 @@
 // eslint-disable-next-line no-undef
 window.a = '你好 world!';
+
+// eslint-disable-next-line no-undef
+window.b = {
+  Д: 'A',
+  Å: 'A',
+  Ð: 'D',
+  Þ: 'o',
+  å: 'a',
+  ð: 'd',
+  þ: 'o',
+};
