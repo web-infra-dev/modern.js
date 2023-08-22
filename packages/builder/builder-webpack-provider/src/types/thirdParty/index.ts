85cbf8619ce86131cd44e9547ec05a30f99bc045
chenjiahan <chenjiahan.jait@bytedance.com>
2023-08-22 16:19:13 +0800

    chore(builder): using unified version of webpack-chain

--- packages/builder/builder-webpack-provider/src/types/thirdParty/index.ts
+++ packages/builder/builder-webpack-provider/src/types/thirdParty/index.ts
@@ -1,4 +1,4 @@
-import type WebpackChain from '@modern-js/builder-shared/webpack-5-chain';
+import type { WebpackChain } from '@modern-js/utils';
 import type webpack from 'webpack';
 import type { Configuration as WebpackConfig } from 'webpack';
 import type CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
