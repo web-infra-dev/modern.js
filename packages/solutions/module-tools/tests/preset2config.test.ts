aee23109ee6b90d9fb204a89445bd4a72cb29b2c
10Derozan <fupeng.antik@gmail.com>
2023-08-22 14:04:23 +0800

    test(module-tools): add merge config tests for module tools

---
+++ packages/solutions/module-tools/tests/preset2config.test.ts
@@ -0,0 +1,33 @@
+import { presetToConfig } from '../src/config/normalize';
+import { BuildPreset } from '../src/types';
+
+describe('presetToConfig', () => {
+  it('extendPreset', async () => {
+    const extendPreset: BuildPreset = function ({ extendPreset }) {
+      return extendPreset('npm-library', {
+        define: {
+          VERSION: '1.0.1',
+        },
+      });
+    };
+    const buildConfig = await presetToConfig(extendPreset);
+    buildConfig!.forEach(c => {
+      expect(c.define!.VERSION).toBe('1.0.1');
+    });
+  });
+  it('preset', async () => {
+    const preset: BuildPreset = function ({ preset }) {
+      const { NPM_LIBRARY } = preset;
+      return NPM_LIBRARY.map(config => {
+        config.define = {
+          VERSION: '1.0.2',
+        };
+        return config;
+      });
+    };
+    const buildConfig = await presetToConfig(preset);
+    buildConfig!.forEach(c => {
+      expect(c.define!.VERSION).toBe('1.0.2');
+    });
+  });
+});
