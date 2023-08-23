db4698d8803f0a53c7a6114c1907121786b397d0
yangxingyuan <yangxingyuan@bytedance.com>
2023-08-23 13:38:49 +0800

    fix(doc-core): reletive link error in runtime

--- packages/cli/doc-core/src/theme-default/layout/DocLayout/docComponents/link.tsx
+++ packages/cli/doc-core/src/theme-default/layout/DocLayout/docComponents/link.tsx
@@ -8,15 +8,22 @@ export const A = (props: ComponentProps<'a'>) => {
   const lang = useLang();
   const pageData = usePageData();
   const defaultLang = pageData.siteData.lang;
+  const startWithLang = removeBase(href).startsWith(`/${lang}`);
+
   if (defaultLang && !isExternalUrl(href) && !href.startsWith('#')) {
     href = normalizeSlash(href);
     // Add lang prefix if not default lang
-    if (lang !== defaultLang && !removeBase(href).startsWith(`/${lang}`)) {
+    if (lang !== defaultLang && !startWithLang) {
       href = `/${lang}${href}`;
+    }
+
+    if (lang === defaultLang && startWithLang) {
+      href = removeBase(href).replace(`/${lang}`, '');
     }
 
     href = withBase(href || '');
   }
+
   return (
     <a {...props} className={`${styles.link} ${props.className}`} href={href} />
   );
