388440252c8e40e8adaa276d56a9fdfa5c239220
GiveMe-A-Name <a455300764b@hotmail.com>
2023-08-28 17:30:54 +0800

    fix: ssr html modify

--- packages/runtime/plugin-runtime/src/ssr/index.node.tsx
+++ packages/runtime/plugin-runtime/src/ssr/index.node.tsx
@@ -23,7 +23,11 @@ export const ssr = (config: SSRPluginConfig = {}): Plugin => ({
 
         context.ssrContext!.request = formatServer(request);
         context.ssrContext!.mode = config.mode;
-        context.ssrContext!.htmlModifiers = [];
+
+        if (context.ssrContext!.htmlModifiers) {
+          context.ssrContext!.htmlModifiers = [];
+        }
+
         return next({ context });
       },
       pickContext: ({ context, pickedContext }, next) => {
