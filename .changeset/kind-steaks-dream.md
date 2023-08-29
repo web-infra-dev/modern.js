52cf1b77207ef940d6d4edb90dfd8a670135436b
chenjiahan <chenjiahan.jait@bytedance.com>
2023-08-29 16:56:29 +0800

    chore: remove doc-generator and related code

---
+++ .changeset/kind-steaks-dream.md
@@ -0,0 +1,9 @@
+---
+'@modern-js/repo-generator': patch
+'@modern-js/generator-common': patch
+'@modern-js/generator-utils': patch
+---
+
+ c
+
+chore: 移除 doc-generator 和相关代码
--- packages/generator/generators/doc-generator/src/locale/en.ts
+++
@@ -1,9 +0,0 @@
-export const EN_LOCALE = {
-  get_packages_error:
-    'get all packages failed，please check this environment and try again',
-  success: `Success！
-You can run the following command in the directory of the new project：
-{packageManager} dev          # Starting the dev server
-{packageManager} build        # Build the document site for production
-{packageManager} serve        # Preview the production build locally`,
-};
