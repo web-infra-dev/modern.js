039f5e3bb885c8ffb3f33aeb429761f6388e987b
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2023-08-31 19:05:13 +0800

    chore: distinguish swc test cases

--- tests/e2e/builder/cases/lodash/index.test.ts
+++ tests/e2e/builder/cases/lodash/index.test.ts
@@ -1,24 +1,7 @@
-import assert from 'assert';
 import * as path from 'path';
 import { expect } from '@modern-js/e2e/playwright';
 import { webpackOnlyTest } from '@scripts/helper';
 import { build } from '@scripts/shared';
-import { builderPluginSwc } from '@modern-js/builder-plugin-swc';
-
-const getIndexFile = async (builder: Awaited<ReturnType<typeof build>>) => {
-  const files = await builder.unwrapOutputJSON();
-  const [lodashBundle, content] =
-    Object.entries(files).find(
-      ([file]) => file.includes('index') && file.endsWith('.js'),
-    ) || [];
-
-  assert(lodashBundle);
-
-  return {
-    content: content!,
-    size: content!.length / 1024,
-  };
-};
 
 // TODO: needs builtin:swc-loader
 webpackOnlyTest('should optimize lodash bundle size by default', async () => {
@@ -37,35 +20,10 @@ webpackOnlyTest('should optimize lodash bundle size by default', async () => {
     runServer: false,
   });
 
-  const { content, size } = await getIndexFile(builder);
+  const { content, size } = await builder.getIndexFile();
   expect(content.includes('debounce')).toBeFalsy();
   expect(size < 10).toBeTruthy();
 });
-
-webpackOnlyTest(
-  'should optimize lodash bundle size when using SWC plugin',
-  async () => {
-    const builder = await build({
-      cwd: __dirname,
-      entry: {
-        index: path.resolve(__dirname, './src/index.ts'),
-      },
-      builderConfig: {
-        performance: {
-          chunkSplit: {
-            strategy: 'all-in-one',
-          },
-        },
-      },
-      plugins: [builderPluginSwc()],
-      runServer: false,
-    });
-
-    const { content, size } = await getIndexFile(builder);
-    expect(content.includes('debounce')).toBeFalsy();
-    expect(size < 10).toBeTruthy();
-  },
-);
 
 webpackOnlyTest(
   'should not optimize lodash bundle size when transformLodash is false',
@@ -86,33 +44,7 @@ webpackOnlyTest(
       runServer: false,
     });
 
-    const { content, size } = await getIndexFile(builder);
-    expect(content.includes('debounce')).toBeTruthy();
-    expect(size > 30).toBeTruthy();
-  },
-);
-
-webpackOnlyTest(
-  'should not optimize lodash bundle size when transformLodash is false and using SWC plugin',
-  async () => {
-    const builder = await build({
-      cwd: __dirname,
-      entry: {
-        index: path.resolve(__dirname, './src/index.ts'),
-      },
-      builderConfig: {
-        performance: {
-          transformLodash: false,
-          chunkSplit: {
-            strategy: 'all-in-one',
-          },
-        },
-      },
-      plugins: [builderPluginSwc()],
-      runServer: false,
-    });
-
-    const { content, size } = await getIndexFile(builder);
+    const { content, size } = await builder.getIndexFile();
     expect(content.includes('debounce')).toBeTruthy();
     expect(size > 30).toBeTruthy();
   },
