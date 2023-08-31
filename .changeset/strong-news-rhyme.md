a7b976c22bdf3badb8af5eebf5f7fe086a7d296d
jserfeng <1114550440@qq.com>
2023-08-31 13:05:00 +0800

    fix(plugin-swc): update core-js

---
+++ .changeset/strong-news-rhyme.md
@@ -0,0 +1,7 @@
+---
+'@modern-js/builder-plugin-swc': patch
+---
+
+fix(plugin-swc): update core-js
+
+fix(plugin-swc): 升级 core-js
---
+++ tests/e2e/builder/cases/swc/src/core-js-usage.ts
@@ -0,0 +1,4 @@
+/* eslint-disable node/no-unsupported-features/node-builtins */
+/* eslint-disable node/prefer-global/url-search-params */
+const params = new URLSearchParams();
+params.delete('foo');
