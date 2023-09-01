c4b9c1a34dcf991a66071474541a3cebfd140eff
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2023-09-01 15:25:54 +0800

    fix: update inlineRuntimeChunk test case

--- tests/e2e/builder/cases/inline-chunk/index.test.ts
+++ tests/e2e/builder/cases/inline-chunk/index.test.ts
@@ -4,11 +4,10 @@ import { webpackOnlyTest } from '@scripts/helper';
 import { build, getHrefByEntryName } from '@scripts/shared';
 import { BundlerChain, RUNTIME_CHUNK_NAME } from '@modern-js/builder-shared';
 
-// todo: Rspack not output RUNTIME_CHUNK_NAME.js.map
+// Rspack will not output builder runtime source map, but it not necessary
+// Identify whether the builder runtime chunk is included through some specific code snippets
 const isRuntimeChunkInHtml = (html: string): boolean =>
-  Boolean(
-    html.match(new RegExp(`static/js/${RUNTIME_CHUNK_NAME}\\.(.+)\\.js\\.map`)),
-  );
+  Boolean(html.includes('builder-runtime') && html.includes('Loading chunk'));
 
 // use source-map for easy to test. By default, builder use hidden-source-map
 const toolsConfig = {
@@ -62,18 +61,9 @@ test.describe('disableInlineRuntimeChunk', () => {
       ),
     ).toBe(true);
   });
-
-  webpackOnlyTest('should emit source map of builder runtime', async () => {
-    expect(
-      Object.keys(files).some(
-        fileName =>
-          fileName.includes(RUNTIME_CHUNK_NAME) && fileName.endsWith('.js.map'),
-      ),
-    ).toBe(true);
-  });
 });
 
-webpackOnlyTest('inline runtime chunk by default', async ({ page }) => {
+test('inline runtime chunk by default', async ({ page }) => {
   const builder = await build({
     cwd: __dirname,
     entry: { index: path.resolve(__dirname, './src/index.js') },
@@ -98,14 +88,6 @@ webpackOnlyTest('inline runtime chunk by default', async ({ page }) => {
     ),
   ).toBe(false);
 
-  // should emit source map of builder runtime
-  expect(
-    Object.keys(files).some(
-      fileName =>
-        fileName.includes(RUNTIME_CHUNK_NAME) && fileName.endsWith('.js.map'),
-    ),
-  ).toBe(true);
-
   // found builder-runtime file in html
   const indexHtml =
     files[path.resolve(__dirname, './dist/html/index/index.html')];
@@ -115,66 +97,60 @@ webpackOnlyTest('inline runtime chunk by default', async ({ page }) => {
   builder.close();
 });
 
-webpackOnlyTest(
-  'inline runtime chunk and remove source map when devtool is "hidden-source-map"',
-  async () => {
-    const builder = await build({
-      cwd: __dirname,
-      entry: { index: path.resolve(__dirname, './src/index.js') },
-      builderConfig: {
-        tools: {
-          bundlerChain(chain) {
-            chain.devtool('hidden-source-map');
-          },
+test('inline runtime chunk and remove source map when devtool is "hidden-source-map"', async () => {
+  const builder = await build({
+    cwd: __dirname,
+    entry: { index: path.resolve(__dirname, './src/index.js') },
+    builderConfig: {
+      tools: {
+        bundlerChain(chain) {
+          chain.devtool('hidden-source-map');
         },
       },
-    });
+    },
+  });
 
-    const files = await builder.unwrapOutputJSON(false);
+  const files = await builder.unwrapOutputJSON(false);
 
-    // should not emit source map of builder runtime
-    expect(
-      Object.keys(files).some(
-        fileName =>
-          fileName.includes(RUNTIME_CHUNK_NAME) && fileName.endsWith('.js.map'),
-      ),
-    ).toBe(false);
-  },
-);
+  // should not emit source map of builder runtime
+  expect(
+    Object.keys(files).some(
+      fileName =>
+        fileName.includes(RUNTIME_CHUNK_NAME) && fileName.endsWith('.js.map'),
+    ),
+  ).toBe(false);
+});
 
-webpackOnlyTest(
-  'inline runtime chunk by default with multiple entries',
-  async () => {
-    const builder = await build({
-      cwd: __dirname,
-      entry: {
-        index: path.resolve(__dirname, './src/index.js'),
-        another: path.resolve(__dirname, './src/another.js'),
-      },
-      builderConfig: {
-        tools: toolsConfig,
-      },
-    });
-    const files = await builder.unwrapOutputJSON(false);
+test('inline runtime chunk by default with multiple entries', async () => {
+  const builder = await build({
+    cwd: __dirname,
+    entry: {
+      index: path.resolve(__dirname, './src/index.js'),
+      another: path.resolve(__dirname, './src/another.js'),
+    },
+    builderConfig: {
+      tools: toolsConfig,
+    },
+  });
+  const files = await builder.unwrapOutputJSON(false);
 
-    // no builder-runtime file in output
-    expect(
-      Object.keys(files).some(
-        fileName =>
-          fileName.includes(RUNTIME_CHUNK_NAME) && fileName.endsWith('.js'),
-      ),
-    ).toBe(false);
+  // no builder-runtime file in output
+  expect(
+    Object.keys(files).some(
+      fileName =>
+        fileName.includes(RUNTIME_CHUNK_NAME) && fileName.endsWith('.js'),
+    ),
+  ).toBe(false);
 
-    // found builder-runtime file in html
-    const indexHtml =
-      files[path.resolve(__dirname, './dist/html/index/index.html')];
-    const anotherHtml =
-      files[path.resolve(__dirname, './dist/html/another/index.html')];
+  // found builder-runtime file in html
+  const indexHtml =
+    files[path.resolve(__dirname, './dist/html/index/index.html')];
+  const anotherHtml =
+    files[path.resolve(__dirname, './dist/html/another/index.html')];
 
-    expect(isRuntimeChunkInHtml(indexHtml)).toBeTruthy();
-    expect(isRuntimeChunkInHtml(anotherHtml)).toBeTruthy();
-  },
-);
+  expect(isRuntimeChunkInHtml(indexHtml)).toBeTruthy();
+  expect(isRuntimeChunkInHtml(anotherHtml)).toBeTruthy();
+});
 
 webpackOnlyTest(
   'inline all scripts and emit all source maps',
--- tests/e2e/builder/cases/top-level-await/index.swc.test.ts
+++ tests/e2e/builder/cases/top-level-await/index.swc.test.ts
@@ -1,14 +1,17 @@
 import * as path from 'path';
-import { expect } from '@modern-js/e2e/playwright';
+import { expect, test } from '@modern-js/e2e/playwright';
 import { build, getHrefByEntryName } from '@scripts/shared';
-import { webpackOnlyTest } from '../../scripts/helper';
+import { builderPluginSwc } from '@modern-js/builder-plugin-swc';
 
-webpackOnlyTest('should run top level await correctly', async ({ page }) => {
+test('should run top level await correctly when using SWC', async ({
+  page,
+}) => {
   const builder = await build({
     cwd: __dirname,
     entry: {
       index: path.resolve(__dirname, './src/index.ts'),
     },
+    plugins: [builderPluginSwc()],
     runServer: true,
   });
 
