852ec81c8b209becd9db30ba8f23be8247a4c7bd
chenwenjie <chenwenjie.asuka@bytedance.com>
2023-09-04 14:41:16 +0800

    feat: add loading cover component for `FrameBox`

--- packages/devtools/mount/src/components/Devtools/FrameBox.tsx
+++ packages/devtools/mount/src/components/Devtools/FrameBox.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { LoaderIcon } from '../LoadingIcon';
 import styles from './FrameBox.module.scss';
 
 export interface FrameBoxProps {
@@ -6,7 +7,28 @@ export interface FrameBoxProps {
 }
 
 const FrameBox: React.FC<FrameBoxProps> = ({ src }) => {
-  return <iframe className={styles.container} src={src}></iframe>;
+  const [showFrame, setShowFrame] = useState(false);
+
+  const handleFrameMount = (el: HTMLIFrameElement | null) => {
+    const handleLoad = () => setShowFrame(true);
+    el?.addEventListener('load', handleLoad, { once: true });
+  };
+
+  return (
+    <div className={styles.container}>
+      <div
+        className={styles.backdrop}
+        style={{ display: showFrame ? 'none' : undefined }}
+      >
+        <LoaderIcon className={styles.loading} />
+      </div>
+      <iframe
+        className={styles.frame}
+        ref={handleFrameMount}
+        src={src}
+      ></iframe>
+    </div>
+  );
 };
 
 export default FrameBox;
