2e567fbab5f96c209951a617db117d54340ef382
chenwenjie <chenwenjie.asuka@bytedance.com>
2023-09-06 17:27:22 +0800

    build: use modern.config.js instead of modern.config.ts in order to avoid building error

--- packages/devtools/kit/modern.config.ts
+++
@@ -1,6 +0,0 @@
-import { PartialBaseBuildConfig, defineConfig } from '@modern-js/module-tools';
-import { universalBuildConfig } from '@scripts/build';
-
-export default defineConfig({
-  buildConfig: universalBuildConfig as PartialBaseBuildConfig[],
-});
