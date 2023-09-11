7e2e1b408460d7ffa49639bd217b10e928ff07d6
jserfeng <1114550440@qq.com>
2023-09-11 17:04:45 +0800

    fix: use cjs version runtime addon

--- packages/storybook/builder/src/addons/preset/preview.ts
+++ packages/storybook/builder/src/addons/preset/preview.ts
@@ -1,7 +1,3 @@
-// import { withPluginRuntime } from '../withPluginRuntime';
+import { withPluginRuntime } from '../withPluginRuntime';
 
-// FIXME: @modern-js/runtime now is ESM only, but this package
-// currently focus on CommonJS, so it cannot import @modern-js/runtime
-// directly.
-// export const decorators = [withPluginRuntime];
-export const decorators = [];
+export const decorators = [withPluginRuntime];
