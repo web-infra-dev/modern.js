c716b46e8d6a862d1ce5b7ffbac559e7970c9cc0
jserfeng <1114550440@qq.com>
2023-09-11 14:51:15 +0800

    feat(storybook): add upgrade script

---
+++ packages/storybook/framework/src/upgrade.ts
@@ -0,0 +1,8 @@
+/**
+ * Old storybook:
+ * - `path/config/storybook` move to `path/.storybook/main.ts`
+ * - stories folder -> configure in `.storybook/main.ts`
+ */
+export function upgrade(cwd: string) {
+  console.log(cwd);
+}
