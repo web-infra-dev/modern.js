a641a8b2a5ab761551a3c5c4e33fe04f67ee6d8f
jserfeng <1114550440@qq.com>
2023-09-12 16:52:02 +0800

    refactor: import from webpack subpath

--- packages/builder/builder-webpack-provider/src/types/thirdParty/index.ts
+++ packages/builder/builder-webpack-provider/src/types/thirdParty/index.ts
@@ -1,10 +1,6 @@
 import type WebpackChain from '@modern-js/utils/webpack-chain';
 import type webpack from 'webpack';
-import type {
-  Configuration as WebpackConfig,
-  Compiler as WebpackCompiler,
-  MultiCompiler as WebpackMultiCompiler,
-} from 'webpack';
+import type { Configuration as WebpackConfig } from 'webpack';
 import type { Options as RawTSLoaderOptions } from 'ts-loader';
 
 export type { BabelTransformOptions, BabelConfigUtils } from '@modern-js/types';
@@ -15,13 +11,7 @@ export type { SubresourceIntegrityPluginOptions as SubresourceIntegrityOptions }
 
 export type TSLoaderOptions = Partial<RawTSLoaderOptions>;
 
-export type {
-  webpack,
-  WebpackChain,
-  WebpackConfig,
-  WebpackCompiler,
-  WebpackMultiCompiler,
-};
+export type { webpack, WebpackChain, WebpackConfig };
 
 export type {
   CSSExtractOptions,
