14b09067987d9b9350c9122f14d427c6c243fedc
Fy <1114550440@qq.com>
2023-09-20 11:14:42 +0800

    fix(swc): transform core-js-pure incorrectly, allow using new decorator for js (#4680)

---
+++ packages/builder/plugin-swc/tests/fixtures/extensions/lock-corejs-versions/expected.js
@@ -0,0 +1,4 @@
+/** should be transformed */ import "/path/to/corejs/foo/bar";
+import "/path/to/helper/foo/bar";
+/** should stay as is */ import "<CORE_JS>-pure/foo/bar";
+import "<SWC_HELPER>-custom/foo/bar";
