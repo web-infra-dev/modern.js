8639748121b37c652ad7beb515a7a7e629c80bdc
chenjiahan <chenjiahan.jait@bytedance.com>
2023-09-21 11:14:33 +0800

    chore: improve

--- packages/builder/builder-shared/src/prettyTime.ts
+++ packages/builder/builder-shared/src/prettyTime.ts
@@ -1,11 +1,15 @@
 import basePrettyTime from '../compiled/pretty-time';
 import chalk from '@modern-js/utils/chalk';
 
+const TIME_REGEXP = /(\d+)([a-zA-Z]+)/;
+
 export const prettyTime = (time: number | [number, number], digits: number) => {
   const timeStr: string = basePrettyTime(time, digits);
 
-  return timeStr.replace(
-    /(\d+)([a-zA-Z]+)/,
-    (match, p1, p2) => `${chalk.bold(p1)} ${p2}`,
-  );
+  return timeStr.replace(TIME_REGEXP, (match, p1, p2) => {
+    if (p1 && p2) {
+      return `${chalk.bold(p1)} ${p2}`;
+    }
+    return chalk.bold(match);
+  });
 };
