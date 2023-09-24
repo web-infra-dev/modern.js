d1d4062fba594bdad0110a8f14c33ae374fefb2e
chenwenjie <chenwenjie.asuka@bytedance.com>
2023-09-25 01:18:50 +0800

    feat: better draggable ux

--- packages/devtools/mount/src/components/Devtools/Action.tsx
+++ packages/devtools/mount/src/components/Devtools/Action.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { useGetSet, useToggle } from 'react-use';
-import { withQuery, stringifyParsedURL, parseURL } from 'ufo';
 import { ROUTE_BASENAME, SetupClientOptions } from '@modern-js/devtools-kit';
+import React from 'react';
+import { useToggle } from 'react-use';
+import { parseURL, stringifyParsedURL, withQuery } from 'ufo';
 import Visible from '../Visible';
 import styles from './Action.module.scss';
 import FrameBox from './FrameBox';
+import { useStickyDraggable } from '@/utils/draggable';
 
 const parseDataSource = (url: string) => {
   const newSrc = parseURL(url);
@@ -14,58 +15,6 @@ const parseDataSource = (url: string) => {
     ...newSrc,
     pathname: newSrc.pathname || `${ROUTE_BASENAME}/rpc`,
   });
-};
-
-const useStickyDraggable = () => {
-  const [isDragging, setIsDragging] = useGetSet(false);
-  const handleMouseDown = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
-    const target = e.currentTarget;
-    if (!(target instanceof HTMLElement)) {
-      return;
-    }
-    const { offsetX, offsetY } = e.nativeEvent;
-    const handleMousemove = (e: MouseEvent) => {
-      if (e.movementX + e.movementY > 1) {
-        setIsDragging(true);
-      }
-
-      const x = e.clientX - offsetX;
-      const y = e.clientY - offsetY;
-      const distances = [
-        { prop: 'top', value: e.clientY } as const,
-        { prop: 'bottom', value: window.innerHeight - e.clientY } as const,
-        { prop: 'left', value: e.clientX } as const,
-        { prop: 'right', value: window.innerWidth - e.clientX } as const,
-      ];
-      const [primary, ...rest] = distances.sort((a, b) => a.value - b.value);
-      target.style[primary.prop] = '10px';
-      for (const unset of rest) {
-        target.style.removeProperty(unset.prop);
-      }
-      if (['top', 'bottom'].includes(primary.prop)) {
-        target.style.left = `${x}px`;
-      } else {
-        target.style.top = `${y}px`;
-      }
-    };
-    window.addEventListener('mousemove', handleMousemove);
-    window.addEventListener('blur', () => {
-      setTimeout(() => setIsDragging(false), 0);
-      window.removeEventListener('mousemove', handleMousemove);
-    });
-    window.addEventListener(
-      'mouseup',
-      () => {
-        setTimeout(() => setIsDragging(false), 0);
-        window.removeEventListener('mousemove', handleMousemove);
-      },
-      { once: true },
-    );
-  };
-  return {
-    onMouseDown: handleMouseDown,
-    isDragging: isDragging(),
-  };
 };
 
 const DevtoolsAction: React.FC<SetupClientOptions> = props => {
@@ -80,28 +29,26 @@ const DevtoolsAction: React.FC<SetupClientOptions> = props => {
   let src = opts.endpoint;
   src = withQuery(src, { src: opts.dataSource });
 
-  const { isDragging, onMouseDown } = useStickyDraggable();
+  const draggable = useStickyDraggable({ clamp: true });
 
   return (
     <>
-      <button
-        className={styles.fab}
-        onClick={() => {
-          isDragging || toggleDevtools();
-        }}
-        onMouseDown={onMouseDown}
-      >
-        <img className={styles.logo} src={logoSrc} alt="" />
-        <span>Toggle DevTools</span>
-      </button>
       <Visible when={showDevtools} keepAlive={true}>
         <div className={styles.container}>
           <FrameBox
             src={src}
-            style={{ pointerEvents: isDragging ? 'none' : 'auto' }}
+            style={{ pointerEvents: draggable.isDragging ? 'none' : 'auto' }}
           />
         </div>
       </Visible>
+      <button
+        className={styles.fab}
+        onClick={() => toggleDevtools()}
+        {...draggable.props}
+      >
+        <img className={styles.logo} src={logoSrc} alt="" />
+        <span>Toggle DevTools</span>
+      </button>
     </>
   );
 };
