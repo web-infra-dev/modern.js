4e8bfc7583c3f1ddcdb9cfbda5b2f42dd3cd386c
chenwenjie <chenwenjie.asuka@bytedance.com>
2023-09-25 20:07:30 +0800

    feat: remove chinese text

--- tests/integration/devtools/src/main/routes/post/[id]/page.tsx
+++ tests/integration/devtools/src/main/routes/post/[id]/page.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 const Page: React.FC = () => (
   <div>
-    <h3>约定式路由</h3>
+    <h3>Conventional Routing</h3>
     <p>
-      以 routes/ 为约定的入口，EdenX 会自动基于文件系统，生成对应的路由结构。
+      With routes/ as the convention for entry points, Modern.js automatically
+      generates the corresponding routing structure based on the file system.
     </p>
     <p>
-      EdenX 支持了业界流行的约定式路由模式：嵌套路由，使用嵌套路由时，页面的路由
-      与 UI 结构是相呼应的，我们将会详细介绍这种路由模式。
+      Modern.js supports the popular conventional routing mode in the industry:
+      Nested Routing. When using nested routing, the page's routing corresponds
+      to the UI structure, and we will introduce this routing mode in detail.
     </p>
   </div>
 );
