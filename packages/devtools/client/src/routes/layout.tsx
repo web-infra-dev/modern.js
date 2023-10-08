074c196464d6d5240609ff32202e8d638c61e6aa
chenwenjie <chenwenjie.asuka@bytedance.com>
2023-10-08 16:04:48 +0800

    feat: improve theme handling

--- packages/devtools/client/src/routes/layout.tsx
+++ packages/devtools/client/src/routes/layout.tsx
@@ -1,14 +1,18 @@
 import '@radix-ui/themes/styles.css';
 import './layout.css';
 import styled from '@emotion/styled';
-import { Box, Theme } from '@radix-ui/themes';
+import { Box, ThemePanel } from '@radix-ui/themes';
 import { RootTabs } from './RootTabs';
 import { StoreContextProvider } from '@/stores';
+import { Theme } from '@/components/Theme';
 
 export default function Layout() {
   return (
     <StoreContextProvider>
-      <Theme appearance="dark" className="dark">
+      <Theme panelBackground="solid">
+        {process.env.NODE_ENV === 'development' && (
+          <ThemePanel defaultOpen={false} />
+        )}
         <AppContainer>
           <RootTabs />
         </AppContainer>
