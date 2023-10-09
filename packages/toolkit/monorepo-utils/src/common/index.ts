ec771bfbb46b5c5fef5c39f51ef9cefa42c02a4a
chenjiahan <chenjiahan.jait@bytedance.com>
2023-10-09 19:45:12 +0800

    chore(builder): migrate monorepo-utils to rsbuild repo

--- packages/toolkit/monorepo-utils/src/common/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './getBaseData';
-export * from './isMonorepo';
-
-export { getMonorepoSubProjects } from './getProjects';
-export { getProjects as getPnpmMonorepoSubProjects } from './pnpm';
-export { getProjects as getRushMonorepoSubProjects } from './rush';
-
-export type { GetProjectsFunc } from './getProjects';
--- packages/toolkit/monorepo-utils/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './common';
-export * from './project';
-export * from './project-utils';
-export * from './constants';
-export * from './types';
