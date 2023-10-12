b83979788ca645fb7b743c3310b9e6b7c7b2f766
neverland <chenjiahan.jait@bytedance.com>
2023-10-12 18:41:30 +0800

    refactor(builder): replace babel config with @rsbuild/babel-preset (#4783)

--- packages/builder/builder-webpack-provider/src/types/thirdParty/index.ts
+++ packages/builder/builder-webpack-provider/src/types/thirdParty/index.ts
@@ -3,13 +3,11 @@ import type webpack from 'webpack';
 import type { Configuration as WebpackConfig } from 'webpack';
 import type { Options as RawTSLoaderOptions } from 'ts-loader';
 
-export type { BabelTransformOptions } from '@modern-js/types';
+export type { BabelTransformOptions, BabelConfigUtils } from '@modern-js/types';
 export type { InspectorPluginOptions } from '@modern-js/inspector-webpack-plugin';
 export type { CopyPluginOptions } from './CopyWebpackPlugin';
 export type { Options as HTMLPluginOptions } from 'html-webpack-plugin';
 export type { SubresourceIntegrityPluginOptions as SubresourceIntegrityOptions } from 'webpack-subresource-integrity';
-
-export type { BabelConfigUtils } from '@modern-js/babel-preset-app';
 
 export type TSLoaderOptions = Partial<RawTSLoaderOptions>;
 
