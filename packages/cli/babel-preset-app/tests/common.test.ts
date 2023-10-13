7dc1512febefd426f0c70e27203df2d3687e9a08
neverland <chenjiahan.jait@bytedance.com>
2023-10-13 10:30:00 +0800

    refactor: replace babel-preset-app with rsbuild babel-preset (#4785)

--- packages/cli/babel-preset-app/tests/common.test.ts
+++
@@ -1,8 +0,0 @@
-import { genCommon } from '../src/common';
-
-describe('test common', () => {
-  it('base usage', () => {
-    const commonConfig = genCommon({ appDirectory: 'project' });
-    expect(commonConfig).not.toBeNull();
-  });
-});
