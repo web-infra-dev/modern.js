03a8e9f7a2d6ceb59ca9da622ffb0cbf66d36348
neverland <chenjiahan.jait@bytedance.com>
2023-10-13 16:19:40 +0800

    chore: replace babel-preset-base with rsbuild babel-preset (#4787)

--- packages/server/utils/src/compilers/babel/preset/types.ts
+++ packages/server/utils/src/compilers/babel/preset/types.ts
@@ -1,10 +1,3 @@
-import type { IStyledComponentOptions } from '@modern-js/babel-preset-base';
-
-export interface ISyntaxOption {
-  syntax: 'es5' | 'es6+';
-  type: 'module' | 'commonjs';
-}
-
 export interface AliasOption {
   absoluteBaseUrl: string;
   paths?: Record<string, string | string[]>;
@@ -14,11 +7,7 @@ export interface AliasOption {
 
 export interface ILibPresetOption {
   appDirectory: string;
-  enableTypescriptPreset?: boolean;
-  enableReactPreset?: boolean;
   alias?: AliasOption;
-  jsxTransformRuntime?: JsxTransformRuntimeType;
-  styledComponentsOptions?: IStyledComponentOptions;
 }
 
 export type JsxTransformRuntimeType = 'automatic' | 'classic';
