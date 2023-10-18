b0288b52e73c1fe6d234017f1f1956eb4596fdc1
bohan <bohan-zhang@foxmail.com>
2023-10-18 21:16:05 +0800

    chore: disable coverage in ci

--- packages/toolkit/plugin/jest.config.js
+++ packages/toolkit/plugin/jest.config.js
@@ -3,6 +3,5 @@ const sharedConfig = require('@scripts/jest-config');
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   ...sharedConfig,
-  coverageProvider: 'v8',
   rootDir: __dirname,
 };
