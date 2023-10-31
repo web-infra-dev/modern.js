7a9cc289863fbefd0fe30d9364488f37b3f132ce
10Derozan <fupeng.antik@gmail.com>
2023-10-31 21:40:49 +0800

    ci: split module test and add workflow

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -3,7 +3,10 @@ module.exports = {
   preset: 'jest-puppeteer',
   rootDir: __dirname,
   setupFilesAfterEnv: ['./utils/jest.setup.js'],
-  testMatch: ['<rootDir>/integration/**/*.(spec|test).[tj]s?(x)'],
+  testMatch: [
+    '<rootDir>/integration/**/*.(spec|test).[tj]s?(x)',
+    '!**/module/**/*.(spec|test).[tj]s?(x)',
+  ],
   testPathIgnorePatterns: [
     '/node_modules/',
     '/api-service-koa/api/',
