3766e74ca0f45b3366dc9bcb806a55a95520e207
clchenliang <clchenliang@hotmail.com>
2023-11-13 12:50:54 +0800

    fix: divide dev build test into two files

--- tests/integration/app-document/tests/index.test.ts
+++ tests/integration/app-document/tests/index.test.ts
@@ -7,197 +7,226 @@ import {
   killApp,
   getPort,
   modernBuild,
-  modernServe,
   launchOptions,
 } from '../../../utils/modernTestUtils';
+import { printFileTogether } from '../../../utils/printDir';
+import { SequenceWait } from '../../../utils/testInSequence';
 
 const appDir = path.resolve(__dirname, '../');
 
 function existsSync(filePath: string) {
   return fs.existsSync(path.join(appDir, 'dist', filePath));
 }
+describe('test dev and build', () => {
+  const curSequenceWait = new SequenceWait();
+  curSequenceWait.add('test-dev');
+  curSequenceWait.add('test-rem');
 
-describe('test dev', () => {
-  let app: any;
-  let appPort: number;
-  let errors;
-  let browser: Browser;
-  let page: Page;
-  beforeAll(async () => {
-    appPort = await getPort();
-    app = await launchApp(appDir, appPort, {}, {});
-    errors = [];
-    browser = await puppeteer.launch(launchOptions as any);
-    page = await browser.newPage();
-    page.on('pageerror', error => {
-      errors.push(error.message);
+  describe('test build', () => {
+    let buildRes: any;
+    beforeAll(async () => {
+      buildRes = await modernBuild(appDir);
     });
-  });
-  afterAll(async () => {
-    await killApp(app);
-    await page.close();
-    await browser.close();
-  });
-
-  test(`should render page test correctly`, async () => {
-    await page.goto(`http://localhost:${appPort}/test`, {
-      waitUntil: ['networkidle0'],
+    afterAll(() => {
+      curSequenceWait.done('test-dev');
     });
 
-    const root = await page.$('#root');
-    const targetText = await page.evaluate(el => el?.textContent, root);
-    expect(targetText?.trim()).toEqual('A');
-    expect(errors.length).toEqual(0);
-  });
-
-  test(`should render page sub correctly`, async () => {
-    await page.goto(`http://localhost:${appPort}/sub`, {
-      waitUntil: ['networkidle0'],
+    test(`should get right alias build!`, async () => {
+      if (buildRes.code !== 0) {
+        console.log('\n===> build failed, stdout: ', buildRes.stdout);
+        console.log('\n===> build failed, stderr: ', buildRes.stderr);
+      }
+      expect(buildRes.code).toEqual(0);
+      expect(existsSync('route.json')).toBe(true);
+      expect(existsSync('html/test/index.html')).toBe(true);
+      expect(existsSync('html/sub/index.html')).toBe(true);
     });
 
-    const root = await page.$('#root');
-    const targetText = await page.evaluate(el => el?.textContent, root);
-    expect(targetText?.trim()).toEqual('去 A去 B');
-    expect(errors.length).toEqual(0);
-  });
-
-  test(`should render page sub route a correctly`, async () => {
-    await page.goto(`http://localhost:${appPort}/sub/a`, {
-      waitUntil: ['networkidle0'],
+    test('should have the test html and the correct content', async () => {
+      const htmlNoDoc = fs.readFileSync(
+        path.join(appDir, 'dist', 'html/test/index.html'),
+        'utf-8',
+      );
+      expect(htmlNoDoc.includes('<div id="root"><!--<?- html ?>--></div>'));
     });
 
-    const root = await page.$('#root');
-    const targetText = await page.evaluate(el => el?.textContent, root);
-    expect(targetText?.trim()).toEqual('去 A去 B');
-    expect(errors.length).toEqual(0);
-  });
-});
+    test('should have the sub html and the correct content', async () => {
+      const htmlWithDoc = fs.readFileSync(
+        path.join(appDir, 'dist', 'html/sub/index.html'),
+        'utf-8',
+      );
+      expect(htmlWithDoc.includes('<div id="root"><!--<?- html ?>--><h1'));
+    });
 
-describe('test build', () => {
-  let port = 8080;
-  let buildRes: any;
-  let app: any;
-  beforeAll(async () => {
-    port = await getPort();
+    test('should has comment in Head', async () => {
+      const htmlWithDoc = fs.readFileSync(
+        path.join(appDir, 'dist', 'html/sub/index.html'),
+        'utf-8',
+      );
 
-    buildRes = await modernBuild(appDir);
+      expect(htmlWithDoc.includes('<!-- COMMENT BY APP -->')).toBe(true);
+      expect(htmlWithDoc.includes('== COMMENT BY APP in inline ==')).toBe(true);
+      expect(htmlWithDoc.includes('== COMMENT BY APP but inline ==')).toBe(
+        false,
+      );
+    });
 
-    app = await modernServe(appDir, port, {
-      cwd: appDir,
+    test('should has style in Head', async () => {
+      const htmlWithDoc = fs.readFileSync(
+        path.join(appDir, 'dist', 'html/sub/index.html'),
+        'utf-8',
+      );
+
+      expect(htmlWithDoc.includes('.logo-spin>div:last-child')).toBe(true);
+    });
+
+    test('should has lang property in html', async () => {
+      const htmlWithDoc = fs.readFileSync(
+        path.join(appDir, 'dist', 'html/sub/index.html'),
+        'utf-8',
+      );
+
+      expect(htmlWithDoc.includes(`html lang="cn"`)).toBe(true);
+    });
+
+    test('should has dir property in body', async () => {
+      const htmlWithDoc = fs.readFileSync(
+        path.join(appDir, 'dist', 'html/sub/index.html'),
+        'utf-8',
+      );
+
+      expect(htmlWithDoc.includes(`body dir="ltr"`)).toBe(true);
+    });
+
+    test('should has class property in root div', async () => {
+      const htmlWithDoc = fs.readFileSync(
+        path.join(appDir, 'dist', 'html/sub/index.html'),
+        'utf-8',
+      );
+
+      expect(htmlWithDoc.includes(`div id="root" class="root"`)).toBe(true);
+    });
+
+    test('should has class property in root div', async () => {
+      const htmlWithDoc = fs.readFileSync(
+        path.join(appDir, 'dist', 'html/sub/index.html'),
+        'utf-8',
+      );
+
+      expect(htmlWithDoc.includes(`head class="head"`)).toBe(true);
+    });
+
+    test('when not set partial html should normal', async () => {
+      const normalHtml = fs.readFileSync(
+        path.join(appDir, 'dist', 'html/differentProperities/index.html'),
+        'utf-8',
+      );
+      const partialPlaceholder = encodeURIComponent(
+        '<!--<?- partials.top ?>-->',
+      );
+      expect(new RegExp(partialPlaceholder).test(normalHtml)).toBe(false);
+    });
+
+    test('should injected partial html content to html', async () => {
+      const htmlWithDoc = fs.readFileSync(
+        path.join(appDir, 'dist', 'html/sub/index.html'),
+        'utf-8',
+      );
+
+      expect(
+        /<head class="head"><script>window.abc="hjk"<\/script>/.test(
+          htmlWithDoc,
+        ),
+      ).toBe(true);
+      expect(
+        /<head[\s\S]*<script>console.log\("abc"\)<\/script>[\s\S]*<\/head>/.test(
+          htmlWithDoc,
+        ),
+      ).toBe(true);
+
+      expect(
+        /<body[\s\S]*<script>console.log\(abc\)<\/script>[\s\S]*<\/body>/.test(
+          htmlWithDoc,
+        ),
+      ).toBe(true);
     });
   });
 
-  afterAll(async () => {
-    await killApp(app);
+  describe('test dev', () => {
+    let app: any;
+    let appPort: number;
+    let errors;
+    let browser: Browser;
+    let page: Page;
+    beforeAll(async () => {
+      await curSequenceWait.waitUntil('test-dev');
+      appPort = await getPort();
+      app = await launchApp(appDir, appPort, {}, {});
+      errors = [];
+      browser = await puppeteer.launch(launchOptions as any);
+      page = await browser.newPage();
+      page.on('pageerror', error => {
+        errors.push(error.message);
+      });
+    });
+    afterAll(async () => {
+      await killApp(app);
+      await page.close();
+      await browser.close();
+      curSequenceWait.done('test-rem');
+    });
+
+    test(`should render page test correctly`, async () => {
+      await page.goto(`http://localhost:${appPort}/test`, {
+        waitUntil: ['networkidle0'],
+      });
+
+      const root = await page.$('#root');
+      const targetText = await page.evaluate(el => el?.textContent, root);
+      expect(targetText?.trim()).toEqual('A');
+      expect(errors.length).toEqual(0);
+    });
+
+    test(`should render page sub correctly`, async () => {
+      printFileTogether(path.join(__dirname, '../node_modules/.modern-js'));
+
+      await page.goto(`http://localhost:${appPort}/sub`, {
+        waitUntil: ['networkidle0'],
+      });
+
+      await page.waitForSelector('#root a');
+      const root = await page.$('#root');
+      const targetText = await page.evaluate(el => el?.textContent, root);
+      expect(targetText?.trim()).toEqual('去 A去 B');
+      expect(errors.length).toEqual(0);
+    });
+
+    test(`should render page sub route a correctly`, async () => {
+      printFileTogether(path.join(__dirname, '../node_modules/.modern-js'));
+      await page.goto(`http://localhost:${appPort}/sub/a`, {
+        waitUntil: ['networkidle0'],
+      });
+
+      await page.waitForSelector('#root a');
+      const root = await page.$('#root');
+      const targetText = await page.evaluate(el => el?.textContent, root);
+      expect(targetText?.trim()).toEqual('去 A去 B');
+      expect(errors.length).toEqual(0);
+    });
   });
 
-  test(`should get right alias build!`, async () => {
-    expect(buildRes.code === 0).toBe(true);
-    expect(existsSync('route.json')).toBe(true);
-    expect(existsSync('html/test/index.html')).toBe(true);
-    expect(existsSync('html/sub/index.html')).toBe(true);
-  });
+  describe('fix rem', () => {
+    beforeAll(async () => {
+      await curSequenceWait.waitUntil('test-rem');
+      await modernBuild(appDir, ['-c', 'modern-rem.config.ts']);
+    });
 
-  test('should have the test html and the correct content', async () => {
-    const htmlNoDoc = fs.readFileSync(
-      path.join(appDir, 'dist', 'html/test/index.html'),
-      'utf-8',
-    );
-    expect(htmlNoDoc.includes('<div id="root"><!--<?- html ?>--></div>'));
-  });
-
-  test('should have the sub html and the correct content', async () => {
-    const htmlWithDoc = fs.readFileSync(
-      path.join(appDir, 'dist', 'html/sub/index.html'),
-      'utf-8',
-    );
-    expect(htmlWithDoc.includes('<div id="root"><!--<?- html ?>--><h1'));
-  });
-
-  test('should has comment in Head', async () => {
-    const htmlWithDoc = fs.readFileSync(
-      path.join(appDir, 'dist', 'html/sub/index.html'),
-      'utf-8',
-    );
-
-    expect(htmlWithDoc.includes('<!-- COMMENT BY APP -->')).toBe(true);
-    expect(htmlWithDoc.includes('== COMMENT BY APP in inline ==')).toBe(true);
-    expect(htmlWithDoc.includes('== COMMENT BY APP but inline ==')).toBe(false);
-  });
-
-  test('should has style in Head', async () => {
-    const htmlWithDoc = fs.readFileSync(
-      path.join(appDir, 'dist', 'html/sub/index.html'),
-      'utf-8',
-    );
-
-    expect(htmlWithDoc.includes('.logo-spin>div:last-child')).toBe(true);
-  });
-
-  test('should has lang property in html', async () => {
-    const htmlWithDoc = fs.readFileSync(
-      path.join(appDir, 'dist', 'html/sub/index.html'),
-      'utf-8',
-    );
-
-    expect(htmlWithDoc.includes(`html lang="cn"`)).toBe(true);
-  });
-
-  test('should has dir property in body', async () => {
-    const htmlWithDoc = fs.readFileSync(
-      path.join(appDir, 'dist', 'html/sub/index.html'),
-      'utf-8',
-    );
-
-    expect(htmlWithDoc.includes(`body dir="ltr"`)).toBe(true);
-  });
-
-  test('should has class property in root div', async () => {
-    const htmlWithDoc = fs.readFileSync(
-      path.join(appDir, 'dist', 'html/sub/index.html'),
-      'utf-8',
-    );
-
-    expect(htmlWithDoc.includes(`div id="root" class="root"`)).toBe(true);
-  });
-
-  test('should has class property in root div', async () => {
-    const htmlWithDoc = fs.readFileSync(
-      path.join(appDir, 'dist', 'html/sub/index.html'),
-      'utf-8',
-    );
-
-    expect(htmlWithDoc.includes(`head class="head"`)).toBe(true);
-  });
-
-  test('when not set partial html should normal', async () => {
-    const normalHtml = fs.readFileSync(
-      path.join(appDir, 'dist', 'html/differentProperities/index.html'),
-      'utf-8',
-    );
-    const partialPlaceholder = encodeURIComponent('<!--<?- partials.top ?>-->');
-    expect(new RegExp(partialPlaceholder).test(normalHtml)).toBe(false);
-  });
-
-  test('should injected partial html content to html', async () => {
-    const htmlWithDoc = fs.readFileSync(
-      path.join(appDir, 'dist', 'html/sub/index.html'),
-      'utf-8',
-    );
-
-    expect(
-      /<head class="head"><script>window.abc="hjk"<\/script>/.test(htmlWithDoc),
-    ).toBe(true);
-    expect(
-      /<head[\s\S]*<script>console.log\("abc"\)<\/script>[\s\S]*<\/head>/.test(
-        htmlWithDoc,
-      ),
-    ).toBe(true);
-
-    expect(
-      /<body[\s\S]*<script>console.log\(abc\)<\/script>[\s\S]*<\/body>/.test(
-        htmlWithDoc,
-      ),
-    ).toBe(true);
+    test('should add rem resource correct', async () => {
+      const htmlNoDoc = fs.readFileSync(
+        path.join(appDir, 'dist-1', 'html/test/index.html'),
+        'utf-8',
+      );
+      expect(htmlNoDoc.includes('/static/js/convert-rem.'));
+    });
   });
 });
--- tests/integration/routes/src/four/routes/user/profile/page.tsx
+++ tests/integration/routes/src/four/routes/user/profile/page.tsx
@@ -2,6 +2,7 @@ import { useFetcher, useLoaderData } from '@modern-js/runtime/router';
 
 const Page = () => {
   const data = useLoaderData() as string;
+  const dataWrap = data && <span className="data-wrapper">{data}</span>;
   const { submit } = useFetcher();
   const handleClick = () => {
     return submit({ name: 'modern_four_action' }, { method: 'post' });
@@ -10,7 +11,7 @@ const Page = () => {
   return (
     <div>
       <span>profile page</span>
-      <span className="modern-test-name">{data}</span>
+      <span className="modern-test-name">{dataWrap}</span>
       <div className="action-btn" onClick={handleClick}>
         action-button
       </div>
