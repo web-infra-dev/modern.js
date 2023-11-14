b63b282f687084aec6bf2b29dfb33c089fcaebad
jserfeng <1114550440@qq.com>
2023-11-14 16:13:57 +0800

    feat: apply addon babel

--- packages/storybook/builder/src/core.ts
+++ packages/storybook/builder/src/core.ts
@@ -2,6 +2,8 @@ import { createBuilder } from '@modern-js/builder';
 import { loadConfig } from '@modern-js/core';
 import type { Options } from '@storybook/types';
 import type { Compiler } from '@modern-js/builder-shared/webpack-dev-middleware';
+import type { PluginItem } from '@babel/core';
+import { applyOptionsChain, logger } from '@modern-js/utils';
 import type { BuilderConfig, BuilderOptions } from './types';
 import { getConfigFileName, getProvider, runWithErrorMsg } from './utils';
 import { pluginStorybook } from './plugin-storybook';
@@ -14,6 +16,7 @@ export async function getCompiler(
   const bundler = builderOptions.bundler || 'webpack';
 
   const { presets } = options;
+
   const entries = await presets.apply<string[]>('entries', []);
 
   const res = await runWithErrorMsg(
@@ -25,6 +28,8 @@ export async function getCompiler(
   const finalConfig =
     (await presets.apply<BuilderConfig | void>('modern', loadedConfig)) ||
     loadedConfig;
+
+  await addonAdapter(finalConfig, options);
 
   const provider = await getProvider(bundler, finalConfig);
 
@@ -47,4 +52,71 @@ export async function getCompiler(
   ]);
 
   return builder.createCompiler() as Promise<Compiler>;
+}
+
+/**
+ * Some addons expose babel plugins and presets, or modify webpack
+ */
+async function addonAdapter(finalConfig: BuilderConfig, options: Options) {
+  const { presets } = options;
+  const babelOptions = await presets.apply('babel', {}, { ...options });
+
+  finalConfig.tools ??= {};
+  finalConfig.tools.babel = config => {
+    const getPluginName = (plugin: PluginItem) =>
+      Array.isArray(plugin) ? plugin[0] : plugin;
+    const getOptions = (plugin: PluginItem) =>
+      Array.isArray(plugin) ? plugin[1] : null;
+
+    const replaceOrInsert = (plugin: PluginItem, plugins: PluginItem[]) => {
+      const pluginName = getPluginName(plugin);
+
+      const append = [];
+      for (let i = 0; i < plugins.length; i++) {
+        if (getPluginName(plugins[i]) === pluginName) {
+          if (getOptions(plugin)) {
+            logger.info(
+              'Detected duplicated babel plugin, overrides with the new one',
+            );
+            plugins[i] = plugin;
+          }
+        } else {
+          append.push(plugin);
+        }
+      }
+
+      plugins.push(...append);
+    };
+
+    const currentPlugins = config.plugins || [];
+
+    // O(n * n) but the number of plugins should be small
+    for (const plugin of babelOptions.plugins || []) {
+      replaceOrInsert(plugin, currentPlugins);
+    }
+
+    return {
+      ...config,
+      ...babelOptions,
+      plugins: currentPlugins,
+      presets: config.presets,
+    };
+  };
+
+  finalConfig.tools ??= {};
+
+  // @ts-expect-error tools.webpack not present in rspack config, but it's OK, we only
+  // adapt webpack addon now
+  finalConfig.tools.webpack = applyOptionsChain(
+    // @ts-expect-error tools.webpack not present in rspack config, but it's OK, we only
+    finalConfig.tools.webpack,
+    async (defaultConfig: any) => {
+      const finalDefaultConfig = await presets.apply(
+        'webpackFinal',
+        defaultConfig,
+        options,
+      );
+      return finalDefaultConfig;
+    },
+  );
 }
