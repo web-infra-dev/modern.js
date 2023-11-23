81bfee159551f0742a540458cac763d9a2646b8f
Asuka109 <asuka109@foxmail.com>
2023-11-23 16:37:43 +0800

    build(devtools): build service worker with rsbuild

--- packages/devtools/client/plugins/ServiceWorkerCompilerPlugin.ts
+++ packages/devtools/client/plugins/ServiceWorkerCompilerPlugin.ts
@@ -1,41 +1,40 @@
 import path from 'path';
-import { createCompiler, Compiler, DefinePlugin } from '@rspack/core';
+import { createRsbuild } from '@rsbuild/core';
+import { RspackCompiler } from '@modern-js/builder-rspack-provider';
 import { logger } from '@modern-js/utils/logger';
 import { version } from '../package.json';
 
-const workspace = path.resolve(__dirname, '../');
-
 export class ServiceWorkerCompilerPlugin {
   name = 'ServiceWorkerCompilerPlugin';
-  apply(compiler: Compiler) {
+  apply(compiler: RspackCompiler) {
     compiler.hooks.beforeCompile.tapPromise(this.name, async () => {
-      const watch = compiler.watchMode ?? false;
-      watch && logger.info('Build service worker in watch mode.');
+      const cwd = path.resolve(__dirname, '../');
 
-      const childCompiler = createCompiler({
-        mode: 'production',
-        context: workspace,
-        entry: path.resolve(workspace, './src/service.worker.ts'),
-        target: 'webworker',
-        devtool: false,
-        watch,
-        output: {
-          path: path.resolve(workspace, 'dist/public'),
-          filename: 'sw-proxy.js',
+      const builder = await createRsbuild({
+        cwd,
+        target: 'service-worker',
+        rsbuildConfig: {
+          source: {
+            entry: { 'sw-proxy': path.resolve(cwd, './src/service.worker.ts') },
+            define: { 'process.env.VERSION': JSON.stringify(version) },
+          },
+          output: {
+            disableSourceMap: true,
+            distPath: {
+              root: './dist',
+              worker: './public',
+            },
+          },
+          dev: { progressBar: false },
         },
-        plugins: [
-          new DefinePlugin({
-            'process.env.VERSION': JSON.stringify(version),
-          }),
-        ],
       });
-      childCompiler.run((e: any) => {
-        if (e) {
-          logger.error(e);
-        } else {
-          logger.info('Build service worker successfully.');
-        }
-      });
+
+      if (compiler.watchMode) {
+        logger.info('Build service worker in watch mode.');
+        builder.build({ watch: true });
+      } else {
+        await builder.build();
+      }
     });
   }
 }
