dcc7a0b2caf092f8f32204ba0c54b1b1e44f2fcc
GiveMe-A-Name <a455300764b@hotmail.com>
2023-12-12 15:57:37 +0800

    fix: use string instance of regexp

--- packages/runtime/plugin-runtime/src/ssr/serverRender/renderToString/buildHtml.ts
+++ packages/runtime/plugin-runtime/src/ssr/serverRender/renderToString/buildHtml.ts
@@ -21,21 +21,21 @@ export function buildHtml(template: string, callbacks: BuildHtmlCb[]) {
 }
 
 export function createReplaceHtml(html: string): BuildHtmlCb {
-  const HTML_REG = /<!--<\?-\s*html\s*\?>-->/;
-  return (template: string) => unsafeReplace(template, HTML_REG, html);
+  const HTML_REMARK = '<!--<?- html ?>-->';
+  return (template: string) => unsafeReplace(template, HTML_REMARK, html);
 }
 
 export function createReplaceSSRDataScript(data: string): BuildHtmlCb {
-  const SSR_DATA_REG = /<!--<\?-\s*SSRDataScript\s*\?>-->/;
-  return (template: string) => unsafeReplace(template, SSR_DATA_REG, data);
+  const SSR_DATA_REMARK = '<!--<?- SSRDataScript ?>-->';
+  return (template: string) => unsafeReplace(template, SSR_DATA_REMARK, data);
 }
 
 export function createReplaceChunkJs(js: string): BuildHtmlCb {
-  const CHUNK_JS_REG = /<!--<\?-\s*chunksMap\.js\s*\?>-->/;
-  return (template: string) => unsafeReplace(template, CHUNK_JS_REG, js);
+  const CHUNK_JS_REMARK = '<!--<?- chunksMap.js ?>-->';
+  return (template: string) => unsafeReplace(template, CHUNK_JS_REMARK, js);
 }
 
 export function createReplaceChunkCss(css: string): BuildHtmlCb {
-  const CHUNK_CSS_REG = /<!--<\?-\s*chunksMap\.css\s*\?>-->/;
+  const CHUNK_CSS_REG = '<!--<?- chunksMap.css ?>-->';
   return (template: string) => unsafeReplace(template, CHUNK_CSS_REG, css);
 }
