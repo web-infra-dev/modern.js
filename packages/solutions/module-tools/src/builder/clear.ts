e4dd3538c9bb3ad4c795de2c95edb873305385e8
10Derozan <fupeng.antik@gmail.com>
2023-12-12 15:45:43 +0800

    fix(module-tools): use tsc --clean to clear the tsbuildinfo and d.ts (#5064)

--- packages/solutions/module-tools/src/builder/clear.ts
+++ packages/solutions/module-tools/src/builder/clear.ts
@@ -1,6 +1,7 @@
-import { fs, logger, chalk } from '@modern-js/utils';
+import { fs, logger, chalk, execa } from '@modern-js/utils';
 import type { BaseBuildConfig } from '../types';
 import { i18n, localeKeys } from '../locale';
+import { getTscBinPath } from '../utils';
 
 export const clearBuildConfigPaths = async (
   configs: BaseBuildConfig[],
@@ -11,6 +12,20 @@ export const clearBuildConfigPaths = async (
       logger.warn(chalk.bgYellowBright(i18n.t(localeKeys.warns.clearRootPath)));
     } else {
       await fs.remove(config.outDir);
+    }
+
+    // tsc --build --clean
+    if (config.buildType === 'bundleless' && config.dts) {
+      const tscBinFile = await getTscBinPath(projectAbsRootPath);
+      const childProgress = execa(tscBinFile, ['--build', '--clean'], {
+        stdio: 'pipe',
+        cwd: projectAbsRootPath,
+      });
+      try {
+        await childProgress;
+      } catch (e) {
+        logger.error(e);
+      }
     }
   }
 };
