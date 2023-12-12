cc55af2fc48b065cbf485ac2a3135bf5b0936e47
GiveMe-A-Name <a455300764b@hotmail.com>
2023-12-12 11:32:06 +0800

    fix: if the second args of string.replace is a string, it would as RegExp

--- packages/runtime/plugin-runtime/src/ssr/serverRender/renderToString/buildHtml.ts
+++ packages/runtime/plugin-runtime/src/ssr/serverRender/renderToString/buildHtml.ts
@@ -6,20 +6,20 @@ export function buildHtml(template: string, callbacks: BuildHtmlCb[]) {
 
 export function createReplaceHtml(html: string): BuildHtmlCb {
   const HTML_REG = /<!--<\?-\s*html\s*\?>-->/;
-  return (template: string) => template.replace(HTML_REG, html);
+  return (template: string) => template.replace(HTML_REG, () => html);
 }
 
 export function createReplaceSSRDataScript(data: string): BuildHtmlCb {
   const SSR_DATA_REG = /<!--<\?-\s*SSRDataScript\s*\?>-->/;
-  return (template: string) => template.replace(SSR_DATA_REG, data);
+  return (template: string) => template.replace(SSR_DATA_REG, () => data);
 }
 
 export function createReplaceChunkJs(js: string): BuildHtmlCb {
   const CHUNK_JS_REG = /<!--<\?-\s*chunksMap\.js\s*\?>-->/;
-  return (template: string) => template.replace(CHUNK_JS_REG, js);
+  return (template: string) => template.replace(CHUNK_JS_REG, () => js);
 }
 
 export function createReplaceChunkCss(css: string): BuildHtmlCb {
   const CHUNK_CSS_REG = /<!--<\?-\s*chunksMap\.css\s*\?>-->/;
-  return (template: string) => template.replace(CHUNK_CSS_REG, css);
+  return (template: string) => template.replace(CHUNK_CSS_REG, () => css);
 }
