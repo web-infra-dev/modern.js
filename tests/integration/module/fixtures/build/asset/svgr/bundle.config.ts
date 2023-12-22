bf498d8ac5c164570ad4c09390984c26ad1dc451
10Derozan <fupeng.antik@gmail.com>
2023-12-22 17:27:17 +0800

    feat(module-tools): support svgr with url-loader (#5108)

--- tests/integration/module/fixtures/build/asset/svgr/bundle.config.ts
+++ tests/integration/module/fixtures/build/asset/svgr/bundle.config.ts
@@ -1,14 +1,29 @@
 import { defineConfig } from '@modern-js/module-tools/defineConfig';
 
 export default defineConfig({
-  buildConfig: {
-    target: 'es2021',
-    buildType: 'bundle',
-    outDir: './dist/bundle',
-    externals: [/^react\/?/],
-    format: 'esm',
-    asset: {
-      svgr: true,
+  buildConfig: [
+    {
+      target: 'es2021',
+      buildType: 'bundle',
+      outDir: './dist/bundle',
+      externals: [/^react\/?/],
+      format: 'esm',
+      asset: {
+        svgr: true,
+      },
     },
-  },
+    {
+      input: ['src/namedExport.js'],
+      target: 'es2021',
+      buildType: 'bundle',
+      outDir: './dist/namedExport',
+      externals: [/^react\/?/],
+      format: 'esm',
+      asset: {
+        svgr: {
+          exportType: 'named',
+        },
+      },
+    },
+  ],
 });
--- tests/integration/module/fixtures/build/asset/svgr/bundleless.config.ts
+++ tests/integration/module/fixtures/build/asset/svgr/bundleless.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from '@modern-js/module-tools/defineConfig';
 
 export default defineConfig({
   buildConfig: {
+    input: ['src/index.js', 'src/logo.svg'],
     target: 'es2021',
     buildType: 'bundleless',
     outDir: './dist/bundleless',
