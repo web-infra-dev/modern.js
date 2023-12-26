e25a5e303b10cf607993c1f47a94f730f458798e
qixuan <58852732+GiveMe-A-Name@users.noreply.github.com>
2023-12-26 14:53:52 +0800

    fix(ssr): support stream ssr inline asserts (#5146)

--- packages/runtime/plugin-runtime/src/ssr/serverRender/renderToStream/bulidTemplate.before.ts
+++ packages/runtime/plugin-runtime/src/ssr/serverRender/renderToStream/bulidTemplate.before.ts
@@ -2,17 +2,49 @@ import ReactHelmet, { HelmetData } from 'react-helmet';
 // Todo: This import will introduce router code, like remix, even if router config is false
 import { matchRoutes } from '@modern-js/runtime-utils/router';
 import helmetReplace from '../helmet';
-import { RuntimeContext } from '../types';
+import { RuntimeContext, SSRPluginConfig } from '../types';
 import { CHUNK_CSS_PLACEHOLDER } from '../constants';
-import { safeReplace } from '../utils';
+import { checkIsNode, safeReplace } from '../utils';
 import {
   HEAD_REG_EXP,
   BuildTemplateCb,
   buildTemplate,
 } from './buildTemplate.share';
 
+const readAsset = async (chunk: string) => {
+  // working node env
+  const fs = await import('fs/promises');
+  const path = await import('path');
+
+  // only working in 'production' env
+  // we need ensure the assetsDir is same as ssr bundles.
+  const filepath = path.join(__dirname, chunk);
+
+  return fs.readFile(filepath, 'utf-8');
+};
+
+const checkIsInline = (
+  chunk: string,
+  enableInline: boolean | RegExp | undefined,
+) => {
+  // only production apply the inline config
+  if (process.env.NODE_ENV === 'production') {
+    if (enableInline instanceof RegExp) {
+      return enableInline.test(chunk);
+    } else {
+      return Boolean(enableInline);
+    }
+  } else {
+    return false;
+  }
+};
+
 // build head template
-function getHeadTemplate(beforeEntryTemplate: string, context: RuntimeContext) {
+function getHeadTemplate(
+  beforeEntryTemplate: string,
+  context: RuntimeContext,
+  pluginConfig: SSRPluginConfig,
+) {
   const callbacks: BuildTemplateCb[] = [
     headTemplate => {
       const helmetData: HelmetData = ReactHelmet.renderStatic();
@@ -30,10 +62,12 @@ function getHeadTemplate(beforeEntryTemplate: string, context: RuntimeContext) {
   }
   return buildTemplate(headTemplate, callbacks);
 
-  function injectCss(headTemplate: string) {
-    return safeReplace(headTemplate, CHUNK_CSS_PLACEHOLDER, getCssChunks());
+  async function injectCss(headTemplate: string) {
+    const css = await getCssChunks();
 
-    function getCssChunks() {
+    return safeReplace(headTemplate, CHUNK_CSS_PLACEHOLDER, css);
+
+    async function getCssChunks() {
       const { routeManifest, routerContext, routes } = context;
       if (!routeManifest || !routerContext || !routes) {
         return '';
@@ -69,20 +103,38 @@ function getHeadTemplate(beforeEntryTemplate: string, context: RuntimeContext) {
         }
       });
 
-      const styleLinks = cssChunks.map((chunk: string) => {
-        return `<link href="${chunk}" rel="stylesheet" />`;
-      });
+      const { enableInlineStyles } = pluginConfig;
+      const styles = await Promise.all(
+        cssChunks.map(async chunk => {
+          const link = `<link href="${chunk}" rel="stylesheet" />`;
+          if (checkIsNode() && checkIsInline(chunk, enableInlineStyles)) {
+            return readAsset(chunk)
+              .then(content => `<style>${content}</style>`)
+              .catch(_ => {
+                // if read file occur error, we should return link to import css assets.
+                return link;
+              });
+          } else {
+            return link;
+          }
+        }),
+      );
 
-      return `${styleLinks.join('')}`;
+      return `${styles.join('')}`;
     }
   }
 }
 
 // build script
-export function buildShellBeforeTemplate(
+export async function buildShellBeforeTemplate(
   beforeAppTemplate: string,
   context: RuntimeContext,
+  pluginConfig: SSRPluginConfig,
 ) {
-  const headTemplate = getHeadTemplate(beforeAppTemplate, context);
+  const headTemplate = await getHeadTemplate(
+    beforeAppTemplate,
+    context,
+    pluginConfig,
+  );
   return beforeAppTemplate.replace(HEAD_REG_EXP, headTemplate);
 }
