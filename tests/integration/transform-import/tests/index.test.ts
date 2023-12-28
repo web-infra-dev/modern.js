27dd36f34530211bf6135c76e0700521de0bfb4d
Fy <1114550440@qq.com>
2023-12-28 14:12:41 +0800

    fix(integration-tests): add missing dependencies (#5167)

--- tests/integration/transform-import/tests/index.test.ts
+++ tests/integration/transform-import/tests/index.test.ts
@@ -3,13 +3,13 @@ import { modernBuild } from '../../../utils/modernTestUtils';
 
 const appDir = path.resolve(__dirname, '../');
 
-describe('test temp-dir', () => {
+describe('test transformImport type', () => {
   let buildRes: { code: number };
   beforeAll(async () => {
     buildRes = await modernBuild(appDir);
   });
 
   test(`should build`, async () => {
-    expect(buildRes.code === 0).toBe(true);
+    expect(buildRes.code).toBe(0);
   });
 });
