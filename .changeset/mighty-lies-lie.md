6e81cdba3136a46f4ac053848b97f045861bfacf
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2024-01-03 19:53:54 +0800

    chore(builder-cli): add deprecated warning

---
+++ .changeset/mighty-lies-lie.md
@@ -0,0 +1,7 @@
+---
+'@modern-js/builder-cli': patch
+---
+
+chore(builder-cli): add deprecated warning, please use rsbuild instead
+
+chore(builder-cli): 添加废弃声明，推荐使用 rsbuild 代替
--- packages/builder/builder-cli/bin/builder.js
+++ packages/builder/builder-cli/bin/builder.js
@@ -5,6 +5,10 @@ async function main() {
   const { version } = require('../package.json');
   logger.greet(`  ${`Modern.js Builder v${version}`}\n`);
 
+  logger.warn(
+    '@modern-js/builder-cli is no longer maintained, please use rsbuild(https://rsbuild.dev/) instead.',
+  );
+
   try {
     const { runCli } = require('@modern-js/builder/cli');
     await runCli();
