5e6c84dec636ed4f627dda5cf9ba1fd880c8ea77
Asuka109 <asuka109@foxmail.com>
2024-01-12 18:14:31 +0800

    fix: failed to fetch file system routes

--- packages/devtools/client/src/components/ServerRoute/EntryRoute/ClientRoute/Stats.tsx
+++ packages/devtools/client/src/components/ServerRoute/EntryRoute/ClientRoute/Stats.tsx
@@ -1,23 +1,17 @@
 import { FileSystemRoutes } from '@modern-js/devtools-kit';
 import type { RouteLegacy, ServerRoute } from '@modern-js/types';
 import _ from 'lodash';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { proxy, useSnapshot } from 'valtio';
 import { Promisable } from 'type-fest';
 import { LegacyRouteStats } from './LegacyRoute/Stats';
 import { RemixRouteStats } from './RemixRoute/Stats';
 import { $framework, $server } from '@/entries/client/routes/state';
+import { useThrowable } from '@/utils';
 
 export const $fileSystemRoutes = proxy<
   Record<string, Promisable<FileSystemRoutes>>
 >({});
-
-$server.then(({ hooks, remote }) => {
-  remote.getFileSystemRoutes('');
-  hooks.hook('updateFileSystemRoutes', ({ entrypoint, routes }) => {
-    $fileSystemRoutes[entrypoint.entryName] = routes;
-  });
-});
 
 export interface ClientRouteStatsProps {
   route: ServerRoute;
@@ -26,8 +20,8 @@ export interface ClientRouteStatsProps {
 export const ClientRouteStats: React.FC<ClientRouteStatsProps> = ({
   route,
 }) => {
+  const server = useThrowable($server);
   const framework = useSnapshot($framework);
-  const fileSystemRoutes = useSnapshot($fileSystemRoutes);
   const { entrypoints } = framework.context;
   const entrypoint =
     route.entryName && _.find(entrypoints, { entryName: route.entryName });
@@ -38,15 +32,25 @@ export const ClientRouteStats: React.FC<ClientRouteStatsProps> = ({
     );
   }
 
-  const fileSystemRoute = fileSystemRoutes[entrypoint.entryName];
+  const $fileSystemRoute = server.remote.getFileSystemRoutes(
+    entrypoint.entryName,
+  );
 
-  if (isLegacyRoutes(fileSystemRoute as any)) {
-    return <LegacyRouteStats />;
-  } else {
-    return (
-      <RemixRouteStats remixRoutes={fileSystemRoute as any} route={route} />
-    );
-  }
+  const Dispatcher: React.FC = () => {
+    const fileSystemRoute = useThrowable($fileSystemRoute);
+    if (isLegacyRoutes(fileSystemRoute as any)) {
+      return <LegacyRouteStats />;
+    } else {
+      return (
+        <RemixRouteStats remixRoutes={fileSystemRoute as any} route={route} />
+      );
+    }
+  };
+  return (
+    <Suspense>
+      <Dispatcher />
+    </Suspense>
+  );
 };
 
 const isLegacyRoutes = (routes: FileSystemRoutes): routes is RouteLegacy[] =>
