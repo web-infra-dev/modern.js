75f10197bdcb95a8e644ea9d8fa17c2eed2a6a80
qixuan <58852732+GiveMe-A-Name@users.noreply.github.com>
2024-01-17 16:02:58 +0800

    feat: add node static middleware (#5235)

--- packages/server/prod-server-new/src/index.ts
+++ packages/server/prod-server-new/src/index.ts
@@ -1,11 +1,27 @@
 import { Server as NodeServer } from 'node:http';
 import type { ServerCoreOptions } from '@modern-js/server-base';
-import { createServerBase, createNodeServer } from '@modern-js/server-base';
+import {
+  createServerBase,
+  createNodeServer,
+  createStaticMiddleware,
+} from '@modern-js/server-base';
 
 export default async (
   options: Omit<ServerCoreOptions, 'app'>,
 ): Promise<NodeServer> => {
+  const { config, pwd } = options;
+
   const server = await createServerBase(options);
+
+  const staticMiddleware = createStaticMiddleware({
+    pwd,
+    output: config?.output || {},
+    html: config?.html || {},
+  });
+  server.use(staticMiddleware);
+
+  await server.init();
+
   const nodeServer = createNodeServer(server.handle.bind(server));
 
   return nodeServer;
