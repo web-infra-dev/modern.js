69f50c80361c9838056ffd862c74819e8426ffe4
wangyiming.fe <wangyiming.fe@bytedance.com>
2024-01-17 14:35:08 +0800

    feat: gather hook

--- packages/server/server-base/src/libs/utils.ts
+++ packages/server/server-base/src/libs/utils.ts
@@ -4,3 +4,36 @@ export const getRuntimeEnv = () => {
   }
   return 'other';
 };
+
+export type CollectMiddlewaresResult = {
+  web: any[];
+  api: any[];
+};
+
+export const mergeExtension = (users: any[]) => {
+  const output: any[] = [];
+  return { middleware: output.concat(users) };
+};
+
+export const createMiddlewareCollecter = () => {
+  const webMiddlewares: any[] = [];
+  const apiMiddlewares: any[] = [];
+
+  const addWebMiddleware = (input: any) => {
+    webMiddlewares.push(input);
+  };
+
+  const addAPIMiddleware = (input: any) => {
+    apiMiddlewares.push(input);
+  };
+
+  const getMiddlewares = (): CollectMiddlewaresResult => ({
+    web: webMiddlewares,
+    api: apiMiddlewares,
+  });
+  return {
+    getMiddlewares,
+    addWebMiddleware,
+    addAPIMiddleware,
+  };
+};
