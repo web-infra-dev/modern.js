79bfe934145ee683150786c6c7928567c579ef4e
GiveMe-A-Name <a455300764b@hotmail.com>
2024-01-18 17:31:43 +0800

    fix: move prepare Handler to prod-server

--- packages/server/server-base/src/ServerCore.ts
+++ packages/server/server-base/src/ServerCore.ts
@@ -21,7 +21,7 @@ import {
   fs,
   isWebOnly,
 } from '@modern-js/utils';
-import { ISAppContext, ServerRoute } from '@modern-js/types';
+import { ISAppContext } from '@modern-js/types';
 import { defaultMetrics } from './libs/default';
 import {
   ConfWithBFF,
@@ -44,7 +44,6 @@ import {
   createMiddlewareCollecter,
 } from './libs/utils';
 import { favionFallbackMiddleware } from './middlewares/faviconFallback';
-import { createRenderHandler } from './renderHandler';
 
 export class ServerCore {
   public options: ServerCoreOptions;
@@ -61,13 +60,10 @@ export class ServerCore {
 
   private conf: ServerOptions;
 
-  private routes: ServerRoute[];
-
   constructor(options: ServerCoreOptions) {
     options.logger = options.logger || createLogger({ level: 'warn' });
     options.metrics = options.metrics || defaultMetrics;
     this.options = options;
-    this.routes = options.routes;
 
     this.app = options.app;
     this.serverConfig = {};
@@ -123,8 +119,6 @@ export class ServerCore {
 
     // eslint-disable-next-line @typescript-eslint/await-thenable
     await this.runner.prepare();
-
-    await this.prepareHandler();
 
     return this;
   }
@@ -338,26 +332,6 @@ export class ServerCore {
       serverConfig: finalServerConfig,
       resolvedConfigPath,
     });
-  }
-
-  private async prepareHandler() {
-    // prepare render handler
-    // TODO: get server config from server.ssr & server.ssrByEntries
-    const ssrConfig = this.conf.server?.ssr;
-    const forceCSR = typeof ssrConfig === 'object' ? ssrConfig.forceCSR : false;
-    for (const route of this.routes) {
-      const { entryPath } = route;
-
-      const handler = await createRenderHandler({
-        distDir: this.distDir,
-        routeInfo: route,
-        staticGenerate: this.options.staticGenerate,
-        forceCSR,
-        metaName: this.options.metaName || 'modern.js',
-      });
-
-      this.app.get(entryPath, handler);
-    }
   }
 
   get use() {
