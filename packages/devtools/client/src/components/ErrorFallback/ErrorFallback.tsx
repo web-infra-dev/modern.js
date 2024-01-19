9eb6bcf6fa695f6fea7ff440f83f065dff563674
Asuka109 <asuka109@foxmail.com>
2024-01-19 16:13:25 +0800

    feat: better error boundary styling

--- packages/devtools/client/src/components/ErrorFallback/ErrorFallback.tsx
+++ packages/devtools/client/src/components/ErrorFallback/ErrorFallback.tsx
@@ -1,31 +1,31 @@
 import React from 'react';
 import { FallbackProps } from 'react-error-boundary';
 import { useRouteError } from '@modern-js/runtime/router';
-import { Box, Heading, Text } from '@radix-ui/themes';
+import { Box, Heading, Text, TextArea } from '@radix-ui/themes';
 
 export type ErrorFallbackProps = FallbackProps;
 
 export const ErrorFallback: React.FC<ErrorFallbackProps> = props => {
   const { error } = props;
-  if (error instanceof Error) {
-    return (
-      <Box p="3">
-        <Heading as="h4">
-          <Text as="span">
-            {error.name}: {error.message}
-          </Text>
-        </Heading>
-        <pre>{error.stack}</pre>
-      </Box>
-    );
-  } else {
-    return (
-      <Box p="3">
-        <Heading as="h3">Unknown Error</Heading>
-        <pre>{error.message}</pre>
-      </Box>
-    );
-  }
+  const title =
+    error instanceof Error
+      ? `${error.name}: ${error.message}`
+      : 'Unknown Error';
+  const description =
+    error instanceof Error
+      ? error.stack
+      : Object.prototype.toString.call(error);
+
+  return (
+    <Box p="4">
+      <Heading as="h4" mb="4">
+        <Text as="span">{title}</Text>
+      </Heading>
+      <TextArea disabled style={{ height: '50vh' }}>
+        {description}
+      </TextArea>
+    </Box>
+  );
 };
 
 export const ErrorRouteHandler: React.FC<ErrorFallbackProps> = props => {
