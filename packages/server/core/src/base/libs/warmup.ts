2fe1059729f2de35cc37b85505d1e25833bec712
GiveMe-A-Name <a455300764b@hotmail.com>
2024-01-22 16:24:19 +0800

    fix: warmup in dev

--- packages/server/core/src/base/libs/warmup.ts
+++ packages/server/core/src/base/libs/warmup.ts
@@ -1,15 +1,10 @@
-// warmup bundle in production env
-import { checkIsProd } from './utils';
-
 export function warmup(bundles: Array<string | undefined>) {
-  if (checkIsProd()) {
-    bundles.forEach(bundle => {
-      try {
-        bundle && import(bundle);
-      } catch (_) {
-        // ignore errors, if we can't import the bundle
-        // such as: the bundle is not exists.
-      }
-    });
-  }
+  bundles.forEach(bundle => {
+    try {
+      bundle && import(bundle);
+    } catch (_) {
+      // ignore errors, if we can't import the bundle
+      // such as: the bundle is not exists.
+    }
+  });
 }
