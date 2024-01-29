4fd1da7e46fb95c3f09bd4145e6ee5a1340bbf0a
Asuka109 <chenwenjie.asuka@bytedance.com>
2024-01-29 16:17:24 +0800

    feat(devtools): use loader to redirect to welcome page (#5323)

--- packages/devtools/client/src/entries/client/routes/headers/page.tsx
+++ packages/devtools/client/src/entries/client/routes/headers/page.tsx
@@ -1,17 +1,1 @@
-import React, { useEffect } from 'react';
-import { useSnapshot } from 'valtio';
-import { useNavigate } from '@modern-js/runtime/router';
-import { $state } from './state';
-
-const Page: React.FC = () => {
-  const { service } = useSnapshot($state);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate(service.rules ? './editor' : './welcome');
-  }, []);
-
-  return null;
-};
-
-export default Page;
+export default null;
