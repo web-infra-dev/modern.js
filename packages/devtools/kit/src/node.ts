9fd6ec06123f77148be99b63059da47e0f3f3b7b
Asuka109 <asuka109@foxmail.com>
2024-01-31 19:08:21 +0800

    feat(devtools): check runtime environment before re-export

--- packages/devtools/kit/src/node.ts
+++ packages/devtools/kit/src/node.ts
@@ -1,6 +1,11 @@
+if (typeof globalThis.process === 'undefined') {
+  throw new Error('@modern-js/devtools-kit/node should not be used in browser');
+}
+
 export * from './server';
 export * from './client';
 export * from './mount-point';
 export * from './utils';
 export * from './constants';
 export * from './channel';
+export * from './rsdoctor';
--- packages/devtools/kit/src/runtime.ts
+++ packages/devtools/kit/src/runtime.ts
@@ -1,6 +1,13 @@
+if (typeof globalThis.window === 'undefined') {
+  throw new Error(
+    '@modern-js/devtools-kit/runtime should not be used in Node.js',
+  );
+}
+
 export type * from './server';
 export * from './client';
 export * from './mount-point';
 export * from './utils';
 export * from './constants';
 export * from './channel';
+export type * from './rsdoctor';
