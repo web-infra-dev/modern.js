b0e3080f5f889433687733b080405b561567d830
GiveMe-A-Name <a455300764b@hotmail.com>
2024-02-19 17:14:08 +0800

    chore: remove unnecessary code

--- packages/server/core/src/base/adapters/node/loadServer.ts
+++ packages/server/core/src/base/adapters/node/loadServer.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { fs, dotenv, dotenvExpand } from '@modern-js/utils';
 import { ServerBaseOptions } from '../../../core/server';
 
+/** 读取 .env.{process.env.MODERN_ENV} 文件，加载环境变量 */
 export async function loadServerEnv(options: ServerBaseOptions) {
   const { pwd } = options;
   const serverEnv = process.env.MODERN_ENV;
--- packages/server/core/src/base/libs/warmup.ts
+++
@@ -1,9 +0,0 @@
-export function warmup(bundles: Array<string | undefined>) {
-  bundles.forEach(bundle => {
-    bundle &&
-      import(bundle).catch(_ => {
-        // ignore errors, if we can't import the bundle
-        // such as: the bundle is not exists.
-      });
-  });
-}
