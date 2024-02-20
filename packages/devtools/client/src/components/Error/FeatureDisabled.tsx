e3d1fe4430e72c1f93274785f87fc270cc9cc39f
chenwenjie <chenwenjie.asuka@bytedance.com>
2024-02-20 19:49:45 +0800

    feat(devtools): improve error page

---
+++ packages/devtools/client/src/components/Error/FeatureDisabled.tsx
@@ -0,0 +1,25 @@
+import React, { PropsWithChildren } from 'react';
+import { Box, Flex, Heading } from '@radix-ui/themes';
+import { HiExclamationCircle } from 'react-icons/hi2';
+import styles from './FeatureDisabled.module.scss';
+
+export interface FeatureDisabledProps extends PropsWithChildren {
+  title: string;
+}
+
+export const FeatureDisabled: React.FC<FeatureDisabledProps> = props => {
+  const { title, children } = props;
+  return (
+    <Flex className={styles.container} mt="9" mx="auto" gap="2">
+      <Flex height="9" align="center">
+        <HiExclamationCircle size="36" />
+      </Flex>
+      <Box>
+        <Flex height="9" align="center">
+          <Heading>{title}</Heading>
+        </Flex>
+        <Box className={styles.children}>{children}</Box>
+      </Box>
+    </Flex>
+  );
+};
