8e988b7d0b2917e29972968246d72b696158b57c
Ming <527990618@163.com>
2024-02-23 17:01:31 +0800

    fix: ssr.inlineScript shouldn't works in streaming ssr

---
+++ .changeset/shiny-parents-explain.md
@@ -0,0 +1,6 @@
+---
+'@modern-js/runtime': patch
+---
+
+fix: `ssr.inlineScript` shouldn't works in streaming ssr
+fix: `ssr.inlineScript` 不应该在 streaming ssr 模式下工作
--- tests/integration/routes/modern.config.ts
+++ tests/integration/routes/modern.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
   server: {
     ssr: {
       mode: 'stream',
+      inlineScript: false,
     },
     ssrByEntries: {
       one: false,
