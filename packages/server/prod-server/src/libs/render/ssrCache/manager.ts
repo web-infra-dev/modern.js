e8cfce87ef013da8ad0295cc5e2658cf65f20222
GiveMe-A-Name <a455300764b@hotmail.com>
2024-03-11 11:11:34 +0800

    fix: remove comment

--- packages/server/prod-server/src/libs/render/ssrCache/manager.ts
+++ packages/server/prod-server/src/libs/render/ssrCache/manager.ts
@@ -127,15 +127,3 @@ export class CacheManager {
     }
   }
 }
-
-// // we use `pathname.replace(/\/+$/, '')` to remove the '/' with end.
-// // examples:
-// // pathname1: '/api', pathname2: '/api/'
-// // pathname1 as same as pathname2
-// export function getPath(pathname: string) {
-//   if (pathname === '/') {
-//     return pathname;
-//   } else {
-//     return pathname.replace(/\/+$/, '');
-//   }
-// }
