d19df241907a79db95c86a44dc821cd7f4e8c747
chenwenjie <chenwenjie.asuka@bytedance.com>
2024-03-19 16:03:20 +0800

    feat: add cookie, local storage, and session storage RPC functions

--- packages/devtools/kit/src/storage-preset.ts
+++ packages/devtools/kit/src/storage-preset.ts
@@ -1,32 +1,12 @@
-import type { CookieSerializeOptions } from 'cookie-es';
-
-export interface LocalStoragePresetItem {
-  type: 'local-storage';
+export interface StorageRecord {
+  type: 'local-storage' | 'session-storage' | 'cookie';
   key: string;
   value: string | object | number;
 }
-
-export interface SessionStoragePresetItem {
-  type: 'session-storage';
-  key: string;
-  value: string | object | number;
-}
-
-export interface CookiePresetItem
-  extends Omit<CookieSerializeOptions, 'encode'> {
-  type: 'cookie';
-  key: string;
-  value: string | object | number;
-}
-
-export type StoragePreset =
-  | LocalStoragePresetItem
-  | SessionStoragePresetItem
-  | CookiePresetItem;
 
 export interface StoragePresetOptions {
   name: string;
-  items: StoragePreset[];
+  items: StorageRecord[];
 }
 
 export interface StoragePresetContext extends StoragePresetOptions {
