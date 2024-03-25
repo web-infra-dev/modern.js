6a44910cba8d336cda41266ad4e13b8e1971b1ec
qixuan <58852732+GiveMe-A-Name@users.noreply.github.com>
2024-03-25 13:05:00 +0800

    fix: resolved reload infinite & custom server request can be set (#5574)

--- packages/server/core/src/base/middlewares/renderHandler/ssrRender.ts
+++ packages/server/core/src/base/middlewares/renderHandler/ssrRender.ts
@@ -5,7 +5,13 @@ import {
   MAIN_ENTRY_NAME,
 } from '@modern-js/utils/universal/constants';
 import * as isbot from 'isbot';
-import { getRuntimeEnv, parseHeaders, parseQuery, getHost } from '../../utils';
+import {
+  getRuntimeEnv,
+  parseHeaders,
+  parseQuery,
+  getHost,
+  getPathname,
+} from '../../utils';
 import {
   SSRServerContext,
   ServerManifest,
@@ -76,15 +82,22 @@ export async function ssrRender(
   const query = parseQuery(request);
   const headers = parseHeaders(request);
 
+  if (nodeReq) {
+    for (const key in nodeReq.headers) {
+      if (!headers[key]) {
+        headers[key] = nodeReq.headers[key] as string;
+      }
+    }
+  }
+
   const ssrContext: SSRServerContext = {
     request: {
       baseUrl: routeInfo.urlPath,
       params: {} as Record<string, string>,
-      // eslint-disable-next-line node/no-unsupported-features/node-builtins, node/prefer-global/url
-      pathname: new URL(request.url).pathname,
+      pathname: nodeReq?.url || getPathname(request),
       host,
       query,
-      url: request.url,
+      url: nodeReq ? getHrefFromNodeReq(nodeReq) : request.url,
       headers,
     },
     response: {
@@ -221,7 +234,34 @@ class IncomingMessgeProxy {
 
     this.method = req.method;
 
-    // eslint-disable-next-line node/no-unsupported-features/node-builtins, node/prefer-global/url
-    this.url = new URL(req.url).pathname;
+    this.url = getPathname(req);
   }
+}
+
+function getHrefFromNodeReq(nodeReq: IncomingMessage) {
+  function getProtocal() {
+    if ((nodeReq.socket as any).encrypted) {
+      return 'https';
+    }
+
+    const proto = nodeReq.headers['x-forwarded-proto'];
+    return proto ? (proto as string).split(/\s*,\s*/, 1)[0] : 'http';
+  }
+
+  function getHost() {
+    let host = nodeReq.headers['x-forwarded-host'];
+    if (!host) {
+      // eslint-disable-next-line prefer-destructuring
+      host = nodeReq.headers.host;
+    }
+
+    host = (host as string).split(/\s*,\s*/, 1)[0] || 'undefined';
+    // the host = '',if we can't cat Host or X-Forwarded-Host header
+    // but the this.href would assign a invalid value:`http[s]://${pathname}`
+    // so we need assign host a no-empty value.
+    return host;
+  }
+
+  const href = `${getProtocal()}://${getHost()}${nodeReq.url || ''}`;
+  return href;
 }
