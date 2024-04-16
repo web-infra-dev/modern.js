a83fcd97adf7d19f00ff4d3830801b21bfd06cb3
gaoyuan <gaoyuan.1226@bytedance.com>
2024-04-16 11:53:53 +0800

    fix: remove crossOrigin inaccurate test case (#5647)

--- tests/e2e/builder/cases/html/cross-origin/index.test.ts
+++ tests/e2e/builder/cases/html/cross-origin/index.test.ts
@@ -19,24 +19,6 @@ test('should not apply crossOrigin by default', async () => {
   expect(html).not.toContain('crossorigin');
 });
 
-test('should not apply crossOrigin when same origin', async () => {
-  const builder = await build({
-    cwd: __dirname,
-    entry: { index: path.resolve(__dirname, './src/index.js') },
-    builderConfig: {
-      html: {
-        scriptLoading: 'blocking',
-        crossorigin: 'anonymous',
-      },
-    },
-  });
-  const files = await builder.unwrapOutputJSON();
-  const html =
-    files[Object.keys(files).find(file => file.endsWith('index.html'))!];
-
-  expect(html).not.toContain('crossorigin');
-});
-
 test('should apply crossOrigin when crossorigin is "anonymous" and not same origin', async () => {
   const builder = await build({
     cwd: __dirname,
