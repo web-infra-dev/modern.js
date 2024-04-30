b34407ecd748c9bd050ffc97dba879d050b88151
wangyiming <wangyiming.fe@bytedance.com>
2024-04-30 12:14:18 +0800

    fix: data.ts should not import into the client bundle in the ssg project

--- tests/integration/ssg/fixtures/nested-routes/src/routes/user/page.data.js
+++ tests/integration/ssg/fixtures/nested-routes/src/routes/user/page.data.js
@@ -1,7 +1,7 @@
+// Test that this file is not imported into the client bundle
+// eslint-disable-next-line no-unused-vars
 const fs = require('fs');
 
 export const loader = () => {
-  // Test that this file is not imported into the client bundle
-  console.log(typeof fs.readFile);
   return 'Hello, User';
 };
