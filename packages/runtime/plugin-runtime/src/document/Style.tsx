596866b94bba28a8153ec34bc3391aed2b281ce0
clchenliang <clchenliang@hotmail.com>
2024-05-15 11:31:38 +0800

    feat: add Body Root Script typings

--- packages/runtime/plugin-runtime/src/document/Style.tsx
+++ packages/runtime/plugin-runtime/src/document/Style.tsx
@@ -1,11 +1,15 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React from 'react';
 import {
   DOCUMENT_STYLE_PLACEHOLDER_START,
   DOCUMENT_STYLE_PLACEHOLDER_END,
 } from './constants';
 
-export function Style(props: { children?: string; content?: string }) {
+export function Style(
+  props: {
+    children?: string;
+    content?: string;
+  } & React.DOMAttributes<HTMLStyleElement>,
+) {
   const { content, children } = props;
   const contentStr = children || content;
   const styleContent = encodeURIComponent(`${contentStr}`);
