4fb045c83d5ac56522aaf1a5f1e07d04416bdaf0
caohuilin <caohuilin@bytedance.com>
2024-06-01 02:49:24 +0800

    feat: render function with react17 and react18

--- packages/runtime-v2/plugin-runtime/src/runtime/client/index.tsx
+++ packages/runtime-v2/plugin-runtime/src/runtime/client/index.tsx
@@ -1,6 +1,7 @@
-import ReactDOM from 'react-dom/client';
 import { RuntimeReactContext, getInitialContext } from '../context/runtime';
 import { getGlobalRunner } from '../plugin/runner';
+
+const IS_REACT18 = process.env.IS_REACT18 === 'true';
 
 export async function render(App: React.ReactNode, id?: string) {
   const runner = getGlobalRunner();
@@ -11,9 +12,27 @@ export async function render(App: React.ReactNode, id?: string) {
     },
   );
   const rootElement = document.getElementById(id || 'root')!;
-  ReactDOM.createRoot(rootElement).render(
+  const renderFunc = IS_REACT18 ? renderWithReact18 : renderWithReact17;
+  renderFunc(
     <RuntimeReactContext.Provider value={context}>
       {App}
     </RuntimeReactContext.Provider>,
+    rootElement,
   );
+}
+
+async function renderWithReact18(
+  App: React.ReactElement,
+  rootElement: HTMLElement,
+) {
+  const ReactDOM = await import('react-dom/client');
+  ReactDOM.createRoot(rootElement).render(App);
+}
+
+async function renderWithReact17(
+  App: React.ReactElement,
+  rootElement: HTMLElement,
+) {
+  const ReactDOM = await import('react-dom');
+  ReactDOM.render(App, rootElement);
 }
