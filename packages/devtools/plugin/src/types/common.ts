75477a51fff7b4ff9f2aed437674ad0930bdf51f
chenwenjie <chenwenjie.asuka@bytedance.com>
2024-06-03 19:09:40 +0800

    feat: build plugin system for devtools

--- packages/devtools/plugin/src/types/common.ts
+++ packages/devtools/plugin/src/types/common.ts
@@ -1,8 +1,15 @@
 /* eslint-disable node/no-unsupported-features/es-builtins */
 import type { Buffer } from 'buffer';
 import { BaseHooks } from '@modern-js/core';
-import type { AppTools, AppToolsHooks, CliPlugin } from '@modern-js/app-tools';
+import type {
+  AppTools,
+  AppToolsHooks,
+  CliPlugin,
+  UserConfig,
+} from '@modern-js/app-tools';
 import type { ServerPlugin, ToThreads } from '@modern-js/server-core';
+import type { RsbuildPluginAPI } from '@rsbuild/core';
+import { Hookable } from 'hookable';
 
 export type CliPluginAPI = Parameters<
   NonNullable<CliPlugin<AppTools>['setup']>
@@ -28,9 +35,57 @@ export type BufferLike =
   | { valueOf: () => string }
   | { [Symbol.toPrimitive]: (hint: string) => string };
 
-export type Hooks = BaseHooks<any> & AppToolsHooks<any>;
+export type CleanHooks<T> = {
+  [K in keyof T]: T[K] extends (...args: [...infer Params]) => any
+    ? (...args: [...Params]) => void
+    : never;
+};
 
-export type InjectedHooks = Pick<
-  ToThreads<Hooks>,
-  'prepare' | 'modifyFileSystemRoutes' | 'afterCreateCompiler'
+export type $FrameworkHooks = CleanHooks<
+  Pick<
+    ToThreads<BaseHooks<any> & AppToolsHooks<any>>,
+    | 'prepare'
+    | 'modifyFileSystemRoutes'
+    | 'modifyServerRoutes'
+    | 'afterCreateCompiler'
+    | 'beforeRestart'
+    | 'beforeExit'
+  >
 >;
+
+export interface FrameworkHooks extends $FrameworkHooks {
+  config: (config: UserConfig<AppTools>) => void;
+}
+
+type UnwrapBuilderHooks<T> = {
+  [K in keyof T]: T[K] extends (...args: any) => any
+    ? (...params: [...Parameters<Parameters<T[K]>[0]>]) => void
+    : never;
+};
+
+export type $BuilderHooks = UnwrapBuilderHooks<
+  Pick<
+    RsbuildPluginAPI,
+    | 'modifyBundlerChain'
+    | 'modifyWebpackConfig'
+    | 'modifyRspackConfig'
+    | 'onBeforeCreateCompiler'
+    | 'onAfterCreateCompiler'
+    | 'onDevCompileDone'
+    | 'onAfterBuild'
+  >
+>;
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface BuilderHooks extends $BuilderHooks {}
+
+export interface PluginApi {
+  frameworkHooks: Hookable<FrameworkHooks>;
+  builderHooks: Hookable<BuilderHooks>;
+  setupFramework: () => Promise<CliPluginAPI>;
+  setupBuilder: () => Promise<RsbuildPluginAPI>;
+}
+
+export interface Plugin {
+  setup: (api: PluginApi) => Promise<void>;
+}
