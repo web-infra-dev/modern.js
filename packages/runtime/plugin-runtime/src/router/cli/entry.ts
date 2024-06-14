648f0a48719e12ab8e486db70454c11854a83f2f
caohuilin <caohuilin@bytedance.com>
2024-06-14 21:37:27 +0800

    fix: modifyEntrypoints for custom conventional routing

--- packages/runtime/plugin-runtime/src/router/cli/entry.ts
+++ packages/runtime/plugin-runtime/src/router/cli/entry.ts
@@ -33,35 +33,41 @@ export const modifyEntrypoints = (
     if (!isHasNestedRoutes && !isHasPages) {
       return entrypoint;
     }
-    entrypoint.fileSystemRoutes = {
-      ...entrypoint.fileSystemRoutes,
-      globalApp: findExists(
-        JS_EXTENSIONS.map(ext =>
-          path.resolve(
-            entrypoint.absoluteEntryDir!,
-            `./${PAGES_DIR_NAME}/${FILE_SYSTEM_ROUTES_GLOBAL_LAYOUT}${ext}`,
-          ),
-        ),
-      ),
-    };
-    if (isHasPages) {
-      entrypoint.entry = path.join(
-        entrypoint.absoluteEntryDir!,
-        PAGES_DIR_NAME,
-      );
-      entrypoint.pageRoutesEntry = entrypoint.entry;
-    }
-    if (isHasNestedRoutes) {
-      entrypoint.entry = path.join(
-        entrypoint.absoluteEntryDir!,
-        NESTED_ROUTES_DIR,
-      );
-      entrypoint.nestedRoutesEntry = entrypoint.entry;
-    }
-
+    // When the user configures a custom entry, and the entry path is a folder, fileSystemRoutes will be set to true during entry recognition.
+    // At this time, the `routes` will be used by default, and react router v5 is not supported.
     if (entrypoint.fileSystemRoutes && !isRouterV5(config)) {
       entrypoint.nestedRoutesEntry = entrypoint.entry;
+    } else if (!entrypoint.fileSystemRoutes) {
+      entrypoint.fileSystemRoutes = {
+        globalApp: findExists(
+          JS_EXTENSIONS.map(ext =>
+            path.resolve(
+              entrypoint.absoluteEntryDir!,
+              `./${PAGES_DIR_NAME}/${FILE_SYSTEM_ROUTES_GLOBAL_LAYOUT}${ext}`,
+            ),
+          ),
+        ),
+      };
+      if (isHasPages) {
+        entrypoint.entry = path.join(
+          entrypoint.absoluteEntryDir!,
+          PAGES_DIR_NAME,
+        );
+        entrypoint.pageRoutesEntry = entrypoint.entry;
+      }
+      if (isHasNestedRoutes) {
+        entrypoint.entry = path.join(
+          entrypoint.absoluteEntryDir!,
+          NESTED_ROUTES_DIR,
+        );
+        entrypoint.nestedRoutesEntry = entrypoint.entry;
+      }
+    } else {
+      throw Error(
+        'Custom entries with conventional routing not support use react router v5!',
+      );
     }
+
     return entrypoint;
   });
 };
