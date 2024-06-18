e7df4cf571a5bde38842dd464c9fa3ea7a8f5971
qixuan <58852732+GiveMe-A-Name@users.noreply.github.com>
2024-06-18 11:01:59 +0800

    Feat/server runtimev2 (#5844)
    Co-authored-by: caohuilin <caohuilin@bytedance.com>

---
+++ packages/runtime/plugin-runtime/src/core/server/stream/shared.ts
@@ -0,0 +1,96 @@
+import { time } from '@modern-js/runtime-utils/time';
+import { createElement } from 'react';
+import { RuntimeContext } from '../../context';
+import { HandleRequestConfig } from '../requestHandler';
+import { RenderStreaming } from '../shared';
+import {
+  SSRErrors,
+  SSRTimings,
+  createOnError,
+  createOnTiming,
+} from '../tracer';
+
+export type CreateReadableStreamFromElementOptions = {
+  runtimeContext: RuntimeContext;
+  config: HandleRequestConfig;
+  htmlTemplate: string;
+  onShellReady?: () => void;
+  onShellError?: (error: unknown) => void;
+  onAllReady?: () => void;
+  onError?: (error: unknown) => void;
+};
+
+export type CreateReadableStreamFromElement = (
+  request: Request,
+  rootElement: React.ReactElement,
+  options: CreateReadableStreamFromElementOptions,
+) => Promise<ReadableStream>;
+
+export enum ShellChunkStatus {
+  START = 0,
+  FINIESH = 1,
+}
+
+let encoder: TextEncoder;
+export function encodeForWebStream(thing: unknown) {
+  if (!encoder) {
+    encoder = new TextEncoder();
+  }
+  if (typeof thing === 'string') {
+    return encoder.encode(thing);
+  }
+  return thing;
+}
+
+export function getReadableStreamFromString(content: string): ReadableStream {
+  const readableStream = new ReadableStream({
+    start(controller) {
+      controller.enqueue(encodeForWebStream(content));
+      controller.close();
+    },
+  });
+
+  return readableStream;
+}
+
+export function createRenderStreaming(
+  createReadableStreamFromElement: CreateReadableStreamFromElement,
+): RenderStreaming {
+  return async (request, serverRoot, options) => {
+    const end = time();
+    const { runtimeContext, config, resource } = options;
+
+    const onError = createOnError(options.onError);
+    const onTiming = createOnTiming(options.onTiming);
+
+    const { htmlTemplate } = resource;
+
+    const rootElement = createElement(serverRoot, {
+      context: Object.assign(runtimeContext || {}, {
+        ssr: true,
+      }),
+    });
+
+    const stream = await createReadableStreamFromElement(request, rootElement, {
+      config,
+      htmlTemplate,
+      runtimeContext,
+      onShellReady() {
+        const cost = end();
+        onTiming(SSRTimings.RENDER_SHELL, cost);
+      },
+      onAllReady() {
+        const cost = end();
+        onTiming(SSRTimings.RENDER_HTML, cost);
+      },
+      onShellError(error) {
+        onError(SSRErrors.RENDER_SHELL, error);
+      },
+      onError(error) {
+        onError(SSRErrors.RENDER_STREAM, error);
+      },
+    });
+
+    return stream;
+  };
+}
---
+++ packages/runtime/plugin-runtime/src/core/server/string/index.ts
@@ -0,0 +1,154 @@
+import ReactHelmet from 'react-helmet';
+import ReactDomServer from 'react-dom/server';
+import React from 'react';
+import { StaticHandlerContext } from '@modern-js/runtime-utils/remix-router';
+import { time } from '@modern-js/runtime-utils/time';
+import { createReplaceHelemt } from '../helmet';
+import { safeReplace } from '../utils';
+import {
+  CHUNK_CSS_PLACEHOLDER,
+  CHUNK_JS_PLACEHOLDER,
+  HTML_PLACEHOLDER,
+  SSR_DATA_PLACEHOLDER,
+} from '../constants';
+import { BuildHtmlCb, RenderLevel, RenderString, buildHtml } from '../shared';
+import {
+  SSRErrors,
+  SSRTimings,
+  Tracer,
+  createOnError,
+  createOnTiming,
+} from '../tracer';
+import { SSRDataCollector } from './ssrData';
+import { LoadableCollector } from './loadable';
+import { Collector, ChunkSet } from './types';
+import { StyledCollector } from './styledComponent';
+import { prefetch } from './prefetch';
+
+export const renderString: RenderString = async (
+  request,
+  serverRoot,
+  options,
+) => {
+  const { resource, runtimeContext, config, onError, onTiming } = options;
+
+  const tracer: Tracer = {
+    onError: createOnError(onError),
+    onTiming: createOnTiming(onTiming),
+  };
+
+  const routerContext = runtimeContext.routerContext as StaticHandlerContext;
+
+  const { htmlTemplate, entryName } = resource;
+
+  const chunkSet: ChunkSet = {
+    renderLevel: RenderLevel.SERVER_RENDER,
+    ssrScripts: '',
+    jsChunk: '',
+    cssChunk: '',
+  };
+
+  let prefetchData = {};
+
+  try {
+    prefetchData = prefetch(serverRoot, runtimeContext, config, tracer);
+    chunkSet.renderLevel = RenderLevel.SERVER_PREFETCH;
+  } catch (e) {
+    chunkSet.renderLevel = RenderLevel.CLIENT_RENDER;
+  }
+
+  const collectors = [
+    new StyledCollector(chunkSet),
+    new LoadableCollector({
+      template: htmlTemplate,
+      entryName,
+      chunkSet,
+      config,
+    }),
+    new SSRDataCollector({
+      request,
+      prefetchData,
+      chunkSet,
+      routerContext,
+    }),
+  ];
+
+  const App = React.createElement(serverRoot, {
+    context: Object.assign(runtimeContext, { ssr: true }),
+  });
+
+  const html = await generateHtml(
+    App,
+    htmlTemplate,
+    chunkSet,
+    collectors,
+    tracer,
+  );
+
+  return html;
+};
+
+async function generateHtml(
+  App: React.ReactElement,
+  htmlTemplate: string,
+  chunkSet: ChunkSet,
+  collectors: Collector[],
+  { onError, onTiming }: Tracer,
+): Promise<string> {
+  const end = time();
+  let html = '';
+  let helmetData;
+
+  try {
+    const finalApp = collectors.reduce(
+      (pre, creator) => creator.collect?.(pre) || pre,
+      App,
+    );
+
+    // react render to string
+    html = ReactDomServer.renderToString(finalApp);
+
+    helmetData = ReactHelmet.renderStatic();
+    chunkSet.renderLevel = RenderLevel.SERVER_RENDER;
+
+    // collectors do effect
+    await Promise.all(collectors.map(component => component.effect()));
+
+    const cost = end();
+
+    onTiming(SSRTimings.RENDER_HTML, cost);
+  } catch (e) {
+    chunkSet.renderLevel = RenderLevel.CLIENT_RENDER;
+    onError(SSRErrors.RENDER_HTML, e);
+  }
+
+  const { ssrScripts, cssChunk, jsChunk } = chunkSet;
+
+  const finalHtml = await buildHtml(htmlTemplate, [
+    createReplaceHtml(html),
+    createReplaceChunkJs(jsChunk),
+    createReplaceChunkCss(cssChunk),
+    createReplaceSSRDataScript(ssrScripts),
+    createReplaceHelemt(helmetData),
+  ]);
+
+  return finalHtml;
+}
+
+function createReplaceHtml(html: string): BuildHtmlCb {
+  return (template: string) => safeReplace(template, HTML_PLACEHOLDER, html);
+}
+
+function createReplaceSSRDataScript(data: string): BuildHtmlCb {
+  return (template: string) =>
+    safeReplace(template, SSR_DATA_PLACEHOLDER, data);
+}
+
+function createReplaceChunkJs(js: string): BuildHtmlCb {
+  return (template: string) => safeReplace(template, CHUNK_JS_PLACEHOLDER, js);
+}
+
+function createReplaceChunkCss(css: string): BuildHtmlCb {
+  return (template: string) =>
+    safeReplace(template, CHUNK_CSS_PLACEHOLDER, css);
+}
