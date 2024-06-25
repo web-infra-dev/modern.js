6e51b73872a657d0f96cb6ee9bc619273c9d0145
GiveMe-A-Name <a455300764b@hotmail.com>
2024-06-25 14:28:52 +0800

    fix: we should add cli config correctly

--- packages/server/prod-server/src/netlify.ts
+++ packages/server/prod-server/src/netlify.ts
@@ -1,4 +1,7 @@
-import { loadServerEnv } from '@modern-js/server-core/node';
+import {
+  loadServerCliConfig,
+  loadServerEnv,
+} from '@modern-js/server-core/node';
 import { createServerBase } from '@modern-js/server-core';
 import { BaseEnv, ProdServerOptions } from './types';
 import { applyPlugins } from './apply';
@@ -6,7 +9,14 @@ import { applyPlugins } from './apply';
 export type { ProdServerOptions, BaseEnv } from './types';
 
 export const createNetlifyFunction = async (options: ProdServerOptions) => {
+  const serverCliConfig = loadServerCliConfig(options.pwd, options.config);
+
+  if (serverCliConfig) {
+    options.config = serverCliConfig;
+  }
+
   const server = createServerBase<BaseEnv>(options);
+
   await loadServerEnv(options);
   await applyPlugins(server, options);
   await server.init();
