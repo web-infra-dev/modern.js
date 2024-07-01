85f72ffe3d24916d242c938d24413874cb931aa0
chenwenjie <chenwenjie.asuka@bytedance.com>
2024-07-01 18:52:42 +0800

    feat: add runtime entry even if prod

--- packages/devtools/plugin/src/plugins/html.ts
+++ packages/devtools/plugin/src/plugins/html.ts
@@ -1,6 +1,5 @@
 import type { AppTools } from '@modern-js/app-tools';
 import type { UserConfig } from '@modern-js/core';
-import { SetupClientParams } from '@modern-js/devtools-kit';
 import { Plugin } from '../types';
 
 export const pluginHtml: Plugin = {
@@ -8,23 +7,17 @@ export const pluginHtml: Plugin = {
   async setup(api) {
     api.frameworkHooks.hook('config', async () => {
       const port = api.vars.http?.port;
-      if (!port) return {};
-      // Inject options to client.
-      const clientOptions: SetupClientParams = {
-        def: api.context.def,
-        src: `http://localhost:${port}/static/html/client/index.html`,
-      };
-      // Keep resource query always existing.
-      Object.assign(clientOptions, { __keep: true });
-
       const config: UserConfig<AppTools> = {
         source: {
           preEntry: [require.resolve('../runtime')],
-          globalVars: {
-            'process.env.__USE_MODERNJS_DEVTOOLS__': `http://localhost:${port}/manifest`,
-          },
         },
       };
+      if (port) {
+        config.source ||= {};
+        config.source.globalVars = {
+          'process.env.__USE_MODERNJS_DEVTOOLS__': `http://localhost:${port}/manifest`,
+        };
+      }
       return config;
     });
   },
