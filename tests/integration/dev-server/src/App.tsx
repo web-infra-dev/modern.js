a2814e9f633c1b5e19af4f7bdf47885f6a4c636a
GiveMe-A-Name <a455300764b@hotmail.com>
2024-07-24 16:25:39 +0800

    fix: move history-api-fallback e2e test to interation test

--- tests/integration/dev-server/src/App.tsx
+++ tests/integration/dev-server/src/App.tsx
@@ -1,3 +1,27 @@
-export default () => {
-  return <div>Hello World</div>;
-};
+import { BrowserRouter, Link, Route, Routes, Outlet } from 'react-router-dom';
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div>
+            home
+            <div>
+              <Link to="/a">A</Link>
+            </div>
+            <div>
+              <Link to="/b">B</Link>
+            </div>
+            <Outlet />
+          </div>
+        }
+      />
+      <Route path="/a" element={<div>A</div>} />
+      <Route path="/b" element={<div>B</div>} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
--- tests/integration/dev-server/tests/index.test.ts
+++ tests/integration/dev-server/tests/index.test.ts
@@ -32,6 +32,17 @@ describe('dev', () => {
     expect(headers['x-plugin']).toBe('test-plugin');
   });
 
+  test('should provide history api fallback correctly', async () => {
+    await page.goto(`http://localhost:${appPort}`);
+    expect(await page.content()).toContain('<div>home<div>');
+
+    await page.goto(`http://localhost:${appPort}/a`);
+    expect(await page.content()).toContain('<div>A</div>');
+
+    await page.goto(`http://localhost:${appPort}/b`);
+    expect(await page.content()).toContain('<div>B</div>');
+  });
+
   afterAll(async () => {
     await killApp(app);
     await page.close();
