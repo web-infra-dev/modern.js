4b97402c359419b4e87e90426ed54244dd315bc4
caohuilin <caohuilin@bytedance.com>
2024-07-24 14:03:38 +0800

    fix: build type error

--- packages/runtime/plugin-router-v5/src/global.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@meta/runtime/context' {
-  export * from '@modern-js/runtime/context';
-}
-
-declare module '@meta/runtime' {
-  export * from '@modern-js/runtime';
-}
--- packages/runtime/plugin-router-v5/src/modern-app-env.d.ts
+++ packages/runtime/plugin-router-v5/src/modern-app-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="@modern-js/types" />
+
+declare module '@meta/runtime/context' {
+  export * from '@modern-js/runtime/context';
+}
+
+declare module '@meta/runtime' {
+  export * from '@modern-js/runtime';
+}
