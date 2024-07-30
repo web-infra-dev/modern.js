e151cd8a41a4ee43843a4123495a58409ae76a42
GiveMe-A-Name <a455300764b@hotmail.com>
2024-07-30 16:31:00 +0800

    fix: we should export `getLoaderCtx` from server-core

---
+++ .changeset/spicy-buses-rush.md
@@ -0,0 +1,6 @@
+---
+'@modern-js/server-core': patch
+---
+
+fix: we should export `getLoaderCtx` from server-core
+fix: 我们应该从 server-core 导出 `getLoaderCtx`
--- packages/server/core/src/plugins/index.ts
+++ packages/server/core/src/plugins/index.ts
@@ -6,6 +6,8 @@ export {
 } from './render';
 export { faviconPlugin } from './favicon';
 export { injectServerTiming, injectloggerPluigin } from './monitors';
+export { processedByPlugin } from './processedBy';
+export { logPlugin } from './log';
 export {
   createDefaultPlugins,
   type CreateDefaultPluginsOptions,
