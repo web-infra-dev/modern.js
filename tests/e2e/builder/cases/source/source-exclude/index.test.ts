c8f2205846726bd769bcc80934374ebe5f19fa75
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2024-08-08 11:45:24 +0800

    fix: e2e

--- tests/e2e/builder/cases/source/source-exclude/index.test.ts
+++ tests/e2e/builder/cases/source/source-exclude/index.test.ts
@@ -1,25 +1,20 @@
 import path from 'path';
-import { expect } from '@modern-js/e2e/playwright';
+import { expect, test } from '@modern-js/e2e/playwright';
 import { build } from '@scripts/shared';
-import { webpackOnlyTest } from '@scripts/helper';
 
-// TODO: needs builtin:swc-loader
-webpackOnlyTest(
-  'should not compile specified file when source.exclude',
-  async () => {
-    await expect(
-      build({
-        cwd: __dirname,
-        entry: { index: path.resolve(__dirname, './src/index.js') },
-        builderConfig: {
-          source: {
-            exclude: [path.resolve(__dirname, './src/test.js')],
-          },
-          security: {
-            checkSyntax: true,
-          },
+test('should not compile specified file when source.exclude', async () => {
+  await expect(
+    build({
+      cwd: __dirname,
+      entry: { index: path.resolve(__dirname, './src/index.js') },
+      builderConfig: {
+        source: {
+          exclude: [path.resolve(__dirname, './src/test.js')],
         },
-      }),
-    ).rejects.toThrowError('[Syntax Checker]');
-  },
-);
+        security: {
+          checkSyntax: true,
+        },
+      },
+    }),
+  ).rejects.toThrowError('incompatible syntax');
+});
--- tests/e2e/builder/cases/source/source-include/index.test.ts
+++ tests/e2e/builder/cases/source/source-include/index.test.ts
@@ -1,25 +1,20 @@
 import path from 'path';
 import { expect, test } from '@modern-js/e2e/playwright';
 import { build } from '@scripts/shared';
-import { webpackOnlyTest } from '@scripts/helper';
 
-// TODO: needs builtin:swc-loader
-webpackOnlyTest(
-  'should not compile file which outside of project by default',
-  async () => {
-    await expect(
-      build({
-        cwd: __dirname,
-        entry: { index: path.resolve(__dirname, './src/index.js') },
-        builderConfig: {
-          security: {
-            checkSyntax: true,
-          },
+test('should not compile file which outside of project by default', async () => {
+  await expect(
+    build({
+      cwd: __dirname,
+      entry: { index: path.resolve(__dirname, './src/index.js') },
+      builderConfig: {
+        security: {
+          checkSyntax: true,
         },
-      }),
-    ).rejects.toThrowError('[Syntax Checker]');
-  },
-);
+      },
+    }),
+  ).rejects.toThrowError('incompatible syntax');
+});
 
 test('should compile specified file when source.include', async () => {
   await expect(
