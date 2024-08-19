52cabe8a0d297041cac465aab430cf8d26b0cc19
caohuilin <caohuilin@bytedance.com>
2024-08-19 16:43:05 +0800

    fix: no useless constructor

--- tests/utils/puppeteer_environment.js
+++ tests/utils/puppeteer_environment.js
@@ -8,10 +8,6 @@ const NodeEnvironment = require('jest-environment-node').TestEnvironment;
 const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup');
 
 class PuppeteerEnvironment extends NodeEnvironment {
-  constructor(config) {
-    super(config);
-  }
-
   async setup() {
     await super.setup();
     // get the wsEndpoint
