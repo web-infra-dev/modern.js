0b581a4f9fb398e6d8a3ae2e87edc7565bb938b8
qixuan <58852732+GiveMe-A-Name@users.noreply.github.com>
2024-08-27 15:55:05 +0800

    fix: remove ssr.preload config (#6062)

--- packages/server/core/src/types/config/server.ts
+++ packages/server/core/src/types/config/server.ts
@@ -11,34 +11,11 @@ type Route =
     };
 export type Routes = Record<string, Route>;
 
-type PreloadLink = { url: string; as?: string; rel?: string };
-type PreloadInclude = Array<string | PreloadLink>;
-interface PreloadAttributes {
-  script?: Record<string, boolean | string>;
-  style?: Record<string, boolean | string>;
-  image?: Record<string, boolean | string>;
-  font?: Record<string, boolean | string>;
-}
-export type SSRPreload = {
-  /** Include external preload links to enhance the page's performance by preloading additional resources. */
-  include?: PreloadInclude;
-
-  /** Utilize string matching to exclude specific preload links. */
-  exclude?: RegExp | string;
-
-  /** Disable preload when the User-Agent is matched.  */
-  userAgentFilter?: RegExp | string;
-
-  /** Include additional attributes to the Header Link.  */
-  attributes?: PreloadAttributes;
-};
-
 export type SSR =
   | boolean
   | {
       forceCSR?: boolean;
       mode?: SSRMode;
-      preload?: boolean | SSRPreload;
       inlineScript?: boolean;
       disablePrerender?: boolean;
       unsafeHeaders?: string[];
