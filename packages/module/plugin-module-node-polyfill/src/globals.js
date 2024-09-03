77bd2a221072fdd485fa1b7e0ef753b06d235734
Timeless0911 <50201324+Timeless0911@users.noreply.github.com>
2024-09-03 13:00:57 +0800

    fix(module-tools): import order in global.js of plugin node polyfill (#6159)

--- packages/module/plugin-module-node-polyfill/src/globals.js
+++ packages/module/plugin-module-node-polyfill/src/globals.js
@@ -1,6 +1,6 @@
 import Buffer from 'buffer';
-import console from 'console-browserify';
 import processBrowser from 'process/browser';
+import console from 'console-browserify';
 
 const buffer = {
   Buffer,
