dc330a6c841470ea71f75646a871c71a35103a94
caohuilin <caohuilin@bytedance.com>
2024-09-09 10:56:17 +0800

    feat: remove eslint config package

--- packages/review/eslint-config/lite.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: [
-    '@modern-js-app/eslint-config/base',
-    '@modern-js-app/eslint-config/ts',
-    '@modern-js-app/eslint-config/prettier',
-    './eslintrc.node.js',
-  ],
-};
