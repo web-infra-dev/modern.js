f2b9cfc9b6dfd57c5e7b54492a2bd4a2a649d41f
zhangxiang <zhangxiang.01@bytedance.com>
2024-09-09 10:49:09 +0800

    docs: refactor builder server config doc

--- packages/document/main-doc/src/components/RsbuildLink/index.tsx
+++ packages/document/main-doc/src/components/RsbuildLink/index.tsx
@@ -3,7 +3,7 @@ import { useLang } from 'rspress/runtime';
 
 const RsbuildLInk = ({ configName }: { configName: string }) => {
   const lang = useLang();
-  const href = `https://rsbuild.dev/${lang === 'zh' ? 'zh' : ''}/config/${configName
+  const href = `https://rsbuild.dev/${lang === 'zh' ? 'zh/' : ''}config/${configName
     .split('.')
     .join('/')
     .replace(/([a-z])([A-Z])/g, '$1-$2')
