f983d7b7f37ca7af0d1cb0807094433345692a17
Ming <527990618@163.com>
2024-09-18 11:38:52 +0800

    fix: the problem of NoSSR with Suspense

--- packages/runtime/plugin-runtime/src/core/server/react/nossr/index.ts
+++ packages/runtime/plugin-runtime/src/core/server/react/nossr/index.ts
@@ -1,13 +1,13 @@
 import { type ReactElement, useEffect, useState } from 'react';
 
 export const NoSSR = (
-  props: React.PropsWithChildren<{ fallback?: ReactElement | string }>,
+  props?: React.PropsWithChildren<{ fallback?: ReactElement | string }>,
 ) => {
   const [isMounted, setMounted] = useState(false);
   useEffect(() => {
     setMounted(true);
   }, []);
 
-  const { children, fallback = null } = props;
+  const { children, fallback = null } = props || {};
   return isMounted ? children : fallback;
 };
