004d2cc6922d48cc807bc90396c5ff7c1b36a448
caohuilin <caohuilin@bytedance.com>
2024-09-30 16:42:13 +0800

    feat: update codesmith version

--- packages/generator/plugins/generator-plugin/modern.config.js
+++ packages/generator/plugins/generator-plugin/modern.config.js
@@ -1,16 +1,5 @@
+const { generatorBuildConfig } = require('@scripts/build');
+
 module.exports = {
-  buildConfig: {
-    target: 'es2019',
-    autoExternal: false,
-    alias: {
-      chalk: '@modern-js/utils/chalk',
-    },
-    dts: false,
-    externals: [
-      'bluebird',
-      '@sigstore/core',
-      '@sigstore/verify',
-      'node-gyp/bin/node-gyp.js',
-    ],
-  },
+  buildConfig: generatorBuildConfig,
 };
