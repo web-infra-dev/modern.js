d7e932a0760a326c2ff0aae6aa5b03c87eadcc74
Soon <sunyiteng@bytedance.com>
2024-10-12 12:00:29 +0800

    fix(builder): `output.sourceMap` not work (#6342)

---
+++ .changeset/spotty-mirrors-knock.md
@@ -0,0 +1,7 @@
+---
+'@modern-js/uni-builder': patch
+---
+
+fix: `output.sourcemap` not work
+
+fix: `output.sourcemap` 不生效
--- packages/cli/uni-builder/src/shared/plugins/devtools.ts
+++ packages/cli/uni-builder/src/shared/plugins/devtools.ts
@@ -1,29 +1,46 @@
-import type { RsbuildPlugin } from '@rsbuild/core';
+import { logger } from '@modern-js/utils';
+import type { RsbuildPlugin, SourceMap } from '@rsbuild/core';
 import type { DisableSourceMapOption } from '../../types';
 
 const isUseJsSourceMap = (disableSourceMap: DisableSourceMapOption = {}) => {
-  return typeof disableSourceMap === 'boolean'
-    ? !disableSourceMap
-    : !disableSourceMap.js;
+  if (typeof disableSourceMap === 'boolean') {
+    return !disableSourceMap;
+  }
+  return !disableSourceMap.js;
 };
 
 export const pluginDevtool = (options: {
   disableSourceMap?: DisableSourceMapOption;
+  sourceMap?: SourceMap;
 }): RsbuildPlugin => ({
   name: 'uni-builder:devtool',
 
   setup(api) {
+    // priority order
+    // 1. output.sourceMap.js, if this value is set, we won't apply this plugin and let rsbuild handles it
+    const devtoolJs = options.sourceMap?.js;
+    if (devtoolJs) {
+      if (!isUseJsSourceMap(options.disableSourceMap)) {
+        logger.warn(
+          'Detected that `output.sourceMap` and `output.disableSourceMap` are used together, use the value of `output.sourceMap`',
+        );
+      }
+      return;
+    }
     api.modifyBundlerChain((chain, { isProd, isServer }) => {
+      // 2. output.disableSourceMap
       if (!isUseJsSourceMap(options.disableSourceMap)) {
         chain.devtool(false);
-      } else {
-        const prodDevTool = isServer ? 'source-map' : 'hidden-source-map';
-        const devtool = isProd
-          ? // hide the source map URL in production to avoid Chrome warning
-            prodDevTool
-          : 'cheap-module-source-map';
-        chain.devtool(devtool);
+        return;
       }
+
+      // 3. default behavior
+      const prodDevTool = isServer ? 'source-map' : 'hidden-source-map';
+      const devtool = isProd
+        ? // hide the source map URL in production to avoid Chrome warning
+          prodDevTool
+        : 'cheap-module-source-map';
+      chain.devtool(devtool);
     });
   },
 });
