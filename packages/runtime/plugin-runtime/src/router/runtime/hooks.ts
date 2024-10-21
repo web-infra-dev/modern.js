e7bd1adfca796d46980f94f78fa012565ea3b3fa
caohuilin <caohuilin@bytedance.com>
2024-10-21 16:40:30 +0800

    feat: router plugin add beforeCreateRoutes hook

--- packages/runtime/plugin-runtime/src/router/runtime/hooks.ts
+++ packages/runtime/plugin-runtime/src/router/runtime/hooks.ts
@@ -1,6 +1,11 @@
-import { createWaterfall } from '@modern-js/plugin';
+import {
+  createAsyncInterruptWorkflow,
+  createWaterfall,
+} from '@modern-js/plugin';
 import type { RouteObject } from '@modern-js/runtime-utils/router';
+import type { RuntimeContext } from '../../core';
 
 const modifyRoutes = createWaterfall<RouteObject[]>();
+const beforeCreateRoutes = createAsyncInterruptWorkflow<RuntimeContext, void>();
 
-export { modifyRoutes };
+export { modifyRoutes, beforeCreateRoutes };
