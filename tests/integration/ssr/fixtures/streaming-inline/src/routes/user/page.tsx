3aa44d8816601d74ed6ae439a1320eba5cf92e2c
targeral <targeral@outlook.com>
2024-10-23 11:10:25 +0800

    test: init tests about stream ssr with inlineScript

---
+++ tests/integration/ssr/fixtures/streaming-inline/src/routes/user/page.tsx
@@ -0,0 +1,18 @@
+import { Outlet, useNavigate } from '@modern-js/runtime/router';
+
+export default function Page() {
+  const nav = useNavigate();
+  return (
+    <div>
+      User page
+      <button
+        onClick={() => {
+          nav('/user/1');
+        }}
+      >
+        go user/1
+      </button>
+      <Outlet />
+    </div>
+  );
+}
