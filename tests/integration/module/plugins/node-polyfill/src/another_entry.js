5c5fd7b4757158d5c514c7a5b91d224adfc7ffa1
yutao <yutao.tao@bytedance.com>
2024-10-24 11:33:28 +0800

    fix: add more import situation

--- tests/integration/module/plugins/node-polyfill/src/another_entry.js
+++ tests/integration/module/plugins/node-polyfill/src/another_entry.js
@@ -1,6 +1,8 @@
-import fs from 'fs';
-import os from 'node:os';
-import path from 'path';
+import fs from 'node:fs';
+import path from 'node:path';
+import os from 'os';
 
+console.log(fs);
+console.log(path);
+console.log(os);
 export const value = Buffer.from('value');
-console.log(typeof fs);
