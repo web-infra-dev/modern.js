895328cc651475451e06494f5e53779c06319daf
caohuilin <caohuilin@bytedance.com>
2024-12-03 11:40:56 +0800

    feat: migrate server build plugin

--- packages/solutions/app-tools/src/plugins/serverBuild.ts
+++ packages/solutions/app-tools/src/plugins/serverBuild.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { compile } from '@modern-js/server-utils';
 import { SERVER_DIR, SHARED_DIR, getMeta } from '@modern-js/utils';
-import type { AppTools, CliPlugin } from '../types';
+import type { AppTools, CliPluginFuture } from '../types';
 
 const TS_CONFIG_FILENAME = 'tsconfig.json';
 
@@ -22,56 +22,54 @@ function checkHasConfig(appDir: string, metaName = 'modern-js') {
   return fs.existsSync(tsFilepath) || fs.existsSync(jsfilepath);
 }
 
-export default (): CliPlugin<AppTools> => ({
+export default (): CliPluginFuture<AppTools> => ({
   name: '@modern-js/server-build',
 
   setup(api) {
-    return {
-      async afterBuild() {
-        const { appDirectory, distDirectory, metaName } = api.useAppContext();
-        if (
-          !checkHasCache(appDirectory) &&
-          !checkHasConfig(appDirectory, metaName)
-        ) {
-          return;
+    api.onAfterBuild(async () => {
+      const { appDirectory, distDirectory, metaName } = api.getAppContext();
+      if (
+        !checkHasCache(appDirectory) &&
+        !checkHasConfig(appDirectory, metaName)
+      ) {
+        return;
+      }
+      const modernConfig = api.getNormalizedConfig();
+
+      const distDir = path.resolve(distDirectory!);
+      const serverDir = path.resolve(appDirectory, SERVER_DIR);
+      const sharedDir = path.resolve(appDirectory, SHARED_DIR);
+      const tsconfigPath = path.resolve(appDirectory, TS_CONFIG_FILENAME);
+
+      const sourceDirs = [];
+      if (fs.existsSync(serverDir)) {
+        sourceDirs.push(serverDir);
+
+        // compile the sharedDir only when serverDir exists
+        if (fs.existsSync(sharedDir)) {
+          sourceDirs.push(sharedDir);
         }
-        const modernConfig = api.useResolvedConfigContext();
+      }
 
-        const distDir = path.resolve(distDirectory);
-        const serverDir = path.resolve(appDirectory, SERVER_DIR);
-        const sharedDir = path.resolve(appDirectory, SHARED_DIR);
-        const tsconfigPath = path.resolve(appDirectory, TS_CONFIG_FILENAME);
+      const { server } = modernConfig;
+      const { alias } = modernConfig.source;
+      const { babel } = modernConfig.tools;
 
-        const sourceDirs = [];
-        if (fs.existsSync(serverDir)) {
-          sourceDirs.push(serverDir);
-
-          // compile the sharedDir only when serverDir exists
-          if (fs.existsSync(sharedDir)) {
-            sourceDirs.push(sharedDir);
-          }
-        }
-
-        const { server } = modernConfig;
-        const { alias } = modernConfig.source;
-        const { babel } = modernConfig.tools;
-
-        if (sourceDirs.length > 0) {
-          await compile(
-            appDirectory,
-            {
-              server,
-              alias,
-              babelConfig: babel,
-            },
-            {
-              sourceDirs,
-              distDir,
-              tsconfigPath,
-            },
-          );
-        }
-      },
-    };
+      if (sourceDirs.length > 0) {
+        await compile(
+          appDirectory,
+          {
+            server,
+            alias,
+            babelConfig: babel,
+          },
+          {
+            sourceDirs,
+            distDir,
+            tsconfigPath,
+          },
+        );
+      }
+    });
   },
 });
