a5fda006e1f6d31a2d6deac3ada97d067bab7320
caohuilin <caohuilin@bytedance.com>
2025-01-08 22:16:33 +0800

    fix: router v5 unit test

--- packages/runtime/plugin-router-v5/src/runtime/hooks.ts
+++ packages/runtime/plugin-router-v5/src/runtime/hooks.ts
@@ -1,8 +1,9 @@
 import { createSyncHook } from '@modern-js/plugin-v2';
 import type { RouteProps } from 'react-router-dom';
+import type { SingleRouteConfig } from './plugin';
 
 // only for inhouse use
 const modifyRoutesHook =
-  createSyncHook<(routes: RouteProps[]) => RouteProps[]>();
+  createSyncHook<(routes: RouteProps[]) => SingleRouteConfig[]>();
 
 export { modifyRoutesHook };
