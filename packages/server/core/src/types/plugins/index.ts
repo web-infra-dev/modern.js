47e46149ed187f0a2c6946bcc8c629e940d12dfe
caohuilin <caohuilin@bytedance.com>
2025-02-18 17:13:54 +0800

    feat: refactor server plugin type

---
+++ packages/server/core/src/types/plugins/index.ts
@@ -0,0 +1,13 @@
+export * from './new';
+export * from './old';
+export type {
+  ServerConfig,
+  CacheConfig,
+  OnFallback,
+  FallbackReason,
+  GetRenderHandlerOptions,
+  NodeServer,
+  NodeRequest,
+  NodeResponse,
+  FileChangeEvent,
+} from './base';
