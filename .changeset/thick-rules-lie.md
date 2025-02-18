b854d7bf711bfbccb07cba7c7f5792a133b354e2
zhangxiang <35088742+keepview@users.noreply.github.com>
2025-02-18 17:50:47 +0800

    fix: bff package path (#6843)

---
+++ .changeset/thick-rules-lie.md
@@ -0,0 +1,6 @@
+---
+'@modern-js/plugin-bff': patch
+---
+
+fix(bff): fixed path issue of generating package.json attribute
+fix(bff): 修复生成 package.json 属性的路径问题
--- tests/integration/bff-corss-project/tests/index.test.ts
+++ tests/integration/bff-corss-project/tests/index.test.ts
@@ -32,283 +32,284 @@ const testApiWorked = async ({
   const text = await res.text();
   expect(text).toBe(JSON.stringify({ message: expectedText }));
 };
+describe('corss project bff', () => {
+  describe('bff client-app in dev', () => {
+    const expectedText = 'Hello get bff-api-app';
+    const port = 3401;
+    const apiPort = 3399;
+    const SSR_PAGE = 'ssr';
+    const BASE_PAGE = 'base';
+    const CUSTOM_PAGE = 'custom-sdk';
+    const UPLOAD_PAGE = 'upload';
+    const host = `http://localhost`;
+    const prefix = '/api-app';
+    let app: any;
+    let apiApp: any;
+    let page: Page;
+    let browser: Browser;
 
-describe('bff client-app in dev', () => {
-  const expectedText = 'Hello get bff-api-app';
-  const port = 3401;
-  const apiPort = 3399;
-  const SSR_PAGE = 'ssr';
-  const BASE_PAGE = 'base';
-  const CUSTOM_PAGE = 'custom-sdk';
-  const UPLOAD_PAGE = 'upload';
-  const host = `http://localhost`;
-  const prefix = '/api-app';
-  let app: any;
-  let apiApp: any;
-  let page: Page;
-  let browser: Browser;
+    beforeAll(async () => {
+      jest.setTimeout(1000 * 60 * 2);
+      apiApp = await launchApp(apiAppDir, apiPort, {});
 
-  beforeAll(async () => {
-    jest.setTimeout(1000 * 60 * 2);
-    apiApp = await launchApp(apiAppDir, apiPort, {});
+      jest.setTimeout(1000 * 60 * 2);
+      app = await launchApp(appDir, port, {});
+      browser = await puppeteer.launch(launchOptions as any);
+      page = await browser.newPage();
+    });
 
-    jest.setTimeout(1000 * 60 * 2);
-    app = await launchApp(appDir, port, {});
-    browser = await puppeteer.launch(launchOptions as any);
-    page = await browser.newPage();
-  });
+    test('api-app should works', async () => {
+      await testApiWorked({
+        host,
+        port: apiPort,
+        prefix,
+      });
+    });
 
-  test('api-app should works', async () => {
-    await testApiWorked({
-      host,
-      port: apiPort,
-      prefix,
+    test('basic usage', async () => {
+      await page.goto(`${host}:${port}/${BASE_PAGE}`, {
+        timeout: 50000,
+      });
+      await new Promise(resolve => setTimeout(resolve, 3000));
+      const text = await page.$eval('.hello', el => el?.textContent);
+      expect(text).toBe(expectedText);
+    });
+
+    test('basic usage with ssr', async () => {
+      await page.goto(`${host}:${port}/${SSR_PAGE}`);
+      await new Promise(resolve => setTimeout(resolve, 3000));
+      const text1 = await page.$eval('.hello', el => el?.textContent);
+      expect(text1).toBe(expectedText);
+    });
+
+    test('support useContext', async () => {
+      const res = await fetch(`${host}:${port}${prefix}/context`);
+      const info = await res.json();
+      expect(res.headers.get('x-id')).toBe('1');
+      expect(info.message).toBe('Hello Modern.js');
+    });
+
+    test('support custom sdk', async () => {
+      await page.goto(`${host}:${port}/${CUSTOM_PAGE}`);
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      const text = await page.$eval('.hello', el => el?.textContent);
+      expect(text).toBe('Hello Custom SDK');
+    });
+
+    test('support uoload', async () => {
+      await page.goto(`${host}:${port}/${UPLOAD_PAGE}`);
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      const text = await page.$eval('.mock_file', el => el?.textContent);
+      expect(text).toBe('mock_image.png');
+    });
+
+    afterAll(async () => {
+      await killApp(app);
+      await killApp(apiApp);
+      await page.close();
+      await browser.close();
     });
   });
 
-  test('basic usage', async () => {
-    await page.goto(`${host}:${port}/${BASE_PAGE}`, {
-      timeout: 50000,
+  describe('bff client-app in prod', () => {
+    const expectedText = 'Hello get bff-api-app';
+    const port = 3401;
+    const apiPort = 3399;
+    const SSR_PAGE = 'ssr';
+    const BASE_PAGE = 'base';
+    const CUSTOM_PAGE = 'custom-sdk';
+    const UPLOAD_PAGE = 'upload';
+    const host = `http://localhost`;
+    const prefix = '/api-app';
+    let app: any;
+    let apiApp: any;
+    let page: Page;
+    let browser: Browser;
+
+    beforeAll(async () => {
+      await modernBuild(apiAppDir, [], {});
+      apiApp = await modernServe(apiAppDir, apiPort, {});
+
+      await modernBuild(appDir, [], {});
+      app = await modernServe(appDir, port, {});
+
+      browser = await puppeteer.launch(launchOptions as any);
+      page = await browser.newPage();
     });
-    await new Promise(resolve => setTimeout(resolve, 3000));
-    const text = await page.$eval('.hello', el => el?.textContent);
-    expect(text).toBe(expectedText);
-  });
 
-  test('basic usage with ssr', async () => {
-    await page.goto(`${host}:${port}/${SSR_PAGE}`);
-    await new Promise(resolve => setTimeout(resolve, 3000));
-    const text1 = await page.$eval('.hello', el => el?.textContent);
-    expect(text1).toBe(expectedText);
-  });
+    test('api-app should works', async () => {
+      await testApiWorked({
+        host,
+        port: apiPort,
+        prefix,
+      });
+    });
 
-  test('support useContext', async () => {
-    const res = await fetch(`${host}:${port}${prefix}/context`);
-    const info = await res.json();
-    expect(res.headers.get('x-id')).toBe('1');
-    expect(info.message).toBe('Hello Modern.js');
-  });
+    test('basic usage', async () => {
+      await page.goto(`${host}:${port}/${BASE_PAGE}`, {
+        timeout: 50000,
+      });
+      await new Promise(resolve => setTimeout(resolve, 3000));
+      const text = await page.$eval('.hello', el => el?.textContent);
+      expect(text).toBe(expectedText);
+    });
 
-  test('support custom sdk', async () => {
-    await page.goto(`${host}:${port}/${CUSTOM_PAGE}`);
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    const text = await page.$eval('.hello', el => el?.textContent);
-    expect(text).toBe('Hello Custom SDK');
-  });
+    test('basic usage with ssr', async () => {
+      await page.goto(`${host}:${port}/${SSR_PAGE}`);
+      const text1 = await page.$eval('.hello', el => el?.textContent);
+      expect(text1).toBe(expectedText);
+    });
 
-  test('support uoload', async () => {
-    await page.goto(`${host}:${port}/${UPLOAD_PAGE}`);
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    const text = await page.$eval('.mock_file', el => el?.textContent);
-    expect(text).toBe('mock_image.png');
-  });
+    test('support useContext', async () => {
+      const res = await fetch(`${host}:${port}${prefix}/context`);
+      const info = await res.json();
+      expect(res.headers.get('x-id')).toBe('1');
+      expect(info.message).toBe('Hello Modern.js');
+    });
 
-  afterAll(async () => {
-    await killApp(app);
-    await killApp(apiApp);
-    await page.close();
-    await browser.close();
-  });
-});
+    test('support custom sdk', async () => {
+      await page.goto(`${host}:${port}/${CUSTOM_PAGE}`);
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      const text = await page.$eval('.hello', el => el?.textContent);
+      expect(text).toBe('Hello Custom SDK');
+    });
 
-describe('bff client-app in prod', () => {
-  const expectedText = 'Hello get bff-api-app';
-  const port = 3401;
-  const apiPort = 3399;
-  const SSR_PAGE = 'ssr';
-  const BASE_PAGE = 'base';
-  const CUSTOM_PAGE = 'custom-sdk';
-  const UPLOAD_PAGE = 'upload';
-  const host = `http://localhost`;
-  const prefix = '/api-app';
-  let app: any;
-  let apiApp: any;
-  let page: Page;
-  let browser: Browser;
+    test('support uoload', async () => {
+      await page.goto(`${host}:${port}/${UPLOAD_PAGE}`);
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      const text = await page.$eval('.mock_file', el => el?.textContent);
+      expect(text).toBe('mock_image.png');
+    });
 
-  beforeAll(async () => {
-    await modernBuild(apiAppDir, [], {});
-    apiApp = await modernServe(apiAppDir, apiPort, {});
-
-    await modernBuild(appDir, [], {});
-    app = await modernServe(appDir, port, {});
-
-    browser = await puppeteer.launch(launchOptions as any);
-    page = await browser.newPage();
-  });
-
-  test('api-app should works', async () => {
-    await testApiWorked({
-      host,
-      port: apiPort,
-      prefix,
+    afterAll(async () => {
+      await killApp(app);
+      await killApp(apiApp);
+      await page.close();
+      await browser.close();
     });
   });
 
-  test('basic usage', async () => {
-    await page.goto(`${host}:${port}/${BASE_PAGE}`, {
-      timeout: 50000,
+  describe('bff indep-client-app in dev', () => {
+    const apiPort = 3399;
+    let port = 8080;
+    const SSR_PAGE = 'ssr';
+    const BASE_PAGE = 'base';
+    const CUSTOM_PAGE = 'custom-sdk';
+    const UPLOAD_PAGE = 'upload';
+    const host = `http://localhost`;
+    let indepClientApp: any;
+    let apiApp: any;
+    let page: Page;
+    let browser: Browser;
+
+    beforeAll(async () => {
+      jest.setTimeout(1000 * 60 * 2);
+      apiApp = await launchApp(apiAppDir, apiPort, {});
+
+      jest.setTimeout(1000 * 60 * 2);
+      port = await getPort();
+      indepClientApp = await launchApp(indepAppDir, port, {});
+      browser = await puppeteer.launch(launchOptions as any);
+      page = await browser.newPage();
     });
-    await new Promise(resolve => setTimeout(resolve, 3000));
-    const text = await page.$eval('.hello', el => el?.textContent);
-    expect(text).toBe(expectedText);
-  });
 
-  test('basic usage with ssr', async () => {
-    await page.goto(`${host}:${port}/${SSR_PAGE}`);
-    const text1 = await page.$eval('.hello', el => el?.textContent);
-    expect(text1).toBe(expectedText);
-  });
-
-  test('support useContext', async () => {
-    const res = await fetch(`${host}:${port}${prefix}/context`);
-    const info = await res.json();
-    expect(res.headers.get('x-id')).toBe('1');
-    expect(info.message).toBe('Hello Modern.js');
-  });
-
-  test('support custom sdk', async () => {
-    await page.goto(`${host}:${port}/${CUSTOM_PAGE}`);
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    const text = await page.$eval('.hello', el => el?.textContent);
-    expect(text).toBe('Hello Custom SDK');
-  });
-
-  test('support uoload', async () => {
-    await page.goto(`${host}:${port}/${UPLOAD_PAGE}`);
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    const text = await page.$eval('.mock_file', el => el?.textContent);
-    expect(text).toBe('mock_image.png');
-  });
-
-  afterAll(async () => {
-    await killApp(app);
-    await killApp(apiApp);
-    await page.close();
-    await browser.close();
-  });
-});
-
-describe('bff indep-client-app in dev', () => {
-  const apiPort = 3399;
-  let port = 8080;
-  const SSR_PAGE = 'ssr';
-  const BASE_PAGE = 'base';
-  const CUSTOM_PAGE = 'custom-sdk';
-  const UPLOAD_PAGE = 'upload';
-  const host = `http://localhost`;
-  let indepClientApp: any;
-  let apiApp: any;
-  let page: Page;
-  let browser: Browser;
-
-  beforeAll(async () => {
-    jest.setTimeout(1000 * 60 * 2);
-    apiApp = await launchApp(apiAppDir, apiPort, {});
-
-    jest.setTimeout(1000 * 60 * 2);
-    port = await getPort();
-    indepClientApp = await launchApp(indepAppDir, port, {});
-    browser = await puppeteer.launch(launchOptions as any);
-    page = await browser.newPage();
-  });
-
-  test('basic usage', async () => {
-    await page.goto(`${host}:${port}/${BASE_PAGE}`, {
-      timeout: 50000,
+    test('basic usage', async () => {
+      await page.goto(`${host}:${port}/${BASE_PAGE}`, {
+        timeout: 50000,
+      });
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      const text = await page.$eval('.hello', el => el?.textContent);
+      expect(text).toBe('hello：Hello get bff-api-app');
     });
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    const text = await page.$eval('.hello', el => el?.textContent);
-    expect(text).toBe('hello：Hello get bff-api-app');
-  });
 
-  test('basic usage with ssr', async () => {
-    await page.goto(`${host}:${port}/${SSR_PAGE}`);
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    const text1 = await page.$eval('.hello', el => el?.textContent);
-    expect(text1).toBe('node-fetch：Hello get bff-api-app');
-  });
-
-  test('support custom sdk', async () => {
-    await page.goto(`${host}:${port}/${CUSTOM_PAGE}`);
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    const text = await page.$eval('.hello', el => el?.textContent);
-    expect(text).toBe('interceptor return：Hello Custom SDK');
-  });
-
-  test('support uoload', async () => {
-    await page.goto(`${host}:${port}/${UPLOAD_PAGE}`);
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    const text = await page.$eval('.mock_file', el => el?.textContent);
-    expect(text).toBe('mock_image.png');
-  });
-
-  afterAll(async () => {
-    await killApp(indepClientApp);
-    await killApp(apiApp);
-    await page.close();
-    await browser.close();
-  });
-});
-
-describe('bff indep-client-app in prod', () => {
-  const apiPort = 3399;
-  let port = 8080;
-  const SSR_PAGE = 'ssr';
-  const BASE_PAGE = 'base';
-  const CUSTOM_PAGE = 'custom-sdk';
-  const UPLOAD_PAGE = 'upload';
-  const host = `http://localhost`;
-  let indepClientApp: any;
-  let apiApp: any;
-  let page: Page;
-  let browser: Browser;
-
-  beforeAll(async () => {
-    await modernBuild(apiAppDir, [], {});
-    apiApp = await modernServe(apiAppDir, apiPort, {});
-
-    port = await getPort();
-    await modernBuild(indepAppDir, [], {});
-    indepClientApp = await modernServe(indepAppDir, port, {});
-
-    browser = await puppeteer.launch(launchOptions as any);
-    page = await browser.newPage();
-  });
-
-  test('basic usage', async () => {
-    await page.goto(`${host}:${port}/${BASE_PAGE}`, {
-      timeout: 50000,
+    test('basic usage with ssr', async () => {
+      await page.goto(`${host}:${port}/${SSR_PAGE}`);
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      const text1 = await page.$eval('.hello', el => el?.textContent);
+      expect(text1).toBe('node-fetch：Hello get bff-api-app');
     });
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    const text = await page.$eval('.hello', el => el?.textContent);
-    expect(text).toBe('hello：Hello get bff-api-app');
+
+    test('support custom sdk', async () => {
+      await page.goto(`${host}:${port}/${CUSTOM_PAGE}`);
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      const text = await page.$eval('.hello', el => el?.textContent);
+      expect(text).toBe('interceptor return：Hello Custom SDK');
+    });
+
+    test('support uoload', async () => {
+      await page.goto(`${host}:${port}/${UPLOAD_PAGE}`);
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      const text = await page.$eval('.mock_file', el => el?.textContent);
+      expect(text).toBe('mock_image.png');
+    });
+
+    afterAll(async () => {
+      await killApp(indepClientApp);
+      await killApp(apiApp);
+      await page.close();
+      await browser.close();
+    });
   });
 
-  test('basic usage with ssr', async () => {
-    await page.goto(`${host}:${port}/${SSR_PAGE}`);
-    const text1 = await page.$eval('.hello', el => el?.textContent);
-    expect(text1).toBe('node-fetch：Hello get bff-api-app');
-  });
+  describe('bff indep-client-app in prod', () => {
+    const apiPort = 3399;
+    let port = 8080;
+    const SSR_PAGE = 'ssr';
+    const BASE_PAGE = 'base';
+    const CUSTOM_PAGE = 'custom-sdk';
+    const UPLOAD_PAGE = 'upload';
+    const host = `http://localhost`;
+    let indepClientApp: any;
+    let apiApp: any;
+    let page: Page;
+    let browser: Browser;
 
-  test('support custom sdk', async () => {
-    await page.goto(`${host}:${port}/${CUSTOM_PAGE}`);
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    const text = await page.$eval('.hello', el => el?.textContent);
-    expect(text).toBe('interceptor return：Hello Custom SDK');
-  });
+    beforeAll(async () => {
+      await modernBuild(apiAppDir, [], {});
+      apiApp = await modernServe(apiAppDir, apiPort, {});
 
-  test('support uoload', async () => {
-    await page.goto(`${host}:${port}/${UPLOAD_PAGE}`);
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    const text = await page.$eval('.mock_file', el => el?.textContent);
-    expect(text).toBe('mock_image.png');
-  });
+      port = await getPort();
+      await modernBuild(indepAppDir, [], {});
+      indepClientApp = await modernServe(indepAppDir, port, {});
 
-  afterAll(async () => {
-    await killApp(indepClientApp);
-    await killApp(apiApp);
-    await page.close();
-    await browser.close();
+      browser = await puppeteer.launch(launchOptions as any);
+      page = await browser.newPage();
+    });
+
+    test('basic usage', async () => {
+      await page.goto(`${host}:${port}/${BASE_PAGE}`, {
+        timeout: 50000,
+      });
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      const text = await page.$eval('.hello', el => el?.textContent);
+      expect(text).toBe('hello：Hello get bff-api-app');
+    });
+
+    test('basic usage with ssr', async () => {
+      await page.goto(`${host}:${port}/${SSR_PAGE}`);
+      const text1 = await page.$eval('.hello', el => el?.textContent);
+      expect(text1).toBe('node-fetch：Hello get bff-api-app');
+    });
+
+    test('support custom sdk', async () => {
+      await page.goto(`${host}:${port}/${CUSTOM_PAGE}`);
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      const text = await page.$eval('.hello', el => el?.textContent);
+      expect(text).toBe('interceptor return：Hello Custom SDK');
+    });
+
+    test('support uoload', async () => {
+      await page.goto(`${host}:${port}/${UPLOAD_PAGE}`);
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      const text = await page.$eval('.mock_file', el => el?.textContent);
+      expect(text).toBe('mock_image.png');
+    });
+
+    afterAll(async () => {
+      await killApp(indepClientApp);
+      await killApp(apiApp);
+      await page.close();
+      await browser.close();
+    });
   });
 });
