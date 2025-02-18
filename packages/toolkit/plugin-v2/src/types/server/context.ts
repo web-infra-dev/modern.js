8923efda97aa48c40c5e808ff4877932f8e474bf
caohuilin <caohuilin@bytedance.com>
2025-02-18 17:11:42 +0800

    feat: server plugin types define

---
+++ packages/toolkit/plugin-v2/src/types/server/context.ts
@@ -0,0 +1,30 @@
+import type { ISAppContext, ServerRoute } from '@modern-js/types';
+import type { ServerPluginAPI } from './api';
+import type { Hooks } from './hooks';
+import type { ServerPlugin, ServerPluginExtends } from './plugin';
+
+export type ServerContext<Extends extends ServerPluginExtends> = Omit<
+  ISAppContext,
+  'plugins'
+> & {
+  routes: ServerRoute[];
+  appDirectory: string;
+  apiDirectory?: string;
+  lambdaDirectory?: string;
+  internalDirectory?: string;
+  sharedDirectory?: string;
+  distDirectory?: string;
+  metaName: string;
+  plugins: ServerPlugin<Extends>[];
+};
+
+export type InternalServerContext<Extends extends ServerPluginExtends> =
+  ServerContext<Extends> & {
+    /** All hooks. */
+    hooks: Hooks<Extends['config']> & Extends['extendHooks'];
+    /** All plugin registry hooks */
+    extendsHooks: Extends['extendHooks'];
+    config: Extends['config'];
+    pluginAPI?: ServerPluginAPI<Extends>;
+    _internalContext?: InternalServerContext<Extends>;
+  };
