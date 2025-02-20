47cbe1673686fec5b4d1298431bb76adcf52f574
caohuilin <caohuilin@bytedance.com>
2025-02-20 10:24:31 +0800

    feat: handleSetupResult

--- packages/server/core/src/plugins/compat/hooks.ts
+++ packages/server/core/src/plugins/compat/hooks.ts
@@ -50,3 +50,36 @@ export function getHookRunners(
     },
   };
 }
+
+export function transformHookRunner(hookRunnerName: string) {
+  switch (hookRunnerName) {
+    case 'config':
+      return 'modifyConfig';
+    case 'prepare':
+      return 'onPrepare';
+    case 'reset':
+      return 'onReset';
+    default:
+      return hookRunnerName;
+  }
+}
+export function handleSetupResult(
+  setupResult: Record<string, (...args: any) => any>,
+  api: Record<string, any>,
+) {
+  if (!setupResult) {
+    return;
+  }
+  Object.keys(setupResult).forEach(key => {
+    const fn = setupResult[key];
+    if (typeof fn === 'function') {
+      const newAPI = transformHookRunner(key);
+      if (api[newAPI]) {
+        api[newAPI]((...params: any) => {
+          const res = fn(...params);
+          return res;
+        });
+      }
+    }
+  });
+}
