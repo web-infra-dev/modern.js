1f83d96f7b8bd50e4afd2b4ab6d9a1bd2b4cc7f9
Ming <wangyiming.fe@bytedance.com>
2025-02-24 14:26:40 +0800

    fix(render): fork react-server-dom-webpack to avoid warnings about installing dependencies (#6880)

--- packages/runtime/render/src/server/ssr/ssr.tsx
+++ packages/runtime/render/src/server/ssr/ssr.tsx
@@ -42,7 +42,7 @@ export const renderSSRStream = async (
     const [{ renderRsc }, { createFromReadableStream }, { injectRSCPayload }] =
       await Promise.all([
         import('../rsc'),
-        import('react-server-dom-webpack/client.edge'),
+        import('@modern-js/utils/react-server-dom-webpack/client.edge'),
         import('../../rsc-html-stream/server'),
       ]);
 
