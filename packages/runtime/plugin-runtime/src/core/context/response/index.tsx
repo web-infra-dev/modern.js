4e5ab6524081ae4afad527e1159a0179b943f0a2
Ming <527990618@163.com>
2025-03-03 18:08:50 +0800

    feat: client redirect

--- packages/runtime/plugin-runtime/src/core/context/response/index.tsx
+++ packages/runtime/plugin-runtime/src/core/context/response/index.tsx
@@ -3,4 +3,8 @@ export const getResponseProxy = () => {
 };
 export const setHeaders = (headers: Record<string, string>) => {};
 export const setStatus = (status: number) => {};
-export const redirect = (url: string, init?: number | ResponseInit) => {};
+export const redirect = (url: string, init?: number | ResponseInit) => {
+  console.warn(
+    `You should not use this API in the browser, please use the router's redirect or useNavigate method.`,
+  );
+};
