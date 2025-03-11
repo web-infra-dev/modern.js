dd20cfcb6aa28138243582f7eb7a64fd88067b15
chenwenjie <chenwenjie.asuka@bytedance.com>
2025-03-11 19:11:31 +0800

    feat: update rslib config

--- packages/runtime/plugin-image/rslib.config.ts
+++ packages/runtime/plugin-image/rslib.config.ts
@@ -2,16 +2,11 @@ import { pluginReact } from '@rsbuild/plugin-react';
 import { defineConfig } from '@rslib/core';
 
 export default defineConfig({
-  source: {
-    entry: {
-      index: ['./src/**'],
-    },
-  },
   lib: [
     {
       id: 'runtime',
       source: {
-        entry: { runtime: ['./src/**', '!**/*.stories.*'] },
+        entry: { index: ['./src/**', '!**/*.stories.*'] },
         tsconfigPath: './tsconfig.runtime.json',
       },
       output: { target: 'web', distPath: { root: 'dist/runtime' } },
@@ -23,7 +18,7 @@ export default defineConfig({
     {
       id: 'plugin',
       source: {
-        entry: { plugin: ['./lib/**'] },
+        entry: { index: ['./lib/**'] },
         tsconfigPath: './tsconfig.plugin.json',
       },
       output: { target: 'node', distPath: { root: 'dist/plugin' } },
