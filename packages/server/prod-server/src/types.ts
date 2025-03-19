4ca5948efda548aa364603ee90517a5dd826e3a6
kongjiacong <kongjiacong@bytedance.com>
2025-03-19 20:34:45 +0800

    feat: resume old config file logic

--- packages/server/prod-server/src/types.ts
+++ packages/server/prod-server/src/types.ts
@@ -7,6 +7,9 @@ import type { Reporter } from '@modern-js/types';
 import type { Logger } from '@modern-js/utils';
 
 interface ProdServerExtraOptions {
+  /** compat modern.server-runtime.config.ts */
+  serverConfigFile?: string;
+
   serverConfigPath: string;
 
   plugins?: ServerPlugin[];
--- packages/server/server/src/types.ts
+++ packages/server/server/src/types.ts
@@ -23,6 +23,9 @@ export type ExtraOptions = {
    */
   compiler?: Rspack.Compiler | Rspack.MultiCompiler;
 
+  /** compat, the default value is modern.server-runtime.config.ts  */
+  serverConfigFile?: string;
+
   serverConfigPath: string;
 
   builder?: UniBuilderInstance;
