6ecd9867ba3ca3c6b9b086fa9001d4d942ee5fee
caohuilin <caohuilin@bytedance.com>
2025-03-19 16:58:07 +0800

    fix: state types

--- packages/runtime/plugin-runtime/types/index.d.ts
+++ packages/runtime/plugin-runtime/types/index.d.ts
@@ -1,17 +1,3 @@
-import type { StateConfig } from '../dist/types';
-
-declare module '@modern-js/app-tools' {
-  interface RuntimeUserConfig {
-    state?: StateConfig | boolean;
-  }
-}
-
-declare module '@modern-js/module-tools' {
-  interface RuntimeUserConfig {
-    state?: StateConfig | boolean;
-  }
-}
-
 declare module 'http' {
   interface ServerResponse {
     locals: Record<string, any>;
