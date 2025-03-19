fb6bf9e37ad4d19dbb3e8deba03c5cb304d501fb
zero <68810266+zllkjc@users.noreply.github.com>
2025-03-19 22:52:49 +0800

    fix: add new mid connet for mock handler (#6955)

--- packages/server/core/src/adapters/node/index.ts
+++ packages/server/core/src/adapters/node/index.ts
@@ -1,4 +1,8 @@
-export { httpCallBack2HonoMid, connectMid2HonoMid } from './hono';
+export {
+  httpCallBack2HonoMid,
+  connectMid2HonoMid,
+  connectMockMid2HonoMid,
+} from './hono';
 export type { ServerNodeContext, ServerNodeMiddleware } from './hono';
 
 export {
