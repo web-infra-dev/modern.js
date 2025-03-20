3de8a3a3aa908b4e81cc7de7ae482b793a0d5578
caohuilin <caohuilin@bytedance.com>
2025-03-20 18:33:48 +0800

    fix: get runtime context

--- packages/runtime/plugin-state/src/runtime/plugin.tsx
+++ packages/runtime/plugin-state/src/runtime/plugin.tsx
@@ -5,8 +5,9 @@ import {
   createStore,
 } from '@modern-js-reduck/store';
 import type { Plugin } from '@modern-js/runtime';
-import { isBrowser, useRuntimeContext } from '@modern-js/runtime';
+import { RuntimeReactContext, isBrowser } from '@modern-js/runtime';
 import { merge } from '@modern-js/runtime-utils/merge';
+import { useContext } from 'react';
 import { autoActions, devtools, effects, immer } from '../plugins';
 
 type StatePluginType = 'immer' | 'effects' | 'autoActions' | 'devtools';
@@ -60,7 +61,7 @@ export const statePlugin = (userConfig: StateConfig = {}): Plugin => ({
     return {
       wrapRoot(App) {
         const getStateApp = (props: any) => {
-          const context = useRuntimeContext();
+          const context = useContext(RuntimeReactContext);
 
           return (
             <Provider store={context.store} config={storeConfig}>
