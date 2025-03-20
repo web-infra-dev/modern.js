da74692b3da32ee16b25ebc2fcc3850641e16936
caohuilin <caohuilin@bytedance.com>
2025-03-20 11:50:21 +0800

    feat: add state alias and types

--- packages/runtime/plugin-state/types/index.d.ts
+++ packages/runtime/plugin-state/types/index.d.ts
@@ -1,4 +1,15 @@
 import type { StateConfig } from '../dist/types';
+import '@modern-js-reduck/plugin-auto-actions';
+import '@modern-js-reduck/plugin-devtools';
+import '@modern-js-reduck/plugin-effects';
+import '@modern-js-reduck/plugin-immutable';
+
+export { default } from '../dist/types/runtime';
+export * from '../dist/types/runtime';
+
+declare module '@modern-js/runtime/model' {
+  export * from '../dist/types/runtime';
+}
 
 declare module '@modern-js/runtime' {
   interface AppConfig {
