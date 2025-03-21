4cbc71004b5f1d7810bcade914ff5c52a426e9d6
kongjiacong <kongjiacong@bytedance.com>
2025-03-21 11:28:03 +0800

    feat: support dev.assetPrefix for static middleware
    docs: add changeset

--- packages/server/server/src/helpers/devOptions.ts
+++ packages/server/server/src/helpers/devOptions.ts
@@ -1,3 +1,4 @@
+import type { UniBuilderInstance } from '@modern-js/uni-builder';
 import { merge } from '@modern-js/utils/lodash';
 import type { ModernDevServerOptions } from '../types';
 import { getDefaultDevOptions } from './constants';
@@ -6,4 +7,28 @@ export const getDevOptions = (options: ModernDevServerOptions) => {
   const devOptions = options.dev;
   const defaultOptions = getDefaultDevOptions();
   return merge(defaultOptions, devOptions);
+};
+
+export const getDevAssetPrefix = (builder?: UniBuilderInstance) => {
+  return new Promise<string>(resolve => {
+    builder?.onAfterCreateCompiler(params => {
+      let webCompiler: typeof params.compiler;
+      if ('compilers' in params.compiler) {
+        webCompiler = params.compiler.compilers.find(c => {
+          return c.name === 'web';
+        })!;
+      } else {
+        webCompiler = params.compiler;
+      }
+
+      const publicPath = webCompiler.options.output.publicPath;
+      if (publicPath && typeof publicPath === 'string') {
+        // remove host and port
+        const formatPublicPath = publicPath.replace(/^https?:\/\/[^/]+/, '');
+        resolve(formatPublicPath);
+      }
+
+      resolve('');
+    });
+  });
 };
