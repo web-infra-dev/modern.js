7522bf0ef0eda744db16271945bb1eb00afbf36a
Ming <wangyiming.fe@bytedance.com>
2025-04-07 20:59:48 +0800

    test: add --no-experimental-require-module for node20 test cases (#7018)

--- packages/toolkit/utils/src/cli/version.ts
+++ packages/toolkit/utils/src/cli/version.ts
@@ -15,3 +15,9 @@ export function isVersionAtLeast22(): boolean {
   const versionArr = nodeVersion.split('.').map(Number);
   return versionArr[0] >= 22;
 }
+
+export function isVersionAtLeast20(): boolean {
+  const nodeVersion = process.versions.node;
+  const versionArr = nodeVersion.split('.').map(Number);
+  return versionArr[0] >= 20;
+}
