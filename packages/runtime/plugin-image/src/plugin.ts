f79530fbf9ca91d273111e2b4e42d3b831574014
chenwenjie <chenwenjie.asuka@bytedance.com>
2025-04-10 01:01:53 +0800

    feat: implement ipx middleware

--- packages/runtime/plugin-image/src/plugin.ts
+++ packages/runtime/plugin-image/src/plugin.ts
@@ -1,14 +1,43 @@
-import type { RsbuildPlugin } from '@rsbuild/core';
+import type { RequestHandler, RsbuildPlugin } from '@rsbuild/core';
+import type { IPX, IPXOptions, createIPXNodeServer } from 'ipx';
+import { logger } from './logger';
+import { invariant, isModuleNotFoundError } from './shared/utils';
 import type { ImageSerializableContext } from './types/image';
 
-export const pluginImage = (
-  options?: ImageSerializableContext,
-): RsbuildPlugin => {
+export interface ExtendedIPXOptions extends Partial<IPXOptions> {
+  basename?: string;
+}
+
+export interface PluginImageOptions extends ImageSerializableContext {
+  ipx?: ExtendedIPXOptions;
+}
+
+class IPXNotFoundError extends Error {
+  constructor() {
+    super(
+      'Failed to load ipx module, try to install it by `pnpm add -D ipx` or leave the `ipx` option empty and setup any other image loader.',
+    );
+    this.name = 'IPXNotFoundError';
+  }
+}
+
+async function loadIPXModule() {
+  try {
+    return await import('ipx');
+  } catch (err) {
+    if (isModuleNotFoundError(err)) throw new IPXNotFoundError();
+    throw err;
+  }
+}
+
+export const pluginImage = (options?: PluginImageOptions): RsbuildPlugin => {
   return {
     name: '@modern-js/rsbuild-plugin-image',
-    setup(api) {
+    async setup(api) {
       // Serialize and inject the options to the runtime context.
-      api.modifyRsbuildConfig((config, { mergeRsbuildConfig }) => {
+      api.modifyRsbuildConfig(async (config, { mergeRsbuildConfig }) => {
+        const ipxModule = options?.ipx && (await loadIPXModule());
+
         return mergeRsbuildConfig(config, {
           source: {
             define: {
@@ -22,6 +51,36 @@ export const pluginImage = (
               }
               return aliases;
             },
+          },
+          dev: {
+            setupMiddlewares: [
+              middlewares => {
+                if (!options?.ipx || !ipxModule) return;
+                const { distPath } = api.context;
+                invariant(distPath, 'distPath is required');
+                const { createIPX, createIPXNodeServer, ipxFSStorage } =
+                  ipxModule;
+                const {
+                  storage = ipxFSStorage({ dir: distPath }),
+                  basename = '/_modern_js/image',
+                  ...rest
+                } = options.ipx;
+                const ipx = createIPX({ storage, ...rest });
+                logger.debug(`Created IPX with local storage from ${distPath}`);
+                logger.debug(`Created IPX with basename ${basename}`);
+                const originalMiddleware = createIPXNodeServer(ipx);
+                middlewares.push((req, res, next) => {
+                  logger.debug(
+                    `Incoming request to the IPX middleware: ${req.url}`,
+                  );
+                  if (req.url?.startsWith(basename)) {
+                    req.url = req.url.slice(basename.length);
+                    return originalMiddleware(req, res);
+                  }
+                  return next();
+                });
+              },
+            ],
           },
         });
       });
--- packages/runtime/plugin-image/src/shared/utils.ts
+++ packages/runtime/plugin-image/src/shared/utils.ts
@@ -6,3 +6,11 @@ export function invariant(
     throw message instanceof Error ? message : new Error(message);
   }
 }
+
+export function isModuleNotFoundError(err: unknown): boolean {
+  return (
+    err instanceof Error &&
+    'code' in err &&
+    (err.code === 'ERR_MODULE_NOT_FOUND' || err.code === 'MODULE_NOT_FOUND')
+  );
+}
