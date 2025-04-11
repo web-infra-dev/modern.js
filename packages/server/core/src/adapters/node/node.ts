7857a401ea0abc0e145d79143f258d3e9ac287a1
Ming <527990618@163.com>
2025-04-11 14:40:53 +0800

    refactor: avoid only one of req and request has a request body

--- packages/server/core/src/adapters/node/node.ts
+++ packages/server/core/src/adapters/node/node.ts
@@ -44,7 +44,6 @@ export const createWebRequest = (
   res.on('close', () => controller.abort('res closed'));
 
   const url = `http://${req.headers.host}${req.url}`;
-  // Since we don't want break changes and now node.req.body will be consumed in bff, custom server, render, so we don't create a stream and consume node.req here by default.
   if (body || !(method === 'GET' || method === 'HEAD')) {
     if (body) {
       init.body = body;
