bac8ba287103e33dda9e8f70b1f90a46c1c28eaa
Ming <527990618@163.com>
2025-04-29 19:40:59 +0800

    feat: router v7 plugin

--- tests/integration/ssr/fixtures/streaming/modern.config.ts
+++ tests/integration/ssr/fixtures/streaming/modern.config.ts
@@ -1,4 +1,4 @@
-import routerPlugin from '@modern-js/plugin-router-v7';
+import { routerPlugin } from '@modern-js/plugin-router-v7';
 import { applyBaseConfig } from '../../../../utils/applyBaseConfig';
 
 export default applyBaseConfig({
