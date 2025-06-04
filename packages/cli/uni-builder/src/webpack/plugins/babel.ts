e21b603e088f9e2e049815a706399e75f09d5322
await-ovo <awaitoo762@gmail.com>
2025-06-04 15:06:06 +0800

    chore: only clone options for plain object

--- packages/cli/uni-builder/src/webpack/plugins/babel.ts
+++ packages/cli/uni-builder/src/webpack/plugins/babel.ts
@@ -1,7 +1,11 @@
 import type { BabelConfig } from '@modern-js/babel-preset';
 import { getBabelConfigForNode } from '@modern-js/babel-preset/node';
 import { getBabelConfigForWeb } from '@modern-js/babel-preset/web';
-import { applyOptionsChain, isSupportAutomaticJsx } from '@modern-js/utils';
+import {
+  applyOptionsChain,
+  isPlainObject,
+  isSupportAutomaticJsx,
+} from '@modern-js/utils';
 import { logger } from '@rsbuild/core';
 import type {
   NormalizedEnvironmentConfig,
@@ -128,7 +132,9 @@ export const pluginBabel = (
           const babelConfig = applyOptionsChain(
             baseBabelConfig,
             // ensure that each environment has its own babel options(plugins, presets, etc.) in multi-environments.
-            lodash.cloneDeep(options?.babelLoaderOptions),
+            isPlainObject(options?.babelLoaderOptions)
+              ? lodash.cloneDeep(options?.babelLoaderOptions)
+              : options?.babelLoaderOptions,
             {
               ...getBabelUtils(baseBabelConfig),
               ...babelUtils,
