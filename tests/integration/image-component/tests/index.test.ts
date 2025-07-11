d60d91da139601329f6b1d4a71b37fe28f28e024
Ming <527990618@163.com>
2025-07-11 17:51:35 +0800

    chore: skip the test case

--- tests/integration/image-component/tests/index.test.ts
+++ tests/integration/image-component/tests/index.test.ts
@@ -21,8 +21,8 @@ function existsSync(filePath: string) {
   return fs.existsSync(resolveDist(filePath));
 }
 
-describe('devtools build', () => {
-  test.skip(`should get right devtools build!`, async () => {
+describe.skip('devtools build', () => {
+  test(`should get right devtools build!`, async () => {
     if (!isVersionAtLeast18()) return;
     const buildRes = await modernBuild(appDir);
     expect(buildRes.code === 0).toBe(true);
