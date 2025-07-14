b2cf440c81583a86243c11739985fb7ab7004ecb
caohuilin <caohuilin@bytedance.com>
2025-07-14 10:39:43 +0800

    fix: format

--- tests/e2e/builder/cases/tools/pug/src/index.ts
+++ tests/e2e/builder/cases/tools/pug/src/index.ts
@@ -2,6 +2,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const domNode = document.getElementById('root')!;
-const root = createRoot(domNode);
-root.render(React.createElement(App));
+const domNode = document.getElementById('root');
+
+if (domNode) {
+  const root = createRoot(domNode);
+  root.render(React.createElement(App));
+}
