a7a99a5a347f936a8f67649b5a31b9998c303cb7
caohuilin <caohuilin@bytedance.com>
2025-07-14 10:37:58 +0800

    fix: builder e2e test

--- tests/e2e/builder/cases/source/resolve-extension-prefix/src/index.js
+++ tests/e2e/builder/cases/source/resolve-extension-prefix/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './ex';
 
-render(React.createElement(App), document.getElementById('root'));
+const domNode = document.getElementById('root');
+if (domNode) {
+  const root = createRoot(domNode);
+  root.render(React.createElement(App));
+}
--- tests/e2e/builder/cases/tools/pug/src/index.ts
+++ tests/e2e/builder/cases/tools/pug/src/index.ts
@@ -1,5 +1,7 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-render(React.createElement(App), document.getElementById('root'));
+const domNode = document.getElementById('root')!;
+const root = createRoot(domNode);
+root.render(React.createElement(App));
