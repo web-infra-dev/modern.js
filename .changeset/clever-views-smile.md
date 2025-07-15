69959ef75c8ccdc15332e455762e29d4c2391ecf
zhangxiang <zhangxiang.01@bytedance.com>
2025-07-15 21:42:07 +0800

    chore: remove bff proxy

---
+++ .changeset/clever-views-smile.md
@@ -0,0 +1,8 @@
+---
+'server-config': patch
+'@modern-js/types': patch
+'@modern-js/server-core': patch
+---
+
+chore: remove bff proxy
+chore: 移除 bff proxy
--- tests/integration/server-config/modern.server-runtime.config.ts
+++ tests/integration/server-config/modern.server-runtime.config.ts
@@ -2,15 +2,5 @@ import { defineConfig } from '@modern-js/app-tools/server';
 import plugin1 from './plugins/serverPlugin';
 
 export default defineConfig({
-  bff: {
-    proxy: {
-      '/api/proxy': {
-        // https://github.com/chimurai/http-proxy-middleware/issues/705
-        target: `http://127.0.0.1:${process.env.PORT}`,
-        pathRewrite: { '/api/proxy': '/api/foo' },
-        changeOrigin: true,
-      },
-    },
-  },
   plugins: [plugin1()],
 });
