b22345fc61d0d37d5f9ccdda7c8721903504e2a3
Belinda Cao <caohuilin@bytedance.com>
2025-07-16 14:16:19 +0800

    feat: remove test plugin package (#7367)

--- packages/runtime/plugin-testing/src/base/index.ts
+++
@@ -1,6 +0,0 @@
-export { runTest, runJest } from './runJest';
-export { getModuleNameMapper } from './utils';
-export { DEFAULT_RESOLVER_PATH } from './config';
-export { TestConfigOperator } from './config/testConfigOperator';
-export { testingHooks } from './hook';
-export * from './types';
--- packages/runtime/plugin-testing/src/base/types/index.ts
+++
@@ -1,1 +0,0 @@
-export type { JestConfig, TestConfig } from '@modern-js/types';
