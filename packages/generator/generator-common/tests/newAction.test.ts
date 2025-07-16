f3e0b11f038d80db5efc06253dfbd4ee7ad57aca
kongjiacong <kongjiacong@bytedance.com>
2025-07-16 15:50:30 +0800

    chore: update test case

--- packages/generator/generator-common/tests/newAction.test.ts
+++ packages/generator/generator-common/tests/newAction.test.ts
@@ -1,9 +1,7 @@
-import { i18n } from '../src/locale';
-import { ActionFunction, ActionFunctionText } from '../src/newAction/common';
 import { MWAActionFunctions } from '../src/newAction/mwa';
 
 describe('new action test', () => {
   it('mwa', () => {
-    expect(MWAActionFunctions.length).toBe(6);
+    expect(MWAActionFunctions.length).toBe(5);
   });
 });
