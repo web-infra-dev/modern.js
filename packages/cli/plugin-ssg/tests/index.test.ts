232f6a7b59243f21336e628d32e2817bece44b06
wangyiming <wangyiming.fe@bytedance.com>
2025-07-25 14:28:37 +0800

    feat: remove the unused test case

--- packages/cli/plugin-ssg/tests/index.test.ts
+++
@@ -1,8 +0,0 @@
-import plugin from '../src';
-
-describe('plugin-ssg', () => {
-  it('default', () => {
-    expect(plugin).toBeDefined();
-    expect(plugin().name).toBe('@modern-js/plugin-ssg');
-  });
-});
