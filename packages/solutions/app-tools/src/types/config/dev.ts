3aa61b6beeb0c35074a782d8e2abec5beb4220bb
caohuilin <caohuilin@bytedance.com>
2025-08-04 14:32:02 +0800

    fix: update lock file and fix build error

--- packages/solutions/app-tools/src/types/config/dev.ts
+++ packages/solutions/app-tools/src/types/config/dev.ts
@@ -1,10 +1,9 @@
 import type { BuilderConfig } from '@modern-js/builder';
-import type { SetupMiddlewares } from '@modern-js/server';
 
 type BuilderDevConfig = Required<BuilderConfig>['dev'];
 
 export type DevProxyOptions = string | Record<string, string>;
 
 export interface DevUserConfig extends BuilderDevConfig {
-  setupMiddlewares?: SetupMiddlewares;
+  setupMiddlewares?: any;
 }
