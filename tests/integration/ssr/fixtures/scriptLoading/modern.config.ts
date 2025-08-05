8b6adc3e2cbcf10c00d33fd4a6ad07fd10016cf2
Ming <527990618@163.com>
2025-08-05 11:46:56 +0800

    fix: remove ssr.scriptLoading

--- tests/integration/ssr/fixtures/scriptLoading/modern.config.ts
+++ tests/integration/ssr/fixtures/scriptLoading/modern.config.ts
@@ -1,12 +1,6 @@
 import { applyBaseConfig } from '../../../../utils/applyBaseConfig';
 
 export default applyBaseConfig({
-  server: {
-    ssr: {
-      scriptLoading: 'async',
-    },
-  },
-
   runtime: {
     router: true,
   },
