c9bd3bd49b5826c7e553b5f0fe3d7cea822ee809
Ming <527990618@163.com>
2025-08-05 13:32:54 +0800

    chore: move types

--- packages/runtime/plugin-runtime/src/core/context/serverPayload/index.server.tsx
+++ packages/runtime/plugin-runtime/src/core/context/serverPayload/index.server.tsx
@@ -1,5 +1,34 @@
 import { storage } from '@modern-js/runtime-utils/node';
-import type { ServerPayload } from '../index';
+import type { RouterState } from '@modern-js/runtime-utils/router';
+
+export type PayloadRoute = {
+  clientAction?: any;
+  clientLoader?: any;
+  element?: React.ReactNode;
+  errorElement?: React.ReactNode;
+  handle?: any;
+  hasAction: boolean;
+  hasErrorBoundary: boolean;
+  hasLoader: boolean;
+  id: string;
+  index?: boolean;
+  params: Record<string, string>;
+  parentId?: string;
+  path?: string;
+  pathname: string;
+  pathnameBase: string;
+  shouldRevalidate?: any;
+};
+
+export type ServerPayload = {
+  type: 'render';
+  actionData: RouterState['actionData'];
+  errors: RouterState['errors'];
+  loaderData: RouterState['loaderData'];
+  location: RouterState['location'];
+  routes: PayloadRoute[];
+  originalRoutes?: PayloadRoute[];
+};
 
 export const getServerPayload = (): ServerPayload | undefined => {
   const context = storage.useContext() as any;
