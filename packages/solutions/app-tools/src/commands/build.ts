76e077682d9300f2f56fb2b427ccc4bee9ce9185
clchenliang <clchenliang@hotmail.com>
2025-08-27 18:48:05 +0800

    fix: alias cant not be destruct

--- packages/solutions/app-tools/src/commands/build.ts
+++ packages/solutions/app-tools/src/commands/build.ts
@@ -21,6 +21,10 @@ export const build = async (
   const appContext = api.getAppContext();
   const hooks = api.getHooks();
 
+  const combinedAlias = []
+    .concat(resolvedConfig?.resolve?.alias)
+    .concat(resolvedConfig?.source?.alias) as ConfigChain<Alias>;
+
   // we need load server plugin to appContext for ssg & deploy commands.
   await loadServerPlugins(api, appContext.appDirectory, appContext.metaName);
 
@@ -29,17 +33,15 @@ export const build = async (
     await registerEsm({
       appDir: appContext.appDirectory,
       distDir: appContext.distDirectory,
-      alias: {
-        ...resolvedConfig.resolve?.alias,
-        ...resolvedConfig.source?.alias,
-      },
+      alias: combinedAlias,
     });
   }
 
-  await registerCompiler(appContext.appDirectory, appContext.distDirectory, {
-    ...resolvedConfig?.resolve?.alias,
-    ...resolvedConfig?.source?.alias,
-  } as ConfigChain<Alias>);
+  await registerCompiler(
+    appContext.appDirectory,
+    appContext.distDirectory,
+    combinedAlias,
+  );
 
   const { apiOnly } = appContext;
 
