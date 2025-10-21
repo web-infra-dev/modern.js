3aa7c3caf9dfa65f9be9baa2f95c778c657d13c5
Ming <527990618@163.com>
2025-10-21 14:34:45 +0800

    feat: support config routes

--- tests/integration/routes/src/three/routes/Product.data.ts
+++ tests/integration/routes/src/three/routes/Product.data.ts
@@ -3,7 +3,6 @@ import type { LoaderFunctionArgs } from '@modern-js/runtime/router';
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   const { id } = params;
 
-  // 模拟根据查询参数抛出错误
   if (id === 'error') {
     throw new Error('Product load error');
   }
