e98d6ec502646fa64a051d5a88c191a270350d37
caohuilin <caohuilin@bytedance.com>
2025-11-13 10:55:17 +0800

    feat: op backend config

---
+++ packages/runtime/plugin-i18n/src/runtime/i18n/backend/defaults.ts
@@ -0,0 +1,4 @@
+export const DEFAULT_I18NEXT_BACKEND_OPTIONS = {
+  loadPath: '/locales/{{lng}}/{{ns}}.json',
+  addPath: '/locales/{{lng}}/{{ns}}.json',
+};
--- packages/runtime/plugin-i18n/src/runtime/i18n/backend/index.ts
+++ packages/runtime/plugin-i18n/src/runtime/i18n/backend/index.ts
@@ -1,13 +1,18 @@
 import type { BaseBackendOptions } from '../../../shared/type';
-import type { I18nInitOptions } from '../instance';
-import { mergeBackendOptions as mergeBackendOptionsUtils } from './config';
+import type { BackendOptions, I18nInitOptions } from '../instance';
+import { mergeBackendOptions as baseMergeBackendOptions } from './config';
+import { DEFAULT_I18NEXT_BACKEND_OPTIONS } from './defaults';
 
 export const mergeBackendOptions = (
   backend?: BaseBackendOptions,
   userInitOptions?: I18nInitOptions,
 ) => {
   const mergedBackend = backend?.enabled
-    ? mergeBackendOptionsUtils(backend, userInitOptions?.backend)
+    ? baseMergeBackendOptions(
+        DEFAULT_I18NEXT_BACKEND_OPTIONS,
+        backend as BackendOptions,
+        userInitOptions?.backend,
+      )
     : userInitOptions?.backend;
   return mergedBackend;
 };
