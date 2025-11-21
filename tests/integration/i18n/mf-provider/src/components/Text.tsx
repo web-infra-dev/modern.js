67717a101466172eaede2ed67d3cbe7262c939ac
caohuilin <caohuilin@bytedance.com>
2025-11-21 10:22:47 +0800

    feat: op css

--- tests/integration/i18n/mf-provider/src/components/Text.tsx
+++ tests/integration/i18n/mf-provider/src/components/Text.tsx
@@ -6,7 +6,10 @@ export default () => {
   const { t } = useTranslation();
   return (
     <div id="about" className={styles.about}>
-      remote components
+      <div className={styles.remoteBadge}>
+        <span className={styles.remoteIcon}>🌐</span>
+        <span className={styles.remoteText}>Remote Component</span>
+      </div>
       {/** use i18next.t to get the key will use provider's i18n instance */}
       <p className={styles.text1}>{i18next.t('key')}</p>
       {/** use useTranslation to get the key will use consumer's i18n instance */}
