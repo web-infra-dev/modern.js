1989703b3e63f66abc7f87471a3c47e714f25e9d
caohuilin <caohuilin@bytedance.com>
2025-11-24 09:56:38 +0800

    fix: is remote check

--- tests/integration/i18n/mf-app-provider/src/i18n.ts
+++ tests/integration/i18n/mf-app-provider/src/i18n.ts
@@ -1,22 +1,24 @@
 import i18next from 'i18next';
 
-i18next.init({
-  lng: 'en',
-  fallbackLng: 'en',
-  resources: {
-    en: {
-      translation: {
-        key: 'Hello World(provider)',
-        about: 'About(provider)',
+if (!i18next.isInitialized) {
+  i18next.init({
+    lng: 'en',
+    fallbackLng: 'en',
+    resources: {
+      en: {
+        translation: {
+          key: 'Hello World(provider)',
+          about: 'About(provider)',
+        },
+      },
+      zh: {
+        translation: {
+          key: '你好，世界(provider)',
+          about: '关于(provider)',
+        },
       },
     },
-    zh: {
-      translation: {
-        key: '你好，世界(provider)',
-        about: '关于(provider)',
-      },
-    },
-  },
-});
+  });
+}
 
 export default i18next;
--- tests/integration/i18n/mf-app-provider/src/routes/layout.tsx
+++ tests/integration/i18n/mf-app-provider/src/routes/layout.tsx
@@ -1,14 +1,13 @@
 import { useModernI18n } from '@modern-js/plugin-i18n/runtime';
+import { getGlobalBasename } from '@modern-js/runtime/context';
 import { Outlet } from '@modern-js/runtime/router';
-import useIsRemote from '../hooks/useIsRemote';
 
 export default function Layout() {
   const { changeLanguage } = useModernI18n();
-  const isRemote = useIsRemote();
-  console.log('isRemote', isRemote);
+  const basename = getGlobalBasename();
   return (
     <div>
-      {!isRemote ? (
+      {!basename ? (
         <div>
           <button id="zh-button" onClick={() => changeLanguage('zh')}>
             zh
