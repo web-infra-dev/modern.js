c7332beaef5beab3d3c7f87cc548d897e89c723a
yifancong <congzhe@bytedance.com>
2025-11-24 20:43:26 +0800

    Chore: add diff action on main branch (#7919)

---
+++ benchmark/bundle-diff/modern.config.ts
@@ -0,0 +1,32 @@
+import { appTools, defineConfig } from '@modern-js/app-tools';
+import { RsdoctorRspackPlugin } from '@rsdoctor/rspack-plugin';
+
+const isCI = process.env.CI === 'true';
+
+// https://modernjs.dev/en/configure/app/usage
+export default defineConfig({
+  runtime: {
+    router: true,
+  },
+
+  tools: {
+    bundlerChain: chain => {
+      if (process.env.RSDOCTOR) {
+        chain.plugin('rsdoctor').use(RsdoctorRspackPlugin, [
+          {
+            output: isCI
+              ? {
+                  mode: 'brief',
+                  options: {
+                    type: ['json'],
+                  },
+                }
+              : {},
+            features: isCI ? ['bundle'] : ['bundle', 'loader', 'plugins'],
+          },
+        ]);
+      }
+    },
+  },
+  plugins: [appTools()],
+});
