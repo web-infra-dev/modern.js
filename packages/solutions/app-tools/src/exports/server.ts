4d4f1382bba29d51ad9c1eeb22fbeae2cdb757bb
wangyiming <wangyiming.fe@bytedance.com>
2025-12-08 15:15:00 +0800

    chore: remove node-bundle-require

--- packages/solutions/app-tools/src/exports/server.ts
+++ packages/solutions/app-tools/src/exports/server.ts
@@ -1,3 +1,1 @@
 export type { ServerPlugin } from '@modern-js/server-core';
-
-export { defineServerConfig as defineConfig } from '../utils/config';
--- packages/toolkit/node-bundle-require/tests/fixture/inputImportTs.ts
+++
@@ -1,5 +0,0 @@
-import { bar } from 'test-package-ts';
-
-export default {
-  bar,
-};
