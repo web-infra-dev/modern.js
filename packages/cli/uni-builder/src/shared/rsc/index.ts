a66d987e2c38ca136fb69fe954380bd084fdeb5b
GiveMe-A-Name <a455300764b@hotmail.com>
2025-12-09 15:30:05 +0800

    chore: re-export rsbuildRscPlugin for compatibility with other frameworks

--- packages/cli/uni-builder/src/shared/rsc/index.ts
+++ packages/cli/uni-builder/src/shared/rsc/index.ts
@@ -1,2 +1,3 @@
 export { RscClientPlugin } from './plugins/rsc-client-plugin';
 export { RscServerPlugin } from './plugins/rsc-server-plugin';
+export { rsbuildRscPlugin } from './plugins/rsbuild-rsc-plugin';
