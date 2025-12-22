70e0275fb6c77209b9d0e9d64027dec0b3552255
Ming <wangyiming.fe@bytedance.com>
2025-12-22 20:42:46 +0800

    fix: test cases (#8074)

--- tests/integration/i18n/app-ssr/modern.ssg.config.ts
+++ tests/integration/i18n/app-ssr/modern.ssg.config.ts
@@ -4,7 +4,12 @@ import { ssgPlugin } from '@modern-js/plugin-ssg';
 
 export default defineConfig({
   server: {
-    ssr: process.env.NODE_ENV === 'development',
+    ssr:
+      process.env.NODE_ENV === 'development'
+        ? {
+            mode: 'string',
+          }
+        : false,
     publicDir: './locales',
   },
   output: {
