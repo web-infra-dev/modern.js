effa9e76e9035e51adf384c723a9be31f9be3515
9aoy <9aoyuao@gmail.com>
2026-01-20 15:37:07 +0800

    test: plugin-bff

--- tests/jest-ut.config.js
+++ tests/jest-ut.config.js
@@ -24,56 +24,6 @@ module.exports = {
           },
         ],
       },
-      moduleNameMapper: {
-        '^@modern-js/runtime/browser$':
-          '<rootDir>/packages/runtime/plugin-runtime/src/core/browser',
-        '^@modern-js/runtime/react$':
-          '<rootDir>/packages/runtime/plugin-runtime/src/core/react',
-        '^@modern-js/runtime$':
-          '<rootDir>/packages/runtime/plugin-runtime/src/index',
-        '^@modern-js/plugin/runtime$':
-          '<rootDir>/packages/toolkit/plugin/src/runtime/index',
-        '^./async_storage$': './async_storage.server',
-      },
-      globals: {},
-      resolver: '<rootDir>/tests/jest.resolver.js',
-      transformIgnorePatterns: ['/node_modules/.pnpm/(?!(@babel))'],
-      modulePathIgnorePatterns: [
-        '<rootDir>/packages/cli/builder/',
-        '<rootDir>/packages/cli/plugin-data-loader/',
-        '<rootDir>/packages/cli/babel-preset/',
-        '<rootDir>/packages/toolkit/e2e/',
-        '<rootDir>/packages/solutions/module-tools/compiled/',
-        '<rootDir>/packages/toolkit/utils/compiled/',
-        '<rootDir>/.nx-cache',
-        '<rootDir>/.nx',
-      ],
-      rootDir: path.join(__dirname, '../'),
-      testEnvironment: '<rootDir>/tests/jest.env.js',
-      testMatch: [
-        '<rootDir>/packages/cli/plugin-bff/tests/**/*.test.[jt]s?(x)',
-      ],
-    },
-    {
-      setupFiles: ['<rootDir>/tests/setEnvVars.js'],
-      transform: {
-        '\\.[jt]sx?$': [
-          require.resolve('@swc/jest'),
-          {
-            jsc: {
-              parser: {
-                syntax: 'typescript',
-                decorators: true,
-              },
-              transform: {
-                react: {
-                  runtime: 'automatic',
-                },
-              },
-            },
-          },
-        ],
-      },
       moduleNameMapper: {},
       globals: {},
       resolver: '<rootDir>/tests/jest.resolver.js',
