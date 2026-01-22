7333713392c1f71059d15774c8a6c27bb89be2bc
Ming <527990618@163.com>
2026-01-22 20:49:46 +0800

    fix: add XSS protection for csr + rsc projects

--- packages/runtime/render/src/server/rsc/processRSCStream.ts
+++ packages/runtime/render/src/server/rsc/processRSCStream.ts
@@ -1,3 +1,11 @@
+// Escape closing script tags and HTML comments in JS content.
+// https://www.w3.org/TR/html52/semantics-scripting.html#restrictions-for-contents-of-script-elements
+// Avoid replacing </script with <\/script as it would break the following valid JS: 0</script/ (i.e. regexp literal).
+// Instead, escape the s character.
+function escapeScript(script: string): string {
+  return script.replace(/<!--/g, '<\\!--').replace(/<\/(script)/gi, '</\\$1');
+}
+
 export async function processRSCStream(
   rscStream: ReadableStream,
   controller: ReadableStreamDefaultController<Uint8Array>,
@@ -13,18 +21,21 @@ export async function processRSCStream(
 
       try {
         const chunk = JSON.stringify(decoder.decode(value, { stream: true }));
-        const scriptTag = `<script>(self.__FLIGHT_DATA||=[]).push(${chunk})</script>`;
+        const scriptContent = `(self.__FLIGHT_DATA||=[]).push(${chunk})`;
+        const scriptTag = `<script>${escapeScript(scriptContent)}</script>`;
         controller.enqueue(encoder.encode(scriptTag));
       } catch (err) {
         const base64 = JSON.stringify(btoa(String.fromCodePoint(...value)));
-        const scriptTag = `<script>(self.__FLIGHT_DATA||=[]).push(Uint8Array.from(atob(${base64}), m => m.codePointAt(0)))</script>`;
+        const scriptContent = `(self.__FLIGHT_DATA||=[]).push(Uint8Array.from(atob(${base64}), m => m.codePointAt(0)))`;
+        const scriptTag = `<script>${escapeScript(scriptContent)}</script>`;
         controller.enqueue(encoder.encode(scriptTag));
       }
     }
 
     const remaining = decoder.decode();
     if (remaining.length) {
-      const scriptTag = `<script>(self.__FLIGHT_DATA||=[]).push(${JSON.stringify(remaining)})</script>`;
+      const scriptContent = `(self.__FLIGHT_DATA||=[]).push(${JSON.stringify(remaining)})`;
+      const scriptTag = `<script>${escapeScript(scriptContent)}</script>`;
       controller.enqueue(encoder.encode(scriptTag));
     }
 
