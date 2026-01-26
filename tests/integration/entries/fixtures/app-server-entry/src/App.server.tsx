8bbe2185c2f7e91e7c86afae280a649a1990a0ad
caohuilin <caohuilin@bytedance.com>
2026-01-26 16:20:41 +0800

    feat: use runtime context

--- tests/integration/entries/fixtures/app-server-entry/src/App.server.tsx
+++ tests/integration/entries/fixtures/app-server-entry/src/App.server.tsx
@@ -1,14 +1,24 @@
-import { useRuntimeContext } from '@modern-js/runtime';
+import { RuntimeContext } from '@modern-js/runtime';
 import {
+  type StaticHandlerContext,
   StaticRouterProvider,
   createStaticRouter,
 } from '@modern-js/runtime-utils/router';
+import { use } from 'react';
 import { routes } from './routes';
 
 const App = () => {
-  const { customRouterContext } = useRuntimeContext();
-  const router = createStaticRouter(routes, customRouterContext);
-  return <StaticRouterProvider router={router} context={customRouterContext} />;
+  const { customRouterContext } = use(RuntimeContext);
+  const router = createStaticRouter(
+    routes,
+    customRouterContext as StaticHandlerContext,
+  );
+  return (
+    <StaticRouterProvider
+      router={router}
+      context={customRouterContext as StaticHandlerContext}
+    />
+  );
 };
 
 export default App;
