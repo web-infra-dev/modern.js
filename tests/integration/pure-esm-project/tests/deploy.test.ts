165b088d1475867e70c29f5de1440d04a194dcb5
ShuangYa <i@sylingd.com>
2026-01-31 21:30:10 +0800

    test: pure esm deploy

--- tests/integration/pure-esm-project/tests/deploy.test.ts
+++ tests/integration/pure-esm-project/tests/deploy.test.ts
@@ -11,21 +11,24 @@ const appDir = path.resolve(__dirname, '../');
 
 async function checkAppRun(host: string) {
   // Page render
-  const onePage = await fetch(`${host}/one`);
-  expect(onePage.status).toBe(200);
-  expect(await onePage.text()).toContain('<div id="item">');
+  const page = await fetch(`${host}/`);
+  expect(page.status).toBe(200);
+  expect(await page.text()).toContain('<div id="item">');
 
   // Loader
-  const oneLoader = await fetch(`${host}/one?__loader=one_page`);
-  expect(oneLoader.status).toBe(200);
-  expect(await oneLoader.text()).toContain('Hello Modern.js');
+  const loader = await fetch(`${host}/one?__loader=page&name=name`);
+  expect(loader.status).toBe(200);
+  expect(await loader.text()).toContain(
+    JSON.stringify({ name: 'name', age: 18 }),
+  );
 
   // API
-  const api = await fetch(`${host}/api/context`);
+  const api = await fetch(`${host}/api/info`);
   expect(api.status).toBe(200);
-  expect(api.headers.get('x-id')).toBe('1');
   expect(await api.json()).toEqual({
-    message: 'Hello Modern.js',
+    company: 'bytedance',
+    addRes: 3,
+    url: '/api/info',
   });
 }
 
@@ -39,8 +42,6 @@ describe('deploy', () => {
 
   afterAll(async () => {
     await Promise.all([...apps].map(x => killApp(x)));
-    await fse.remove(path.join(appDir, '.vercel'));
-    await fse.remove(path.join(appDir, '.netlify'));
     await fse.remove(path.join(appDir, '.output'));
     await fse.remove(path.join(appDir, '.output-server-bundle'));
   });
@@ -113,69 +114,5 @@ describe('deploy', () => {
     await checkAppRun(`http://localhost:${port}`);
     await killApp(app);
     apps.delete(app);
-  });
-
-  test('support server when deploy target is vercel', async () => {
-    await execa('npx modern deploy --skip-build', {
-      shell: true,
-      cwd: appDir,
-      stdio: 'inherit',
-      env: {
-        ...process.env,
-        MODERNJS_DEPLOY: 'vercel',
-      },
-    });
-
-    const outputDirectory = path.join(appDir, '.vercel/output');
-    const staticDirectory = path.join(outputDirectory, 'static');
-    const funcsDirectory = path.join(
-      outputDirectory,
-      'functions',
-      'index.func',
-    );
-    const funcsConfigPath = path.join(funcsDirectory, '.vc-config.json');
-    const funcsConfig = await import(funcsConfigPath);
-    const bootstrapFile = path.join(funcsDirectory, 'index.js');
-    const htmlDirectory = path.join(funcsDirectory, 'html');
-    const publicDirectory = path.join(staticDirectory, 'static');
-    const configPath = path.join(outputDirectory, 'config.json');
-    const config = await import(configPath);
-
-    expect(await fse.pathExists(staticDirectory)).toBe(true);
-    expect(await fse.pathExists(htmlDirectory)).toBe(true);
-    expect(await fse.pathExists(publicDirectory)).toBe(true);
-    expect(await fse.pathExists(bootstrapFile)).toBe(true);
-    expect(config).toMatchSnapshot();
-    expect({
-      ...funcsConfig.default,
-      runtime: expect.any(String),
-    }).toMatchSnapshot();
-  });
-
-  test('support server when deploy target is netlify', async () => {
-    await execa('npx modern deploy --skip-build', {
-      shell: true,
-      cwd: appDir,
-      stdio: 'inherit',
-      env: {
-        ...process.env,
-        MODERNJS_DEPLOY: 'netlify',
-      },
-    });
-
-    const publishDir = path.join(appDir, 'dist');
-    const outputDirectory = path.join(appDir, '.netlify');
-    const staticDirectory = path.join(publishDir, 'static');
-    const funcsDirectory = path.join(outputDirectory, 'functions');
-    const redirectFilePath = path.join(publishDir, '_redirects');
-    const redirects = (await fse.readFile(redirectFilePath)).toString();
-
-    const bootstrapFile = path.join(funcsDirectory, 'index.js');
-    const htmlDirectory = path.join(funcsDirectory, 'html');
-
-    expect(await fse.pathExists(staticDirectory)).toBe(true);
-    expect(await fse.pathExists(htmlDirectory)).toBe(true);
-    expect(await fse.pathExists(bootstrapFile)).toBe(true);
-    expect(redirects).toMatchSnapshot();
   });
 });
