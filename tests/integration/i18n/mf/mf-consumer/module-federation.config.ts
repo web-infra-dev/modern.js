61552d9610c8acb707d24dacf220f71ef1cbbe21
Belinda Cao <caohuilin@bytedance.com>
2026-02-09 17:25:40 +0800

    feat: update mf modern plugin to v3 (#8329)

--- tests/integration/i18n/mf/mf-consumer/module-federation.config.ts
+++ tests/integration/i18n/mf/mf-consumer/module-federation.config.ts
@@ -1,4 +1,4 @@
-import { createModuleFederationConfig } from '@module-federation/modern-js';
+import { createModuleFederationConfig } from '@module-federation/modern-js-v3';
 
 export default createModuleFederationConfig({
   name: 'consumer',
