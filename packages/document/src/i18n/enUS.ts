ac87a5a5b2edeb11b033d598aa40cfbd8c2140e0
kky_kongjiacong <68810266+zllkjc@users.noreply.github.com>
2026-02-12 17:01:36 +0800

    chore: use rsbuild.rs instead of rsbuild.dev (#8345)

--- packages/document/src/i18n/enUS.ts
+++ packages/document/src/i18n/enUS.ts
@@ -20,7 +20,7 @@ export const EN_US = {
   feature3: 'Nested Routes',
   featureDesc3: 'File-as-route, comes with lots performance optimizations.',
   feature4: 'Multi-Rendering Mode',
-  featureDesc4: 'SSR, SSG, SPR, all out of the box for you.',
+  featureDesc4: 'RSC, SSR, SSG, all out of the box for you.',
   feature5: 'CSS Solutions',
   featureDesc5: 'CSS Modules, CSS-in-JS, Tailwind CSS, take your pick.',
   feature6: 'Easy to Configure',
