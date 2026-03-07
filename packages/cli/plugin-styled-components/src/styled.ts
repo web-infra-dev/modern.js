b41167e3c36a162cd531559f15e9a3e0fd3b09b6
zhangxiang <zhangxiang.01@bytedance.com>
2026-03-07 17:29:14 +0800

    fix: stabilize styled default export

--- packages/cli/plugin-styled-components/src/styled.ts
+++ packages/cli/plugin-styled-components/src/styled.ts
@@ -1,5 +1,10 @@
-import styled from 'styled-components';
+import * as styledComponents from 'styled-components';
+
+const maybeDefault = Reflect.get(styledComponents as any, 'default');
+const maybeNestedDefault = maybeDefault
+  ? Reflect.get(maybeDefault as any, 'default')
+  : undefined;
+const styled = maybeNestedDefault ?? maybeDefault ?? styledComponents;
 
 export default styled;
-
 export * from 'styled-components';
