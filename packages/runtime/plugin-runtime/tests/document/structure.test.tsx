b9c592a6162b2ba85deedef0fd1099cdef02cfe8
clchenliang <clchenliang@hotmail.com>
2026-03-17 16:21:44 +0800

    test(document): add root test

--- packages/runtime/plugin-runtime/tests/document/structure.test.tsx
+++ packages/runtime/plugin-runtime/tests/document/structure.test.tsx
@@ -5,11 +5,7 @@ import { Body, Head, Html, Root } from '../../src/document';
 describe('debug Root component type', () => {
   it('should have correct type name', () => {
     const rootElement = <Root rootId="custom" />;
-
-    console.log('=== Root Element Type Info ===');
-    console.log('type:', (rootElement as any).type);
-    console.log('type.name:', (rootElement as any).type?.name);
-    console.log('typeof type:', typeof (rootElement as any).type);
+    expect((rootElement as any).type?.name).toBe('Root');
   });
 
   it('should render document with Root', () => {
@@ -24,16 +20,25 @@ describe('debug Root component type', () => {
 
     const docHtml = ReactDomServer.renderToString(document);
 
-    console.log('=== HTML OUTPUT ===');
-    console.log(docHtml);
-
     const hasCustomId = docHtml.includes('id="custom"');
     const hasRootId = docHtml.includes('id="root"');
 
-    console.log('Has custom id:', hasCustomId);
-    console.log('Has root id:', hasRootId);
+    expect(hasCustomId).toBe(true);
+    expect(hasRootId).toBe(false);
+  });
 
-    // If both are true, it means there are 2 Root divs (bug!)
-    expect(hasCustomId && hasRootId).toBe(false);
+  it('should render document without Root', () => {
+    const document = (
+      <Html>
+        <Head />
+        <Body />
+      </Html>
+    );
+
+    const docHtml = ReactDomServer.renderToString(document);
+
+    const hasRootId = docHtml.includes('id="root"');
+
+    expect(hasRootId).toBe(true);
   });
 });
