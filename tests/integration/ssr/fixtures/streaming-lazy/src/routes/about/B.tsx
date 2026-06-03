23ceab42f1d0f09caee7df591f5ea5b664e5b0ac
zhangxiang <zhangxiang.01@bytedance.com>
2026-06-03 03:33:37 -0700

    feat: add subcomponent

--- tests/integration/ssr/fixtures/streaming-lazy/src/routes/about/B.tsx
+++ tests/integration/ssr/fixtures/streaming-lazy/src/routes/about/B.tsx
@@ -1,9 +1,20 @@
+import loadable from '@modern-js/runtime/loadable';
+import { Suspense, lazy } from 'react';
 import './b.css';
+
+const C = lazy(() => import('./C'));
+const D = loadable(() => import('./D'), {
+  fallback: <div>Loading D...</div>,
+});
 
 export default () => {
   return (
     <div className="b-deep">
       <div>B content</div>
+      <Suspense fallback={<div>Loading C...</div>}>
+        <C />
+      </Suspense>
+      <D />
     </div>
   );
 };
---
+++ tests/integration/ssr/fixtures/streaming-lazy/src/routes/about/D.tsx
@@ -0,0 +1,9 @@
+import './d.css';
+
+export default () => {
+  return (
+    <div className="d-deep">
+      <div>D content</div>
+    </div>
+  );
+};
