537858345df28ff1639cc2b0f6b8f871bceff44b
Jiahan Chen <chenjiahan.jait@bytedance.com>
2026-07-31 10:46:33 +0800

    chore(ci): speed up Playwright browser setup (#8786)

--- tests/e2e/builder/playwright.config.ts
+++ tests/e2e/builder/playwright.config.ts
@@ -1,3 +1,10 @@
 import { defineConfig } from '@playwright/test';
 
-export default defineConfig({});
+const isCI = Boolean(process.env.CI);
+
+export default defineConfig({
+  use: {
+    // Use the built-in Chrome browser to speed up CI tests
+    channel: isCI ? 'chrome' : undefined,
+  },
+});
